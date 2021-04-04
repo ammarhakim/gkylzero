df2af6a2f5be89004dac47c843389d9bfa638b2e
ammarhakim <a.hakim777@gmail.com>
2021-04-04 10:33:30 -0400

    Updating documentation in ini reader

--- minus/rxi_ini.h
+++ minus/rxi_ini.h
@@ -8,16 +8,39 @@
  *
  * - Appending rxi to public API
  * - Changed ini_sget to take variable number of arguments
+ * - Adding API documentation
  */
 
 #pragma once
 
-#define INI_VERSION "0.1.1"
-
 typedef struct rxi_ini_t rxi_ini_t;
 
+// Read file "filename" and return ini reader object. NULL if read failed.
 rxi_ini_t* rxi_ini_load(const char *filename);
+
+// Delete ini reader object.
 void rxi_ini_free(rxi_ini_t *ini);
+
+// Number of sections in ini file
 int rxi_num_sections(rxi_ini_t *ini);
+
+// Value associated with "key" in "section". Returns NULL if such
+// section/key does not exist
 const char* rxi_ini_get(rxi_ini_t *ini, const char *section, const char *key);
+
+// Read value into provided output variables specified by "scanfmt"
+// with "key" in "section". Returns 1 if read succeeded and 0
+// otherwise. The scanfmt is the one used by formatted output family
+// of function in C. Please see table at
+// https://en.cppreference.com/w/c/io/fprintf for full format
+// specifications.
+//
+// NOTE: You must use the exact format specifier or you will read
+// junk. For example, to read a double "charge" from [electron] and
+// store it in elc_charge do:
+//
+// rxi_ini_sget(ini, "electron", "charge", "%lg", &elc_charge);
+//
+// The format-specifier "%g" will NOT work to read double as it
+// specifies float and not double.
 int rxi_ini_sget(rxi_ini_t *ini, const char *section, const char *key, const char *scanfmt, ...);
