9dcb242c349c957af49841572b21fb0b241614a0
ammarhakim <a.hakim777@gmail.com>
2021-06-03 09:59:11 -0400

    I believe all function pointer, including to generated kernels, are properly copied now. Not tested completely but first need to do the same for Vlasov eqn object.

--- zero/dg_maxwell_cu.cu
+++ zero/dg_maxwell_cu.cu
@@ -6,7 +6,33 @@ extern "C" {
 #include <gkyl_dg_maxwell_priv.h>
 }
 
-#include <stdio.h>
+// Volume kernel list
+__device__ static struct { maxwell_vol_t kernels[3]; } p_vol_kernels[] = {
+  { NULL, &maxwell_vol_1x_ser_p1, &maxwell_vol_1x_ser_p2 }, // 0
+  { NULL, &maxwell_vol_2x_ser_p1, &maxwell_vol_2x_ser_p2 }, // 1
+  { NULL, &maxwell_vol_3x_ser_p1, NULL },              // 2
+};
+
+// Surface kernel list: x-direction
+__device__ static struct { maxwell_surf_t kernels[3]; } p_surf_x_kernels[] = {
+  { NULL, &maxwell_surfx_1x_ser_p1, &maxwell_surfx_1x_ser_p2 }, // 0
+  { NULL, &maxwell_surfx_2x_ser_p1, &maxwell_surfx_2x_ser_p2 }, // 1
+  { NULL, &maxwell_surfx_3x_ser_p1, NULL },                 // 2
+};
+
+// Surface kernel list: y-direction
+__device__ static struct { maxwell_surf_t kernels[3]; } p_surf_y_kernels[] = {
+  { NULL, NULL, NULL }, // 0
+  { NULL, &maxwell_surfy_2x_ser_p1, &maxwell_surfy_2x_ser_p2 }, // 1
+  { NULL, &maxwell_surfy_3x_ser_p1, NULL },                 // 2
+};
+
+// Surface kernel list: z-direction
+__device__ static struct { maxwell_surf_t kernels[3]; } p_surf_z_kernels[] = {
+  { NULL, NULL, NULL },                 // 0
+  { NULL, NULL, NULL },                 // 1
+  { NULL, &maxwell_surfz_3x_ser_p1, NULL }, // 2
+};
 
 // various pointers to functions on device
 __device__ static vol_termf_t p_vol = &vol;
@@ -19,33 +45,33 @@ gkyl_dg_maxwell_cu_dev_new(const struct gkyl_basis* cbasis,
 {
   struct dg_maxwell *maxwell = (struct dg_maxwell*) gkyl_malloc(sizeof(struct dg_maxwell));
 
+  // set basic parameters
+  maxwell->eqn.num_equations = 8;
+  maxwell->maxwell_data.c = lightSpeed;
+  maxwell->maxwell_data.chi = lightSpeed*elcErrorSpeedFactor;
+  maxwell->maxwell_data.gamma = lightSpeed*mgnErrorSpeedFactor;  
+
   int cdim = cbasis->ndim;
   int polyOrder = cbasis->polyOrder;
 
-  maxwell->eqn.num_equations = 8;
-
+  // copy appropriate device function pointers to host to store in struct
   cudaMemcpyFromSymbol(&maxwell->eqn.vol_term, p_vol, sizeof(vol_termf_t));
   cudaMemcpyFromSymbol(&maxwell->eqn.surf_term, p_surf, sizeof(surf_termf_t));
   cudaMemcpyFromSymbol(&maxwell->eqn.boundary_surf_term, p_boundary_surf, sizeof(boundary_surf_termf_t));
 
-  maxwell->maxwell_data.c = lightSpeed;
-  maxwell->maxwell_data.chi = lightSpeed*elcErrorSpeedFactor;
-  maxwell->maxwell_data.gamma = lightSpeed*mgnErrorSpeedFactor;
+  cudaMemcpyFromSymbol(&maxwell->vol, CK(p_vol_kernels, cdim, polyOrder), sizeof(maxwell_vol_t));
 
-  maxwell->vol = CK(vol_kernels, cdim, polyOrder);
-
-  maxwell->surf[0] = CK(surf_x_kernels, cdim, polyOrder);
+  cudaMemcpyFromSymbol(&maxwell->surf[0], CK(p_surf_x_kernels, cdim, polyOrder), sizeof(maxwell_surf_t));
   if (cdim>1)
-    maxwell->surf[1] = CK(surf_y_kernels, cdim, polyOrder);
+    cudaMemcpyFromSymbol(&maxwell->surf[1], CK(p_surf_y_kernels, cdim, polyOrder), sizeof(maxwell_surf_t));
   if (cdim>2)
-    maxwell->surf[2] = CK(surf_z_kernels, cdim, polyOrder);
+    cudaMemcpyFromSymbol(&maxwell->surf[2], CK(p_surf_z_kernels, cdim, polyOrder), sizeof(maxwell_surf_t));
 
-  // copy to device
+  // copy the host struct to device struct
   struct dg_maxwell *maxwell_cu = (struct dg_maxwell*) gkyl_cu_malloc(sizeof(struct dg_maxwell));
   gkyl_cu_memcpy(maxwell_cu, maxwell, sizeof(struct dg_maxwell), GKYL_CU_MEMCPY_H2D);
 
-  // cleanup
   gkyl_free(maxwell);
-
+  
   return &maxwell_cu->eqn;
 }
