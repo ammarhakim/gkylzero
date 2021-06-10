25b8706e18223a56271b9e208d28d358284afe35
manauref <mfrancisquez@gmail.com>
2021-06-10 12:34:59 -0400

    Clear mout in mom_calc_advance_cu.

--- zero/mom_calc_cu.cu
+++ zero/mom_calc_cu.cu
@@ -1,6 +1,7 @@
 extern "C" {
 #include <gkyl_mom_calc.h>
 #include <gkyl_util.h>
+#include <gkyl_array_ops.h>
 }
 
 __global__ void gkyl_mom_calc_advance_cu_ker(const gkyl_mom_calc* mcalc,
@@ -49,6 +50,8 @@ gkyl_mom_calc_advance_cu(const gkyl_mom_calc* mcalc,
 {
 
   int nblocks = phase_range.nblocks, nthreads = phase_range.nthreads;
+
+  gkyl_array_clear_range(mout, 0.0, conf_range);
 
   gkyl_mom_calc_advance_cu_ker<<<nblocks, nthreads>>>(mcalc, phase_range, conf_range, fin->on_device, mout->on_device);
 
