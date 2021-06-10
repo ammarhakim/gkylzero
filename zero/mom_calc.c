d280ec80fa602fe47f7c6f0ad8a846e2c74dddc0
Noah Mandell <nrmandell@gmail.com>
2021-06-10 12:06:16 -0400

    fixing grid in mom_calc_cu_dev_new, but cuda errors remain.

--- zero/mom_calc.c
+++ zero/mom_calc.c
@@ -18,9 +18,11 @@ gkyl_mom_calc_cu_dev_new(const struct gkyl_rect_grid *grid,
 {
   gkyl_mom_calc *up = gkyl_malloc(sizeof(gkyl_mom_calc));
   up->momt = momt;
+  up->grid = *grid;
 
   gkyl_mom_calc *up_cu = gkyl_cu_malloc(sizeof(gkyl_mom_calc));
   gkyl_cu_memcpy(up_cu, up, sizeof(gkyl_mom_calc), GKYL_CU_MEMCPY_H2D);
+
 
   gkyl_free(up);
   return up_cu;
