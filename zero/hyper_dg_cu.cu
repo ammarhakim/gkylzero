65821f09cc17443e790e854075ea09e797c5d8bc
Noah Mandell <nrmandell@gmail.com>
2021-06-10 09:07:44 -0400

    not sure what happened, but the last commit didn't do what I wanted

--- zero/hyper_dg_cu.cu
+++ zero/hyper_dg_cu.cu
@@ -89,81 +89,6 @@ gkyl_hyper_dg_advance_cu_kernel(gkyl_hyper_dg* hdg, struct gkyl_range update_ran
   }
 }
 
-__global__ void
-gkyl_hyper_dg_advance_cu_kernel_shared(gkyl_hyper_dg* hdg, const struct gkyl_range update_range,
-  const struct gkyl_array* GKYL_RESTRICT fIn, struct gkyl_array* GKYL_RESTRICT cflrate,
-  struct gkyl_array* GKYL_RESTRICT rhs, struct gkyl_array* GKYL_RESTRICT maxs_by_cell)
-{
-  int ndim = hdg->ndim;
-  int idxl[GKYL_MAX_DIM], idxc[GKYL_MAX_DIM], idxr[GKYL_MAX_DIM];
-  double xcl[GKYL_MAX_DIM], xcc[GKYL_MAX_DIM], xcr[GKYL_MAX_DIM];
-  // integer used for selecting between left-edge zero-flux BCs and right-edge zero-flux BCs
-  int edge;
-
-  for(unsigned long linc1 = threadIdx.x + blockIdx.x*blockDim.x; 
-      linc1 < update_range.volume;
-      linc1 += blockDim.x*gridDim.x)
-  {
-    // inverse index from linc1 to idxc
-    // must use gkyl_sub_range_inv_idx so that linc1=0 maps to idxc={0,0,...}
-    // since update_range is a subrange
-    gkyl_sub_range_inv_idx(&update_range, linc1, idxc);
-    gkyl_rect_grid_cell_center(&hdg->grid, idxc, xcc);
-
-    // convert back to a linear index on the super-range (with ghost cells)
-    // linc will have jumps in it to jump over ghost cells
-    long linc = gkyl_range_idx(&update_range, idxc);
-
-    if (hdg->update_vol_term) {
-      double cflr = hdg->equation->vol_term(
-        hdg->equation, xcc, hdg->grid.dx, idxc,
-        (double*) gkyl_array_cfetch(fIn, linc), (double*) gkyl_array_fetch(rhs, linc)
-      );
-      double *cflrate_d = (double*) gkyl_array_fetch(cflrate, linc);
-      cflrate_d[0] += cflr; // frequencies are additive
-    }
-    
-    double *maxs_by_cell_d = (double*) gkyl_array_cfetch(maxs_by_cell, linc);
-    for (int d=0; d<hdg->num_up_dirs; ++d) {
-      int dir = hdg->update_dirs[d];
-      gkyl_copy_int_arr(ndim, idxc, idxl);
-      gkyl_copy_int_arr(ndim, idxc, idxr);
-      // TODO: fix for arbitrary subrange
-      if (hdg->zero_flux_flags[d] && (idxc[dir] == update_range.lower[dir] || idxc[dir] == update_range.upper[dir])) {
-        edge = (idxc[dir] == update_range.lower[dir]) ? -1 : 1;
-        // use idxl to store interior edge index (first index away from skin cell)
-        idxl[dir] = idxl[dir]-edge;
-
-        gkyl_rect_grid_cell_center(&hdg->grid, idxl, xcl);
-        long linl = gkyl_range_idx(&update_range, idxl);
-
-        double mdir = hdg->equation->boundary_surf_term(hdg->equation,
-          dir, xcl, xcc, hdg->grid.dx, hdg->grid.dx,
-          hdg->maxs[dir], idxl, idxc, edge,
-          (double*) gkyl_array_cfetch(fIn, linl), (double*) gkyl_array_cfetch(fIn, linc),
-          (double*) gkyl_array_fetch(rhs, linc)
-        );
-        maxs_by_cell_d[dir] = mdir;
-      }
-      else {
-        idxl[dir] = idxl[dir]-1; idxr[dir] = idxr[dir]+1;
-        gkyl_rect_grid_cell_center(&hdg->grid, idxl, xcl);
-        gkyl_rect_grid_cell_center(&hdg->grid, idxr, xcr);
-        long linl = gkyl_range_idx(&update_range, idxl); 
-        long linr = gkyl_range_idx(&update_range, idxr);
-
-        double mdir = hdg->equation->surf_term(hdg->equation,
-          dir, xcl, xcc, xcr, hdg->grid.dx, hdg->grid.dx, hdg->grid.dx,
-          hdg->maxs[dir], idxl, idxc, idxr,
-          (double*) gkyl_array_cfetch(fIn, linl), (double*) gkyl_array_cfetch(fIn, linc), (double*) gkyl_array_cfetch(fIn, linr),
-          (double*) gkyl_array_fetch(rhs, linc)
-        );
-        maxs_by_cell_d[dir] = mdir;
-      }
-    }
-  }
-}
-
 // wrapper to call advance kernel on device
 void
 gkyl_hyper_dg_advance_cu(gkyl_hyper_dg* hdg, struct gkyl_range update_range,
@@ -174,9 +99,6 @@ gkyl_hyper_dg_advance_cu(gkyl_hyper_dg* hdg, struct gkyl_range update_range,
   int nthreads = update_range.nthreads;
 
   gkyl_hyper_dg_advance_cu_kernel<<<nblocks, nthreads>>>(hdg, update_range,
-    fIn->on_device, cflrate->on_device, rhs->on_device, maxs_by_cell->on_device);
-
-  gkyl_hyper_dg_advance_cu_kernel_shared<<<nblocks, nthreads, >>>(hdg, update_range,
     fIn->on_device, cflrate->on_device, rhs->on_device, maxs_by_cell->on_device);
 }
 
