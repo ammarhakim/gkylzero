4ff6bf9e1f9c9494fb528ac3f109e15c6f02c874
ammarhakim <a.hakim777@gmail.com>
2021-06-22 13:10:01 -0700

    Refactoring of update_fsm. Seems like horribly complicated. Not sure if it really adds anything

--- apps/gkyl_app.h
+++ apps/gkyl_app.h
@@ -1,3 +1,10 @@
 #pragma once
 
-#include <gkyl_update_fsm.h>
+#include <stdbool.h>
+
+// Update status
+struct gkyl_update_status {
+  bool success; // status of update
+  double dt_actual; // actual time-step taken
+  double dt_suggested; // suggested stable time-step
+};
