4a8e0655cc420df0b40c045442cd1bea17b1654c
ammarhakim <a.hakim777@gmail.com>
2021-07-04 10:52:13 -0700

    Adding read method to grid module. Added unit tests. Tests pass.

--- zero/rect_grid.c
+++ zero/rect_grid.c
@@ -34,3 +34,28 @@ gkyl_rect_grid_write(const struct gkyl_rect_grid *grid, FILE *fp)
   fwrite(grid->upper, sizeof(double), grid->ndim, fp);
 }
 
+bool
+gkyl_rect_grid_read(struct gkyl_rect_grid *grid, FILE *fp)
+{
+  uint64_t ndim = grid->ndim;
+  uint64_t cells64[GKYL_MAX_DIM];
+
+  if (1 != fread(&ndim, sizeof(uint64_t), 1, fp))
+    return false;
+  if (ndim != fread(cells64, sizeof(uint64_t), ndim, fp))
+    return false;
+
+  double lower[GKYL_MAX_DIM], upper[GKYL_MAX_DIM];
+  if (ndim != fread(lower, sizeof(double), ndim, fp))
+    return false;
+  if (ndim != fread(upper, sizeof(double), ndim, fp))
+    return false;
+
+  // copy into regular int array
+  int cells[GKYL_MAX_DIM];
+  for (int d=0; d<ndim; ++d) cells[d] = cells64[d];
+
+  gkyl_rect_grid_init(grid, ndim, lower, upper, cells);
+
+  return true;
+}
