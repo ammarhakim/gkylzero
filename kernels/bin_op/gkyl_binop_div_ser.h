dbd7a0faf749e444694c5e7449d0fdc2128fbd79
kolterb <kolterb@vt.edu>
2021-07-29 17:55:47 -0400

    added div header file and example kernel for debugging

---
+++ kernels/bin_op/gkyl_binop_div_ser.h
@@ -0,0 +1,14 @@
+// Mon Jul 19 01:21:18 2021
+#pragma once
+#include <gkyl_util.h>
+EXTERN_C_BEG
+void binop_div_1d_ser_p0(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
+void binop_div_1d_ser_p1(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
+void binop_div_1d_ser_p2(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
+void binop_div_2d_ser_p0(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
+void binop_div_2d_ser_p1(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
+void binop_div_2d_ser_p2(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
+void binop_div_3d_ser_p0(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
+void binop_div_3d_ser_p1(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
+void binop_div_3d_ser_p2(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
+EXTERN_C_END
