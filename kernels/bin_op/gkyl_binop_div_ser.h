3bc2f252eb71b312634c3caf45cf2fed7ac7743c
kolterb <kolterb@vt.edu>
2021-07-30 00:01:12 -0400

    added p=0 kernels into div header

--- kernels/bin_op/gkyl_binop_div_ser.h
+++ kernels/bin_op/gkyl_binop_div_ser.h
@@ -3,12 +3,15 @@
 #include <gkyl_util.h>
 #include <gkyl_mat.h>
 EXTERN_C_BEG
+void binop_div_1d_ser_p0(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
 void binop_div_1d_ser_p1(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
 void binop_div_1d_ser_p2(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
 void binop_div_1d_ser_p3(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
+void binop_div_2d_ser_p0(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
 void binop_div_2d_ser_p1(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
 void binop_div_2d_ser_p2(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
 void binop_div_2d_ser_p3(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
+void binop_div_3d_ser_p0(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
 void binop_div_3d_ser_p1(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
 void binop_div_3d_ser_p2(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
 void binop_div_3d_ser_p3(struct gkyl_mat *A, struct gkyl_mat *rhs, const double *f, const double *g, double *fdivg );
