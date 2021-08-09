143f0147bb3b3382078212a7485f7d286163a55b
ammarhakim <a.hakim777@gmail.com>
2021-08-09 19:49:56 -0400

    Completed the project Maxwellian on basis functions code that takes lab-moments and computes the Maxwellian. The code is very complex and perhaps explains why this could be a bottle-neck if Maxwellians are required every time-step (like in BGK operator).
    Added test for 1x1v_p1 case at present and it "seems" to
    work. However, need more careful tests to ensure it is actually
    working!

---
+++ unit/ctest_proj_maxwellian_on_basis.c
@@ -0,0 +1,136 @@
+#include <acutest.h>
+
+#include <gkyl_array_rio.h>
+#include <gkyl_proj_maxwellian_on_basis.h>
+#include <gkyl_proj_on_basis.h>
+#include <gkyl_range.h>
+#include <gkyl_rect_decomp.h>
+#include <gkyl_rect_grid.h>
+
+// allocate array (filled with zeros)
+static struct gkyl_array*
+mkarr(long nc, long size)
+{
+  struct gkyl_array* a = gkyl_array_new(GKYL_DOUBLE, nc, size);
+  return a;
+}
+
+struct skin_ghost_ranges {
+  struct gkyl_range lower_skin[GKYL_MAX_DIM];
+  struct gkyl_range lower_ghost[GKYL_MAX_DIM];
+
+  struct gkyl_range upper_skin[GKYL_MAX_DIM];
+  struct gkyl_range upper_ghost[GKYL_MAX_DIM];
+};
+
+// Create ghost and skin sub-ranges given a parent range
+static void
+skin_ghost_ranges_init(struct skin_ghost_ranges *sgr,
+  const struct gkyl_range *parent, const int *ghost)
+{
+  int ndim = parent->ndim;
+
+  for (int d=0; d<ndim; ++d) {
+    gkyl_skin_ghost_ranges(&sgr->lower_skin[d], &sgr->lower_ghost[d],
+      d, GKYL_LOWER_EDGE, parent, ghost);
+    gkyl_skin_ghost_ranges(&sgr->upper_skin[d], &sgr->upper_ghost[d],
+      d, GKYL_UPPER_EDGE, parent, ghost);
+  }
+}
+
+void eval_M0(double t, const double *xn, double* restrict fout, void *ctx)
+{
+  double x = xn[0];
+  fout[0] = 1.0;
+}
+
+void eval_M1i_1v(double t, const double *xn, double* restrict fout, void *ctx)
+{
+  double x = xn[0];
+  fout[0] = 0.0;
+}
+
+void eval_M2(double t, const double *xn, double* restrict fout, void *ctx)
+{
+  double x = xn[0];
+  fout[0] = 1.0;
+}
+
+void
+test_1x1v_p1()
+{
+  int poly_order = 1;
+  double lower[] = {0.1, -6.0}, upper[] = {1.0, 6.0};
+  int cells[] = {2, 32};
+  int vdim = 1, cdim = 1;
+  int ndim = cdim+vdim;
+
+  double confLower[] = {lower[0]}, confUpper[] = {upper[0]};
+  int confCells[] = {cells[0]};
+
+  // grids
+  struct gkyl_rect_grid grid;
+  gkyl_rect_grid_init(&grid, ndim, lower, upper, cells);
+  struct gkyl_rect_grid confGrid;
+  gkyl_rect_grid_init(&confGrid, cdim, confLower, confUpper, confCells);
+
+  // basis functions
+  struct gkyl_basis basis, confBasis;
+  gkyl_cart_modal_serendip(&basis, ndim, poly_order);
+  gkyl_cart_modal_serendip(&confBasis, cdim, poly_order);
+
+  int confGhost[] = { 1 };
+  struct gkyl_range confLocal, confLocal_ext; // local, local-ext conf-space ranges
+  gkyl_create_grid_ranges(&confGrid, confGhost, &confLocal_ext, &confLocal);
+  struct skin_ghost_ranges confSkin_ghost; // conf-space skin/ghost
+  skin_ghost_ranges_init(&confSkin_ghost, &confLocal_ext, confGhost);
+
+  int ghost[] = { confGhost[0], 0 };
+  struct gkyl_range local, local_ext; // local, local-ext phase-space ranges
+  gkyl_create_grid_ranges(&grid, ghost, &local_ext, &local);
+  struct skin_ghost_ranges skin_ghost; // phase-space skin/ghost
+  skin_ghost_ranges_init(&skin_ghost, &local_ext, ghost);
+
+  // create moment arrays
+  struct gkyl_array *m0, *m1i, *m2;
+  m0 = mkarr(confBasis.num_basis, confLocal_ext.volume);
+  m1i = mkarr(vdim*confBasis.num_basis, confLocal_ext.volume);
+  m2 = mkarr(confBasis.num_basis, confLocal_ext.volume);
+
+  gkyl_proj_on_basis *proj_m0 = gkyl_proj_on_basis_new(&confGrid, &confBasis,
+    poly_order+1, 1, eval_M0, NULL);
+  gkyl_proj_on_basis *proj_m1i = gkyl_proj_on_basis_new(&confGrid, &confBasis,
+    poly_order+1, vdim, eval_M1i_1v, NULL);
+  gkyl_proj_on_basis *proj_m2 = gkyl_proj_on_basis_new(&confGrid, &confBasis,
+    poly_order+1, 1, eval_M2, NULL);
+
+  gkyl_proj_on_basis_advance(proj_m0, 0.0, &confLocal, m0);
+  gkyl_proj_on_basis_advance(proj_m1i, 0.0, &confLocal, m1i);
+  gkyl_proj_on_basis_advance(proj_m2, 0.0, &confLocal, m2);
+
+  // create distribution function array
+  struct gkyl_array *distf;
+  distf = mkarr(basis.num_basis, local_ext.volume);
+
+  // projection updater to compute Maxwellian
+  gkyl_proj_maxwellian_on_basis *proj_max = gkyl_proj_maxwellian_on_basis_new(&grid,
+    &confBasis, &basis, poly_order+1);
+
+  gkyl_proj_maxwellian_on_basis_lab_mom(proj_max, local, confLocal, m0, m1i, m2, distf);
+
+  // write distribution function to file
+  gkyl_grid_sub_array_write(&grid, &local, distf, "ctest_proj_maxwellian_on_basis_test_1x1v_p1.gkyl");
+
+  // release memory for moment data object
+  gkyl_array_release(m0); gkyl_array_release(m1i); gkyl_array_release(m2);
+  gkyl_array_release(distf);
+  gkyl_proj_maxwellian_on_basis_release(proj_max);
+  gkyl_proj_on_basis_release(proj_m0);
+  gkyl_proj_on_basis_release(proj_m1i);
+  gkyl_proj_on_basis_release(proj_m2);
+}
+
+TEST_LIST = {
+  { "test_1x1v_p1", test_1x1v_p1 },
+  { NULL, NULL },
+};
