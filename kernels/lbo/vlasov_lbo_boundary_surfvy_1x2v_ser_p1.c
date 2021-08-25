722478363d3fae150df1d1fe1e4c5708eb27eb52
kolterb <kolterb@vt.edu>
2021-08-25 15:59:23 -0400

    regenerated kernels, removed const_nu from function names, and fixed signature issues in the dg_vlasov_lbo private header

---
+++ kernels/lbo/vlasov_lbo_boundary_surfvy_1x2v_ser_p1.c
@@ -0,0 +1,139 @@
+#include <gkyl_vlasov_lbo_kernels.h> 
+#include <gkyl_basis_ser_1x2v_p1_surfvy_quad.h> 
+GKYL_CU_DH void vlasov_lbo_boundary_surfvy_1x2v_ser_p1(const double *w, const double *dxv, const double nuSum, const double *nuUSum, const double *nuVtSqSum, const int edge, const double *fSkin, const double *fEdge, double* GKYL_RESTRICT out) 
+{ 
+  // w[3]:         Cell-center coordinates. 
+  // dxv[3]:       Cell spacing. 
+  // nuSum:         collisionalities added (self and cross species collisionalities). 
+  // nuUSum[4]:    sum of bulk velocities times their respective collisionalities. 
+  // nuVtSqSum[2]: sum of thermal speeds squared time their respective collisionalities. 
+  // fSkin/Edge:    Distribution function in cells 
+  // out:           Incremented distribution function in cell 
+  double rdv2 = 2.0/dxv[2]; 
+  double rdvSq4 = 4.0/(dxv[2]*dxv[2]); 
+
+  const double *sumNuUy = &nuUSum[2]; 
+
+  double alphaDrSurf[4] = {0.0}; 
+  double fUpwindQuad[4] = {0.0};
+  double fUpwind[4] = {0.0};;
+  double Ghat[4] = {0.0}; 
+  double Gdiff[4] = {0.0}; 
+  double Gdiff2[4] = {0.0}; 
+
+  if (edge == -1) { 
+
+  alphaDrSurf[0] = (2.0*w[2]+dxv[2])*nuSum-1.414213562373095*sumNuUy[0]; 
+  alphaDrSurf[1] = -1.414213562373095*sumNuUy[1]; 
+
+  if (alphaDrSurf[0]-alphaDrSurf[1] > 0) { 
+    fUpwindQuad[0] = ser_1x2v_p1_surfvy_quad_0(1, fSkin); 
+  } else { 
+
+    fUpwindQuad[0] = ser_1x2v_p1_surfvy_quad_0(-1, fEdge); 
+  } 
+  if (alphaDrSurf[1]+alphaDrSurf[0] > 0) { 
+    fUpwindQuad[1] = ser_1x2v_p1_surfvy_quad_1(1, fSkin); 
+  } else { 
+
+    fUpwindQuad[1] = ser_1x2v_p1_surfvy_quad_1(-1, fEdge); 
+  } 
+  if (alphaDrSurf[0]-alphaDrSurf[1] > 0) { 
+    fUpwindQuad[2] = ser_1x2v_p1_surfvy_quad_2(1, fSkin); 
+  } else { 
+
+    fUpwindQuad[2] = ser_1x2v_p1_surfvy_quad_2(-1, fEdge); 
+  } 
+  if (alphaDrSurf[1]+alphaDrSurf[0] > 0) { 
+    fUpwindQuad[3] = ser_1x2v_p1_surfvy_quad_3(1, fSkin); 
+  } else { 
+
+    fUpwindQuad[3] = ser_1x2v_p1_surfvy_quad_3(-1, fEdge); 
+  } 
+
+  fUpwind[0] = 0.5*(fUpwindQuad[3]+fUpwindQuad[2]+fUpwindQuad[1]+fUpwindQuad[0]); 
+  fUpwind[1] = 0.5*(fUpwindQuad[3]-1.0*fUpwindQuad[2]+fUpwindQuad[1]-1.0*fUpwindQuad[0]); 
+  fUpwind[2] = 0.5*(fUpwindQuad[3]+fUpwindQuad[2]-1.0*(fUpwindQuad[1]+fUpwindQuad[0])); 
+  fUpwind[3] = 0.5*(fUpwindQuad[3]-1.0*(fUpwindQuad[2]+fUpwindQuad[1])+fUpwindQuad[0]); 
+
+  Gdiff2[0] = 0.2886751345948129*nuVtSqSum[1]*fSkin[5]-0.2886751345948129*nuVtSqSum[1]*fEdge[5]+0.2886751345948129*nuVtSqSum[0]*fSkin[3]-0.2886751345948129*nuVtSqSum[0]*fEdge[3]+0.25*fSkin[1]*nuVtSqSum[1]+0.25*fEdge[1]*nuVtSqSum[1]+0.25*fSkin[0]*nuVtSqSum[0]+0.25*fEdge[0]*nuVtSqSum[0]; 
+  Gdiff2[1] = 0.2886751345948129*nuVtSqSum[0]*fSkin[5]-0.2886751345948129*nuVtSqSum[0]*fEdge[5]+0.2886751345948129*nuVtSqSum[1]*fSkin[3]-0.2886751345948129*nuVtSqSum[1]*fEdge[3]+0.25*fSkin[0]*nuVtSqSum[1]+0.25*fEdge[0]*nuVtSqSum[1]+0.25*nuVtSqSum[0]*fSkin[1]+0.25*nuVtSqSum[0]*fEdge[1]; 
+  Gdiff2[2] = 0.2886751345948129*nuVtSqSum[1]*fSkin[7]-0.2886751345948129*nuVtSqSum[1]*fEdge[7]+0.2886751345948129*nuVtSqSum[0]*fSkin[6]-0.2886751345948129*nuVtSqSum[0]*fEdge[6]+0.25*nuVtSqSum[1]*fSkin[4]+0.25*nuVtSqSum[1]*fEdge[4]+0.25*nuVtSqSum[0]*fSkin[2]+0.25*nuVtSqSum[0]*fEdge[2]; 
+  Gdiff2[3] = 0.2886751345948129*nuVtSqSum[0]*fSkin[7]-0.2886751345948129*nuVtSqSum[0]*fEdge[7]+0.2886751345948129*nuVtSqSum[1]*fSkin[6]-0.2886751345948129*nuVtSqSum[1]*fEdge[6]+0.25*nuVtSqSum[0]*fSkin[4]+0.25*nuVtSqSum[0]*fEdge[4]+0.25*nuVtSqSum[1]*fSkin[2]+0.25*nuVtSqSum[1]*fEdge[2]; 
+
+  Gdiff[0] = (-0.5412658773652741*nuVtSqSum[1]*fSkin[5])-0.5412658773652741*nuVtSqSum[1]*fEdge[5]-0.5412658773652741*nuVtSqSum[0]*fSkin[3]-0.5412658773652741*nuVtSqSum[0]*fEdge[3]-0.5625*fSkin[1]*nuVtSqSum[1]+0.5625*fEdge[1]*nuVtSqSum[1]-0.5625*fSkin[0]*nuVtSqSum[0]+0.5625*fEdge[0]*nuVtSqSum[0]; 
+  Gdiff[1] = (-0.5412658773652741*nuVtSqSum[0]*fSkin[5])-0.5412658773652741*nuVtSqSum[0]*fEdge[5]-0.5412658773652741*nuVtSqSum[1]*fSkin[3]-0.5412658773652741*nuVtSqSum[1]*fEdge[3]-0.5625*fSkin[0]*nuVtSqSum[1]+0.5625*fEdge[0]*nuVtSqSum[1]-0.5625*nuVtSqSum[0]*fSkin[1]+0.5625*nuVtSqSum[0]*fEdge[1]; 
+  Gdiff[2] = (-0.5412658773652741*nuVtSqSum[1]*fSkin[7])-0.5412658773652741*nuVtSqSum[1]*fEdge[7]-0.5412658773652741*nuVtSqSum[0]*fSkin[6]-0.5412658773652741*nuVtSqSum[0]*fEdge[6]-0.5625*nuVtSqSum[1]*fSkin[4]+0.5625*nuVtSqSum[1]*fEdge[4]-0.5625*nuVtSqSum[0]*fSkin[2]+0.5625*nuVtSqSum[0]*fEdge[2]; 
+  Gdiff[3] = (-0.5412658773652741*nuVtSqSum[0]*fSkin[7])-0.5412658773652741*nuVtSqSum[0]*fEdge[7]-0.5412658773652741*nuVtSqSum[1]*fSkin[6]-0.5412658773652741*nuVtSqSum[1]*fEdge[6]-0.5625*nuVtSqSum[0]*fSkin[4]+0.5625*nuVtSqSum[0]*fEdge[4]-0.5625*nuVtSqSum[1]*fSkin[2]+0.5625*nuVtSqSum[1]*fEdge[2]; 
+
+  Ghat[0] = (-1.0*Gdiff[0]*rdv2)+0.5*alphaDrSurf[1]*fUpwind[1]+0.5*alphaDrSurf[0]*fUpwind[0]; 
+  Ghat[1] = (-1.0*Gdiff[1]*rdv2)+0.5*alphaDrSurf[0]*fUpwind[1]+0.5*fUpwind[0]*alphaDrSurf[1]; 
+  Ghat[2] = (-1.0*Gdiff[2]*rdv2)+0.5*alphaDrSurf[1]*fUpwind[3]+0.5*alphaDrSurf[0]*fUpwind[2]; 
+  Ghat[3] = (-1.0*Gdiff[3]*rdv2)+0.5*alphaDrSurf[0]*fUpwind[3]+0.5*alphaDrSurf[1]*fUpwind[2]; 
+
+  out[0] += -0.7071067811865475*Ghat[0]*rdv2; 
+  out[1] += -0.7071067811865475*Ghat[1]*rdv2; 
+  out[2] += -0.7071067811865475*Ghat[2]*rdv2; 
+  out[3] += 1.060660171779821*nuVtSqSum[1]*fSkin[5]*rdvSq4+1.060660171779821*nuVtSqSum[0]*fSkin[3]*rdvSq4-0.6123724356957944*fSkin[1]*nuVtSqSum[1]*rdvSq4-0.6123724356957944*fSkin[0]*nuVtSqSum[0]*rdvSq4+1.224744871391589*Gdiff2[0]*rdvSq4-1.224744871391589*Ghat[0]*rdv2; 
+  out[4] += -0.7071067811865475*Ghat[3]*rdv2; 
+  out[5] += 1.060660171779821*nuVtSqSum[0]*fSkin[5]*rdvSq4+1.060660171779821*nuVtSqSum[1]*fSkin[3]*rdvSq4-0.6123724356957944*fSkin[0]*nuVtSqSum[1]*rdvSq4-0.6123724356957944*nuVtSqSum[0]*fSkin[1]*rdvSq4+1.224744871391589*Gdiff2[1]*rdvSq4-1.224744871391589*Ghat[1]*rdv2; 
+  out[6] += 1.060660171779821*nuVtSqSum[1]*fSkin[7]*rdvSq4+1.060660171779821*nuVtSqSum[0]*fSkin[6]*rdvSq4-0.6123724356957944*nuVtSqSum[1]*fSkin[4]*rdvSq4-0.6123724356957944*nuVtSqSum[0]*fSkin[2]*rdvSq4+1.224744871391589*Gdiff2[2]*rdvSq4-1.224744871391589*Ghat[2]*rdv2; 
+  out[7] += 1.060660171779821*nuVtSqSum[0]*fSkin[7]*rdvSq4+1.060660171779821*nuVtSqSum[1]*fSkin[6]*rdvSq4-0.6123724356957944*nuVtSqSum[0]*fSkin[4]*rdvSq4+1.224744871391589*Gdiff2[3]*rdvSq4-0.6123724356957944*nuVtSqSum[1]*fSkin[2]*rdvSq4-1.224744871391589*Ghat[3]*rdv2; 
+
+  } else { 
+
+  alphaDrSurf[0] = (2.0*w[2]+dxv[2])*nuSum-1.414213562373095*sumNuUy[0]; 
+  alphaDrSurf[1] = -1.414213562373095*sumNuUy[1]; 
+
+  if (alphaDrSurf[0]-alphaDrSurf[1] > 0) { 
+    fUpwindQuad[0] = ser_1x2v_p1_surfvy_quad_0(1, fEdge); 
+  } else { 
+    fUpwindQuad[0] = ser_1x2v_p1_surfvy_quad_0(-1, fSkin); 
+  } 
+  if (alphaDrSurf[1]+alphaDrSurf[0] > 0) { 
+    fUpwindQuad[1] = ser_1x2v_p1_surfvy_quad_1(1, fEdge); 
+  } else { 
+    fUpwindQuad[1] = ser_1x2v_p1_surfvy_quad_1(-1, fSkin); 
+  } 
+  if (alphaDrSurf[0]-alphaDrSurf[1] > 0) { 
+    fUpwindQuad[2] = ser_1x2v_p1_surfvy_quad_2(1, fEdge); 
+  } else { 
+    fUpwindQuad[2] = ser_1x2v_p1_surfvy_quad_2(-1, fSkin); 
+  } 
+  if (alphaDrSurf[1]+alphaDrSurf[0] > 0) { 
+    fUpwindQuad[3] = ser_1x2v_p1_surfvy_quad_3(1, fEdge); 
+  } else { 
+    fUpwindQuad[3] = ser_1x2v_p1_surfvy_quad_3(-1, fSkin); 
+  } 
+
+  fUpwind[0] = 0.5*(fUpwindQuad[3]+fUpwindQuad[2]+fUpwindQuad[1]+fUpwindQuad[0]); 
+  fUpwind[1] = 0.5*(fUpwindQuad[3]-1.0*fUpwindQuad[2]+fUpwindQuad[1]-1.0*fUpwindQuad[0]); 
+  fUpwind[2] = 0.5*(fUpwindQuad[3]+fUpwindQuad[2]-1.0*(fUpwindQuad[1]+fUpwindQuad[0])); 
+  fUpwind[3] = 0.5*(fUpwindQuad[3]-1.0*(fUpwindQuad[2]+fUpwindQuad[1])+fUpwindQuad[0]); 
+
+  Gdiff2[0] = (-0.2886751345948129*nuVtSqSum[1]*fSkin[5])+0.2886751345948129*nuVtSqSum[1]*fEdge[5]-0.2886751345948129*nuVtSqSum[0]*fSkin[3]+0.2886751345948129*nuVtSqSum[0]*fEdge[3]+0.25*fSkin[1]*nuVtSqSum[1]+0.25*fEdge[1]*nuVtSqSum[1]+0.25*fSkin[0]*nuVtSqSum[0]+0.25*fEdge[0]*nuVtSqSum[0]; 
+  Gdiff2[1] = (-0.2886751345948129*nuVtSqSum[0]*fSkin[5])+0.2886751345948129*nuVtSqSum[0]*fEdge[5]-0.2886751345948129*nuVtSqSum[1]*fSkin[3]+0.2886751345948129*nuVtSqSum[1]*fEdge[3]+0.25*fSkin[0]*nuVtSqSum[1]+0.25*fEdge[0]*nuVtSqSum[1]+0.25*nuVtSqSum[0]*fSkin[1]+0.25*nuVtSqSum[0]*fEdge[1]; 
+  Gdiff2[2] = (-0.2886751345948129*nuVtSqSum[1]*fSkin[7])+0.2886751345948129*nuVtSqSum[1]*fEdge[7]-0.2886751345948129*nuVtSqSum[0]*fSkin[6]+0.2886751345948129*nuVtSqSum[0]*fEdge[6]+0.25*nuVtSqSum[1]*fSkin[4]+0.25*nuVtSqSum[1]*fEdge[4]+0.25*nuVtSqSum[0]*fSkin[2]+0.25*nuVtSqSum[0]*fEdge[2]; 
+  Gdiff2[3] = (-0.2886751345948129*nuVtSqSum[0]*fSkin[7])+0.2886751345948129*nuVtSqSum[0]*fEdge[7]-0.2886751345948129*nuVtSqSum[1]*fSkin[6]+0.2886751345948129*nuVtSqSum[1]*fEdge[6]+0.25*nuVtSqSum[0]*fSkin[4]+0.25*nuVtSqSum[0]*fEdge[4]+0.25*nuVtSqSum[1]*fSkin[2]+0.25*nuVtSqSum[1]*fEdge[2]; 
+
+  Gdiff[0] = (-0.5412658773652741*nuVtSqSum[1]*fSkin[5])-0.5412658773652741*nuVtSqSum[1]*fEdge[5]-0.5412658773652741*nuVtSqSum[0]*fSkin[3]-0.5412658773652741*nuVtSqSum[0]*fEdge[3]+0.5625*fSkin[1]*nuVtSqSum[1]-0.5625*fEdge[1]*nuVtSqSum[1]+0.5625*fSkin[0]*nuVtSqSum[0]-0.5625*fEdge[0]*nuVtSqSum[0]; 
+  Gdiff[1] = (-0.5412658773652741*nuVtSqSum[0]*fSkin[5])-0.5412658773652741*nuVtSqSum[0]*fEdge[5]-0.5412658773652741*nuVtSqSum[1]*fSkin[3]-0.5412658773652741*nuVtSqSum[1]*fEdge[3]+0.5625*fSkin[0]*nuVtSqSum[1]-0.5625*fEdge[0]*nuVtSqSum[1]+0.5625*nuVtSqSum[0]*fSkin[1]-0.5625*nuVtSqSum[0]*fEdge[1]; 
+  Gdiff[2] = (-0.5412658773652741*nuVtSqSum[1]*fSkin[7])-0.5412658773652741*nuVtSqSum[1]*fEdge[7]-0.5412658773652741*nuVtSqSum[0]*fSkin[6]-0.5412658773652741*nuVtSqSum[0]*fEdge[6]+0.5625*nuVtSqSum[1]*fSkin[4]-0.5625*nuVtSqSum[1]*fEdge[4]+0.5625*nuVtSqSum[0]*fSkin[2]-0.5625*nuVtSqSum[0]*fEdge[2]; 
+  Gdiff[3] = (-0.5412658773652741*nuVtSqSum[0]*fSkin[7])-0.5412658773652741*nuVtSqSum[0]*fEdge[7]-0.5412658773652741*nuVtSqSum[1]*fSkin[6]-0.5412658773652741*nuVtSqSum[1]*fEdge[6]+0.5625*nuVtSqSum[0]*fSkin[4]-0.5625*nuVtSqSum[0]*fEdge[4]+0.5625*nuVtSqSum[1]*fSkin[2]-0.5625*nuVtSqSum[1]*fEdge[2]; 
+
+  Ghat[0] = Gdiff[0]*rdv2+0.5*alphaDrSurf[1]*fUpwind[1]+0.5*alphaDrSurf[0]*fUpwind[0]; 
+  Ghat[1] = Gdiff[1]*rdv2+0.5*alphaDrSurf[0]*fUpwind[1]+0.5*fUpwind[0]*alphaDrSurf[1]; 
+  Ghat[2] = Gdiff[2]*rdv2+0.5*alphaDrSurf[1]*fUpwind[3]+0.5*alphaDrSurf[0]*fUpwind[2]; 
+  Ghat[3] = Gdiff[3]*rdv2+0.5*alphaDrSurf[0]*fUpwind[3]+0.5*alphaDrSurf[1]*fUpwind[2]; 
+
+  out[0] += 0.7071067811865475*Ghat[0]*rdv2; 
+  out[1] += 0.7071067811865475*Ghat[1]*rdv2; 
+  out[2] += 0.7071067811865475*Ghat[2]*rdv2; 
+  out[3] += 1.060660171779821*nuVtSqSum[1]*fSkin[5]*rdvSq4+1.060660171779821*nuVtSqSum[0]*fSkin[3]*rdvSq4+0.6123724356957944*fSkin[1]*nuVtSqSum[1]*rdvSq4+0.6123724356957944*fSkin[0]*nuVtSqSum[0]*rdvSq4-1.224744871391589*Gdiff2[0]*rdvSq4-1.224744871391589*Ghat[0]*rdv2; 
+  out[4] += 0.7071067811865475*Ghat[3]*rdv2; 
+  out[5] += 1.060660171779821*nuVtSqSum[0]*fSkin[5]*rdvSq4+1.060660171779821*nuVtSqSum[1]*fSkin[3]*rdvSq4+0.6123724356957944*fSkin[0]*nuVtSqSum[1]*rdvSq4+0.6123724356957944*nuVtSqSum[0]*fSkin[1]*rdvSq4-1.224744871391589*Gdiff2[1]*rdvSq4-1.224744871391589*Ghat[1]*rdv2; 
+  out[6] += 1.060660171779821*nuVtSqSum[1]*fSkin[7]*rdvSq4+1.060660171779821*nuVtSqSum[0]*fSkin[6]*rdvSq4+0.6123724356957944*nuVtSqSum[1]*fSkin[4]*rdvSq4+0.6123724356957944*nuVtSqSum[0]*fSkin[2]*rdvSq4-1.224744871391589*Gdiff2[2]*rdvSq4-1.224744871391589*Ghat[2]*rdv2; 
+  out[7] += 1.060660171779821*nuVtSqSum[0]*fSkin[7]*rdvSq4+1.060660171779821*nuVtSqSum[1]*fSkin[6]*rdvSq4+0.6123724356957944*nuVtSqSum[0]*fSkin[4]*rdvSq4-1.224744871391589*Gdiff2[3]*rdvSq4+0.6123724356957944*nuVtSqSum[1]*fSkin[2]*rdvSq4-1.224744871391589*Ghat[3]*rdv2; 
+
+  } 
+} 
---
+++ zero/gkyl_dg_vlasov_lbo_priv.h
@@ -0,0 +1,153 @@
+#pragma once
+
+// Private header, not for direct use in user code
+
+#include <gkyl_vlasov_lbo_kernels.h>
+
+// Types for various kernels
+typedef void (*vlasov_lbo_surf_t)(const double *w, const double *dxv,
+  double nuSum, const double *nuUSum,
+  const double *nuVtSqSum, const double *fl,
+  const double *fc, const double *fr, double* GKYL_RESTRICT out);
+
+typedef void (*vlasov_lbo_boundary_surf_t)(const double *w, const double *dxv,
+  double nuSum, const double *nuUSum,
+  const double *nuVtSqSum, const int edge,
+  const double *fSkin, const double *fEdge, double* GKYL_RESTRICT out);
+
+// The cv_index[cd].vdim[vd] is used to index the various list of
+// kernels below
+static struct { int vdim[4]; } cv_index[] = {
+  {-1, -1, -1, -1}, // 0x makes no sense
+  {-1,  0,  1,  2}, // 1x kernel indices
+  {-1, -1,  3,  4}, // 2x kernel indices
+  {-1, -1, -1,  5}, // 3x kernel indices  
+};
+
+// Constant nu surface kernel list: vx-direction
+GKYL_CU_D
+static struct { vlasov_lbo_surf_t kernels[3]; } surf_vx_kernels[] = {
+  // 1x kernels
+  { NULL, vlasov_lbo_surfvx_1x1v_ser_p1, vlasov_lbo_surfvx_1x1v_ser_p2 }, // 0
+  { NULL, vlasov_lbo_surfvx_1x2v_ser_p1, vlasov_lbo_surfvx_1x2v_ser_p2 }, // 1
+  { NULL, vlasov_lbo_surfvx_1x3v_ser_p1, vlasov_lbo_surfvx_1x3v_ser_p2 }, // 2
+  // 2x kernels
+  { NULL, vlasov_lbo_surfvx_2x2v_ser_p1, vlasov_lbo_surfvx_2x2v_ser_p2 }, // 3
+  { NULL, vlasov_lbo_surfvx_2x3v_ser_p1, NULL }, // 4
+  // 3x kernels
+  { NULL, vlasov_lbo_surfvx_3x3v_ser_p1, NULL                   }, // 5
+};
+
+// Constant nu surface kernel list: vy-direction
+GKYL_CU_D
+static struct { vlasov_lbo_surf_t kernels[3]; } surf_vy_kernels[] = {
+  // 1x kernels
+  { NULL, NULL, NULL }, // 0
+  { NULL, vlasov_lbo_surfvy_1x2v_ser_p1, vlasov_lbo_surfvy_1x2v_ser_p2 }, // 1
+  { NULL, vlasov_lbo_surfvy_1x3v_ser_p1, vlasov_lbo_surfvy_1x3v_ser_p2 }, // 2
+  // 2x kernels
+  { NULL, vlasov_lbo_surfvy_2x2v_ser_p1, vlasov_lbo_surfvy_2x2v_ser_p2 }, // 3
+  { NULL, vlasov_lbo_surfvy_2x3v_ser_p1, NULL }, // 4
+  // 3x kernels
+  { NULL, vlasov_lbo_surfvy_3x3v_ser_p1, NULL                   }, // 5
+};
+
+// Constant nu surface kernel list: vz-direction
+GKYL_CU_D
+static struct { vlasov_lbo_surf_t kernels[3]; } surf_vz_kernels[] = {
+  // 1x kernels
+  { NULL, NULL, NULL }, // 0
+  { NULL, NULL, NULL }, // 1
+  { NULL, vlasov_lbo_surfvz_1x3v_ser_p1, vlasov_lbo_surfvz_1x3v_ser_p2 }, // 2
+  // 2x kernels
+  { NULL, NULL, NULL }, // 3
+  { NULL, vlasov_lbo_surfvz_2x3v_ser_p1, NULL }, // 4
+  // 3x kernels
+  { NULL, vlasov_lbo_surfvz_3x3v_ser_p1, NULL }, // 5
+};
+
+// Constant nu boundary surface kernel (zero-flux BCs) list: vx-direction
+GKYL_CU_D
+static struct { vlasov_lbo_boundary_surf_t kernels[3]; } boundary_surf_vx_kernels[] = {
+  // 1x kernels
+  { NULL, NULL, NULL }, // 0
+  { NULL, vlasov_lbo_boundary_surfvx_1x2v_ser_p1, vlasov_lbo_boundary_surfvx_1x2v_ser_p2 }, // 1
+  { NULL, vlasov_lbo_boundary_surfvx_1x3v_ser_p1, vlasov_lbo_boundary_surfvx_1x3v_ser_p2 }, // 2
+  // 2x kernels
+  { NULL, vlasov_lbo_boundary_surfvx_2x2v_ser_p1, vlasov_lbo_boundary_surfvx_2x2v_ser_p2 }, // 3
+  { NULL, vlasov_lbo_boundary_surfvx_2x3v_ser_p1, NULL }, // 4
+  // 3x kernels
+  { NULL, vlasov_lbo_boundary_surfvx_3x3v_ser_p1, NULL                   }, // 5
+};
+
+// Constant nu boundary surface kernel (zero-flux BCs) list: vy-direction
+GKYL_CU_D
+static struct { vlasov_lbo_boundary_surf_t kernels[3]; } boundary_surf_vy_kernels[] = {
+  // 1x kernels
+  { NULL, NULL, NULL }, // 0
+  { NULL, vlasov_lbo_boundary_surfvy_1x2v_ser_p1, vlasov_lbo_boundary_surfvy_1x2v_ser_p2 }, // 1
+  { NULL, vlasov_lbo_boundary_surfvy_1x3v_ser_p1, vlasov_lbo_boundary_surfvy_1x3v_ser_p2 }, // 2
+  // 2x kernels
+  { NULL, vlasov_lbo_boundary_surfvy_2x2v_ser_p1, vlasov_lbo_boundary_surfvy_2x2v_ser_p2 }, // 3
+  { NULL, vlasov_lbo_boundary_surfvy_2x3v_ser_p1, NULL }, // 4
+  // 3x kernels
+  { NULL, vlasov_lbo_boundary_surfvy_3x3v_ser_p1, NULL                   }, // 5
+};
+
+// Constant nu boundary surface kernel (zero-flux BCs) list: vz-direction
+GKYL_CU_D
+static struct { vlasov_lbo_boundary_surf_t kernels[3]; } boundary_surf_vz_kernels[] = {
+  // 1x kernels
+  { NULL, NULL, NULL }, // 0
+  { NULL, NULL, NULL }, // 1
+  { NULL, vlasov_lbo_boundary_surfvz_1x3v_ser_p1, vlasov_lbo_boundary_surfvz_1x3v_ser_p2 }, // 2
+  // 2x kernels
+  { NULL, NULL, NULL }, // 3
+  { NULL, vlasov_lbo_boundary_surfvz_2x3v_ser_p1, NULL }, // 4
+  // 3x kernels
+  { NULL, vlasov_lbo_boundary_surfvz_3x3v_ser_p1, NULL }, // 5
+};
+
+// "Choose Kernel" based on cdim, vdim and polyorder
+#define CK(lst, cdim, vd, poly_order) lst[cv_index[cdim].vdim[vd]].kernels[poly_order]
+
+struct dg_vlasov_lbo {
+  struct gkyl_dg_eqn eqn; // Base object
+  int cdim; // Config-space dimensions
+  int pdim; // Phase-space dimensions
+  vlasov_lbo_surf_t surf[3]; // Surface terms for acceleration
+  vlasov_lbo_boundary_surf_t boundary_surf[3]; // Surface terms for acceleration
+  double nuSum;
+  const double* nuUSum;
+  const double* nuVtSqSum;
+};
+
+GKYL_CU_D
+static void
+surf(const struct gkyl_dg_eqn *eqn, 
+  int dir,
+  const double*  xcL, const double*  xcC, const double*  xcR, 
+  const double*  dxL, const double* dxC, const double* dxR,
+  const int*  idxL, const int*  idxC, const int*  idxR,
+  const double* qInL, const double*  qInC, const double*  qInR, double* GKYL_RESTRICT qRhsOut);
+  struct dg_vlasov_lbo *vlasov_lbo = container_of(eqn, struct dg_vlasov_lbo, eqn);
+{
+  struct dg_vlasov_lbo *vlasov_lbo = container_of(eqn, struct dg_vlasov_lbo, eqn);
+
+  vlasov_lbo->surf[dir](xcC, dxC, vlasov_lbo->nuSum, vlasov_lbo->nuUSum, vlasov_lbo->nuVtSqSum, qInL, qInC, qInR, qRhsOut);
+}
+
+GKYL_CU_D
+static void
+boundary_surf(const struct gkyl_dg_eqn *eqn,
+  int dir,
+  const double*  xcEdge, const double*  xcSkin,
+  const double*  dxEdge, const double* dxSkin,
+  const int* idxEdge, const int* idxSkin, const int edge,
+  const double* qInEdge, const double* qInSkin, double* GKYL_RESTRICT qRhsOut)
+{
+  struct dg_vlasov_lbo *vlasov_lbo = container_of(eqn, struct dg_vlasov_lbo, eqn);
+
+  vlasov_lbo->boundary_surf[dir](xcSkin, dxSkin, vlasov_lbo->nuSum, vlasov_lbo->nuUSum, vlasov_lbo->nuVtSqSum, edge, qInSkin, qInEdge, qRhsOut);
+}
+
