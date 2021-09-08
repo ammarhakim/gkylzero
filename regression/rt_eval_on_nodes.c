f3489e0e8f6cbfd92ca9f92051b1a0213e5b2e41
ammarhakim <a.hakim777@gmail.com>
2021-09-07 22:10:06 -0400

    Added new regression test to check eval_on_nodes updater. Does not really do anything as the updater itself is not completed yet.

---
+++ regression/rt_eval_on_nodes.c
@@ -0,0 +1,71 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <gkyl_util.h>
+#include <gkyl_eval_on_nodes.h>
+#include <gkyl_range.h>
+#include <gkyl_rect_grid.h>
+#include <gkyl_array_rio.h>
+#include <thpool.h>
+
+void evalFunc(double t, const double *xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  double x = xn[0], y = xn[1], z = xn[2];
+  fout[0] = x*x + sin(3*y)*cos(z) + z*z;
+}
+
+void
+test_eval(enum gkyl_basis_type type)
+{
+  int poly_order = 2;
+  double lower[] = {-2.0, -2.0, -2.0}, upper[] = {2.0, 2.0, 2.0};
+  int cells[] = {8, 8, 8};
+  struct gkyl_rect_grid grid;
+  gkyl_rect_grid_init(&grid, 3, lower, upper, cells);
+
+  // basis functions
+  struct gkyl_basis basis;
+  if (type == GKYL_BASIS_MODAL_SERENDIPITY)
+    gkyl_cart_modal_serendip(&basis, 3, poly_order);
+  else
+    gkyl_cart_modal_tensor(&basis, 3, poly_order);
+
+  // projection updater for dist-function
+  gkyl_eval_on_nodes *evalDistf = gkyl_eval_on_nodes_new(&grid, &basis,1, evalFunc, 0);
+
+  // create array range: no ghost-cells 
+  struct gkyl_range arr_range;
+  gkyl_range_init_from_shape(&arr_range, 3, cells);
+
+  // create distribution function
+  struct gkyl_array *distf = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, arr_range.volume);
+
+  // run updater
+  gkyl_eval_on_nodes_advance(evalDistf, 0.0, &arr_range, distf);
+
+  // construct file name and write data out
+  const char *fmt[] = {
+    [GKYL_BASIS_MODAL_SERENDIPITY] = "%s-ser.gkyl",
+    [GKYL_BASIS_MODAL_TENSOR] = "%s-ten.gkyl",
+  };
+  int sz = snprintf(0, 0, fmt[type], "rt_eval_on_nodes");
+  char fileNm[sz+1]; // ensures no buffer overflow  
+  snprintf(fileNm, sizeof fileNm, fmt[type], "rt_eval_on_nodes");
+  
+  gkyl_grid_sub_array_write(&grid, &arr_range, distf, fileNm);
+  
+  gkyl_eval_on_nodes_release(evalDistf);
+  gkyl_array_release(distf);
+  
+}
+
+int
+main(int argc, char **argv)
+{
+  test_eval(GKYL_BASIS_MODAL_SERENDIPITY);
+  test_eval(GKYL_BASIS_MODAL_TENSOR);
+
+  return 0;
+}
