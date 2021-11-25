5bd91fc56af2e632db088d0db12078b040e9c74f
ammarhakim <a.hakim777@gmail.com>
2021-11-25 12:13:40 -0500

    Adding white-spaces so one can actually read the code: there is no need to stuff as much code in a single line as possible!

--- zero/mom_bcorr.c
+++ zero/mom_bcorr.c
@@ -30,23 +30,33 @@ gkyl_mom_bcorr_advance(gkyl_mom_bcorr *bcorr,
 
   gkyl_array_clear_range(out, 0.0, conf_rng);
 
-  // the outer loop is over configuration space cells; for each
-  // config-space cell the inner loop walks over the edges of velocity space
+  // outer loop is over configuration space cells; for each
+  // config-space cell inner loop walks over the edges of velocity
+  // space
   gkyl_range_iter_init(&conf_iter, &conf_rng);
   while (gkyl_range_iter_next(&conf_iter)) {
     long midx = gkyl_range_idx(&conf_rng, conf_iter.idx);
-    for (int d=0; d<conf_rng.ndim; ++d) viter_idx[d] = conf_iter.idx[d];
+    
+    for (int d=0; d<conf_rng.ndim; ++d)
+      viter_idx[d] = conf_iter.idx[d];
+    
     for (int d=0; d<phase_rng.ndim - conf_rng.ndim; ++d) {
       rem_dir[conf_rng.ndim + d] = 1;
+      
       // loop over upper edge of velocity space
       viter_idx[conf_rng.ndim + d] = phase_rng.upper[conf_rng.ndim + d];
       gkyl_range_deflate(&vel_rng, &phase_rng, rem_dir, viter_idx);
       gkyl_range_iter_no_split_init(&vel_iter, &vel_rng);
+      
       while (gkyl_range_iter_next(&vel_iter)) {
         copy_idx_arrays(conf_rng.ndim, phase_rng.ndim, conf_iter.idx, vel_iter.idx, pidx);
-        // pidx used to store identifiers for which dimension and edge of velocity space are being used for the kernel
-	      pidx[phase_rng.ndim-1] = viter_idx[conf_rng.ndim + d];
-	      pidx[phase_rng.ndim] = d;
+        
+        // pidx used to store identifiers for which dimension and edge
+        // of velocity space are being used for the kernel: THIS NEEDS
+        // TO CHANGE. (AHH, 11/25/21)
+        pidx[phase_rng.ndim-1] = viter_idx[conf_rng.ndim + d];
+        pidx[phase_rng.ndim] = d;
+        
         gkyl_rect_grid_cell_center(&bcorr->grid, pidx, xc);
       
         long fidx = gkyl_range_idx(&vel_rng, vel_iter.idx);
@@ -54,14 +64,18 @@ gkyl_mom_bcorr_advance(gkyl_mom_bcorr *bcorr,
           gkyl_array_cfetch(fIn, fidx), gkyl_array_fetch(out, midx)
         );
       }
+      
       // loop over lower edge of velocity space
       viter_idx[conf_rng.ndim + d] = phase_rng.lower[conf_rng.ndim + d];
       gkyl_range_deflate(&vel_rng, &phase_rng, rem_dir, viter_idx);
       gkyl_range_iter_no_split_init(&vel_iter, &vel_rng);
+      
       while (gkyl_range_iter_next(&vel_iter)) {
         copy_idx_arrays(conf_rng.ndim, phase_rng.ndim, conf_iter.idx, vel_iter.idx, pidx);
-	      pidx[phase_rng.ndim-1] = viter_idx[conf_rng.ndim + d];
-	      pidx[phase_rng.ndim] = d;
+        
+        pidx[phase_rng.ndim-1] = viter_idx[conf_rng.ndim + d];
+        pidx[phase_rng.ndim] = d;
+        
         gkyl_rect_grid_cell_center(&bcorr->grid, pidx, xc);
       
         long fidx = gkyl_range_idx(&vel_rng, vel_iter.idx);
