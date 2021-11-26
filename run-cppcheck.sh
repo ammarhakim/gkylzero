03dcf5ed2c82269b100b79d354afa37665b02b4a
ammarhakim <a.hakim777@gmail.com>
2021-11-26 11:29:14 -0500

    Fixing issues found by cppcheck. Though it did not manage to find the bug the LBO initialization. The latter we have now figured out and is being fixed

--- run-cppcheck.sh
+++ run-cppcheck.sh
@@ -1,6 +1,7 @@
-# This assumes cppcheck is installed in the gkylsoft directory. Please
-# download latest from http://cppcheck.sourceforge.net and use cmake
-# to build it:
+# This assumes cppcheck is in the PATH
+#
+# On Linux use apt install to get cppchecl. If needed, download latest
+# from http://cppcheck.sourceforge.net and use cmake to build it:
 #
 # cmake -DCMAKE_INSTALL_PREFIX=$HOME/gkylsoft/cppcheck -DCMAKE_BUILD_TYPE=Release .
 # make -j4 install
@@ -8,4 +9,4 @@
 # Errors are written to cppcheck-err.txt. Please ensure all code is
 # "cppcheck clean"
 
-$HOME/gkylsoft/cppcheck/bin/cppcheck --enable=warning,performance,portability,information zero apps regression 2>cppcheck-err.txt
+cppcheck --enable=warning,performance,portability,information zero apps regression 2>cppcheck-err.txt
