5e0be4387046cb4e09f2a4451fae03ccd093a635
Kolter Bradshaw <kb8689@traverse.princeton.edu>
2022-01-13 15:52:55 -0500

    Fixed the iterator issues in line with what was done with the range binop. Fixed an error in how nc was being set. Still getting cudaMalloc issues.

--- zero/prim_lbo_calc_cu.cu
+++ zero/prim_lbo_calc_cu.cu
@@ -1,5 +1,5 @@
 /* -*- c++ -*- */
-
+#include <stdio.h>
 extern "C" {
 #include <gkyl_alloc.h>
 #include <gkyl_array_ops_priv.h>
@@ -9,33 +9,28 @@ extern "C" {
 #include <gkyl_util.h>
 }
 
-// start ID for use in various loops
-#define START_ID (threadIdx.x + blockIdx.x*blockDim.x)
-
 static void
-gkyl_get_array_range_kernel_launch_dims(dim3* dimGrid, dim3* dimBlock, gkyl_range range, int ncomp)
+gkyl_get_array_range_kernel_launch_dims(dim3* dimGrid, dim3* dimBlock, gkyl_range range)
 {
   int volume = range.volume;
   int ndim = range.ndim;
   // ac1 = size of last dimension of range (fastest moving dimension)
   int ac1 = range.iac[ndim-1] > 0 ? range.iac[ndim-1] : 1;
-  dimBlock->x = min(ncomp*ac1, GKYL_DEFAULT_NUM_THREADS);
-  dimGrid->x = gkyl_int_div_up(ncomp*ac1, dimBlock->x);
+  dimBlock->x = min(ac1, GKYL_DEFAULT_NUM_THREADS);
+  dimGrid->x = gkyl_int_div_up(ac1, dimBlock->x);
 
-  dimBlock->y = gkyl_int_div_up(GKYL_DEFAULT_NUM_THREADS, ncomp*ac1);
+  dimBlock->y = gkyl_int_div_up(GKYL_DEFAULT_NUM_THREADS, ac1);
   dimGrid->y = gkyl_int_div_up(volume, ac1*dimBlock->y);
 }
 
 __global__ void
-gkyl_prim_lbo_calc_advance_cu_ker(gkyl_prim_lbo_calc* calc, struct gkyl_nmat *As, struct gkyl_nmat *xs, struct gkyl_basis cbasis, struct gkyl_range conf_rng, const struct gkyl_array* m0, const struct gkyl_array* m1, const struct gkyl_array* m2, const struct gkyl_array* cM, const struct gkyl_array* cE, struct gkyl_array* uout, struct gkyl_array* vtSqout)
+gkyl_prim_lbo_calc_set_cu_ker(gkyl_prim_lbo_calc* calc, struct gkyl_nmat *As, struct gkyl_nmat *xs, struct gkyl_basis cbasis, struct gkyl_range conf_rng, const struct gkyl_array* m0, const struct gkyl_array* m1, const struct gkyl_array* m2, const struct gkyl_array* cM, const struct gkyl_array* cE)
 {
   int ndim = cbasis.ndim;
-  long n = NCOM(uout);
   int idx[GKYL_MAX_DIM];
   // ac1 = size of last dimension of range (fastest moving dimension)
   long ac1 = conf_rng.iac[ndim-1] > 0 ? conf_rng.iac[ndim-1] : 1;
 
-  long count = 0;
   // 2D thread grid
   // linc1 = c + n*idx1 (contiguous data, including component index c, with idx1 = 0,.., ac1-1)
   long linc1 = threadIdx.x + blockIdx.x*blockDim.x;
@@ -49,23 +44,23 @@ gkyl_prim_lbo_calc_advance_cu_ker(gkyl_prim_lbo_calc* calc, struct gkyl_nmat *As
     // we want to find the start linear index of each contiguous data block, 
     // which corresponds to idx1 = 0. 
     // so linear index of start of contiguous block is ac1*linc2.
-    gkyl_sub_range_inv_idx(&conf_rng, ac1*linc2, idx);
-    long start = gkyl_range_idx(&conf_rng, idx);
+    if (linc1 < ac1) {
+      gkyl_sub_range_inv_idx(&conf_rng, linc1 + ac1*linc2, idx);
+      long start = gkyl_range_idx(&conf_rng, idx);
     
-    struct gkyl_mat lhs = gkyl_nmat_get(As, count);
-    struct gkyl_mat rhs = gkyl_nmat_get(xs, count);
-    const double *m0_d = (const double*) gkyl_array_cfetch(m0, start);
-    const double *m1_d = (const double*) gkyl_array_cfetch(m1, start);
-    const double *m2_d = (const double*) gkyl_array_cfetch(m2, start);
-    const double *cM_d = (const double*) gkyl_array_cfetch(cM, start);
-    const double *cE_d = (const double*) gkyl_array_cfetch(cE, start);
+      struct gkyl_mat lhs = gkyl_nmat_get(As, linc1 + ac1*linc2);
+      struct gkyl_mat rhs = gkyl_nmat_get(xs, linc1 + ac1*linc2);
+      const double *m0_d = (const double*) gkyl_array_cfetch(m0, start);
+      const double *m1_d = (const double*) gkyl_array_cfetch(m1, start);
+      const double *m2_d = (const double*) gkyl_array_cfetch(m2, start);
+      const double *cM_d = (const double*) gkyl_array_cfetch(cM, start);
+      const double *cE_d = (const double*) gkyl_array_cfetch(cE, start);
+      printf("indices = %ld, %ld\n", linc1 + ac1*linc2, start);
 
-    gkyl_mat_clear(&lhs, 0.0); gkyl_mat_clear(&rhs, 0.0);
-    // do operation on contiguous data block
-    if (linc1 < n*ac1)
+      gkyl_mat_clear(&lhs, 0.0); gkyl_mat_clear(&rhs, 0.0);
+
       calc->prim->self_prim(calc->prim, &lhs, &rhs, m0_d, m1_d, m2_d, cM_d, cE_d);
-
-    count += 1;
+    }
   }
 }
 
@@ -74,13 +69,11 @@ gkyl_prim_lbo_copy_sol_cu_ker(struct gkyl_nmat *xs, struct gkyl_basis cbasis, st
   int nc, int vdim, struct gkyl_array* uout, struct gkyl_array* vtSqout)
 {
   int ndim = cbasis.ndim;
-  long n = NCOM(uout);
   int idx[GKYL_MAX_DIM];
 
   // ac1 = size of last dimension of range (fastest moving dimension)
   long ac1 = conf_rng.iac[ndim-1] > 0 ? conf_rng.iac[ndim-1] : 1;
-
-  long count = 0;
+  
   // 2D thread grid
   // linc1 = c + n*idx1 (contiguous data, including component index c, with idx1 = 0,.., ac1-1)
   long linc1 = threadIdx.x + blockIdx.x*blockDim.x;
@@ -94,18 +87,17 @@ gkyl_prim_lbo_copy_sol_cu_ker(struct gkyl_nmat *xs, struct gkyl_basis cbasis, st
     // we want to find the start linear index of each contiguous data block, 
     // which corresponds to idx1 = 0. 
     // so linear index of start of contiguous block is ac1*linc2.
-    gkyl_sub_range_inv_idx(&conf_rng, ac1*linc2, idx);
-    long start = gkyl_range_idx(&conf_rng, idx);
+    if (linc1 < ac1) {
+      gkyl_sub_range_inv_idx(&conf_rng, linc1 + ac1*linc2, idx);
+      long start = gkyl_range_idx(&conf_rng, idx);
 
-    struct gkyl_mat out_d = gkyl_nmat_get(xs, count);
-    double *u_d = (double*) gkyl_array_fetch(uout, start);
-    double *vtSq_d = (double*) gkyl_array_fetch(vtSqout, start);
-    // do operation on contiguous data block
-    if (linc1 < n*ac1)
+      struct gkyl_mat out_d = gkyl_nmat_get(xs, linc1 + ac1*linc2);
+      double *u_d = (double*) gkyl_array_fetch(uout, start);
+      double *vtSq_d = (double*) gkyl_array_fetch(vtSqout, start);
+    
       prim_lbo_copy_sol(&out_d, nc, vdim, u_d, vtSq_d);
-
-    count += 1;
-  }  
+    }
+  }
 }
 
 void
@@ -115,16 +107,22 @@ gkyl_prim_lbo_calc_advance_cu(gkyl_prim_lbo_calc* calc, const struct gkyl_basis 
   struct gkyl_array* uout, struct gkyl_array* vtSqout)
 {
   dim3 dimGrid, dimBlock;
-  gkyl_get_array_range_kernel_launch_dims(&dimGrid, &dimBlock, conf_rng, uout->ncomp);
-  int nc = conf_rng.ndim;
-  int vdim = phase_rng.ndim - nc;
+  gkyl_get_array_range_kernel_launch_dims(&dimGrid, &dimBlock, conf_rng);
+  int nc = cbasis.num_basis;
+  int cdim = conf_rng.ndim;
+  int vdim = phase_rng.ndim - cdim;
   int N = nc*(vdim + 1);      
   struct gkyl_nmat *A_d = gkyl_nmat_cu_dev_new(conf_rng.volume, N, N);
   struct gkyl_nmat *x_d = gkyl_nmat_cu_dev_new(conf_rng.volume, N, 1);
 
-  gkyl_prim_lbo_calc_advance_cu_ker<<<dimGrid, dimBlock>>>(calc, A_d->on_dev, x_d->on_dev, cbasis, conf_rng, m0->on_dev, m1->on_dev, m2->on_dev, cM->on_dev, cE->on_dev, uout->on_dev, vtSqout->on_dev);
-
+  gkyl_prim_lbo_calc_set_cu_ker<<<dimGrid, dimBlock>>>(calc, A_d->on_dev, x_d->on_dev, cbasis, conf_rng, m0->on_dev, m1->on_dev, m2->on_dev, cM->on_dev, cE->on_dev);
+  printf("params = [%d, %d, %d]\n", nc, vdim, N);
+  printf("Completed set!\n");
+  fflush(stdout);
   bool status = gkyl_nmat_linsolve_lu(A_d, x_d);
+  printf("Completed linsolve!\n");
+  fflush(stdout);
+
   gkyl_prim_lbo_copy_sol_cu_ker<<<dimGrid, dimBlock>>>(x_d->on_dev, cbasis, conf_rng, nc, vdim, uout->on_dev, vtSqout->on_dev);
 
   gkyl_nmat_release(A_d);
