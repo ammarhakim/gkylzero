fae5e4ba37cca7af4bccfddcf21d1d71124b6990
kolterb <kolterb@vt.edu>
2022-02-03 09:52:07 -0500

    Fixed issue where the drift primitive moment was losing any non-x components during the solution copy.

--- kernels/lbo/prim_lbo_copy_sol.c
+++ kernels/lbo/prim_lbo_copy_sol.c
@@ -3,8 +3,10 @@
  
 GKYL_CU_DH void prim_lbo_copy_sol(const struct gkyl_mat *rhs, const int nc, const int vdim, double* GKYL_RESTRICT u, double* GKYL_RESTRICT vtSq)
 {
-  for(size_t i=0; i<nc; i++) { 
+  for(size_t i=0; i<vdim*nc; i++) { 
     u[i] = gkyl_mat_get(rhs, i, 0);
+  }
+  for(size_t i=0; i<nc; i++) { 
     vtSq[i] = gkyl_mat_get(rhs, i+nc*vdim, 0);
   }
 }
