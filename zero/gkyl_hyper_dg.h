016337f8098b87b6e4cc7d41d245ec564ec9e49e
ammarhakim <a.hakim777@gmail.com>
2022-02-27 09:21:36 -0500

    Remving the function gkyl_hyper_dg_advance_no_iter as this is never used. This commit message can be grep-ed if we ever want to restore it.

--- zero/gkyl_hyper_dg.h
+++ zero/gkyl_hyper_dg.h
@@ -57,12 +57,7 @@ gkyl_hyper_dg* gkyl_hyper_dg_cu_dev_new(const struct gkyl_rect_grid *grid_cu,
 void gkyl_hyper_dg_advance(gkyl_hyper_dg *hdg, struct gkyl_range update_rng,
   const struct gkyl_array *fIn, struct gkyl_array *cflrate, struct gkyl_array *rhs);
 
-/* 
- * Same as gkyl_hyper_dg_advance, but doesn't use iterators 
- */
-void gkyl_hyper_dg_advance_no_iter(gkyl_hyper_dg *hdg, struct gkyl_range update_rng,
-  const struct gkyl_array *fIn, struct gkyl_array *cflrate, struct gkyl_array *rhs);
-
+// CUDA call
 void gkyl_hyper_dg_advance_cu(gkyl_hyper_dg* hdg, const struct gkyl_range update_range,
   const struct gkyl_array* GKYL_RESTRICT fIn, struct gkyl_array* GKYL_RESTRICT cflrate,
   struct gkyl_array* GKYL_RESTRICT rhs);
