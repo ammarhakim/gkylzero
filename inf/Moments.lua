aee89e46576383d5a7e6ed7bf2fd660fe706ed3f
ammarhakim <a.hakim777@gmail.com>
2022-03-21 10:31:58 -0400

    Completed simple Moment wrapper around G0-moment app.

--- inf/Moments.lua
+++ inf/Moments.lua
@@ -270,6 +270,15 @@ struct gkyl_moment_stat {
 // Object representing moments app
 typedef struct gkyl_moment_app gkyl_moment_app;
 
+// Container to store pointer to app and other data
+struct gkyl_moment_app_cont {
+  double t0, tend; // start and end times
+  int nframe; // number of frames to write
+  int nspecies; // number of species
+
+  gkyl_moment_app *app; // pointer to app
+};
+
 /**
  * Construct a new moments app.
  *
@@ -417,8 +426,308 @@ local wv_euler_mt = {
    end,
    __gc = function(self)
       C.gkyl_wv_eqn_release(self.eqn)
-   end
+   end,
+   __index = {
+      bcWall = C.GKYL_SPECIES_WALL,
+      bcCopy = C.GKYL_SPECIES_COPY
+   }
 }
 _M.Euler = ffi.metatype(wv_euler_type, wv_euler_mt)
+
+-- Wraps user given init function in a function that can be passed to
+-- the C callback APIs
+local function gkyl_eval_moment(func)
+   return function(t, xn, fout, ctx)
+      local xnl = ffi.new("double[10]")
+      for i=1, 3 do xnl[i] = xn[i-1] end -- might not be safe?
+      local ret = { func(t, xnl) } -- package return into table
+      for i=1,#ret do
+	 fout[i-1] = ret[i]
+      end
+   end
+end
+
+-- Species object: table structure is as follows:
+--
+-- local elc = {
+--    charge = -1.0,
+--    mass = 1.0,
+--    equation = Moments.Euler { gasGamma = 1.4 },
+--    init = function(t, xn)
+--       -- return initial conditions
+--    end
+
+-- }
+
+local limiter_tags = {
+   ["no-limiter"] = C.GKYL_NO_LIMITER,
+   ["min-mod"] = C.GKYL_MIN_MOD,
+   ["superbee"] = C.GKYL_SUPERBEE,
+   ["van-leer"] = C.GKYL_VAN_LEER,
+   ["monotonized-centered"] = C.GKYL_MONOTONIZED_CENTERED,
+   ["beam-warming"] = C.GKYL_BEAM_WARMING,
+   ["zero"] = C.GKYL_ZERO,
+}
+
+local species_type = ffi.typeof("struct gkyl_moment_species")
+local species_mt = {
+   __new = function(self, tbl)
+      local s = ffi.new(self)
+      s.charge = tbl.charge
+      s.mass = tbl.mass
+
+      s.limiter = limiter_tags["monotonized-centered"]
+      if tbl.limiter then
+	 s.limiter = limiter_tags[tbl.limiter]
+      end
+
+      s.equation = tbl.equation.eqn
+
+      -- initial conditions
+      s.ctx = nil -- no need for a context
+      s.init = gkyl_eval_moment(tbl.init)
+
+      -- boundary conditions
+      if tbl.bcx then
+	 s.bcx[0], s.bcx[1] = tbl.bcx[1], tbl.bcx[2]
+      end
+      if tbl.bcy then
+	 s.bcy[0], s.bcy[1] = tbl.bcy[1], tbl.bcy[2]
+      end
+      if tbl.bcz then
+	 s.bcz[0], s.bcz[1] = tbl.bcz[1], tbl.bcz[2]
+      end
+
+      return s
+   end,
+   __index = {
+      -- we need this here also to be consistent with G2 App
+      bcWall = C.GKYL_SPECIES_WALL,
+      bcCopy = C.GKYL_SPECIES_COPY      
+   }
+}
+_M.Species = ffi.metatype(species_type, species_mt)
+
+-- Wraps user given field initialization function in a function that
+-- can be passed to the C callback APIs
+local function gkyl_eval_field(func)
+   return function(t, xn, fout, ctx)
+      local xnl = ffi.new("double[10]")
+      for i=1, 6 do xnl[i] = xn[i-1] end -- might not be safe?
+
+      local ex,ey,ez,bx,by,bz = func(t, xnl)
+
+      fout[0] = ex; fout[1] = ey; fout[2] = ez
+      fout[3] = bx; fout[4] = by; fout[5] = bz
+      fout[6] = 0.0; fout[7] = 0.0
+   end
+end
+
+-- Field object: table structure is as follows:
+--
+-- local field = {
+--    epsilon0 = 1.0,  mu0 = 1.0,
+--    elcErrorSpeedFactor = 0.0, mgnErrorSpeedFactor = 0.0,
+--    init = function(t, xn)
+--       -- return EM field: 6 components Ex,Ey,Ez,Bx,By,Bz
+--    end
+--    evolve = true,
+-- }
+
+local field_type = ffi.typeof("struct gkyl_moment_field")
+local field_mt = {
+   __new = function(self, tbl)
+      local f = ffi.new(self)
+      f.epsilon0 = tbl.epsilon0
+      f.mu0 = tbl.mu0
+
+      f.elc_error_speed_fact = 0.0
+      if (tbl.elcErrorSpeedFactor) then
+	 f.elc_error_speed_fact = tbl.elcErrorSpeedFactor
+      end
+
+      f.mag_error_speed_fact = 1.0
+      if (tbl.mgnErrorSpeedFactor) then
+	 f.mag_error_speed_fact = tbl.mgnErrorSpeedFactor
+      end
+
+      f.ctx = nil -- no need for context
+      f.init = gkyl_eval_field(tbl.init)
+
+      -- boundary conditions
+      if tbl.bcx then
+	 f.bcx[0], f.bcx[1] = tbl.bcx[1], tbl.bcx[2]
+      end
+      if tbl.bcy then
+	 f.bcy[0], f.bcy[1] = tbl.bcy[1], tbl.bcy[2]
+      end
+      if tbl.bcz then
+	 f.bcz[0], f.bcz[1] = tbl.bcz[1], tbl.bcz[2]
+      end
+
+      return f
+   end,
+   __index = {
+      bcOpen = C.GKYL_FIELD_COPY,
+      bcCopy = C.GKYL_FIELD_COPY,
+      bcReflect = C.GKYL_FIELD_PEC_WALL,
+   }
+}
+_M.Field = ffi.metatype(field_type, field_mt)
+
+-- App
+local app_type = ffi.typeof("struct gkyl_moment_app_cont")
+local app_mt = {
+   __new = function(self, tbl)
+      local vm = ffi.new("struct gkyl_moment")
+
+      local species = { }
+      local field = nil
+
+      -- first determine all species in system
+      for k,v in pairs(tbl) do
+	 if ffi.istype(species_type, v) then
+	    v.name = k -- assign species name here
+	    table.insert(species, v)
+	 end
+	 if ffi.istype(field_type, v) then
+	    field = v -- only one field can be present
+	 end
+      end
+      local num_species = #species
+
+      local name = "vlasov"
+      if GKYL_OUT_PREFIX then
+	 -- if G0 is being run from gkyl then GKYL_OUT_PREFIX is
+	 -- defined
+	 name = GKYL_OUT_PREFIX
+      else
+	 local s, e = string.find(arg[0], ".lua")
+	 name = string.sub(arg[0], 1, s-1)
+      end
+	 
+      -- set values in input struct
+      vm.name = name
+      vm.ndim = #tbl.cells
+
+      -- set configuration space grid data
+      for d=1, vm.ndim do
+	 vm.lower[d-1] = tbl.lower[d]
+	 vm.upper[d-1] = tbl.upper[d]
+	 vm.cells[d-1] = tbl.cells[d]
+      end
+
+      vm.cfl_frac = 1.0
+      if tbl.cflFrac then
+	 vm.cfl_frac = tbl.cflFrac
+      end
+
+      -- determine periodic BCs
+      vm.num_periodic_dir = 0
+      if tbl.periodicDirs then
+	 vm.num_periodic_dir = #tbl.periodicDirs
+	 for i=1, #tbl.periodicDirs do
+	    vm.periodic_dirs[i-1] = tbl.periodicDirs[i]-1 -- note indexing transforms
+	 end
+      end
+
+      -- set species
+      vm.num_species = #species
+      for i=1, #species do
+	 vm.species[i-1] = species[i]
+      end
+
+      -- set field
+      if field then
+	 vm.field = field
+      end      
+
+      -- create new Moments app object
+      local a = ffi.new(self)
+
+      -- we need to store some stuff in container struct
+      a.nspecies = num_species
+      if tbl.tStart then
+	 a.t0 = tbl.tStart
+      end
+      a.tend = tbl.tEnd
+      a.nframe = tbl.nFrame
+
+      -- initialize app from input struct
+      a.app = C.gkyl_moment_app_new(vm)
+      return a
+   end,
+   __gc = function(self)
+      C.gkyl_moment_app_release(self.app)
+   end,
+   __index = {
+      init = function(self)
+	 C.gkyl_moment_app_apply_ic(self.app, self.t0)
+      end,
+      writeField = function(self, tm, frame)
+	 C.gkyl_moment_app_write_field(self.app, tm, frame)
+      end,
+      writeSpecies = function(self, tm, frame)
+	 for i=1, self.nspecies do
+	    C.gkyl_moment_app_write_species(self.app, i-1, tm, frame)
+	 end
+      end,
+      write = function(self, tm, frame)
+	 C.gkyl_moment_app_write(self.app, tm, frame)
+      end,
+      writeStat = function(self)
+	 C.gkyl_moment_app_stat_write(self.app)
+      end,
+      update = function(self, dt)
+	 return C.gkyl_moment_update(self.app, dt)
+      end,
+      run = function(self)
+	 
+	 local frame_trig = _M.TimeTrigger(self.tend/self.nframe)
+
+	 -- function to write data to file
+	 local function writeData(tcurr)
+	    if frame_trig:checkAndBump(tcurr) then
+	       self:write(tcurr, frame_trig.curr-1)
+	    end
+	 end
+
+	 local count = 0
+	 local p1_trig = _M.TimeTrigger(self.tend/99)
+	 -- log messages
+	 local function writeLogMessage(tcurr, step, dt)
+	    if p1_trig:checkAndBump(tcurr) then
+	       if count % 10 == 0 then
+		  io.write(string.format(" Step %6d %.4e. Time-step  %.6e \n", step, tcurr, dt))
+	       end
+	       count = count+1
+	    end
+	 end
+
+	 io.write(string.format("Starting GkeyllZero simulation\n"))
+	 self:init()
+	 writeData(0.0)
+
+	 local tcurr, tend = 0.0, self.tend
+	 local dt = tend-tcurr
+	 local step = 1
+	 while tcurr < tend do
+	    local status = self:update(dt);
+	    tcurr = tcurr + status.dt_actual
+
+	    dt = status.dt_suggested
+	    writeLogMessage(tcurr, step, dt)
+	    writeData(tcurr)
+
+	    step = step + 1
+	 end
+	 io.write(string.format("Completed in %d steps. Final time-step %.6e\n", step-1, dt))
+
+	 self:writeStat()
+	 
+      end,
+   }
+}
+_M.App = ffi.metatype(app_type, app_mt)
 
 return _M
