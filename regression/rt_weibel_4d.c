28a230260ac3dc6cba17b82f3284fc982dd512c9
ammarhakim <a.hakim777@gmail.com>
2022-05-18 10:52:20 -0400

    Extended tend for Wiebel 4D. At this res it clearly shows a nice linear stage of growth. Need to run at higher res.

--- regression/rt_weibel_4d.c
+++ regression/rt_weibel_4d.c
@@ -171,7 +171,7 @@ main(int argc, char **argv)
   gkyl_vlasov_app *app = gkyl_vlasov_app_new(&vm);
 
   // start, end and initial time-step
-  double tcurr = 0.0, tend = 5.0;
+  double tcurr = 0.0, tend = 80.0;
   double dt = tend-tcurr;
   int nframe = 1;
   // create trigger for IO
@@ -180,12 +180,17 @@ main(int argc, char **argv)
   // initialize simulation
   gkyl_vlasov_app_apply_ic(app, tcurr);
   write_data(&io_trig, app, tcurr);
+  gkyl_vlasov_app_calc_integrated_mom(app, tcurr);
+  gkyl_vlasov_app_calc_field_energy(app, tcurr);  
 
   long step = 1, num_steps = app_args.num_steps;
   while ((tcurr < tend) && (step <= num_steps)) {
     printf("Taking time-step at t = %g ...", tcurr);
     struct gkyl_update_status status = gkyl_vlasov_update(app, dt);
     printf(" dt = %g\n", status.dt_actual);
+
+    gkyl_vlasov_app_calc_integrated_mom(app, tcurr);
+    gkyl_vlasov_app_calc_field_energy(app, tcurr);    
     
     if (!status.success) {
       printf("** Update method failed! Aborting simulation ....\n");
@@ -199,6 +204,8 @@ main(int argc, char **argv)
   }
 
   gkyl_vlasov_app_stat_write(app);
+  gkyl_vlasov_app_write_integrated_mom(app);
+  gkyl_vlasov_app_write_field_energy(app);  
 
   // fetch simulation statistics
   struct gkyl_vlasov_stat stat = gkyl_vlasov_app_stat(app);
