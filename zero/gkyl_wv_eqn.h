62c70d87ca3b44328d0ac27c7fb1ddc4e1597814
Junoravin <junoravin@gmail.com>
2022-06-01 13:28:09 -0400

    Adding back in comments since these comments are very instructive for what these function pointers point to

--- zero/gkyl_wv_eqn.h
+++ zero/gkyl_wv_eqn.h
@@ -8,25 +8,54 @@
 // Forward declare for use in function pointers
 struct gkyl_wv_eqn;
 
-// Function pointer for compute waves from RP solver
+/**
+ * Function pointer for computing waves and speeds from left/right conserved variables. The
+ * 'waves' array has size num_equations X num_waves in length. The 'm'
+ * wave (m = 0 ... num_waves-1) is stored starting at location
+ * waves[m*num_equations].
+ *
+ * @param eqn Equation object
+ * @param delta Jump across interface to split
+ * @param ql Conserved variables on left of interface
+ * @param qr Conserved variables on right of interface
+ * @param waves On output, waves 
+ * @param speeds On output wave speeds[num_wave]
+ * @return Maximum wave speed.
+ */
 typedef double (*wv_waves_t)(const struct gkyl_wv_eqn *eqn, 
   const double *delta, const double *ql, const double *qr, double *waves, double *speeds);
 
-// Function pointer for compute q-fluctuations from waves
+// Function pointer for computing q-fluctuations from waves
 typedef void (*wv_qfluct_t)(const struct gkyl_wv_eqn *eqn, 
   const double *ql, const double *qr, const double *waves, const double *speeds,
   double *amdq, double *apdq);
 
-// Function pointer to compute maximum speed given local state
+// Function pointer for computing maximum speed given local state
 typedef double (*wv_max_speed_t)(const struct gkyl_wv_eqn *eqn, const double *q);
 
-// Function pointer to rotate conserved variables to local
-// tangent-normal frame: tau1 X tau2 = norm
+/**
+ * Function pointer to rotate state (conserved/primitive) vector to local tangent-normal coordinate frame.
+ *
+ * @param eqn Equation object
+ * @param tau1 Tangent vector
+ * @param tau2 Tangent vector
+ * @param norm Normal vector such that norm = tau1 x tau2
+ * @param qglobal State vector in global coordinates
+ * @param qlocal State vector in local coordinates
+ */
 typedef void (*wv_rotate_to_local)(const double *tau1, const double *tau2, const double *norm,
   const double *qglobal, double *qlocal);
 
-// Function pointer to rotate conserved variables to local
-// tangent-normal frame: tau1 X tau2 = norm
+/**
+ * Function pointer to rotate state (conserved/primitive) vector to local global coordinate frame.
+ *
+ * @param eqn Equation object
+ * @param tau1 Tangent vector
+ * @param tau2 Tangent vector
+ * @param norm Normal vector such that norm = tau1 x tau2
+ * @param qglobal State vector in global coordinates
+ * @param qlocal State vector in local coordinates
+ */
 typedef void (*wv_rotate_to_global)(const double *tau1, const double *tau2, const double *norm,
   const double *qlocal, double *qglobal);
 
