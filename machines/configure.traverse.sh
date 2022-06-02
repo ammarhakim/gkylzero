bc99e5a5b0159490b78032a2cd44f6bc7a210545
ammarhakim <a.hakim777@gmail.com>
2022-06-02 10:53:54 -0700

    Adding mahcine file for Traverse. Not checked yet. Will do so after ci

---
+++ machines/configure.traverse.sh
@@ -0,0 +1,1 @@
+./configure CC=nvcc ARCH_FLAGS="-mcpu=native" --superlu-lib=${HOME}/gkylsoft/superlu/lib64/libsuperlu.a
---
+++ machines/mkdeps.traverse.sh
@@ -0,0 +1,1 @@
+./install-deps/mkdeps.sh --build-openblas=yes --build-superlu=yes
