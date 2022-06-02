83335d56847021e3e7a18945434476ac14055d46
Noah Mandell <nrmandell@gmail.com>
2022-06-02 16:45:04 -0400

    force superlu to install as "lib" (not "lib64"). hopefully this ends this nightmare...

--- machines/configure.stellar.sh
+++ machines/configure.stellar.sh
@@ -1,1 +1,1 @@
-./configure CC=nvcc ARCH_FLAGS="-march=native" CUDA_ARCH=80 --superlu-lib=${HOME}/gkylsoft/superlu/lib64/libsuperlu.a
+./configure CC=nvcc ARCH_FLAGS="-march=native" CUDA_ARCH=80 
--- machines/configure.traverse.sh
+++ machines/configure.traverse.sh
@@ -1,1 +1,1 @@
-./configure CC=nvcc  ARCH_FLAGS="-mcpu=native" --superlu-lib=${HOME}/gkylsoft/superlu/lib64/libsuperlu.a
+./configure CC=nvcc  ARCH_FLAGS="-mcpu=native"
