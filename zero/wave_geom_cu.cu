67d9f531e0b3363242a2d0642c82d16fd0167dd4
liangwang0734 <liang.wang.phys@gmail.com>
2022-06-03 16:57:59 -0400

    wave_geom_cu: allow a separate cuda copy of the cell geom array

--- zero/wave_geom_cu.cu
+++ zero/wave_geom_cu.cu
@@ -5,6 +5,13 @@ extern "C" {
 #include <gkyl_wave_geom_priv.h>
 }
 
+__global__ static void
+wave_geom_set_cu_dev_ptrs(struct gkyl_wave_geom *wg_dev,
+                          struct gkyl_array *geom_dev)
+{
+  wg_dev->geom = geom_dev;
+}
+
 // CPU interface to create and track a GPU object
 struct gkyl_wave_geom*
 gkyl_wave_geom_cu_dev_new(const struct gkyl_rect_grid *grid,
@@ -19,19 +26,16 @@ gkyl_wave_geom_cu_dev_new(const struct gkyl_rect_grid *grid,
   // STEP: SET HOST OR COMMON HOST/DEVICE DATA IN HOST OBJECT
   wg->range = *range;
 
-  // STEP: CREATE NECESSARY DEVICE DATA AND TRACK THEM IN HOST OBJECT
-  struct gkyl_array *geom = gkyl_array_new(
+  wg->geom = gkyl_array_new(
       GKYL_USER, sizeof(struct gkyl_wave_cell_geom), range->volume);
   double xc[GKYL_MAX_CDIM];
   struct gkyl_range_iter iter;
   gkyl_range_iter_init(&iter, range);
   while (gkyl_range_iter_next(&iter)) {
     gkyl_rect_grid_cell_center(grid, iter.idx, xc);
-
     struct gkyl_wave_cell_geom *geo =
       (struct gkyl_wave_cell_geom*) gkyl_array_fetch(
-          geom, gkyl_range_idx(range, iter.idx));
-
+          wg->geom, gkyl_range_idx(range, iter.idx));
     switch (grid->ndim) {
       case 1:
         calc_geom_1d(grid->dx, xc, mapc2p ? mapc2p : nomapc2p, ctx, geo);
@@ -44,22 +48,22 @@ gkyl_wave_geom_cu_dev_new(const struct gkyl_rect_grid *grid,
         break;
     };
   }
-  wg->geom = gkyl_array_cu_dev_new(
-      GKYL_USER, sizeof(struct gkyl_wave_cell_geom), range->volume);
-  gkyl_array_copy(wg->geom, geom);
-  gkyl_array_release(geom);
 
   wg->ref_count = gkyl_ref_count_init(wave_geom_free);
 
   // STEP: COPY HOST OBJECT TO DEVICE OBJECT
-  struct gkyl_wave_geom *wg_cu =
+  struct gkyl_wave_geom *wg_dev =
     (struct gkyl_wave_geom*) gkyl_cu_malloc(sizeof(struct gkyl_wave_geom));
-  gkyl_cu_memcpy(wg_cu, wg, sizeof(struct gkyl_wave_geom), GKYL_CU_MEMCPY_H2D);
+  gkyl_cu_memcpy(wg_dev, wg, sizeof(struct gkyl_wave_geom), GKYL_CU_MEMCPY_H2D);
 
-  // STEP: KEEP POINTERS TO EXTERNAL DEVICE FUNCTIONS IN DEVICE OBJECT
+  // STEP: SET OTHER DEVICE DATA IN DEVICE OBJECT
+  struct gkyl_array *geom_dev = gkyl_array_cu_dev_new(
+      GKYL_USER, sizeof(struct gkyl_wave_cell_geom), range->volume);
+  gkyl_array_copy(geom_dev, wg->geom);
+  wave_geom_set_cu_dev_ptrs<<<1, 1>>>(wg_dev, geom_dev->on_dev);
 
   // STEP: KEEP POINTER TO THE DEVICE OBJECT
-  wg->on_dev = wg_cu;
+  wg->on_dev = wg_dev;
 
   return wg;
 }
