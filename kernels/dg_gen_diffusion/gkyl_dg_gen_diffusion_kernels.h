759f680ae668a302cc98f633b1ad7068353ae58b
Petr Cagas <pcagas@vt.edu>
2022-07-06 12:50:46 -0400

    Adding p=2 surfave terms and p=1,2 volume terms

--- kernels/dg_gen_diffusion/gkyl_dg_gen_diffusion_kernels.h
+++ kernels/dg_gen_diffusion/gkyl_dg_gen_diffusion_kernels.h
@@ -4,7 +4,12 @@
 
 EXTERN_C_BEG
 
+GKYL_CU_DH double dg_gen_diffusion_vol_2x_ser_p1(const double* w, const double* dx, const double* D, const double* q, double* GKYL_RESTRICT out);
+GKYL_CU_DH double dg_gen_diffusion_vol_2x_ser_p2(const double* w, const double* dx, const double* D, const double* q, double* GKYL_RESTRICT out);
+
 GKYL_CU_DH void dg_gen_diffusion_surfx_2x_ser_p1(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+GKYL_CU_DH void dg_gen_diffusion_surfx_2x_ser_p2(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
 GKYL_CU_DH void dg_gen_diffusion_surfy_2x_ser_p1(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+GKYL_CU_DH void dg_gen_diffusion_surfy_2x_ser_p2(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
 
 EXTERN_C_END
