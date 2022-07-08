7539a8f0d9e992ac06faa3f4774e56580141a178
Petr Cagas <pcagas@vt.edu>
2022-07-08 11:12:02 -0400

    Splitting the general diffusion surface kernels; in 2D surfx is splitted into surfxx and surfxy.

--- kernels/dg_gen_diffusion/gkyl_dg_gen_diffusion_kernels.h
+++ kernels/dg_gen_diffusion/gkyl_dg_gen_diffusion_kernels.h
@@ -7,10 +7,19 @@ EXTERN_C_BEG
 GKYL_CU_DH double dg_gen_diffusion_vol_2x_ser_p1(const double* w, const double* dx, const double* D, const double* q, double* GKYL_RESTRICT out);
 GKYL_CU_DH double dg_gen_diffusion_vol_2x_ser_p2(const double* w, const double* dx, const double* D, const double* q, double* GKYL_RESTRICT out);
 
-GKYL_CU_DH void dg_gen_diffusion_surfx_2x_ser_p1(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
-GKYL_CU_DH void dg_gen_diffusion_surfy_2x_ser_p1(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
-GKYL_CU_DH void dg_gen_diffusion_surfx_2x_ser_p2(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
-GKYL_CU_DH void dg_gen_diffusion_surfy_2x_ser_p2(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+//GKYL_CU_DH void dg_gen_diffusion_surfx_2x_ser_p1(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+//GKYL_CU_DH void dg_gen_diffusion_surfy_2x_ser_p1(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+//GKYL_CU_DH void dg_gen_diffusion_surfx_2x_ser_p2(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+//GKYL_CU_DH void dg_gen_diffusion_surfy_2x_ser_p2(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+
+GKYL_CU_DH void dg_gen_diffusion_surfxx_2x_ser_p1(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+GKYL_CU_DH void dg_gen_diffusion_surfxx_2x_ser_p2(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+GKYL_CU_DH void dg_gen_diffusion_surfxy_2x_ser_p1(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+GKYL_CU_DH void dg_gen_diffusion_surfxy_2x_ser_p2(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+GKYL_CU_DH void dg_gen_diffusion_surfyx_2x_ser_p1(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+GKYL_CU_DH void dg_gen_diffusion_surfyx_2x_ser_p2(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+GKYL_CU_DH void dg_gen_diffusion_surfyy_2x_ser_p1(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
+GKYL_CU_DH void dg_gen_diffusion_surfyy_2x_ser_p2(const double* w, const double* dx, const double* Dij, const double* q[], double* GKYL_RESTRICT out);
 
 GKYL_CU_DH double dg_gen_diffusion_vol_3x_ser_p1(const double* w, const double* dx, const double* D, const double* q, double* GKYL_RESTRICT out);
 GKYL_CU_DH double dg_gen_diffusion_vol_3x_ser_p2(const double* w, const double* dx, const double* D, const double* q, double* GKYL_RESTRICT out);
