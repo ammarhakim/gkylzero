5960aad7bd0e0b46dd003c81c25045c3fe7c2713
manauref <mfrancisquez@gmail.com>
2022-07-12 15:21:21 -0700

    Add files forgotten in previous commit.

---
+++ kernels/bin_op/binop_cross_mul_3d_5d_ser_p0.c
@@ -0,0 +1,9 @@
+// Thu Jul  7 14:01:03 2022
+#include <gkyl_binop_cross_mul_ser.h>
+GKYL_CU_DH
+void
+binop_cross_mul_3d_5d_ser_p0(const double *f, const double *g, double *fg )
+{
+  fg[0] = 3.5355339059327379e-01*f[0]*g[0];
+}
+
