6ba0c7aaf47a66128cba2b6b464d599798bbf428
manauref <mfrancisquez@gmail.com>
2022-07-13 12:19:34 -0700

    New kernels after some maxima fixes to support p=1 gkhybrid in GK (there hard been a mixup of Hybrid and GkHybrid in some Maxima scripts). 1x1v Landau damping test with hybrid basis gives correct result now.Generated with 70c03ef3e65aa52db2608eece0c7fe82e125e948 in gkylcas.

--- kernels/basis/gkyl_basis_gkhyb_1x1v_p1_upwind_quad_to_modal.h
+++ kernels/basis/gkyl_basis_gkhyb_1x1v_p1_upwind_quad_to_modal.h
@@ -8,10 +8,8 @@ gkhyb_1x1v_p1_xdir_upwind_quad_to_modal(const double* fUpwindQuad, double* GKYL_
 
 GKYL_CU_DH static inline void 
 gkhyb_1x1v_p1_vpardir_upwind_quad_to_modal(const double* fUpwindQuad, double* GKYL_RESTRICT fUpwind) { 
-  fUpwind[0] = 0.5*fUpwindQuad[1]+0.5*fUpwindQuad[0]; 
-  fUpwind[1] = 0.5*fUpwindQuad[1]-0.5*fUpwindQuad[0]; 
-  fUpwind[2] = 0.8660254037844386*fUpwindQuad[1]*mu+0.8660254037844386*fUpwindQuad[0]*mu; 
-  fUpwind[3] = 0.8660254037844386*fUpwindQuad[1]*mu-0.8660254037844386*fUpwindQuad[0]*mu; 
+  fUpwind[0] = 0.7071067811865475*fUpwindQuad[1]+0.7071067811865475*fUpwindQuad[0]; 
+  fUpwind[1] = 0.7071067811865475*fUpwindQuad[1]-0.7071067811865475*fUpwindQuad[0]; 
 
 } 
 
--- kernels/gyrokinetic/gyrokinetic_vol_1x1v_ser_p1.c
+++ kernels/gyrokinetic/gyrokinetic_vol_1x1v_ser_p1.c
@@ -28,12 +28,13 @@ GKYL_CU_DH double gyrokinetic_vol_1x1v_ser_p1(const double *w, const double *dxv
   const double *b_y = &b_i[2];
   const double *b_z = &b_i[4];
 
-  double hamil[4] = {0.}; 
+  double hamil[6] = {0.}; 
   hamil[0] = (0.3333333333333333*(3.0*rdvpar2Sq*(m_*wvparSq+1.414213562373095*phi[0]*q_)+m_))/rdvpar2Sq; 
   hamil[1] = 1.414213562373095*phi[1]*q_; 
   hamil[2] = (1.154700538379252*m_*wvpar)/rdvpar2; 
+  hamil[4] = (0.2981423969999719*m_)/rdvpar2Sq; 
 
-  double BstarZdBmag[4] = {0.}; 
+  double BstarZdBmag[6] = {0.}; 
   BstarZdBmag[0] = (1.732050807568877*jacobtot_inv[0]*b_y[1]*m_*rdx2*wvpar+(cmag[1]*jacobtot_inv[1]+cmag[0]*jacobtot_inv[0])*q_)/q_; 
   BstarZdBmag[1] = (1.732050807568877*b_y[1]*jacobtot_inv[1]*m_*rdx2*wvpar+(cmag[0]*jacobtot_inv[1]+jacobtot_inv[0]*cmag[1])*q_)/q_; 
   BstarZdBmag[2] = (jacobtot_inv[0]*b_y[1]*m_*rdx2)/(q_*rdvpar2); 
@@ -43,41 +44,49 @@ GKYL_CU_DH double gyrokinetic_vol_1x1v_ser_p1(const double *w, const double *dxv
   double alphaL = 0.0; 
   double alphaR = 0.0; 
 
-  double alphax[4] = {0.}; 
-  alphax[0] = (0.8660254037844386*BstarZdBmag[0]*hamil[2]*rdvpar2*rdx2)/m_; 
-  alphax[1] = (0.8660254037844386*BstarZdBmag[1]*hamil[2]*rdvpar2*rdx2)/m_; 
-  alphax[2] = (0.8660254037844386*BstarZdBmag[2]*hamil[2]*rdvpar2*rdx2)/m_; 
-  alphax[3] = (0.8660254037844386*hamil[2]*BstarZdBmag[3]*rdvpar2*rdx2)/m_; 
+  double alphax[6] = {0.}; 
+  alphax[0] = (0.8660254037844386*(2.23606797749979*BstarZdBmag[2]*hamil[4]+BstarZdBmag[0]*hamil[2])*rdvpar2*rdx2)/m_; 
+  alphax[1] = (0.8660254037844386*(2.23606797749979*BstarZdBmag[3]*hamil[4]+BstarZdBmag[1]*hamil[2])*rdvpar2*rdx2)/m_; 
+  alphax[2] = (0.8660254037844386*(2.23606797749979*BstarZdBmag[0]*hamil[4]+BstarZdBmag[2]*hamil[2])*rdvpar2*rdx2)/m_; 
+  alphax[3] = (0.8660254037844386*(2.23606797749979*BstarZdBmag[1]*hamil[4]+hamil[2]*BstarZdBmag[3])*rdvpar2*rdx2)/m_; 
+  alphax[4] = (1.732050807568877*BstarZdBmag[2]*hamil[4]*rdvpar2*rdx2)/m_; 
+  alphax[5] = (1.732050807568877*BstarZdBmag[3]*hamil[4]*rdvpar2*rdx2)/m_; 
   // Evaluate alpha at left surface quadrature points.
-  alphaL = 0.25*(0.8660254037844386*alphax[3]-0.4999999999999999*alphax[2]-0.8660254037844386*alphax[1]+0.5*alphax[0]); 
+  alphaL = 0.25*((-0.7745966692414833*alphax[5])+0.4472135954999579*alphax[4]+1.161895003862225*alphax[3]-0.6708203932499369*alphax[2]-0.8660254037844386*alphax[1]+0.5*alphax[0]); 
   cflFreq += -0.5*(alphaL-fabs(alphaL)); 
-  alphaL = 0.25*((-0.8660254037844386*alphax[3])+0.4999999999999999*alphax[2]-0.8660254037844386*alphax[1]+0.5*alphax[0]); 
+  alphaL = 0.25*(0.9682458365518543*alphax[5]-0.5590169943749475*alphax[4]-0.8660254037844386*alphax[1]+0.5*alphax[0]); 
+  cflFreq += -0.5*(alphaL-fabs(alphaL)); 
+  alphaL = 0.25*((-0.7745966692414833*alphax[5])+0.4472135954999579*alphax[4]-1.161895003862225*alphax[3]+0.6708203932499369*alphax[2]-0.8660254037844386*alphax[1]+0.5*alphax[0]); 
   cflFreq += -0.5*(alphaL-fabs(alphaL)); 
   // Evaluate alpha at right surface quadrature points.
-  alphaR = 0.25*((-0.8660254037844386*alphax[3])-0.4999999999999999*alphax[2]+0.8660254037844386*alphax[1]+0.5*alphax[0]); 
+  alphaR = 0.25*(0.7745966692414833*alphax[5]+0.4472135954999579*alphax[4]-1.161895003862225*alphax[3]-0.6708203932499369*alphax[2]+0.8660254037844386*alphax[1]+0.5*alphax[0]); 
   cflFreq += 0.5*(alphaR+fabs(alphaR)); 
-  alphaR = 0.25*(0.8660254037844386*alphax[3]+0.4999999999999999*alphax[2]+0.8660254037844386*alphax[1]+0.5*alphax[0]); 
+  alphaR = 0.25*((-0.9682458365518543*alphax[5])-0.5590169943749475*alphax[4]+0.8660254037844386*alphax[1]+0.5*alphax[0]); 
+  cflFreq += 0.5*(alphaR+fabs(alphaR)); 
+  alphaR = 0.25*(0.7745966692414833*alphax[5]+0.4472135954999579*alphax[4]+1.161895003862225*alphax[3]+0.6708203932499369*alphax[2]+0.8660254037844386*alphax[1]+0.5*alphax[0]); 
   cflFreq += 0.5*(alphaR+fabs(alphaR)); 
 
-  double alphavpar[4] = {0.}; 
+  double alphavpar[6] = {0.}; 
   alphavpar[0] = -(0.8660254037844386*BstarZdBmag[0]*hamil[1]*rdvpar2*rdx2)/m_; 
   alphavpar[1] = -(0.8660254037844386*BstarZdBmag[1]*hamil[1]*rdvpar2*rdx2)/m_; 
   alphavpar[2] = -(0.8660254037844386*hamil[1]*BstarZdBmag[2]*rdvpar2*rdx2)/m_; 
   alphavpar[3] = -(0.8660254037844386*hamil[1]*BstarZdBmag[3]*rdvpar2*rdx2)/m_; 
   // Evaluate alpha at left surface quadrature points.
-  alphaL = 0.25*(0.8660254037844386*alphavpar[3]-0.8660254037844386*alphavpar[2]-0.4999999999999999*alphavpar[1]+0.5*alphavpar[0]); 
+  alphaL = 0.25*(0.8660254037844386*alphavpar[3]-0.8660254037844386*alphavpar[2]-0.5*alphavpar[1]+0.5*alphavpar[0]); 
   cflFreq += -0.5*(alphaL-fabs(alphaL)); 
-  alphaL = 0.25*((-0.8660254037844386*(alphavpar[3]+alphavpar[2]))+0.4999999999999999*alphavpar[1]+0.5*alphavpar[0]); 
+  alphaL = 0.25*(0.5*(alphavpar[1]+alphavpar[0])-0.8660254037844386*(alphavpar[3]+alphavpar[2])); 
   cflFreq += -0.5*(alphaL-fabs(alphaL)); 
   // Evaluate alpha at right surface quadrature points.
-  alphaR = 0.25*((-0.8660254037844386*alphavpar[3])+0.8660254037844386*alphavpar[2]-0.4999999999999999*alphavpar[1]+0.5*alphavpar[0]); 
+  alphaR = 0.25*((-0.8660254037844386*alphavpar[3])+0.8660254037844386*alphavpar[2]-0.5*alphavpar[1]+0.5*alphavpar[0]); 
   cflFreq += 0.5*(alphaR+fabs(alphaR)); 
-  alphaR = 0.25*(0.8660254037844386*(alphavpar[3]+alphavpar[2])+0.4999999999999999*alphavpar[1]+0.5*alphavpar[0]); 
+  alphaR = 0.25*(0.8660254037844386*(alphavpar[3]+alphavpar[2])+0.5*(alphavpar[1]+alphavpar[0])); 
   cflFreq += 0.5*(alphaR+fabs(alphaR)); 
 
-  out[1] += 0.8660254037844386*(alphax[3]*fin[3]+alphax[2]*fin[2]+alphax[1]*fin[1]+alphax[0]*fin[0]); 
+  out[1] += 0.8660254037844386*(alphax[5]*fin[5]+alphax[4]*fin[4]+alphax[3]*fin[3]+alphax[2]*fin[2]+alphax[1]*fin[1]+alphax[0]*fin[0]); 
   out[2] += 0.8660254037844386*(alphavpar[3]*fin[3]+alphavpar[2]*fin[2]+alphavpar[1]*fin[1]+alphavpar[0]*fin[0]); 
-  out[3] += 0.8660254037844386*((alphavpar[2]+alphax[1])*fin[3]+fin[1]*alphax[3]+fin[2]*(alphavpar[3]+alphax[0])+fin[0]*alphax[2]+alphavpar[0]*fin[1]+fin[0]*alphavpar[1]); 
+  out[3] += 0.1*(7.745966692414834*(alphax[3]*fin[5]+fin[3]*alphax[5]+alphax[2]*fin[4]+fin[2]*alphax[4])+8.660254037844386*((alphavpar[2]+alphax[1])*fin[3]+fin[1]*alphax[3]+fin[2]*(alphavpar[3]+alphax[0])+fin[0]*alphax[2]+alphavpar[0]*fin[1]+fin[0]*alphavpar[1])); 
+  out[4] += 0.1*(17.32050807568877*alphavpar[3]*fin[5]+17.32050807568877*alphavpar[2]*fin[4]+19.36491673103709*(alphavpar[1]*fin[3]+fin[1]*alphavpar[3]+alphavpar[0]*fin[2]+fin[0]*alphavpar[2])); 
+  out[5] += 0.01428571428571429*((38.72983346207417*alphax[5]+121.2435565298214*alphavpar[2])*fin[5]+60.6217782649107*(alphax[1]*fin[5]+fin[1]*alphax[5])+(38.72983346207417*alphax[4]+121.2435565298214*alphavpar[3])*fin[4]+60.62177826491071*(alphax[0]*fin[4]+fin[0]*alphax[4])+54.22176684690384*alphax[3]*fin[3]+135.5544171172596*(alphavpar[0]*fin[3]+fin[0]*alphavpar[3])+54.22176684690384*alphax[2]*fin[2]+135.5544171172596*(alphavpar[1]*fin[2]+fin[1]*alphavpar[2])); 
 
   return cflFreq; 
 } 
@@ -93,18 +102,20 @@ GKYL_CU_DH double gyrokinetic_step2_vol_1x1v_ser_p1(const double *w, const doubl
   double rdvpar2 = 2.0/dxv[1]; 
   out[2] += -(1.224744871391589*(apardot[1]*fin[1]+apardot[0]*fin[0])*q_*rdvpar2)/m_; 
   out[3] += -(1.224744871391589*(apardot[0]*fin[1]+fin[0]*apardot[1])*q_*rdvpar2)/m_; 
+  out[4] += -(2.738612787525831*(apardot[1]*fin[3]+apardot[0]*fin[2])*q_*rdvpar2)/m_; 
+  out[5] += -(2.738612787525831*(apardot[0]*fin[3]+apardot[1]*fin[2])*q_*rdvpar2)/m_; 
   double cflFreq = 0.0; 
   double alphaL = 0.0; 
   double alphaR = 0.0; 
   // Evaluate alpha at left surface quadrature points.
-  alphaL = -(0.25*(0.7071067811865475*apardot[0]-0.7071067811865474*apardot[1])*q_*rdvpar2)/m_; 
+  alphaL = -(0.25*(0.7071067811865475*apardot[0]-0.7071067811865475*apardot[1])*q_*rdvpar2)/m_; 
   cflFreq += -0.5*(alphaL-fabs(alphaL)); 
-  alphaL = -(0.25*(0.7071067811865474*apardot[1]+0.7071067811865475*apardot[0])*q_*rdvpar2)/m_; 
+  alphaL = -(0.1767766952966369*(apardot[1]+apardot[0])*q_*rdvpar2)/m_; 
   cflFreq += -0.5*(alphaL-fabs(alphaL)); 
   // Evaluate alpha at right surface quadrature points.
-  alphaR = -(0.25*(0.7071067811865475*apardot[0]-0.7071067811865474*apardot[1])*q_*rdvpar2)/m_; 
+  alphaR = -(0.25*(0.7071067811865475*apardot[0]-0.7071067811865475*apardot[1])*q_*rdvpar2)/m_; 
   cflFreq += 0.5*(alphaR+fabs(alphaR)); 
-  alphaR = -(0.25*(0.7071067811865474*apardot[1]+0.7071067811865475*apardot[0])*q_*rdvpar2)/m_; 
+  alphaR = -(0.1767766952966369*(apardot[1]+apardot[0])*q_*rdvpar2)/m_; 
   cflFreq += 0.5*(alphaR+fabs(alphaR)); 
 
   return cflFreq; 
