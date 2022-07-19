ad296e5302c6cff4c7bf01343558b8cdd93668f9
mrquell <maxwell@mhrosen-lt.pppl.gov>
2022-07-19 10:29:41 -0400

    Changed the way the dimensionality is configured within each unit test

--- unit/ctest_bc_vlasov.c
+++ unit/ctest_bc_vlasov.c
@@ -33,7 +33,7 @@ void evalFunc_2x2v(double t, const double *xn, double* restrict fout, void *ctx)
 }
 
 // allocate array (filled with zeros)
-static struct gkyl_array*
+  static struct gkyl_array*
 mkarr(long nc, long size)
 {
   struct gkyl_array* a = gkyl_array_new(GKYL_DOUBLE, nc, size);
@@ -41,7 +41,7 @@ mkarr(long nc, long size)
 }
 
 // allocate cu_dev array
-static struct gkyl_array*
+  static struct gkyl_array*
 mkarr_cu(long nc, long size)
 {
   struct gkyl_array* a = gkyl_array_cu_dev_new(GKYL_DOUBLE, nc, size);
@@ -57,43 +57,54 @@ struct skin_ghost_ranges {
 };
 
 // Create ghost and skin sub-ranges given a parent range
-static void
+  static void
 skin_ghost_ranges_init(struct skin_ghost_ranges *sgr,
-  const struct gkyl_range *parent, const int *ghost)
+    const struct gkyl_range *parent, const int *ghost)
 {
   int ndim = parent->ndim;
 
   for (int d=0; d<ndim; ++d) {
     gkyl_skin_ghost_ranges(&sgr->lower_skin[d], &sgr->lower_ghost[d],
-      d, GKYL_LOWER_EDGE, parent, ghost);
+        d, GKYL_LOWER_EDGE, parent, ghost);
     gkyl_skin_ghost_ranges(&sgr->upper_skin[d], &sgr->upper_ghost[d],
-      d, GKYL_UPPER_EDGE, parent, ghost);
+        d, GKYL_UPPER_EDGE, parent, ghost);
   }
 }
 
 void test_bc_wall_1x1v()
 {
+  // Change these inputs for going to different dimensions
+  int cdim = 1;
+  int vdim = 1;
+  
   int poly_order = 1;
-  double lower[] = {-2.0, -2.0}, upper[] = {2.0, 2.0};
-  int cells[] = {4, 2};
-  int ndim = sizeof(lower)/sizeof(lower[0]);
+  int ndim = cdim + vdim;
+  double lower[ndim], upper[ndim];
+  int cells[ndim];
+  for (int ix=0; ix<ndim; ix++){
+    lower[ix] =-2.0;
+    upper[ix] = 2.0;
+    cells[ix] = ix<cdim ? 4 : 2; 
+  }
+  double confLower[cdim], confUpper[cdim];
+  int confCells[cdim];
+  for (int ix=0; ix<cdim; ix++){
+    confLower[ix] = lower[ix];
+    confUpper[ix] = upper[ix];
+    confCells[ix] = cells[ix];
+  }
 
-  double confLower[] = {lower[0]}, confUpper[] = {upper[0]};
-  int confCells[] = {cells[0]};
-
-  int cdim = sizeof(confLower)/sizeof(confLower[0]); //number of dimensions of configuration space
-  int vdim = ndim - cdim; //number of velocity space dimensions
   // grids
   struct gkyl_rect_grid grid;
   gkyl_rect_grid_init(&grid, ndim, lower, upper, cells);
   struct gkyl_rect_grid confGrid;
   gkyl_rect_grid_init(&confGrid, cdim, confLower, confUpper, confCells);
-  
+
   // basis functions
   struct gkyl_basis basis, confBasis;
   gkyl_cart_modal_serendip(&basis, ndim, poly_order);
   gkyl_cart_modal_serendip(&confBasis, cdim, poly_order);
-  
+
   int confGhost[cdim];
   for (int d=0; d<cdim; d++){confGhost[d] = 1;}
   struct gkyl_range confLocal, confLocal_ext; // local, local-ext conf-space ranges
@@ -104,30 +115,31 @@ void test_bc_wall_1x1v()
   int ghost[ndim];
   for (int d=0; d<cdim; d++){ghost[d] = confGhost[d];}
   for (int d=cdim; d<ndim; d++){ghost[d] = 0;}
+
   struct gkyl_range local, local_ext; // local, local-ext phase-space ranges
   gkyl_create_grid_ranges(&grid, ghost, &local_ext, &local);
   struct skin_ghost_ranges skin_ghost; // phase-space skin/ghost
   skin_ghost_ranges_init(&skin_ghost, &local_ext, ghost);
- 
+
   // projection updater for dist-function
   gkyl_proj_on_basis *projDistf = gkyl_proj_on_basis_new(&grid, &basis,
-    poly_order+1, 1, evalFunc_1x1v, NULL);
+      poly_order+1, 1, evalFunc_1x1v, NULL);
 
   // create distribution function array
   struct gkyl_array *distf;
   distf = mkarr(basis.num_basis, local_ext.volume);
-  
+
   // project distribution function on basis
   gkyl_proj_on_basis_advance(projDistf, 0.0, &local_ext, distf);
 
   // Make a DG equation object
   struct gkyl_dg_eqn* eqn = gkyl_dg_vlasov_new(&confBasis, &basis, &local, GKYL_FIELD_E_B, false);
-  
+
   //Create the boundary condition
   struct gkyl_array_copy_func* bc;
   for (int bc_dir=0; bc_dir<cdim; bc_dir++){
     bc =  gkyl_vlasov_wall_bc_create(eqn, bc_dir, &basis);
-  // struct gkyl_array_copy_func* bc =  gkyl_vlasov_absorb_bc_create(eqn, bc_dir, &basis);
+    // struct gkyl_array_copy_func* bc =  gkyl_vlasov_absorb_bc_create(eqn, bc_dir, &basis);
     // Determine the size of the BC buffer
     long buff_sz = 0;
     for (int d=0; d<cdim; ++d) {
@@ -160,13 +172,13 @@ void test_bc_wall_1x1v()
 
       // Flip the skin value to manually apply wall BC to skin cell
       double val_correct[basis.num_basis];
-      basis.flip_odd_sign(d,  val_skin,   val_correct);
-      basis.flip_odd_sign(d+1,val_correct,val_correct);
+      basis.flip_odd_sign(d,   val_skin,   val_correct);
+      basis.flip_odd_sign(d+1, val_correct,val_correct);
 
       // Check values
       for (int i=0;i<basis.num_basis;i++){
         TEST_CHECK(gkyl_compare(val_ghost[i],val_correct[i],1e-12));
-    }}
+      }}
 
     // Check upper ghost cells after applying BC
     gkyl_range_iter_init(&iter, &skin_ghost.upper_ghost[d]);
@@ -186,7 +198,7 @@ void test_bc_wall_1x1v()
       // Check values
       for (int i=0;i<basis.num_basis;i++){
         TEST_CHECK(gkyl_compare(val_ghost[i],val_correct[i],1e-12));
-  }}}
+      }}}
 
   // release memory for moment data object
   gkyl_proj_on_basis_release(projDistf);
@@ -197,12 +209,12 @@ void test_bc_wall_1x1v()
 
 TEST_LIST = {
   { "test_bc_wall_1x1v", test_bc_wall_1x1v },
-//  { "test_bc_absorb_1x1v", test_bc_absorb_1x1v },
-//  { "test_bc_wall_1x2v", test_bc_wall_1x2v },
-//  { "test_bc_absorb_1x2v", test_bc_absorb_1x2v },
-//  { "test_bc_wall_2x2v", test_bc_wall_2x2v },
-//  { "test_bc_absorb_2x2v", test_bc_absorb_2x2v },
-//  { "test_bc_wall_3x2v", test_bc_wall_3x2v },
-//  { "test_bc_absorb_3x2v", test_bc_absorb_3x2v },
+  //  { "test_bc_absorb_1x1v", test_bc_absorb_1x1v },
+  //  { "test_bc_wall_1x2v", test_bc_wall_1x2v },
+  //  { "test_bc_absorb_1x2v", test_bc_absorb_1x2v },
+  //  { "test_bc_wall_2x2v", test_bc_wall_2x2v },
+  //  { "test_bc_absorb_2x2v", test_bc_absorb_2x2v },
+  //  { "test_bc_wall_3x2v", test_bc_wall_3x2v },
+  //  { "test_bc_absorb_3x2v", test_bc_absorb_3x2v },
   { NULL, NULL },
 };
