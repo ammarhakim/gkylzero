553fe6937873984866397fc4fb88ff1b30102fc2
Akash Shukla <as6384@stellar-amd.princeton.edu>
2022-08-02 16:55:35 -0400

    removed code duplication in rt_mirror_lorentzian_1x1v_p_perp.c

--- regression/rt_mirror_lorentzian_1x1v_p_perp.c
+++ regression/rt_mirror_lorentzian_1x1v_p_perp.c
@@ -35,6 +35,19 @@ struct mirror_ctx {
 static inline double sq(double x) { return x*x; }
 static inline double cu(double x) { return x*x*x; }
 
+double calcMagB(const double * GKYL_RESTRICT xn, void *ctx)
+{
+  struct mirror_ctx *app = ctx;
+  double x = xn[0];
+  double gamma = app->gamma;
+  double loc = app->loc;
+  double mag = app->mag;
+  double magB = mag/(gamma*(1.0 + sq((x-loc)/gamma))) + mag/(gamma*(1.0 + sq((x+loc)/gamma)));
+  return magB;
+}
+
+
+
 void
 evalDistFuncElc(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
 {
@@ -42,10 +55,11 @@ evalDistFuncElc(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT
   double x = xn[0], v = xn[1];
   double vt = app->vte, n0 = app->n0;
   double fv = n0*exp(-sq(x))/sqrt(2.0*M_PI*sq(vt))*(exp(-sq(v)/(2*sq(vt))));
-  double gamma = app->gamma;
-  double loc = app->loc;
-  double mag = app->mag;
-  double magB = mag/(gamma*(1.0 + sq((x-loc)/gamma))) + mag/(gamma*(1.0 + sq((x+loc)/gamma)));
+  //double gamma = app->gamma;
+  //double loc = app->loc;
+  //double mag = app->mag;
+  //double magB = mag/(gamma*(1.0 + sq((x-loc)/gamma))) + mag/(gamma*(1.0 + sq((x+loc)/gamma)));
+  double magB = calcMagB(xn,ctx);
   fout[0] = fv/magB;
 }
 
@@ -56,10 +70,11 @@ evalDistFuncIon(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT
   double x = xn[0], v = xn[1];
   double vt = app->vti, n0 = app->n0;
   double fv = n0*exp(-sq(x))/sqrt(2.0*M_PI*sq(vt))*(exp(-sq(v)/(2*sq(vt))));
-  double gamma = app->gamma;
-  double loc = app->loc;
-  double mag = app->mag;
-  double magB = mag/(gamma*(1.0 + sq((x-loc)/gamma))) + mag/(gamma*(1.0 + sq((x+loc)/gamma)));
+  //double gamma = app->gamma;
+  //double loc = app->loc;
+  //double mag = app->mag;
+  //double magB = mag/(gamma*(1.0 + sq((x-loc)/gamma))) + mag/(gamma*(1.0 + sq((x+loc)/gamma)));
+  double magB = calcMagB(xn,ctx);
   fout[0] = fv/magB;
 }
 
@@ -69,10 +84,11 @@ evalPperpElc(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT fo
   struct mirror_ctx *app = ctx;
   double x = xn[0];
   double vt = app->vte, n = app->n0;
-  double gamma = app->gamma;
-  double loc = app->loc;
-  double mag = app->mag;
-  double magB = mag/(gamma*(1.0 + sq((x-loc)/gamma))) + mag/(gamma*(1.0 + sq((x+loc)/gamma)));
+  //double gamma = app->gamma;
+  //double loc = app->loc;
+  //double mag = app->mag;
+  //double magB = mag/(gamma*(1.0 + sq((x-loc)/gamma))) + mag/(gamma*(1.0 + sq((x+loc)/gamma)));
+  double magB = calcMagB(xn,ctx);
   fout[0] = n*exp(-sq(x))*vt*vt/magB;
 }
 
@@ -82,10 +98,11 @@ evalPperpIon(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT fo
   struct mirror_ctx *app = ctx;
   double x = xn[0];
   double vt = app->vti, n = app->n0;
-  double gamma = app->gamma;
-  double loc = app->loc;
-  double mag = app->mag;
-  double magB = mag/(gamma*(1.0 + sq((x-loc)/gamma))) + mag/(gamma*(1.0 + sq((x+loc)/gamma)));
+  //double gamma = app->gamma;
+  //double loc = app->loc;
+  //double mag = app->mag;
+  //double magB = mag/(gamma*(1.0 + sq((x-loc)/gamma))) + mag/(gamma*(1.0 + sq((x+loc)/gamma)));
+  double magB = calcMagB(xn,ctx);
   fout[0] = n*exp(-sq(x))*vt*vt/magB;
 }
 
@@ -121,10 +138,12 @@ evalMagB(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, 
 {
   struct mirror_ctx *app = ctx;
   double x = xn[0];
-  double gamma = app->gamma;
-  double loc = app->loc;
-  double mag = app->mag;
-  fout[0] = mag/(gamma*(1.0 + sq((x-loc)/gamma))) + mag/(gamma*(1.0 + sq((x+loc)/gamma)));
+  //double gamma = app->gamma;
+  //double loc = app->loc;
+  //double mag = app->mag;
+  //fout[0] = mag/(gamma*(1.0 + sq((x-loc)/gamma))) + mag/(gamma*(1.0 + sq((x+loc)/gamma)));
+  double magB = calcMagB(xn,ctx);
+  fout[0] = magB;
 }
 
 void
