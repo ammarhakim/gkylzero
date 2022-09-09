1014fceeba773a88f64d3d05dc83b2808daac12b
manauref <mfrancisquez@gmail.com>
2022-09-08 20:35:12 -0700

    Add a few more const to ranges passed by reference in dg_bin_ops. Unit tests pass.

--- zero/dg_bin_ops.c
+++ zero/dg_bin_ops.c
@@ -185,7 +185,7 @@ void gkyl_dg_dot_product_op_range(struct gkyl_basis basis,
 void gkyl_dg_mul_conf_phase_op_range(struct gkyl_basis *cbasis,
   struct gkyl_basis *pbasis, struct gkyl_array* pout,
   const struct gkyl_array* cop, const struct gkyl_array* pop,
-  struct gkyl_range *crange, struct gkyl_range *prange)
+  const struct gkyl_range *crange, const struct gkyl_range *prange)
 {
 #ifdef GKYL_HAVE_CUDA
   if (gkyl_array_is_cu_dev(pout)) {
--- zero/dg_bin_ops_cu.cu
+++ zero/dg_bin_ops_cu.cu
@@ -137,7 +137,7 @@ void
 gkyl_dg_mul_conf_phase_op_range_cu(struct gkyl_basis *cbasis,
   struct gkyl_basis *pbasis, struct gkyl_array* pout,
   const struct gkyl_array* cop, const struct gkyl_array* pop,
-  struct gkyl_range *crange, struct gkyl_range *prange)
+  const struct gkyl_range *crange, const struct gkyl_range *prange)
 {
   int nblocks = prange->nblocks;
   int nthreads = prange->nthreads;
