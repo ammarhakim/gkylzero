0c27ea035efe3acdc6bbb9559a6aea8716891d75
Manaure Francisquez <mfrancisquez@gmail.com>
2022-09-15 21:06:59 -0400

    Implement GPU offset methods and their unit tests. The non-range methods (_set_offset and _accumulate_offset) could probably be optimized; right now they only parallelize over cells (instead of cells and components) and probably have unaligned memory accesses that could be avoided. We'll optimize later.

--- unit/ctest_array.c
+++ unit/ctest_array.c
@@ -1397,6 +1397,125 @@ void test_cu_array_accumulate_range()
   gkyl_array_release(a2_cu);
 }
 
+void test_cu_array_accumulate_offset()
+{
+  // create host arrays 
+  struct gkyl_array *a1 = gkyl_array_new(GKYL_DOUBLE, 2, 10);
+  struct gkyl_array *a2 = gkyl_array_new(GKYL_DOUBLE, 3*a1->ncomp, a1->size);
+  // make device copies
+  struct gkyl_array *a1_cu = gkyl_array_cu_dev_new(GKYL_DOUBLE, a1->ncomp, a1->size);
+  struct gkyl_array *a2_cu = gkyl_array_cu_dev_new(GKYL_DOUBLE, a2->ncomp, a2->size);
+
+  // initialize data
+  double *a1_d  = a1->data, *a2_d = a2->data;
+  for (unsigned i=0; i<a1->size; ++i)
+    for (unsigned j=0; j<a1->ncomp; ++j)
+      a1_d[i*a1->ncomp+j] = i*1.0+j;
+
+  for (unsigned i=0; i<a2->size; ++i)
+    for (unsigned j=0; j<a2->ncomp/a1->ncomp; ++j)
+      for (unsigned k=0; k<a1->ncomp; ++k)
+        a2_d[i*a2->ncomp+j*a1->ncomp+k] = i*0.1+k;
+
+  // copy initialized arrays to device
+  gkyl_array_copy(a1_cu, a1);
+  gkyl_array_copy(a2_cu, a2);
+
+  // test a1 = 0.1*a2[a1->ncomp]
+  gkyl_array_accumulate_offset(a1_cu, 0.5, a2_cu, 1*a1->ncomp);
+
+  gkyl_array_copy(a1, a1_cu);
+  for (unsigned i=0; i<a1->size; ++i)
+    for (unsigned j=0; j<a1->ncomp; ++j)
+      TEST_CHECK( gkyl_compare(a1_d[i*a1->ncomp+j], i*1.0+j+0.5*(i*0.1+j), 1e-14) );
+
+  // test a2[a1->ncomp] = 0.1*a1
+  for (unsigned i=0; i<a1->size; ++i)
+    for (unsigned j=0; j<a1->ncomp; ++j)
+      a1_d[i*a1->ncomp+j] = i*1.0+j;
+  gkyl_array_copy(a1_cu, a1);
+
+  gkyl_array_accumulate_offset(a2_cu, 0.5, a1_cu, 1*a1->ncomp);
+
+  gkyl_array_copy(a2, a2_cu);
+  for (unsigned i=0; i<a1->size; ++i) {
+    for (unsigned j=0; j<a1->ncomp; ++j) {
+      TEST_CHECK( gkyl_compare(a2_d[i*a2->ncomp+0*a1->ncomp+j], i*0.1+j, 1e-14) );
+      TEST_CHECK( gkyl_compare(a2_d[i*a2->ncomp+1*a1->ncomp+j], i*0.1+j+0.5*(i*1.0+j), 1e-14) );
+      TEST_CHECK( gkyl_compare(a2_d[i*a2->ncomp+2*a1->ncomp+j], i*0.1+j, 1e-14) );
+    }
+  }
+
+  gkyl_array_release(a1);
+  gkyl_array_release(a2);
+  gkyl_array_release(a1_cu);
+  gkyl_array_release(a2_cu);
+}
+
+void test_cu_array_accumulate_offset_range()
+{
+  int shape[] = {20, 10};
+  struct gkyl_range range;
+  gkyl_range_init_from_shape(&range, 2, shape);
+  
+  struct gkyl_array *a1 = gkyl_array_new(GKYL_DOUBLE, 2, range.volume);
+  struct gkyl_array *a2 = gkyl_array_new(GKYL_DOUBLE, 3*a1->ncomp, range.volume);
+
+  // make device copies of arrays
+  struct gkyl_array *a1_cu = gkyl_array_cu_dev_new(GKYL_DOUBLE, a1->ncomp, range.volume);
+  struct gkyl_array *a2_cu = gkyl_array_cu_dev_new(GKYL_DOUBLE, a2->ncomp, range.volume);
+
+  // initialize data
+  double *a1_d  = a1->data, *a2_d = a2->data;
+  for (unsigned i=0; i<a1->size; ++i) {
+    for(unsigned c=0; c<a1->ncomp; ++c) 
+      a1_d[i*a1->ncomp+c] = i*1.0 + .01*c;
+    for(unsigned j=0; j<a2->ncomp/a1->ncomp; ++j)
+      for(unsigned c=0; c<a1->ncomp; ++c) 
+        a2_d[i*a2->ncomp+j*a1->ncomp+c] = i*0.1 + .01*c;
+  }
+
+  // copy initialized arrays to device
+  gkyl_array_copy(a1_cu, a1);
+  gkyl_array_copy(a2_cu, a2);
+
+  // a1 = a1 + 0.5*a2[1*a1->ncomp]
+  gkyl_array_accumulate_offset_range(a1_cu, 0.5, a2_cu, 1*a1->ncomp, range);
+
+  gkyl_array_copy(a1, a1_cu);
+  struct gkyl_range_iter iter;
+  gkyl_range_iter_init(&iter, &range);
+  while (gkyl_range_iter_next(&iter)) {
+    long loc = gkyl_range_idx(&range, iter.idx);
+    double *a1d = gkyl_array_fetch(a1, loc);
+    for (int c=0; c<a1->ncomp; ++c)
+      TEST_CHECK( gkyl_compare( a1d[c], loc*1.0+.01*c + 0.5*(loc*0.1+0.01*c), 1e-14 ) );
+  }
+
+  // test a2[1*a1->ncomp] = a2[1*a1->ncomp] + 0.5*a1
+  gkyl_array_clear(a1_cu, 0.5);
+  gkyl_array_clear(a2_cu, 1.5);
+
+  gkyl_array_accumulate_offset_range(a2_cu, 0.5, a1_cu, 1*a1->ncomp, range);
+
+  gkyl_array_copy(a2, a2_cu);
+  gkyl_range_iter_init(&iter, &range);
+  while (gkyl_range_iter_next(&iter)) {
+    long loc = gkyl_range_idx(&range, iter.idx);
+    double *a2d = gkyl_array_fetch(a2, loc);
+    for (int i=0; i<a1->ncomp; ++i) {
+      TEST_CHECK( a2d[i+0*a1->ncomp] == 1.5 );
+      TEST_CHECK( a2d[i+1*a1->ncomp] == 1.5+0.5*0.5 );
+      TEST_CHECK( a2d[i+2*a1->ncomp] == 1.5 );
+    }
+  }  
+
+  gkyl_array_release(a1);
+  gkyl_array_release(a2);
+  gkyl_array_release(a1_cu);
+  gkyl_array_release(a2_cu);
+}
+
 void test_cu_array_accumulate_range_4d()
 {
   int lower[] = { 1, 1, 1, 1 };
@@ -1575,6 +1694,118 @@ void test_cu_array_set_range()
     double *a2d = gkyl_array_fetch(a2, loc);
     for (int i=0; i<3; ++i)
       TEST_CHECK( a2d[i] == 0.5*0.5 );
+  }  
+
+  gkyl_array_release(a1);
+  gkyl_array_release(a2);
+  gkyl_array_release(a1_cu);
+  gkyl_array_release(a2_cu);
+}
+
+void test_cu_array_set_offset()
+{
+  // create host arrays 
+  struct gkyl_array *a1 = gkyl_array_new(GKYL_DOUBLE, 2, 10);
+  struct gkyl_array *a2 = gkyl_array_new(GKYL_DOUBLE, 3*a1->ncomp, a1->size);
+  // make device copies
+  struct gkyl_array *a1_cu = gkyl_array_cu_dev_new(GKYL_DOUBLE, a1->ncomp, a1->size);
+  struct gkyl_array *a2_cu = gkyl_array_cu_dev_new(GKYL_DOUBLE, a2->ncomp, a2->size);
+
+  // initialize data
+  double *a1_d  = a1->data, *a2_d = a2->data;
+  for (unsigned i=0; i<a1->size; ++i)
+    for (unsigned j=0; j<a1->ncomp; ++j)
+      a1_d[i*a1->ncomp+j] = i*1.0+j;
+
+  for (unsigned i=0; i<a2->size; ++i)
+    for (unsigned j=0; j<a2->ncomp/a1->ncomp; ++j)
+      for (unsigned k=0; k<a1->ncomp; ++k)
+        a2_d[i*a2->ncomp+j*a1->ncomp+k] = i*0.1+k;
+
+  // copy initialized arrays to device
+  gkyl_array_copy(a1_cu, a1);
+  gkyl_array_copy(a2_cu, a2);
+
+  // test a1 = 0.5*a2[a1->ncomp]
+  gkyl_array_set_offset(a1_cu, 0.5, a2_cu, 1*a1->ncomp);
+
+  gkyl_array_copy(a1, a1_cu);
+  for (unsigned i=0; i<a1->size; ++i)
+    for (unsigned j=0; j<a1->ncomp; ++j)
+      TEST_CHECK( gkyl_compare(a1_d[i*a1->ncomp+j], 0.5*(i*0.1+j), 1e-14) );
+
+  // test a2[a1->ncomp] = 0.1*a1
+  for (unsigned i=0; i<a1->size; ++i)
+    for (unsigned j=0; j<a1->ncomp; ++j)
+      a1_d[i*a1->ncomp+j] = i*1.0+j;
+  gkyl_array_copy(a1_cu, a1);
+
+  gkyl_array_set_offset(a2_cu, 0.5, a1_cu, 1*a1->ncomp);
+
+  gkyl_array_copy(a2, a2_cu);
+  for (unsigned i=0; i<a1->size; ++i) {
+    for (unsigned j=0; j<a1->ncomp; ++j) {
+      TEST_CHECK( gkyl_compare(a2_d[i*a2->ncomp+0*a1->ncomp+j], i*0.1+j, 1e-14) );
+      TEST_CHECK( gkyl_compare(a2_d[i*a2->ncomp+1*a1->ncomp+j], 0.5*(i*1.0+j), 1e-14) );
+      TEST_CHECK( gkyl_compare(a2_d[i*a2->ncomp+2*a1->ncomp+j], i*0.1+j, 1e-14) );
+    }
+  }
+
+  gkyl_array_release(a1);
+  gkyl_array_release(a2);
+  gkyl_array_release(a1_cu);
+  gkyl_array_release(a2_cu);
+}
+
+void test_cu_array_set_offset_range()
+{
+  int shape[] = {10, 20};
+  struct gkyl_range range;
+  gkyl_range_init_from_shape(&range, 2, shape);
+  
+  struct gkyl_array *a1 = gkyl_array_new(GKYL_DOUBLE, 2, range.volume);
+  struct gkyl_array *a2 = gkyl_array_new(GKYL_DOUBLE, 3*a1->ncomp, range.volume);
+
+  // make device copies of arrays
+  struct gkyl_array *a1_cu = gkyl_array_cu_dev_new(GKYL_DOUBLE, a1->ncomp, range.volume);
+  struct gkyl_array *a2_cu = gkyl_array_cu_dev_new(GKYL_DOUBLE, a2->ncomp, range.volume);
+
+  // copy host arrays to device
+  gkyl_array_copy(a1_cu, a1);
+  gkyl_array_copy(a2_cu, a2);
+
+  // test a1 = 0.5*a2[a1->ncomp]
+  gkyl_array_clear(a1_cu, 0.5);
+  gkyl_array_clear(a2_cu, 1.5);
+
+  gkyl_array_set_offset_range(a1_cu, 0.5, a2_cu, 1*a1->ncomp, range);
+
+  gkyl_array_copy(a1, a1_cu);
+  struct gkyl_range_iter iter;
+  gkyl_range_iter_init(&iter, &range);
+  while (gkyl_range_iter_next(&iter)) {
+    long loc = gkyl_range_idx(&range, iter.idx);
+    double *a1d = gkyl_array_fetch(a1, loc);
+    for (int i=0; i<a1->ncomp; ++i)
+      TEST_CHECK( a1d[i] == 0.5*1.5 );
+  }
+
+  // test a2[a1->ncomp] = 0.5*a1
+  gkyl_array_clear(a1_cu, 0.5);
+  gkyl_array_clear(a2_cu, 1.5);
+
+  gkyl_array_set_offset_range(a2_cu, 0.5, a1_cu, 1*a1->ncomp, range);
+
+  gkyl_array_copy(a2, a2_cu);
+  gkyl_range_iter_init(&iter, &range);
+  while (gkyl_range_iter_next(&iter)) {
+    long loc = gkyl_range_idx(&range, iter.idx);
+    double *a2d = gkyl_array_fetch(a2, loc);
+    for (int i=0; i<a1->ncomp; ++i) {
+      TEST_CHECK( a2d[i+0*a1->ncomp] == 1.5 );
+      TEST_CHECK( a2d[i+1*a1->ncomp] == 0.5*0.5 );
+      TEST_CHECK( a2d[i+2*a1->ncomp] == 1.5 );
+    }
   }  
 
   gkyl_array_release(a1);
@@ -1959,10 +2190,14 @@ TEST_LIST = {
   { "cu_array_clear_range", test_cu_array_clear_range},
   { "cu_array_accumulate", test_cu_array_accumulate},
   { "cu_array_accumulate_range", test_cu_array_accumulate_range},
+  { "cu_array_accumulate_offset", test_cu_array_accumulate_offset},
+  { "cu_array_accumulate_offset_range", test_cu_array_accumulate_offset_range},
   { "cu_array_accumulate_range_4d", test_cu_array_accumulate_range_4d  },
   { "cu_array_combine", test_cu_array_combine},
   { "cu_array_set", test_cu_array_set },
   { "cu_array_set_range", test_cu_array_set_range },
+  { "cu_array_set_offset", test_cu_array_set_offset },
+  { "cu_array_set_offset_range", test_cu_array_set_offset_range },
   { "cu_array_scale", test_cu_array_scale },
   { "cu_array_scale_by_cell", test_cu_array_scale_by_cell },
   { "cu_array_shiftc0", test_cu_array_shiftc0 },
