804828e0c987c2ebfcff217a36aa783db24481f2
ammarhakim <a.hakim777@gmail.com>
2022-09-28 20:48:21 -0400

    SSP-RK3 code in place. Dummy forward_euler. Need to fill that.

--- apps/gkyl_moment.h
+++ apps/gkyl_moment.h
@@ -91,12 +91,30 @@ struct gkyl_moment {
 // Simulation statistics
 struct gkyl_moment_stat {
   long nup; // calls to update
+  double total_tm; // time for simulation (not including ICs)
+  
   long nfail; // number of failed time-steps
 
-  double total_tm; // time for simulation (not including ICs)
+  //// wave_prop stuff
   double species_tm; // time to compute species updates
   double field_tm; // time to compute field updates
   double sources_tm; // time to compute source terms
+
+  //// stuff for MP-XX/SSP-RK schemes
+  long nfeuler; // calls to forward-Euler method
+    
+  long nstage_2_fail; // number of failed RK stage-2s
+  long nstage_3_fail; // number of failed RK stage-3s
+
+  double stage_2_dt_diff[2]; // [min,max] rel-diff for stage-2 failure
+  double stage_3_dt_diff[2]; // [min,max] rel-diff for stage-3 failure
+    
+  double init_species_tm; // time to initialize all species
+  double init_field_tm; // time to initialize fields
+
+  double species_rhs_tm; // time to compute species collisionless RHS
+  
+  double field_rhs_tm; // time to compute field RHS
 };
 
 // Object representing moments app
--- apps/mom_update_ssp_rk.c
+++ apps/mom_update_ssp_rk.c
@@ -5,12 +5,88 @@
 struct gkyl_update_status
 moment_update_ssp_rk3(gkyl_moment_app* app, double dt0)
 {
-  double dt = dt0;
-  double dt_suggested = dt0;
+  const struct gkyl_array *fin[app->num_species];
+  struct gkyl_array *fout[app->num_species];
+  struct gkyl_update_status st = { .success = true };
+
+  // time-stepper state
+  enum { RK_STAGE_1, RK_STAGE_2, RK_STAGE_3, RK_COMPLETE } state = RK_STAGE_1;
+
+  double tcurr = app->tcurr, dt = dt0;
+  while (state != RK_COMPLETE) {
+    switch (state) {
+      case RK_STAGE_1:
+        for (int i=0; i<app->num_species; ++i) {
+          fin[i] = app->species[i].f0;
+          fout[i] = app->species[i].f1;
+        }
+        //forward_euler(app, tcurr, dt, fin, fout, &st);
+	dt = st.dt_actual;
+        state = RK_STAGE_2;
+        break;
+
+      case RK_STAGE_2:
+        for (int i=0; i<app->num_species; ++i) {
+          fin[i] = app->species[i].f1;
+          fout[i] = app->species[i].fnew;
+        }
+        //forward_euler(app, tcurr+dt, dt, fin, fout, &st);
+	if (st.dt_actual < dt) {
+
+          // collect stats
+          double dt_rel_diff = (dt-st.dt_actual)/st.dt_actual;
+          app->stat.stage_2_dt_diff[0] = fmin(app->stat.stage_2_dt_diff[0],
+            dt_rel_diff);
+          app->stat.stage_2_dt_diff[1] = fmax(app->stat.stage_2_dt_diff[1],
+            dt_rel_diff);
+          app->stat.nstage_2_fail += 1;
+            
+          dt = st.dt_actual;
+          state = RK_STAGE_1; // restart from stage 1
+
+        } else {
+          for (int i=0; i<app->num_species; ++i)
+            array_combine(app->species[i].f1,
+              3.0/4.0, app->species[i].f0, 1.0/4.0, app->species[i].fnew, app->local_ext);
+
+          state = RK_STAGE_3;
+        }
+        break;
+
+      case RK_STAGE_3:
+        for (int i=0; i<app->num_species; ++i) {
+          fin[i] = app->species[i].f1;
+          fout[i] = app->species[i].fnew;
+        }
+        //forward_euler(app, tcurr+dt/2, dt, fin, fout, &st);
+        if (st.dt_actual < dt) {
+          // collect stats
+          double dt_rel_diff = (dt-st.dt_actual)/st.dt_actual;
+          app->stat.stage_3_dt_diff[0] = fmin(app->stat.stage_3_dt_diff[0],
+            dt_rel_diff);
+          app->stat.stage_3_dt_diff[1] = fmax(app->stat.stage_3_dt_diff[1],
+            dt_rel_diff);
+          app->stat.nstage_3_fail += 1;
+            
+          dt = st.dt_actual;
+          state = RK_STAGE_1; // restart from stage 1
+            
+          app->stat.nstage_2_fail += 1;
+        }
+        else {
+          for (int i=0; i<app->num_species; ++i) {
+            array_combine(app->species[i].f1,
+              1.0/3.0, app->species[i].f0, 2.0/3.0, app->species[i].fnew, app->local_ext);
+            gkyl_array_copy_range(app->species[i].f0, app->species[i].f1, app->local_ext);
+          }
+          state = RK_COMPLETE;
+        }
+        break;
+
+      case RK_COMPLETE: // can't happen: suppresses warning
+        break;
+    }
+  }
   
-  return (struct gkyl_update_status) {
-    .success = true,
-    .dt_actual = dt,
-    .dt_suggested = dt_suggested,
-  };  
+  return st;
 }
