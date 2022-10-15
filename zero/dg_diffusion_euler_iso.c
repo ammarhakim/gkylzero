29e7a304f747c3b5ec57520674a65ec7b2162d9e
Junoravin <junoravin@gmail.com>
2022-10-15 14:33:08 -0400

    Fixing isothermal Euler DG equation object so we can build on GPUs.

--- zero/dg_diffusion_euler_iso.c
+++ zero/dg_diffusion_euler_iso.c
@@ -12,53 +12,52 @@
 #define CK(lst,cdim,poly_order) lst[cdim-1].kernels[poly_order]
 
 void
-gkyl_diffusion_e_i_free(const struct gkyl_ref_count* ref)
+gkyl_diffusion_euler_iso_free(const struct gkyl_ref_count* ref)
 {
   struct gkyl_dg_eqn* base = container_of(ref, struct gkyl_dg_eqn, ref_count);
 
   if (gkyl_dg_eqn_is_cu_dev(base)) {
     // free inner on_dev object
-    struct dg_diffusion_e_i* diffusion = container_of(base->on_dev, struct dg_diffusion_e_i, eqn);
+    struct dg_diffusion_euler_iso* diffusion = container_of(base->on_dev, struct dg_diffusion_euler_iso, eqn);
     gkyl_cu_free(diffusion);
   }
 
-  struct dg_diffusion_e_i* diffusion = container_of(base, struct dg_diffusion_e_i, eqn);
+  struct dg_diffusion_euler_iso* diffusion = container_of(base, struct dg_diffusion_euler_iso, eqn);
   gkyl_free(diffusion);
 }
 
 void
-gkyl_diffusion_e_i_set_auxfields(const struct gkyl_dg_eqn* eqn, struct gkyl_dg_diffusion_e_i_auxfields auxin)
+gkyl_diffusion_euler_iso_set_auxfields(const struct gkyl_dg_eqn* eqn, struct gkyl_dg_diffusion_euler_iso_auxfields auxin)
 {
 #ifdef GKYL_HAVE_CUDA
-  if (gkyl_array_is_cu_dev(auxin.D)) { //TODO: check if this needs to be done for u_in too
-    assert(false); //TODO: implement cuda dg euler iso diffusion
-    // gkyl_diffusion_set_auxfields_cu(eqn->on_dev, auxin);
+  if (gkyl_dg_eqn_is_cu_dev(eqn)) {
+    gkyl_diffusion_euler_iso_set_auxfields_cu(eqn->on_dev, auxin);
     return;
   }
 #endif
 
-  struct dg_diffusion_e_i* diffusion = container_of(eqn, struct dg_diffusion_e_i, eqn);
-  diffusion->auxfields.D = auxin.D;
-  diffusion->auxfields.u_i = auxin.u_i;
+  struct dg_diffusion_euler_iso* diffusion_euler_iso = container_of(eqn, struct dg_diffusion_euler_iso, eqn);
+  diffusion_euler_iso->auxfields.D = auxin.D;
+  diffusion_euler_iso->auxfields.u_i = auxin.u_i;
 }
 
 struct gkyl_dg_eqn*
-gkyl_dg_diffusion_e_i_new(const struct gkyl_basis* cbasis, const struct gkyl_range* conf_range, bool use_gpu)
+gkyl_dg_diffusion_euler_iso_new(const struct gkyl_basis* cbasis, const struct gkyl_range* conf_range, bool use_gpu)
 {
 #ifdef GKYL_HAVE_CUDA
   if(use_gpu)
-    return gkyl_dg_diffusion_cu_dev_new(cbasis, conf_range);
+    return gkyl_dg_diffusion_euler_iso_cu_dev_new(cbasis, conf_range);
 #endif
 
-  struct dg_diffusion_e_i* diffusion = gkyl_malloc(sizeof(struct dg_diffusion_e_i));
+  struct dg_diffusion_euler_iso* diffusion_euler_iso = gkyl_malloc(sizeof(struct dg_diffusion_euler_iso));
 
   int cdim = cbasis->ndim;
   int poly_order = cbasis->poly_order;
 
-  const gkyl_dg_diffusion_e_i_vol_kern_list* vol_kernels;
-  const gkyl_dg_diffusion_e_i_surf_kern_list* surf_x_kernels;
-  const gkyl_dg_diffusion_e_i_surf_kern_list* surf_y_kernels;
-  const gkyl_dg_diffusion_e_i_surf_kern_list* surf_z_kernels;
+  const gkyl_dg_diffusion_euler_iso_vol_kern_list* vol_kernels;
+  const gkyl_dg_diffusion_euler_iso_surf_kern_list* surf_x_kernels;
+  const gkyl_dg_diffusion_euler_iso_surf_kern_list* surf_y_kernels;
+  const gkyl_dg_diffusion_euler_iso_surf_kern_list* surf_z_kernels;
 
   switch (cbasis->b_type) {
     case GKYL_BASIS_MODAL_SERENDIPITY:
@@ -73,38 +72,38 @@ gkyl_dg_diffusion_e_i_new(const struct gkyl_basis* cbasis, const struct gkyl_ran
       break;
   }
 
-  diffusion->eqn.num_equations = 4;
-  diffusion->eqn.vol_term = vol;
-  diffusion->eqn.surf_term = surf;
-  //diffusion->eqn.boundary_surf_term = boundary_surf;
+  diffusion_euler_iso->eqn.num_equations = 4;
+  diffusion_euler_iso->eqn.vol_term = vol;
+  diffusion_euler_iso->eqn.surf_term = surf;
+  //diffusion_euler_iso->eqn.boundary_surf_term = boundary_surf;
 
-  diffusion->vol = CK(vol_kernels, cdim, poly_order);
+  diffusion_euler_iso->vol = CK(vol_kernels, cdim, poly_order);
 
-  diffusion->surf[0] = CK(surf_x_kernels, cdim, poly_order);
+  diffusion_euler_iso->surf[0] = CK(surf_x_kernels, cdim, poly_order);
   if (cdim>1)
-    diffusion->surf[1] = CK(surf_y_kernels, cdim, poly_order);
+    diffusion_euler_iso->surf[1] = CK(surf_y_kernels, cdim, poly_order);
   if (cdim>2)
-    diffusion->surf[2] = CK(surf_z_kernels, cdim, poly_order);
+    diffusion_euler_iso->surf[2] = CK(surf_z_kernels, cdim, poly_order);
 
   // ensure non-NULL pointers
-  assert(diffusion->vol);
-  for (int i=0; i<cdim; ++i) assert(diffusion->surf[i]);
+  assert(diffusion_euler_iso->vol);
+  for (int i=0; i<cdim; ++i) assert(diffusion_euler_iso->surf[i]);
 
-  diffusion->auxfields.D = 0;
-  diffusion->auxfields.u_i = 0;
-  diffusion->conf_range = *conf_range;
+  diffusion_euler_iso->auxfields.D = 0;
+  diffusion_euler_iso->auxfields.u_i = 0;
+  diffusion_euler_iso->conf_range = *conf_range;
 
-  diffusion->eqn.flags = 0;
-  diffusion->eqn.ref_count = gkyl_ref_count_init(gkyl_diffusion_e_i_free);
-  diffusion->eqn.on_dev = &diffusion->eqn;
+  diffusion_euler_iso->eqn.flags = 0;
+  diffusion_euler_iso->eqn.ref_count = gkyl_ref_count_init(gkyl_diffusion_euler_iso_free);
+  diffusion_euler_iso->eqn.on_dev = &diffusion_euler_iso->eqn;
 
-  return &diffusion->eqn;
+  return &diffusion_euler_iso->eqn;
 }
 
 #ifndef GKYL_HAVE_CUDA
 
 struct gkyl_dg_eqn*
-gkyl_dg_diffusion_e_i_cu_dev_new(const struct gkyl_basis* cbasis, const struct gkyl_range* conf_range)
+gkyl_dg_diffusion_euler_iso_cu_dev_new(const struct gkyl_basis* cbasis, const struct gkyl_range* conf_range)
 {
   assert(false);
   return 0;
