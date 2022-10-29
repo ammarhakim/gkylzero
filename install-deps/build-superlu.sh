424b55bffe7950cb08c975c5f9cb4190634392ba
manauref <mana@dartmouth.edu>
2022-10-29 15:49:07 -0700

    Change Makefile so /regression and /unit executables are not built by default. That's because currently those executables take way too much space and if you have other stuff in your $HOME, your diskspace can easily fill up and crash the build. Now one has to build these on demand with, for example,
    make cuda-build/regression
    make cuda-build/unit
    
    or specific tests with
    
    make cuda-build/regression/rt_weibel_4d
    make cuda-build/unit/ctest_dg_bin_ops
    
    Additionally, change ```-j``` in scripts that build dependencies to
    ```-j 32``` because Stellar and Perlmutter admins don't like that we hog
    all the cores on the login node.
    
    Adapt config and make files to allow passing location of cuda math
    libraries, because on Perlmutter they are in some other nonstandard
    location. Update Perlmutter machine files with this (also had to unload
    darshan module for some reason, recommended by admins).

--- install-deps/build-superlu.sh
+++ install-deps/build-superlu.sh
@@ -32,7 +32,7 @@ then
     cmake .. -DCMAKE_C_FLAGS="-g -O3 -fPIC" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_LIBDIR=lib -Denable_tests=NO -Denable_internal_blaslib=NO -DXSDK_ENABLE_Fortran=NO
 
     # build and install
-    make -j VERBOSE=1
+    make -j 32 VERBOSE=1
     make install
 
     # soft-link 
--- machines/configure.perlmutter.dev.sh
+++ machines/configure.perlmutter.dev.sh
@@ -1,3 +1,5 @@
-./configure CC=nvcc ARCH_FLAGS="-march=native" CUDA_ARCH=80 --prefix=$HOME/gkylsoft --lapack-inc=${HOME}/gkylsoft/OpenBLAS/include --lapack-lib=${HOME}/gkylsoft/OpenBLAS/lib/libopenblas.a --superlu-inc=${HOME}/gkylsoft/superlu/include --superlu-lib=${HOME}/gkylsoft/superlu/lib/libsuperlu.a;
+module load cudatoolkit/11.7
+module unload darshan
+./configure CC=nvcc ARCH_FLAGS="-march=native" CUDA_ARCH=80 --prefix=$HOME/gkylsoft --lapack-inc=${HOME}/gkylsoft/OpenBLAS/include --lapack-lib=${HOME}/gkylsoft/OpenBLAS/lib/libopenblas.a --superlu-inc=${HOME}/gkylsoft/superlu/include --superlu-lib=${HOME}/gkylsoft/superlu/lib/libsuperlu.a --cudamath-libdir=/opt/nvidia/hpc_sdk/Linux_x86_64/22.5/math_libs/11.7/lib64;
 
 
