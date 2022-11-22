535a27374faef611fd231f4061156142ecb0418d
manauref <mfrancisquez@gmail.com>
2022-11-22 09:54:46 -0800

    Change the signature of prim_cross_m0deltas so it can be used for BGK as well. Add a prim_bgk_cross_calc function to compute cross-prim moments for BGK. Cross BGK reg tests in g2 pass with this new function (only for CPU atm).

--- zero/gkyl_prim_cross_m0deltas.h
+++ zero/gkyl_prim_cross_m0deltas.h
@@ -8,10 +8,10 @@
 typedef struct gkyl_prim_cross_m0deltas gkyl_prim_cross_m0deltas;
 
 /**
- * Create a new updater that computes the m0_s*delta_s prefactor
+ * Create a new updater that computes the m0_s*delta_s*(beta+1) prefactor
  * in the calculation of cross-primitive moments for LBO and BGK
  * collisions. That is:
- *   m0_s*delta_s = m0_s*2*m_r*m0_r*nu_rs/(m_s*m0_s*nu_sr+m_r*m0_r*nu_rs)
+ *   m0_s*delta_s*(beta+1) = m0_s*2*m_r*m0_r*nu_rs*(beta+1)/(m_s*m0_s*nu_sr+m_r*m0_r*nu_rs)
  *
  * @param basis Basis object (configuration space).
  * @param range Range in which we'll compute m0_s*delta_s.
@@ -34,14 +34,15 @@ gkyl_prim_cross_m0deltas* gkyl_prim_cross_m0deltas_new(
  * @param massother Mass of the other species, m_r.
  * @param m0other Number density of the other species, m0_r.
  * @param nuother Cross collision frequency of the other species, nu_rs.
+ * @param prem0s Prefactor, M_0s for LBO, 1 for BGK.
  * @param range Range in which we'll compute m0_s*delta_s.
  * @param out Output array.
  * @return New updater pointer.
  */
 void gkyl_prim_cross_m0deltas_advance(gkyl_prim_cross_m0deltas *up, struct gkyl_basis basis,
-  double massself, struct gkyl_array* m0self, struct gkyl_array* nuself,
-  double massother, struct gkyl_array* m0other, struct gkyl_array* nuother,
-  const struct gkyl_range *range, struct gkyl_array* out);
+  double massself, const struct gkyl_array* m0self, const struct gkyl_array* nuself,
+  double massother, const struct gkyl_array* m0other, const struct gkyl_array* nuother,
+  const struct gkyl_array* prem0s, const struct gkyl_range *range, struct gkyl_array* out);
 
 /**
  * Delete updater.
