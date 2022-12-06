a76e56b45ce7dbd9a487de1aa479d10e72cd54e5
tnbernard <tess.bernard@gmail.com>
2022-12-05 18:18:16 -0800

    Editing vlasov.c if statements so that all surface kernels are initialized properly.

--- zero/dg_vlasov.c
+++ zero/dg_vlasov.c
@@ -110,8 +110,14 @@ gkyl_dg_vlasov_new(const struct gkyl_basis* cbasis, const struct gkyl_basis* pba
       assert(false);
       break;    
   }  
-  if (field_id == GKYL_FIELD_NULL)
+  if (field_id == GKYL_FIELD_NULL) {
     vlasov->vol = CK(stream_vol_kernels,cdim,vdim,poly_order);
+    vlasov->stream_surf[0] = CK(stream_surf_x_kernels,cdim,vdim,poly_order);
+    if (cdim>1)
+      vlasov->stream_surf[1] = CK(stream_surf_y_kernels,cdim,vdim,poly_order);
+    if (cdim>2)
+      vlasov->stream_surf[2] = CK(stream_surf_z_kernels,cdim,vdim,poly_order);
+  }
   else if (field_id == GKYL_FIELD_GEN_GEO) {
     vlasov->vol = CK(stream_gen_geo_vol_kernels,cdim,vdim,poly_order);
     // Neutral gen geo will always be 3x3v.
@@ -127,19 +133,19 @@ gkyl_dg_vlasov_new(const struct gkyl_basis* cbasis, const struct gkyl_basis* pba
       vlasov->stream_surf[1] = CK(stream_surf_y_kernels,cdim,vdim,poly_order);
     if (cdim>2)
       vlasov->stream_surf[2] = CK(stream_surf_z_kernels,cdim,vdim,poly_order);
-
-    vlasov->accel_surf[0] = CK(accel_surf_vx_kernels,cdim,vdim,poly_order);
-    if (vdim>1)
-      vlasov->accel_surf[1] = CK(accel_surf_vy_kernels,cdim,vdim,poly_order);
-    if (vdim>2)
-      vlasov->accel_surf[2] = CK(accel_surf_vz_kernels,cdim,vdim,poly_order);
-
-    vlasov->accel_boundary_surf[0] = CK(accel_boundary_surf_vx_kernels,cdim,vdim,poly_order);
-    if (vdim>1)
-      vlasov->accel_boundary_surf[1] = CK(accel_boundary_surf_vy_kernels,cdim,vdim,poly_order);
-    if (vdim>2)
-      vlasov->accel_boundary_surf[2] = CK(accel_boundary_surf_vz_kernels,cdim,vdim,poly_order);
   }
+
+  vlasov->accel_surf[0] = CK(accel_surf_vx_kernels,cdim,vdim,poly_order);
+  if (vdim>1)
+    vlasov->accel_surf[1] = CK(accel_surf_vy_kernels,cdim,vdim,poly_order);
+  if (vdim>2)
+    vlasov->accel_surf[2] = CK(accel_surf_vz_kernels,cdim,vdim,poly_order);
+
+  vlasov->accel_boundary_surf[0] = CK(accel_boundary_surf_vx_kernels,cdim,vdim,poly_order);
+  if (vdim>1)
+    vlasov->accel_boundary_surf[1] = CK(accel_boundary_surf_vy_kernels,cdim,vdim,poly_order);
+  if (vdim>2)
+    vlasov->accel_boundary_surf[2] = CK(accel_boundary_surf_vz_kernels,cdim,vdim,poly_order); 
 
   // ensure non-NULL pointers
   assert(vlasov->vol);
