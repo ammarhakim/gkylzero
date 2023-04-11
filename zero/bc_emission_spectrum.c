28eca83e49da51cca4a656c3f945b00206cc5bd1
Kolter Bradshaw <kltrbs@DESKTOP-5RI9S0V>
2023-04-11 00:50:45 -0400

    Boundary condition now supports Gaussian fit.

--- zero/bc_emission_spectrum.c
+++ zero/bc_emission_spectrum.c
@@ -5,38 +5,18 @@
 #include <assert.h>
 #include <math.h>
 
-// CHANGE NEEDED - Instead of passing gain coefficients as a precalculated double array, ideally we should calculate them in initialization. If possible, I would like to merge this object with bc_emission, though attempts to do so so far have proven messy. Should the user import the SEY function, or pick it from a list and supply fitting parameters? 
-
-double
-chung_everhart_normalization(double *tot_flux, void *ctx)
-{
-  struct bc_chung_ctx *mc = (struct bc_chung_ctx*) ctx;
-  
-  double effective_gamma = mc->numerator/mc->denominator;
-  return 6.0*effective_gamma*tot_flux[0]*mc->phi*mc->phi*mc->mass/fabs(mc->charge);
-}
-
-static void 
-chung_everhart(double t, const double *xn, double *out, void *ctx)
-{
-  struct bc_chung_ctx *mc = (struct bc_chung_ctx*) ctx;
-  int cdim = mc->cdim;
-  int vdim = mc->vdim;
-  double phi = mc->phi;
-  double mass = mc->mass;
-  double charge = mc->charge;
-  double E = 0.0;
-  for (int d=0; d<vdim; d++) {
-    E += 0.5*mass*xn[cdim+d]*xn[cdim+d]/fabs(charge);
-  }
-  out[0] = E/((E + phi)*(E + phi)*(E + phi)*(E + phi));
-}
+// CHANGE NEEDED - Instead of passing gain coefficients as a precalculated double array,
+// ideally we should calculate them in initialization.
+// Should the user import the SEY function, or pick it from a list and supply fitting parameters? 
+// If possible, I would like to merge this object with bc_emission as they have overall similar logic,
+// though attempts to do so have proven messy.
 
 struct gkyl_bc_emission_spectrum*
 gkyl_bc_emission_spectrum_new(struct gkyl_rect_grid *grid,
   int dir, enum gkyl_edge_loc edge, const struct gkyl_range *local_conf_range_ext,
-  const struct gkyl_range *local_range_ext, const int *num_ghosts, const struct gkyl_basis *cbasis,
-  const struct gkyl_basis *basis, int cdim, int vdim, const double* bc_param, double *gain,
+  const struct gkyl_range *local_range_ext, const int *num_ghosts,
+  enum gkyl_bc_emission_spectrum_type bctype, const struct gkyl_basis *cbasis,
+  const struct gkyl_basis *basis, int cdim, int vdim, double* bc_param, double *gain,
   double *elastic, bool use_gpu)
 {
   // Allocate space for new updater.
@@ -59,6 +39,7 @@ gkyl_bc_emission_spectrum_new(struct gkyl_rect_grid *grid,
   up->use_gpu = use_gpu;
   up->cbasis = cbasis;
   up->grid = grid;
+  up->func = bc_weighted_gamma;
 
   // Create the skin/ghost ranges.
   gkyl_skin_ghost_ranges(&up->skin_r, &up->ghost_r, dir, edge,
@@ -68,23 +49,36 @@ gkyl_bc_emission_spectrum_new(struct gkyl_rect_grid *grid,
   gkyl_pos_neg_ranges(&up->pos_r, &up->neg_r, dir, vdir, edge,
     local_range_ext, num_ghosts, num_pos, num_neg);
 
-  struct bc_chung_ctx *ctx = gkyl_malloc(sizeof(*ctx));
-  up->func = bc_chung_everhart;
+  switch (bctype) {
+    case GKYL_BC_CHUNG_EVERHART:
+      up->proj = chung_everhart_proj;
+      up->norm = chung_everhart_norm;
+      break;
+
+    case GKYL_BC_GAUSSIAN:
+      up->proj = gaussian_proj;
+      up->norm = gaussian_norm;
+      break;
+      
+    default:
+      assert(false);
+      break;
+  }
+
+  struct bc_emission_spectrum_ctx *ctx = gkyl_malloc(sizeof(*ctx));
   ctx->basis = basis;
   ctx->dir = dir;
   ctx->cdim = cdim;
   ctx->vdim = vdim;
   ctx->gain = gain;
   ctx->elastic = elastic;
-  ctx->mass = bc_param[7];
-  ctx->charge = bc_param[8];
-  ctx->phi = bc_param[0];
   ctx->edge = edge;
+  ctx->param = bc_param;
 
   // Projection of emission spectrum
   up->f_proj = gkyl_array_new(GKYL_DOUBLE, basis->num_basis, local_range_ext->volume);
   up->boundary_proj = gkyl_proj_on_basis_new(grid, basis,
-    basis->poly_order+1, 1, chung_everhart, ctx);
+    basis->poly_order+1, 1, up->proj, ctx);
   gkyl_proj_on_basis_advance(up->boundary_proj, 0.0, &up->ghost_r, up->f_proj);
 
   // Moment calculator for fluxes
@@ -100,12 +94,15 @@ gkyl_bc_emission_spectrum_new(struct gkyl_rect_grid *grid,
 }
 
 void
-gkyl_bc_emission_spectrum_advance(const struct gkyl_bc_emission_spectrum *up, struct gkyl_array *buff_arr, struct gkyl_array *f_arr)
+gkyl_bc_emission_spectrum_advance(const struct gkyl_bc_emission_spectrum *up,
+  struct gkyl_array *buff_arr, struct gkyl_array *f_arr)
 {
   if (up->edge == GKYL_LOWER_EDGE) {
-    gkyl_dg_updater_moment_advance(up->mcalc, &up->neg_r, &up->cskin_r, NULL, NULL, NULL, NULL, NULL, NULL, f_arr, up->flux);
+    gkyl_dg_updater_moment_advance(up->mcalc, &up->neg_r, &up->cskin_r, NULL, NULL,
+      NULL, NULL, NULL, NULL, f_arr, up->flux);
   } else {
-    gkyl_dg_updater_moment_advance(up->mcalc, &up->pos_r, &up->cskin_r, NULL, NULL, NULL, NULL, NULL, NULL, f_arr, up->flux);
+    gkyl_dg_updater_moment_advance(up->mcalc, &up->pos_r, &up->cskin_r, NULL, NULL,
+      NULL, NULL, NULL, NULL, f_arr, up->flux);
   }
   struct gkyl_range_iter iter;
   double tot_flux[1] = {0.0};
@@ -134,7 +131,7 @@ gkyl_bc_emission_spectrum_advance(const struct gkyl_bc_emission_spectrum *up, st
     up->func(out, inp, up->ctx, iter.idx, xc);
     count += 1;
   }
-  double k = chung_everhart_normalization(tot_flux, up->ctx);
+  double k = up->norm(tot_flux, up->ctx);
 
   gkyl_array_copy_from_buffer(f_arr, buff_arr->data, up->ghost_r);
   gkyl_array_accumulate_range(f_arr, k, up->f_proj, up->ghost_r);
--- zero/gkyl_bc_emission_spectrum_priv.h
+++ zero/gkyl_bc_emission_spectrum_priv.h
@@ -10,11 +10,15 @@
 #include <assert.h>
 
 typedef void (*emission_spectrum_func_t)(double *out, const double *inp, void *ctx, int idx[GKYL_MAX_DIM], double xc[GKYL_MAX_DIM]);
+typedef void (*emission_spectrum_proj_func_t)(double t, const double *xn, double *out, void *ctx);
+typedef double (*emission_spectrum_norm_func_t)(double *tot_flux, void *ctx);
 
 struct gkyl_bc_emission_spectrum {
   int dir, cdim;
   enum gkyl_edge_loc edge;
   emission_spectrum_func_t func;
+  emission_spectrum_proj_func_t proj;
+  emission_spectrum_norm_func_t norm;
   struct gkyl_rect_grid *grid; // grid object
   void *ctx;
   void *ctx_on_dev;
@@ -28,23 +32,22 @@ struct gkyl_bc_emission_spectrum {
   bool use_gpu;
 };
 
-struct bc_chung_ctx {
+struct bc_emission_spectrum_ctx {
   int dir; // direction for BCs.
   int cdim, vdim; // config-space dimensions.
   double *gain;
   double *elastic;
-  double mass, charge, phi, k;
+  double *param;
   double numerator, denominator;
   enum gkyl_edge_loc edge;
   const struct gkyl_basis *basis; // basis function.
 };
 
-
 GKYL_CU_D
 static void
-bc_chung_everhart(double *out, const double *inp, void *ctx, int idx[GKYL_MAX_DIM], double xc[GKYL_MAX_DIM])
+bc_weighted_gamma(double *out, const double *inp, void *ctx, int idx[GKYL_MAX_DIM], double xc[GKYL_MAX_DIM])
 {
-  struct bc_chung_ctx *mc = (struct bc_chung_ctx*) ctx;
+  struct bc_emission_spectrum_ctx *mc = (struct bc_emission_spectrum_ctx*) ctx;
   int cdim = mc->cdim;
   int dir = mc->dir;
   int nbasis = mc->basis->num_basis;
@@ -60,4 +63,74 @@ bc_chung_everhart(double *out, const double *inp, void *ctx, int idx[GKYL_MAX_DI
     mc->numerator += fabs(xc[cdim+dir])*inp[0]*mc->gain[idx[1] - 1]; // CHANGE NEEDED - store coefficients in gkyl_array and index more rigorously
     mc->denominator += fabs(xc[cdim+dir])*inp[0];
   }
+}
+
+GKYL_CU_D
+double
+chung_everhart_norm(double *tot_flux, void *ctx)
+{
+  struct bc_emission_spectrum_ctx *mc = (struct bc_emission_spectrum_ctx*) ctx;
+
+  double phi = mc->param[0];
+  double mass = mc->param[1];
+  double charge = mc->param[2];
+  
+  double effective_gamma = mc->numerator/mc->denominator;
+  
+  return 6.0*effective_gamma*tot_flux[0]*phi*phi*mass/fabs(charge);
+}
+
+GKYL_CU_D
+static void 
+chung_everhart_proj(double t, const double *xn, double *out, void *ctx)
+{
+  struct bc_emission_spectrum_ctx *mc = (struct bc_emission_spectrum_ctx*) ctx;
+
+  int cdim = mc->cdim;
+  int vdim = mc->vdim;
+  double phi = mc->param[0];
+  double mass = mc->param[1];
+  double charge = mc->param[2];
+  
+  double E = 0.0;
+  for (int d=0; d<vdim; d++) {
+    E += 0.5*mass*xn[cdim+d]*xn[cdim+d]/fabs(charge);
+  }
+  out[0] = E/((E + phi)*(E + phi)*(E + phi)*(E + phi));
+}
+
+GKYL_CU_D
+double
+gaussian_norm(double *tot_flux, void *ctx)
+{
+  struct bc_emission_spectrum_ctx *mc = (struct bc_emission_spectrum_ctx*) ctx;
+
+  double E_0 = mc->param[0];
+  double tau = mc->param[1];
+  double mass = mc->param[2];
+  double charge = mc->param[3];
+  
+  double effective_gamma = mc->numerator/mc->denominator;
+  
+  return effective_gamma*tot_flux[0]*mass/(sqrt(2.0*M_PI)*E_0*tau*exp(tau*tau/2.0)*fabs(charge));
+}
+
+GKYL_CU_D
+static void 
+gaussian_proj(double t, const double *xn, double *out, void *ctx)
+{
+  struct bc_emission_spectrum_ctx *mc = (struct bc_emission_spectrum_ctx*) ctx;
+  
+  int cdim = mc->cdim;
+  int vdim = mc->vdim;
+  double E_0 = mc->param[0];
+  double tau = mc->param[1];
+  double mass = mc->param[2];
+  double charge = mc->param[3];
+  
+  double E = 0.0;
+  for (int d=0; d<vdim; d++) {
+    E += 0.5*mass*xn[cdim+d]*xn[cdim+d]/fabs(charge);
+  }
+  out[0] = exp(-pow(log(E/E_0), 2)/(2.0*tau*tau));
 }
