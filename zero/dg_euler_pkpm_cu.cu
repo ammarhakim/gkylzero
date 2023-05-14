db28d8959e81bd5c9374842a916704cf3f1b3e42
Junoravin <junoravin@gmail.com>
2023-05-14 10:02:42 -0400

    Forgot to add vlasov_pkpm_moms to Euler PKPM CUDA file

--- zero/dg_euler_pkpm_cu.cu
+++ zero/dg_euler_pkpm_cu.cu
@@ -16,9 +16,11 @@ extern "C" {
 // and so its members cannot be modified without a full __global__ kernel on device.
 __global__ static void
 gkyl_euler_pkpm_set_auxfields_cu_kernel(const struct gkyl_dg_eqn *eqn, 
-  const struct gkyl_array *u_i, const struct gkyl_array *div_p, const struct gkyl_array *vth_sq)
+  const struct gkyl_array *vlasov_pkpm_moms, const struct gkyl_array *u_i, 
+  const struct gkyl_array *div_p, const struct gkyl_array *vth_sq)
 {
   struct dg_euler_pkpm *euler_pkpm = container_of(eqn, struct dg_euler_pkpm, eqn);
+  euler_pkpm->auxfields.vlasov_pkpm_moms = vlasov_pkpm_moms;
   euler_pkpm->auxfields.u_i = u_i;
   euler_pkpm->auxfields.div_p = div_p;
   euler_pkpm->auxfields.vth_sq = vth_sq;
@@ -28,12 +30,14 @@ gkyl_euler_pkpm_set_auxfields_cu_kernel(const struct gkyl_dg_eqn *eqn,
 void
 gkyl_euler_pkpm_set_auxfields_cu(const struct gkyl_dg_eqn *eqn, struct gkyl_dg_euler_pkpm_auxfields auxin)
 {
-  gkyl_euler_pkpm_set_auxfields_cu_kernel<<<1,1>>>(eqn, auxin.u_i->on_dev, auxin.div_p->on_dev, auxin.vth_sq->on_dev);
+  gkyl_euler_pkpm_set_auxfields_cu_kernel<<<1,1>>>(eqn, auxin.vlasov_pkpm_moms->on_dev, auxin.u_i->on_dev, 
+    auxin.div_p->on_dev, auxin.vth_sq->on_dev);
 }
 
 __global__ void static
 dg_euler_pkpm_set_cu_dev_ptrs(struct dg_euler_pkpm* euler_pkpm, enum gkyl_basis_type b_type, int cdim, int poly_order)
 {
+  euler_pkpm->auxfields.vlasov_pkpm_moms = 0; 
   euler_pkpm->auxfields.u_i = 0; 
   euler_pkpm->auxfields.div_p = 0;
   euler_pkpm->auxfields.vth_sq = 0; 
