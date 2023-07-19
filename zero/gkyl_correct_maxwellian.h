017222e512d0a42202fc125f7e8c7c2ef7252ab3
Dingyun Liu <dingyunl@stellar-amd.princeton.edu>
2023-07-19 11:42:16 -0400

    Header file for the maxwellian fix updater.

--- zero/gkyl_correct_maxwellian.h
+++ zero/gkyl_correct_maxwellian.h
@@ -15,27 +15,40 @@ typedef struct gkyl_correct_maxwellian gkyl_correct_maxwellian;
  * @param grid Grid on which updater lives
  * @param conf_basis Conf space basis functions
  * @param phase_basis Phase space basis functions
- * @param conf_local_cells Number of cells in local config-space
- * @param conf_local_ext_cells Number of cells in local extended config-space
+ * @param conf_local Local configuration space range
+ * @param conf_local_ext Local extended configuration space range
+ * @param mass Mass of the species
+ * @param use_gpu Bool to determine if on GPU
  */
 gkyl_correct_maxwellian *gkyl_correct_maxwellian_new(
-  const struct gkyl_rect_grid *grid, const struct gkyl_basis *conf_basis,
-  const struct gkyl_basis *phase_basis,
-  long conf_local_ncells, long conf_local_ext_ncells);
+  const struct gkyl_rect_grid *grid, 
+  const struct gkyl_basis *conf_basis, const struct gkyl_basis *phase_basis, 
+  const struct gkyl_range *conf_local, const struct gkyl_range *conf_local_ext,
+  double mass, bool use_gpu);
 
 /**
  * Fix the Maxwellian so that it's moments match desired moments.
  *
- * @param cmax Maxwell correction updater
- * @param fout Distribution function to fix (modified in-place)
- * @param m0 Desired number density
+ * @param cmax Maxwellian-fix updater
+ * @param fM Distribution function to fix (modified in-place)
+ * @param m0_corr Desired number density
+ * @param m1_corr Desired moment density
+ * @param m2_corr Desired kinetic energy density
+ * @param conf_local Local configuration space range
+ * @param conf_local_ext Local extended configuration space range
  * @param phase_local Local phase-space range
  * @param conf_local Local configuration space range
+ * @param poly_order Polynomial order of basis functions
+ * @param use_gpu Bool to determine if on gpu 
  */
 void gkyl_correct_maxwellian_fix(gkyl_correct_maxwellian *cmax,
-  struct gkyl_array *fout,
-  const struct gkyl_array *m0,
-  const struct gkyl_range *phase_local, const struct gkyl_range *conf_local);
+  struct gkyl_array *fM, 
+  const struct gkyl_array *m0_corr, const struct gkyl_array *m1_corr, const struct gkyl_array *m2_corr,
+  const struct gkyl_array *bmag, double mass,
+  double err_max, int iter_max,
+  const struct gkyl_range *conf_local, const struct gkyl_range *conf_local_ext, 
+  const struct gkyl_range *phase_local, 
+  int poly_order, bool use_gpu);
 
 /**
  * Delete updater.
