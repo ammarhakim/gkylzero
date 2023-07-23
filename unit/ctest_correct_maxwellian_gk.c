4d6afa25057d70349fc471e558c3e6a9071769a6
dingyunl <dingyunl@princeton.edu>
2023-07-23 12:05:03 -0400

    Tests passd for 1x1v case.

--- unit/ctest_correct_maxwellian_gk.c
+++ unit/ctest_correct_maxwellian_gk.c
@@ -69,31 +69,16 @@ void eval_M0(double t, const double *xn, double *restrict fout, void *ctx)
   double x = xn[0];
   fout[0] = 1.0;
 }
-void eval_M0_null(double t, const double *xn, double *restrict fout, void *ctx)
-{
-  double x = xn[0];
-  fout[0] = 0.0;
-}
 void eval_M1(double t, const double *xn, double *restrict fout, void *ctx)
 {
   double x = xn[0];
-  fout[0] = 0.5;
-}
-void eval_M1_null(double t, const double *xn, double *restrict fout, void *ctx)
-{
-  double x = xn[0];
-  fout[0] = 0.0;
+  fout[0] = 0.8;
 }
 void eval_M2(double t, const double *xn, double *restrict fout, void *ctx)
 {
-  double T = 1.0;
+  double T = 2.0;
   double x = xn[0];
   fout[0] = T;
-}
-void eval_M2_null(double t, const double *xn, double *restrict fout, void *ctx)
-{
-  double x = xn[0];
-  fout[0] = 0.0;
 }
 void eval_bmag_1x(double t, const double *xn, double* restrict fout, void *ctx)
 {
@@ -112,8 +97,8 @@ void test_1x1v(int poly_order, bool use_gpu)
 {
   double mass = 1.0;
   double err_max = 1e-14, iter_max = 50;
-  double lower[] = {0.1, -10.0}, upper[] = {1.0, 10.0};
-  int cells[] = {2, 32};
+  double lower[] = {-0.5, -10.0}, upper[] = {0.5, 10.0};
+  int cells[] = {2, 8};
   int vdim = 1, cdim = 1;
   int ndim = cdim + vdim;
 
@@ -186,6 +171,15 @@ void test_1x1v(int poly_order, bool use_gpu)
   gkyl_proj_on_basis_advance(proj_m0, 0.0, &confLocal, m0_corr);
   gkyl_proj_on_basis_advance(proj_m1, 0.0, &confLocal, m1_corr);
   gkyl_proj_on_basis_advance(proj_m2, 0.0, &confLocal, m2_corr);
+  // Write the input moments
+  char fname_m0_in[1024], fname_m1_in[1024], fname_m2_in[1024];
+  sprintf(fname_m0_in, "ctest_correct_maxwellian_1x1v_p%d_m0_in.gkyl", poly_order);
+  sprintf(fname_m1_in, "ctest_correct_maxwellian_1x1v_p%d_m1_in.gkyl", poly_order);
+  sprintf(fname_m2_in, "ctest_correct_maxwellian_1x1v_p%d_m2_in.gkyl", poly_order);
+  gkyl_grid_sub_array_write(&confGrid, &confLocal, m0_corr, fname_m0_in);
+  gkyl_grid_sub_array_write(&confGrid, &confLocal, m1_corr, fname_m1_in);
+  gkyl_grid_sub_array_write(&confGrid, &confLocal, m2_corr, fname_m2_in);
+
 
   // Project the Maxwellian on basis
   struct gkyl_array *fM;
@@ -199,7 +193,7 @@ void test_1x1v(int poly_order, bool use_gpu)
   struct gkyl_array *moms;
   if (use_gpu)
   { // (2) copy host array to device
-    moms = gkyl_array_cu_dev_new(GKYL_DOUBLE, (vdim+2)*confBasis.num_basis, confLocal_ext.volume);
+    moms = gkyl_array_cu_dev_new(GKYL_DOUBLE, 3*confBasis.num_basis, confLocal_ext.volume);
     gkyl_array_copy(moms, moms_ho);
   }
   else
@@ -208,7 +202,7 @@ void test_1x1v(int poly_order, bool use_gpu)
   }
   gkyl_proj_gkmaxwellian_on_basis_lab_mom(proj_maxwellian, &local, &confLocal, moms, bmag, jacob_tot, mass, fM);  
 
-  // Calculate the moments
+  // Calculate the uncorrected moments
   struct gkyl_array *m0, *m1, *m2; 
   m0 = mkarr(confBasis.num_basis, confLocal_ext.volume);
   m1 = mkarr(confBasis.num_basis, confLocal_ext.volume);
@@ -225,16 +219,41 @@ void test_1x1v(int poly_order, bool use_gpu)
   gkyl_mom_calc_advance(m0calc, &local, &confLocal, fM, m0);
   gkyl_mom_calc_advance(m1calc, &local, &confLocal, fM, m1);
   gkyl_mom_calc_advance(m2calc, &local, &confLocal, fM, m2);
+  // Write the uncorrected moments  
+  char fname_fM_ic[1024];
+  sprintf(fname_fM_ic, "ctest_correct_maxwellian_1x1v_p%d.gkyl", poly_order);
+  gkyl_grid_sub_array_write(&grid, &local, fM, fname_fM_ic);
+  char fname_m0_ic[1024], fname_m1_ic[1024], fname_m2_ic[1024];
+  sprintf(fname_m0_ic, "ctest_correct_maxwellian_1x1v_p%d_m0_ic.gkyl", poly_order);
+  sprintf(fname_m1_ic, "ctest_correct_maxwellian_1x1v_p%d_m1_ic.gkyl", poly_order);
+  sprintf(fname_m2_ic, "ctest_correct_maxwellian_1x1v_p%d_m2_ic.gkyl", poly_order);
+  gkyl_grid_sub_array_write(&confGrid, &confLocal, m0, fname_m0_ic);
+  gkyl_grid_sub_array_write(&confGrid, &confLocal, m1, fname_m1_ic);
+  gkyl_grid_sub_array_write(&confGrid, &confLocal, m2, fname_m2_ic);
 
   // Create a Maxwellian with corrected moments
   gkyl_correct_maxwellian *corr_max = gkyl_correct_maxwellian_new(&grid, &confBasis, &basis, &confLocal, &confLocal_ext, bmag, mass, false);
   gkyl_correct_maxwellian_gyrokinetic(corr_max, fM, m0_corr, m1_corr, m2_corr, jacob_tot, bmag, mass, err_max, iter_max, &confLocal, &local, &confLocal_ext, poly_order, false);
   gkyl_correct_maxwellian_release(corr_max);
 
+  // Calculate the corrected moments
+  gkyl_array_clear(m0, 0.0);
+  gkyl_array_clear(m1, 0.0);
+  gkyl_array_clear(m2, 0.0);
+  gkyl_mom_calc_advance(m0calc, &local, &confLocal, fM, m0);
+  gkyl_mom_calc_advance(m1calc, &local, &confLocal, fM, m1);
+  gkyl_mom_calc_advance(m2calc, &local, &confLocal, fM, m2);
   // Write the output
-  char fname[1024];
-  sprintf(fname, "ctest_correct_maxwellian_1x1v_p%d.gkyl", poly_order);
-  gkyl_grid_sub_array_write(&grid, &local, fM, fname);
+  char fname_fM_corr[1024];
+  sprintf(fname_fM_corr, "ctest_correct_maxwellian_1x1v_p%d_corr.gkyl", poly_order);
+  gkyl_grid_sub_array_write(&grid, &local, fM, fname_fM_corr);
+  char fname_m0_corr[1024], fname_m1_corr[1024], fname_m2_corr[1024];
+  sprintf(fname_m0_corr, "ctest_correct_maxwellian_1x1v_p%d_m0_corr.gkyl", poly_order);
+  sprintf(fname_m1_corr, "ctest_correct_maxwellian_1x1v_p%d_m1_corr.gkyl", poly_order);
+  sprintf(fname_m2_corr, "ctest_correct_maxwellian_1x1v_p%d_m2_corr.gkyl", poly_order);
+  gkyl_grid_sub_array_write(&confGrid, &confLocal, m0, fname_m0_corr);
+  gkyl_grid_sub_array_write(&confGrid, &confLocal, m1, fname_m1_corr);
+  gkyl_grid_sub_array_write(&confGrid, &confLocal, m2, fname_m2_corr);
 
   // For higher polynomial order basis functions
   /*
@@ -253,14 +272,66 @@ void test_1x1v(int poly_order, bool use_gpu)
   gkyl_proj_on_basis_release(proj_m0);
   gkyl_proj_on_basis_release(proj_m1);
   gkyl_proj_on_basis_release(proj_m2);
+  gkyl_mom_calc_release(m0calc);
+  gkyl_mom_calc_release(m1calc);
+  gkyl_mom_calc_release(m2calc);
 }
+/*
+void test_1x2v(int poly_order, bool use_gpu)
+{
+  double mass = 1.0;
+  double err_max = 1e-14, iter_max = 50;
+  double lower[] = {0.1, -10.0}, upper[] = {1.0, 10.0};
+  int cells[] = {2, 32};
+  int vdim = 2, cdim = 1;
+  int ndim = cdim + vdim;
 
+  double confLower[] = {lower[0]}, confUpper[] = {upper[0]};
+  double velLower[] = {lower[1]}, velUpper[] = {upper[1]};
+  int confCells[] = {cells[0]};
+  int velCells[] = {cells[1]};
+
+  // Grids
+  struct gkyl_rect_grid grid;
+  gkyl_rect_grid_init(&grid, ndim, lower, upper, cells);
+  struct gkyl_rect_grid confGrid;
+  gkyl_rect_grid_init(&confGrid, cdim, confLower, confUpper, confCells);
+  struct gkyl_rect_grid velGrid;
+  gkyl_rect_grid_init(&velGrid, vdim, velLower, velUpper, velCells);
+
+  // Velocity space range
+  int velGhost[] = {0};
+  struct gkyl_range velLocal, velLocal_ext;
+  gkyl_create_grid_ranges(&velGrid, velGhost, &velLocal_ext, &velLocal);
+
+  // Basis functions
+  struct gkyl_basis basis, confBasis, velBasis;
+  gkyl_cart_modal_serendip(&basis, ndim, poly_order);
+  gkyl_cart_modal_serendip(&confBasis, cdim, poly_order);
+  gkyl_cart_modal_serendip(&velBasis, vdim, poly_order);
+
+  // Configuration space range
+  int confGhost[] = {1};
+  struct gkyl_range confLocal, confLocal_ext;
+  gkyl_create_grid_ranges(&confGrid, confGhost, &confLocal_ext, &confLocal);
+  struct skin_ghost_ranges confSkin_ghost;
+  skin_ghost_ranges_init(&confSkin_ghost, &confLocal_ext, confGhost);
+
+  // Phase space range
+  int ghost[] = {confGhost[0], 0};
+  struct gkyl_range local, local_ext;
+  gkyl_create_grid_ranges(&grid, ghost, &local_ext, &local);
+  struct skin_ghost_ranges skin_ghost;
+  skin_ghost_ranges_init(&skin_ghost, &local_ext, ghost);
+
+}
+*/
 // Run the test
 void test_1x1v_p1() {test_1x1v(1, false);}
-void test_1x1v_p2() {test_1x1v(2, false);}
+//void test_1x1v_p2() {test_1x1v(2, false);}
 
 TEST_LIST = {
   {"test_1x1v_p1", test_1x1v_p1},
-  {"test_1x1v_p2", test_1x1v_p2},
+//  {"test_1x1v_p2", test_1x1v_p2},
   {NULL, NULL},
 };
