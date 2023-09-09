71fcd4a65c61b8aa9a2c91ca69671228607ea2d5
manauref <mfrancisquez@gmail.com>
2023-09-09 16:39:01 -0700

    Fixing similar errors in fem_poisson_perp. This exercise has made me realize a potential reason for the periodic case to not work in fem_poisson_perp: the size of rhs_cellavg is wrong because it is likely using local instead of local_ext. I will fix this in another branch, where I'll in fact move the calculation of rhs_cellavg out of fem_poisson.

--- zero/fem_poisson_perp.c
+++ zero/fem_poisson_perp.c
@@ -34,7 +34,7 @@ gkyl_fem_poisson_perp_new(const struct gkyl_range *solve_range, const struct gky
     gkyl_array_copy(kSq_ho, kSq);
   } else {
     up->ishelmholtz = false;
-    kSq_ho = gkyl_array_new(GKYL_DOUBLE, 1, 1);
+    kSq_ho = gkyl_array_new(GKYL_DOUBLE, up->num_basis, 1);
     gkyl_array_clear(kSq_ho, 0.);
   }
 
@@ -367,7 +367,7 @@ void gkyl_fem_poisson_perp_release(struct gkyl_fem_poisson_perp *up)
   gkyl_superlu_prob_release(up->prob);
 #endif
 
-  gkyl_free(up->brhs);
+  gkyl_array_release(up->brhs);
   gkyl_free(up->kernels);
   gkyl_free(up->perp_range);
   gkyl_free(up->globalidx);
