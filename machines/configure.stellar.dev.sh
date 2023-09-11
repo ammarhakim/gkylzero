b92f594eab5f940804c2716dd0967b8a83151cf3
Dingyun Liu <dingyunl@princeton.edu>
2023-09-11 15:59:40 -0400

    Modify the prefix.

--- machines/configure.stellar.dev.sh
+++ machines/configure.stellar.dev.sh
@@ -1,4 +1,4 @@
 export GKYLSOFT=$HOME/gkylsoft-amd
-./configure CC=nvcc ARCH_FLAGS="-march=native" CUDA_ARCH=80 --prefix=$HOME/gkylsoft-amd --lapack-inc=$GKYLSOFT/OpenBLAS/include --lapack-lib=$GKYLSOFT/OpenBLAS/lib/libopenblas.a --superlu-inc=$GKYLSOFT/superlu/include --superlu-lib=$GKYLSOFT/superlu/lib/libsuperlu.a;
+./configure CC=nvcc ARCH_FLAGS="-march=native" CUDA_ARCH=80 --prefix=$GKYLSOFT --lapack-inc=$GKYLSOFT/OpenBLAS/include --lapack-lib=$GKYLSOFT/OpenBLAS/lib/libopenblas.a --superlu-inc=$GKYLSOFT/superlu/include --superlu-lib=$GKYLSOFT/superlu/lib/libsuperlu.a;
 
 
