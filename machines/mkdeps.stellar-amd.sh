eface81d457b77c39bb0ed03e9f109f7cea5267b
Manaure Francisquez <mfrancisquez@gmail.com>
2023-09-19 01:58:11 -0400

    Fix left over paths.

--- machines/mkdeps.stellar-amd.sh
+++ machines/mkdeps.stellar-amd.sh
@@ -1,3 +1,3 @@
 module load cudatoolkit/12.0
 cd install-deps
-./mkdeps.sh --build-openblas=yes --build-superlu=yes --prefix=$HOME/gkeyll/code/g0g2/gkylsoft-amd/
+./mkdeps.sh --build-openblas=yes --build-superlu=yes --prefix=$HOME/gkylsoft-amd/
