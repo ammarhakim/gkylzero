0c6821846c70fb3010f7238fdc9ef9e2ef6cb79a
Akash Shukla <akashukla@utexas.edu>
2023-10-04 13:21:07 -0500

    use a single value of cmag for each yz plane instead of the nodal values of the center cell. This was the intention, it was implemented incorrectly originally. Goes with gkylcas:metric-calc 12a6b4d..ad69c52

--- kernels/derived_geo/gkyl_calc_derived_geo_kernels.h
+++ kernels/derived_geo/gkyl_calc_derived_geo_kernels.h
@@ -4,7 +4,7 @@
 EXTERN_C_BEG 
 
 GKYL_CU_DH void derived_geo_3x_Ser_p1(const double *gij, const double *bmag, double *J, double *Jinv, double *grij, double *bi, double *cmag, double *Jtot, double *Jtotinv, double *bmaginv, double *bmaginvsq, double *gxxJ, double *gxyJ, double *gyyJ); 
-GKYL_CU_DH void adjust_bmag_3x_Ser_p1(const double *cmag, double *gzz, const double *J, const double *bmag, double *gij); 
+GKYL_CU_DH void adjust_bmag_3x_Ser_p1(double cmag, double *gzz, const double *J, const double *bmag, double *gij); 
 GKYL_CU_DH void derived_geo_3x_Ser_p2(const double *gij, const double *bmag, double *J, double *Jinv, double *grij, double *bi, double *cmag, double *Jtot, double *Jtotinv, double *bmaginv, double *bmaginvsq, double *gxxJ, double *gxyJ, double *gyyJ); 
-GKYL_CU_DH void adjust_bmag_3x_Ser_p2(const double *cmag, double *gzz, const double *J, const double *bmag, double *gij); 
+GKYL_CU_DH void adjust_bmag_3x_Ser_p2(double cmag, double *gzz, const double *J, const double *bmag, double *gij); 
 EXTERN_C_END 
