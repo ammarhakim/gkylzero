b61ff357ea0cf9bd4aea1fe24a1c1c9aa1be5f44
johnson452 <grj@princeton.edu>
2023-10-28 03:06:56 -0400

    Fixed compilations errors

--- zero/gkyl_wv_cold_sr_fluid.h
+++ zero/gkyl_wv_cold_sr_fluid.h
@@ -1,7 +1,7 @@
 #pragma once
 
+#include <gkyl_wv_eqn.h>
 #include <gkyl_math.h>
-#include <gkyl_wv_sr_eqn.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,110 +14,12 @@
  * 
  * @return Pointer to cold-relativistic-fluid equation object.
  */
-struct gkyl_wv_sr_eqn* gkyl_wv_cold_sr_fluid_new(void);
+struct gkyl_wv_eqn* gkyl_wv_cold_sr_fluid_new(void);
 
-
-/**
- * Compute the roe-averaged velocity for the sr-cold fluid equations, solved via
- * Ridders' method.
- * 
- * This requries 3 velocity dimensions, and 
- * A. Assumes we are calculating the first index 
- * interface (for instance the direction along Vx for Ux, Uy, Uz)
- * B. Assumes density has already been checked and ql[0] > 0, and qr[0] > 0
- * C. If all velocities are zero, the calculation returns zero
- * D. Non gaurds against div by zero in case_#(...) functions
- * E. c is defined as one.
- * 
- * v_result can be obtained by simple relations because of the contraints in the NL
- * solve
- *
- * @param ql Conserved variables (left)
- * @param qr Conserved variables (right)
- */
-static double
-compute_sr_roe_avereged_velocity_via_ridders(const double ql[4], const double qr[4]) 
-{
-
-  // speed of light
-  double c = 1.0;
-
-  // Isolate varaibles (right/left)
-  double rhoR = qr[0];
-  double uRx = qr[1]/qr[0];
-  double uRy = qr[2]/qr[0];
-  double uRz = qr[3]/qr[0];
-  double rhoL = ql[0];
-  double uLx = ql[1]/ql[0];
-  double uLy = ql[2]/ql[0];
-  double uLz = ql[3]/ql[0];
-
-  // Compute the constants:
-  double dp = rhoR - rhoL;
-  double a = rhoL*vLx - rhoR*vRx;
-  double Ax = - rhoL*vLx*uLx + rhoR*vRx*uRx;
-  double Ay = - rhoL*vLx*uLy + rhoR*vRx*uRy;
-  double Az = - rhoL*vLx*uLz + rhoR*vRx*uRz;
-  double Bx = rhoL*uLx - rhoR*uRx;
-  double By = rhoL*uLy - rhoR*uRy;
-  double Bz = rhoL*uLz - rhoR*uRz;
-  double consts[] = {a,Ay,dp,By,Ax,Bx,Az,Bz,c};
-  double x1, x2;
-  double v_result;
-
-  //Case 1: Ux /= 0, Uy == 0, Uz == 0, 
-  if (uLz == 0.0 && uRz == 0.0 && uLy == 0.0 && uRy == 0.0) {
-
-    // Compute Ridders, case 1
-    (uLx > uRx) ? (x2 = uLx, x1 = uRx) : (x1 = uLx, x2 = uRx);
-    double f1 = case_1(x1, consts), f2 = case_1(x2, consts);
-    struct gkyl_qr_res res = gkyl_ridders(case_1, consts, x1, x2, f1, f2, 100, 1e-12);
-    double Ux = res.res;
-    v_result = (-Ux*a - Ax)/(Ux*dp + Bx);
-
-  // Case 2: Ux /= 0, Uy == 0, Uz /= 0,
-  } elseif (uLy == 0.0 && uRy == 0.0){
-
-    // Compute Ridders, case 2
-    (uLz > uRz) ? (x2 = uLz, x1 = uRz) : (x1 = uLz, x2 = uRz);
-    double f1 = case_2(x1, consts), f2 = case_2(x2, consts);
-    struct gkyl_qr_res res = gkyl_ridders(case_2, consts, x1, x2, f1, f2, 100, 1e-12);
-    double Uz = res.res;
-    v_result = (-Uz*a - Az)/(Uz*dp + Bz);
-
-  // Case 3: Ux /= 0, Uy /= 0, Uz == 0,
-  } elseif (uLz == 0.0 && uRz == 0.0){
-
-    // Compute Ridders, case 3
-    (uLy > uRy) ? (x2 = uLy, x1 = uRy) : (x1 = uLy, x2 = uRy);
-    double f1 = case_3(x1, consts), f2 = case_3(x2, consts);
-    struct gkyl_qr_res res = gkyl_ridders(case_3, consts, x1, x2, f1, f2, 100, 1e-12);
-    double Uy = res.res;
-    v_result = (-Uy*a - Ay)/(Uy*dp + By);
-
-  //Case 4: Ux /= 0, Uy /= 0, Uz /= 0,
-  } elseif ((uRx != 0.0 ) || (uLx != 0.0 )) && ((uRy != 0.0 ) || (uLy != 0.0 )) && ((uRz != 0.0 ) || (uLz != 0.0 )) {
-
-    // Compute Ridders, case 4
-    (uLy > uRy) ? (x2 = uLy, x1 = uRy) : (x1 = uLy, x2 = uRy);
-    double f1 = case_4(x1, consts), f2 = case_4(x2, consts);
-    struct gkyl_qr_res res = gkyl_ridders(case_4, consts, x1, x2, f1, f2, 100, 1e-12);
-    double Uy = res.res;
-    v_result = (-Uy*a - Ay)/(Uy*dp + By);
-
-  } else {
-
-    //Return 0 if the cases all fail
-    v_result = 0.0;
-
-  } // end cases
-
-  return v_result;
-}
 
 // Params: //(Uy,a,Ay,dp,By,Ax,Bx,Az,Bz,c)
 static inline double
-case_3(double Ux, void *ctx)
+case_1(double Ux, void *ctx)
 { 
   double *constants = (double*)ctx;
   double a = constants[0];
@@ -139,9 +41,9 @@ case_2(double Uz, void *ctx)
 { 
   double *constants = (double*)ctx;
   double a = constants[0];
-  double Ay = constants[1];
   double dp = constants[2];
-  double By = constants[3];
+  double Ax = constants[4];
+  double Bx = constants[5];
   double Az = constants[6];
   double Bz = constants[7];
   double c = constants[8];
@@ -197,4 +99,105 @@ case_4(double Uy, void *ctx)
 
   // Eval of the function
   return Vx -  Ux / sqrt(1.0 + (Ux*Ux + Uy*Uy + Uz*Uz)/(c*c));
+}
+
+
+/**
+ * Compute the roe-averaged velocity for the sr-cold fluid equations, solved via
+ * Ridders' method.
+ * 
+ * This requries 3 velocity dimensions, and 
+ * A. Assumes we are calculating the first index 
+ * interface (for instance the direction along Vx for Ux, Uy, Uz)
+ * B. Assumes density has already been checked and ql[0] > 0, and qr[0] > 0
+ * C. If all velocities are zero, the calculation returns zero
+ * D. Non gaurds against div by zero in case_#(...) functions
+ * E. c is defined as one.
+ * 
+ * v_result can be obtained by simple relations because of the contraints in the NL
+ * solve
+ *
+ * @param ql Conserved variables (left)
+ * @param qr Conserved variables (right)
+ */
+static double
+compute_sr_roe_averaged_velocity_via_ridders(const double ql[4], const double qr[4]) 
+{
+
+  // speed of light
+  double c = 1.0;
+
+  // Isolate varaibles (right/left)
+  double rhoR = qr[0];
+  double uRx = qr[1]/qr[0];
+  double uRy = qr[2]/qr[0];
+  double uRz = qr[3]/qr[0];
+  double rhoL = ql[0];
+  double uLx = ql[1]/ql[0];
+  double uLy = ql[2]/ql[0];
+  double uLz = ql[3]/ql[0];
+
+  // Compute the constants:
+  double vLx = uLx/sqrt(1.0 + (uLx*uLx + uLy*uLy + uLz*uLz)/(c*c));
+  double vRx = uRx/sqrt(1.0 + (uRx*uRx + uRy*uRy + uRz*uRz)/(c*c));
+  double dp = rhoR - rhoL;
+  double a = rhoL*vLx - rhoR*vRx;
+  double Ax = - rhoL*vLx*uLx + rhoR*vRx*uRx;
+  double Ay = - rhoL*vLx*uLy + rhoR*vRx*uRy;
+  double Az = - rhoL*vLx*uLz + rhoR*vRx*uRz;
+  double Bx = rhoL*uLx - rhoR*uRx;
+  double By = rhoL*uLy - rhoR*uRy;
+  double Bz = rhoL*uLz - rhoR*uRz;
+  double consts[] = {a,Ay,dp,By,Ax,Bx,Az,Bz,c};
+  double x1, x2;
+  double v_result;
+
+  //Case 1: Ux /= 0, Uy == 0, Uz == 0, 
+  if (uLz == 0.0 && uRz == 0.0 && uLy == 0.0 && uRy == 0.0) {
+
+    // Compute Ridders, case 1
+    (uLx > uRx) ? (x2 = uLx, x1 = uRx) : (x1 = uLx, x2 = uRx);
+    double f1 = case_1(x1, consts), f2 = case_1(x2, consts);
+    struct gkyl_qr_res res = gkyl_ridders(case_1, consts, x1, x2, f1, f2, 100, 1e-12);
+    double Ux = res.res;
+    v_result = (-Ux*a - Ax)/(Ux*dp + Bx);
+
+  // Case 2: Ux /= 0, Uy == 0, Uz /= 0,
+  } else if (uLy == 0.0 && uRy == 0.0){
+
+    // Compute Ridders, case 2
+    (uLz > uRz) ? (x2 = uLz, x1 = uRz) : (x1 = uLz, x2 = uRz);
+    double f1 = case_2(x1, consts), f2 = case_2(x2, consts);
+    struct gkyl_qr_res res = gkyl_ridders(case_2, consts, x1, x2, f1, f2, 100, 1e-12);
+    double Uz = res.res;
+    v_result = (-Uz*a - Az)/(Uz*dp + Bz);
+
+  // Case 3: Ux /= 0, Uy /= 0, Uz == 0,
+  } else if (uLz == 0.0 && uRz == 0.0){
+
+    // Compute Ridders, case 3
+    (uLy > uRy) ? (x2 = uLy, x1 = uRy) : (x1 = uLy, x2 = uRy);
+    double f1 = case_3(x1, consts), f2 = case_3(x2, consts);
+    struct gkyl_qr_res res = gkyl_ridders(case_3, consts, x1, x2, f1, f2, 100, 1e-12);
+    double Uy = res.res;
+    v_result = (-Uy*a - Ay)/(Uy*dp + By);
+
+  //Case 4: Ux /= 0, Uy /= 0, Uz /= 0,
+  } else if (((uRx != 0.0 ) || (uLx != 0.0 )) && ((uRy != 0.0 ) || (uLy != 0.0 )) && ((uRz != 0.0 ) || (uLz != 0.0 ))) {
+
+    // Compute Ridders, case 4
+    (uLy > uRy) ? (x2 = uLy, x1 = uRy) : (x1 = uLy, x2 = uRy);
+    double f1 = case_4(x1, consts), f2 = case_4(x2, consts);
+    struct gkyl_qr_res res = gkyl_ridders(case_4, consts, x1, x2, f1, f2, 100, 1e-12);
+    double Uy = res.res;
+    v_result = (-Uy*a - Ay)/(Uy*dp + By);
+
+  } else {
+
+    //Return 0 if the cases all fail
+    v_result = 0.0;
+
+  } // end cases
+
+  return v_result;
 }
--- zero/wv_cold_sr_fluid.c
+++ zero/wv_cold_sr_fluid.c
@@ -11,7 +11,7 @@
 #define SQ(x) ((x)*(x))
 
 struct wv_cold_sr_fluid {
-  struct gkyl_wv_sr_eqn eqn; // base object
+  struct gkyl_wv_eqn eqn; // base object
 };
 
 void
@@ -27,7 +27,7 @@ cold_sr_fluid_flux(const double q[5], double flux[5])
 }
 
 static inline void
-cold_sr_fluid_cons_to_diag(const struct gkyl_wv_sr_eqn *eqn,
+cold_sr_fluid_cons_to_diag(const struct gkyl_wv_eqn *eqn,
   const double *qin, double *diag)
 {
   // density and moment is copied as-is
@@ -40,13 +40,13 @@ cold_sr_fluid_cons_to_diag(const struct gkyl_wv_sr_eqn *eqn,
 static void
 cold_sr_fluid_free(const struct gkyl_ref_count *ref)
 {
-  struct gkyl_wv_sr_eqn *base = container_of(ref, struct gkyl_wv_sr_eqn, ref_count);
+  struct gkyl_wv_eqn *base = container_of(ref, struct gkyl_wv_eqn, ref_count);
   struct wv_cold_sr_fluid *cold_sr_fluid = container_of(base, struct wv_cold_sr_fluid, eqn);
   gkyl_free(cold_sr_fluid);
 }
 
 static inline void
-cons_to_riem_sr(const struct gkyl_wv_sr_eqn *eqn,
+cons_to_riem_sr(const struct gkyl_wv_eqn *eqn,
   const double *qstate, const double *qin, double *wout)
 {
   // TODO: this should use proper L matrix
@@ -54,7 +54,7 @@ cons_to_riem_sr(const struct gkyl_wv_sr_eqn *eqn,
     wout[i] = qin[i];
 }
 static inline void
-riem_to_cons_sr(const struct gkyl_wv_sr_eqn *eqn,
+riem_to_cons_sr(const struct gkyl_wv_eqn *eqn,
   const double *qstate, const double *win, double *qout)
 {
   // TODO: this should use proper L matrix
@@ -62,9 +62,30 @@ riem_to_cons_sr(const struct gkyl_wv_sr_eqn *eqn,
     qout[i] = win[i];
 }
 
+static inline void
+rot_to_local(const double *tau1, const double *tau2, const double *norm,
+  const double *GKYL_RESTRICT qglobal, double *GKYL_RESTRICT qlocal)
+{
+  qlocal[0] = qglobal[0];
+  qlocal[1] = qglobal[1]*norm[0] + qglobal[2]*norm[1] + qglobal[3]*norm[2];
+  qlocal[2] = qglobal[1]*tau1[0] + qglobal[2]*tau1[1] + qglobal[3]*tau1[2];
+  qlocal[3] = qglobal[1]*tau2[0] + qglobal[2]*tau2[1] + qglobal[3]*tau2[2];
+}
+
+static inline void
+rot_to_global(const double *tau1, const double *tau2, const double *norm,
+  const double *GKYL_RESTRICT qlocal, double *GKYL_RESTRICT qglobal)
+{
+  qglobal[0] = qlocal[0];
+  qglobal[1] = qlocal[1]*norm[0] + qlocal[2]*tau1[0] + qlocal[3]*tau2[0];
+  qglobal[2] = qlocal[1]*norm[1] + qlocal[2]*tau1[1] + qlocal[3]*tau2[1];
+  qglobal[3] = qlocal[1]*norm[2] + qlocal[2]*tau1[2] + qlocal[3]*tau2[2];
+}
+
+
 // Waves and speeds using Roe averaging, correction term
 static double
-wave_roe_sr(const struct gkyl_wv_sr_eqn *eqn, enum gkyl_wv_flux_type type,
+wave_roe_sr(const struct gkyl_wv_eqn *eqn, enum gkyl_wv_flux_type type,
   const double *delta, const double *ql, const double *qr, double *waves, double *s)
 {
   double f[4];
@@ -90,7 +111,7 @@ wave_roe_sr(const struct gkyl_wv_sr_eqn *eqn, enum gkyl_wv_flux_type type,
     double rl = ql[0];
     double rr = qr[0];
     // compute Roe averaged speed
-    double vel = compute_sr_roe_avereged_velocity(ql,qr);
+    double vel = compute_sr_roe_averaged_velocity_via_ridders(ql,qr);
             
     if(vel<0) {
       wv = &waves[0];
@@ -118,7 +139,7 @@ wave_roe_sr(const struct gkyl_wv_sr_eqn *eqn, enum gkyl_wv_flux_type type,
 }
 
 static void
-qfluct_roe(const struct gkyl_wv_sr_eqn *eqn, enum gkyl_wv_flux_type type,
+qfluct_roe(const struct gkyl_wv_eqn *eqn, enum gkyl_wv_flux_type type,
   const double *ql, const double *qr, const double *waves, const double *s,
   double *amdq, double *apdq)
 {
@@ -139,7 +160,7 @@ qfluct_roe(const struct gkyl_wv_sr_eqn *eqn, enum gkyl_wv_flux_type type,
 }
 
 static void
-ffluct_roe(const struct gkyl_wv_sr_eqn *eqn, enum gkyl_wv_flux_type type,
+ffluct_roe(const struct gkyl_wv_eqn *eqn, enum gkyl_wv_flux_type type,
   const double *ql, const double *qr, const double *waves, const double *s,
   double *amdq, double *apdq)
 {
@@ -166,7 +187,7 @@ ffluct_roe(const struct gkyl_wv_sr_eqn *eqn, enum gkyl_wv_flux_type type,
 }
 
 static double
-flux_jump_sr(const struct gkyl_wv_sr_eqn *eqn, const double *ql, const double *qr, double *flux_jump_sr)
+flux_jump_sr(const struct gkyl_wv_eqn *eqn, const double *ql, const double *qr, double *flux_jump_sr)
 {
   double fr[4], fl[4];
   cold_sr_fluid_flux(ql, fl);
@@ -182,24 +203,24 @@ flux_jump_sr(const struct gkyl_wv_sr_eqn *eqn, const double *ql, const double *q
 }
 
 static bool
-check_inv(const struct gkyl_wv_sr_eqn *eqn, const double *q)
+check_inv(const struct gkyl_wv_eqn *eqn, const double *q)
 {
   return q[0] > 0.0;
 }
 
 static double
-max_speed_sr(const struct gkyl_wv_sr_eqn *eqn, const double *q)
+max_speed_sr(const struct gkyl_wv_eqn *eqn, const double *q)
 {
   const struct wv_cold_sr_fluid *cold_sr_fluid = container_of(eqn, struct wv_cold_sr_fluid, eqn);
   return fabs(q[NUX]/sqrt(q[0]*q[0] + (q[NUX]*q[NUX] + q[NUY]*q[NUY] + q[NUZ]*q[NUZ])));
 }
 
-struct gkyl_wv_sr_eqn*
+struct gkyl_wv_eqn*
 gkyl_wv_cold_sr_fluid_new(void)
 {
   struct wv_cold_sr_fluid *cold_sr_fluid = gkyl_malloc(sizeof(struct wv_cold_sr_fluid));
 
-  cold_sr_fluid->eqn.type = GKYL_EQN_cold_sr_flUID;
+  cold_sr_fluid->eqn.type = GKYL_EQN_COLDFLUID_SR;
   cold_sr_fluid->eqn.num_equations = 4;
   cold_sr_fluid->eqn.num_waves = 2;
   cold_sr_fluid->eqn.num_diag = 5; // KE is final component
@@ -207,16 +228,16 @@ gkyl_wv_cold_sr_fluid_new(void)
   cold_sr_fluid->eqn.waves_func = wave_roe_sr;
   cold_sr_fluid->eqn.qfluct_func = qfluct_roe;
   cold_sr_fluid->eqn.ffluct_func = ffluct_roe;
-  cold_sr_fluid->eqn.flux_jump_sr = flux_jump_sr;
+  cold_sr_fluid->eqn.flux_jump = flux_jump_sr;
   
   cold_sr_fluid->eqn.check_inv_func = check_inv;
-  cold_sr_fluid->eqn.max_speed_sr_func = max_speed_sr;
+  cold_sr_fluid->eqn.max_speed_func = max_speed_sr;
 
   cold_sr_fluid->eqn.rotate_to_local_func = rot_to_local;
   cold_sr_fluid->eqn.rotate_to_global_func = rot_to_global;
 
-  cold_sr_fluid->eqn.cons_to_riem_sr = cons_to_riem_sr;
-  cold_sr_fluid->eqn.riem_to_cons_sr = riem_to_cons_sr;
+  cold_sr_fluid->eqn.cons_to_riem = cons_to_riem_sr;
+  cold_sr_fluid->eqn.riem_to_cons = riem_to_cons_sr;
 
   cold_sr_fluid->eqn.cons_to_diag = cold_sr_fluid_cons_to_diag;
 
