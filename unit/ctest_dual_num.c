42ec7533abe3ffb25ef79a29a1575adc84401e40
ammarhakim <a.hakim777@gmail.com>
2023-11-03 23:29:58 -0400

    Added example on how to use the output of the ridders root-finder to compute the derivative of a function known only via its inverse. This is the typical case of GK mapping as we do not know mapc2p but its inverse.

--- unit/ctest_dual_num.c
+++ unit/ctest_dual_num.c
@@ -1,7 +1,9 @@
 #include <acutest.h>
 
 #include <stdio.h>
+
 #include <gkyl_dual_num.h>
+#include <gkyl_math.h>
 
 static inline bool
 cmp_dn(struct gkyl_dn x, struct gkyl_dn xcmp)
@@ -62,7 +64,45 @@ void test_basic(void)
   TEST_CHECK( cmp_dn(res, gdn_new(func_1_0(x10), func_1_1(x10))) );
 }
 
+// for testing root finding
+struct inv_map_ctx { double s0; };
+
+static inline struct gkyl_dn
+gdn_inv_mapc2p(struct gkyl_dn x, void *ctx)
+{
+  struct inv_map_ctx *imctx = ctx;
+  double s0 = imctx->s0;
+  // sqrt(x+4) - 2 - s0
+  return gdn_sadd(-2.0-s0, gdn_sqrt(gdn_sadd(4.0, x)));
+}
+
+static inline double
+inv_mapc2p(double x, void *ctx)
+{
+  return gdn_inv_mapc2p(gdn_new0(x),ctx).x[0];
+}
+
+void test_root(void)
+{
+  struct inv_map_ctx imctx = { 0.75 };
+  double xl = 0.0, xr = 5.0;
+  double fl = inv_mapc2p(xl, &imctx);
+  double fr = inv_mapc2p(xr, &imctx);
+
+  struct gkyl_qr_res root =
+    gkyl_ridders(inv_mapc2p, &imctx, xl, xr, fl, fr, 100, 1e-10);
+
+  // compute derivative at the root
+  struct gkyl_dn res = gdn_inv_mapc2p(gdn_new1(root.res), &imctx);
+  // at this point 1/res.x[1] is the derivative of the mapping wrt s
+  // at the root
+  
+  TEST_CHECK( gkyl_compare_double(root.res, 3.5625, 1e-14) );
+  TEST_CHECK( gkyl_compare_double(1/res.x[1], 5.5, 1e-14) );  
+}
+
 TEST_LIST = {
   { "test_basic", test_basic },
+  { "test_root", test_root },
   { NULL, NULL },  
 };
