89e71964ca1bca40e27c8603aa8a59a46f14404f
Akash Shukla <akashukla@utexas.edu>
2023-11-08 14:17:56 -0600

    add unit test for full array nodal-to-modal and modal-to-nodal conversion

--- zero/gkyl_nodal_ops.h
+++ zero/gkyl_nodal_ops.h
@@ -5,3 +5,6 @@
 #include <gkyl_basis.h>
 
 void gkyl_nodal_ops_n2m(const struct gkyl_basis* cbasis, const struct gkyl_rect_grid *grid, const struct gkyl_range *nrange, const struct gkyl_range *update_range, int num_comp, const struct gkyl_array* nodal_fld, struct gkyl_array *modal_fld);
+
+
+void gkyl_nodal_ops_m2n(const struct gkyl_basis* cbasis, const struct gkyl_rect_grid *grid, const struct gkyl_range *nrange, const struct gkyl_range *update_range, int num_comp, struct gkyl_array* nodal_fld, const struct gkyl_array *modal_fld);
