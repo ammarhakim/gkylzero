a27361b06a2382b27ddb8da196bd1e518f5a8106
johnson452 <grj@princeton.edu>
2023-11-16 23:52:58 -0700

    Added durand_kerner solve for relativistic cold fluid conservation solve

--- zero/gkyl_wv_cold_sr_fluid.h
+++ zero/gkyl_wv_cold_sr_fluid.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <complex.h>
 #include <gkyl_wv_eqn.h>
 #include <gkyl_math.h>
 #include <math.h>
@@ -7,6 +8,11 @@
 #include <stdlib.h>
 
 
+static double
+sqr(double x)
+{
+  return x*x;
+}
 
 
 /**
@@ -16,21 +22,6 @@
  */
 struct gkyl_wv_eqn* gkyl_wv_cold_sr_fluid_new(void);
 
-
-// returns 1 if it passes, 0 if fails
-static bool 
-entropy_check(const double UL[3], const double UR[4], double u[3], const double c)
-{
-  bool cond = 1;
-  double tol = 1e-15;
-  for(int i=0; i<3; ++i){
-      //if (fabs(ql[0]) > 1.e-15 && fabs(qr[0]) > 1.e-15){
-        cond = cond && (fmin(UR[i],UL[i]) - 1e-15) <= u[i] && u[i] <= (fmax(UR[i],UL[i]) + 1e-15);
-      //}
-      //cond = cond && (fabs(NU_cons_func( ql, qr, i, u, c)) <= 1.e-10);
-  }
-  return cond;
-}
 
 // relativistic case
 struct cold_sr {
@@ -47,6 +38,123 @@ calc_ufromv(const double v[3], double u[3])
   u[1] = v[1]*gamma1;
   u[2] = v[2]*gamma1;
 }
+
+double tolerance_dk_4th_order(double complex p, double complex q, double complex r, double complex s,
+  double complex p_old, double complex q_old, double complex r_old, double complex s_old) {
+
+  // Calculate the absolute differences for each element
+  double diff_p = cabs(p - p_old);
+  double diff_q = cabs(q - q_old);
+  double diff_r = cabs(r - r_old);
+  double diff_s = cabs(s - s_old);
+
+  // Sum up the absolute differences
+  double sum_of_differences = diff_p + diff_q + diff_r + diff_s;
+
+  return sum_of_differences;
+}
+
+static double complex inline
+f_poly(double complex x, double a, double b, double c, double d)
+{
+  return cpow(x,4)+a*cpow(x,3)+b*cpow(x,2)+c*x+d;
+}
+
+void
+durand_kerner_method(void *ctx, double complex roots[4])
+{
+
+  // compute the coefficients
+  struct cold_sr *cnr = ctx;
+  double rhol = cnr->rhol, rhor = cnr->rhor;
+  double vl[3], vr[3];
+
+  for (int i=0; i<3; ++i) {
+    vl[i] = cnr->vl[i];
+    vr[i] = cnr->vr[i];
+  }
+
+  double ul[3], ur[3];
+  calc_ufromv(vl, ul);
+  calc_ufromv(vr, ur);
+  double a, drho, A[3], B[3];
+
+  for (int i=0; i<3; ++i) {
+    A[i] = -rhol*ul[i]*vl[0] + rhor*ur[i]*vr[0];
+    B[i] = rhol*ul[i] - rhor*ur[i];
+  }
+
+  drho = rhor-rhol;
+  a = rhol*vl[0] - rhor*vr[0];
+
+  double p4coeff = sqr(drho)+sqr(B[2])+sqr(B[1])+sqr(B[0]); 
+  double p3coeff = 2*a*drho+2*A[2]*B[2]+2*A[1]*B[1]+2*A[0]*B[0]; 
+  double p2coeff = sqr(a)+sqr(A[2])+sqr(A[1])-sqr(B[0])+sqr(A[0]); 
+  double p1coeff = -2*A[0]*B[0]; 
+  double p0coeff = -sqr(A[0]); 
+
+  // Find the four roots to the polynomial Vx equation:
+  // f(x)=x^{4}+ax^{3}+bx^{2}+cx+d,
+  // via durand-kerner: https://en.wikipedia.org/wiki/Durand–Kerner_method
+  double a_poly = p3coeff/p4coeff;
+  double b_poly = p2coeff/p4coeff;
+  double c_poly = p1coeff/p4coeff;
+  double d_poly = p0coeff/p4coeff;
+
+  // Compute the intial conditions for solutions
+  double complex p, q, r, s;
+  double complex p_old = 1.0;
+  double complex q_old = I;
+  double complex r_old = -1.0;
+  double complex s_old = -I;
+
+  // iterate until the desired precision is reached
+  int iter = 0;
+  double tol = 1.0;
+  while(tol > 1e-10 && iter < 10000){
+
+    // Update n-1 -> n for (p, q, r, s)
+    p = p_old - f_poly(p_old,a_poly,b_poly,c_poly,d_poly)/( (p_old - q_old)*(p_old - r_old)*(p_old - s_old) );
+    q = q_old - f_poly(q_old,a_poly,b_poly,c_poly,d_poly)/( (q_old - p)*(q_old - r_old)*(q_old - s_old) );
+    r = r_old - f_poly(r_old,a_poly,b_poly,c_poly,d_poly)/( (r_old - p)*(r_old - q)*(r_old - s_old) );
+    s = s_old - f_poly(s_old,a_poly,b_poly,c_poly,d_poly)/( (s_old - p)*(s_old - q)*(s_old - r) );
+
+    // Compute tolerance 
+    tol = tolerance_dk_4th_order(p,q,r,s,p_old,q_old,r_old,s_old);
+
+    // Update using old values
+    p_old = p;
+    q_old = q;
+    r_old = r;
+    s_old = s;
+
+    // update the iterator
+    iter = iter + 1;
+  }
+
+  // Save the roots to the outputs
+  roots[0] = p;
+  roots[1] = q;
+  roots[2] = r;
+  roots[3] = s;
+
+}
+
+// returns 1 if it passes, 0 if fails
+static bool 
+entropy_check(const double UL[3], const double UR[4], double u[3], const double c)
+{
+  bool cond = 1;
+  double tol = 1e-15;
+  for(int i=0; i<3; ++i){
+      //if (fabs(ql[0]) > 1.e-15 && fabs(qr[0]) > 1.e-15){
+        cond = cond && (fmin(UR[i],UL[i]) - 1e-15) <= u[i] && u[i] <= (fmax(UR[i],UL[i]) + 1e-15);
+      //}
+      //cond = cond && (fabs(NU_cons_func( ql, qr, i, u, c)) <= 1.e-10);
+  }
+  return cond;
+}
+
 
 static double
 cold_sr_func(double vhat, void *ctx)
@@ -237,6 +345,11 @@ compute_sr_roe_averaged_velocity_via_ridders(const double ql[4], const double qr
         double v_hat_avg = u_avg[0]/sqrt(1.0 + u_avg[0]*u_avg[0] + u_avg[1]*u_avg[1] + u_avg[2]*u_avg[2] );
         return c*v_hat_avg;
       }
+    } else {
+      // give the momentum averaged velocitity
+      double u_avg[3] = {(uLx+uRx)/2.0,(uLy+uRy)/2.0,(uLz+uRz)/2.0};
+      double v_hat_avg = u_avg[0]/sqrt(1.0 + u_avg[0]*u_avg[0] + u_avg[1]*u_avg[1] + u_avg[2]*u_avg[2] );
+      return c*v_hat_avg;
     }
 
   } else if (ql[0] <= 0.0 && qr[0] > 0.0) {
