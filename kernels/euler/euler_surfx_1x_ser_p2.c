8b2bc6b9dde4ed3f36599a56287e72b723989c4e
Junoravin <junoravin@gmail.com>
2023-11-24 11:10:32 -0500

    Small formatting change but this commit just documents that the current gkylcas kernel generators create these surface kernels (no modifications added by hand to the surface kernels)

--- kernels/euler/euler_surfx_1x_ser_p2.c
+++ kernels/euler/euler_surfx_1x_ser_p2.c
@@ -8,6 +8,7 @@ GKYL_CU_DH double euler_surfx_1x_ser_p2(const double *w, const double *dxv, cons
 { 
   // w[NDIM]:      Cell-center coordinates.
   // dxv[NDIM]:    Cell spacing.
+  // wv_eqn:       Wave equation for computing fluctuations at the interface for upwinding.
   // u_surf_l/c/r: Input surface expansion of flow velocity in left/center/right cells in each direction.
   // p_surf_l/c/r: Input surface expansion of pressure in left/center/right cells in each direction.
   //               [p_xl, p_xr, p_yl, p_yr, p_zl, p_zr] 
@@ -138,19 +139,15 @@ GKYL_CU_DH double euler_surfx_1x_ser_p2(const double *w, const double *dxv, cons
   double waves_r[15] = {0.0}; 
   double speeds_l[3] = {0.0}; 
   double speeds_r[3] = {0.0}; 
-  double my_max_speed_l = gkyl_wv_eqn_waves(wv_eqn, GKYL_WV_HIGH_ORDER_FLUX, delta_l,
-    q_lr, q_cl, waves_l, speeds_l);
-  double my_max_speed_r = gkyl_wv_eqn_waves(wv_eqn, GKYL_WV_HIGH_ORDER_FLUX, delta_r,
-    q_cr, q_rl, waves_r, speeds_r);
+  double my_max_speed_l = gkyl_wv_eqn_waves(wv_eqn, GKYL_WV_HIGH_ORDER_FLUX, delta_l, q_lr, q_cl, waves_l, speeds_l); 
+  double my_max_speed_r = gkyl_wv_eqn_waves(wv_eqn, GKYL_WV_HIGH_ORDER_FLUX, delta_r, q_cr, q_rl, waves_r, speeds_r); 
 
   double amdq_l[5] = {0.0}; 
   double apdq_l[5] = {0.0}; 
   double amdq_r[5] = {0.0}; 
   double apdq_r[5] = {0.0}; 
-  gkyl_wv_eqn_qfluct(wv_eqn, GKYL_WV_HIGH_ORDER_FLUX, q_lr, q_cl,
-    waves_l, speeds_l, amdq_l, apdq_l);
-  gkyl_wv_eqn_qfluct(wv_eqn, GKYL_WV_HIGH_ORDER_FLUX, q_cr, q_rl,
-    waves_r, speeds_r, amdq_r, apdq_r);
+  gkyl_wv_eqn_qfluct(wv_eqn, GKYL_WV_HIGH_ORDER_FLUX, q_lr, q_cl, waves_l, speeds_l, amdq_l, apdq_l); 
+  gkyl_wv_eqn_qfluct(wv_eqn, GKYL_WV_HIGH_ORDER_FLUX, q_cr, q_rl, waves_r, speeds_r, amdq_r, apdq_r); 
 
   Ghat_rho_l = 0.3952847075210474*rho_l[2]*uxl_r+0.3952847075210474*rho_c[2]*uxl_r+0.3061862178478972*rho_l[1]*uxl_r-0.3061862178478972*rho_c[1]*uxl_r+0.1767766952966369*rho_l[0]*uxl_r+0.1767766952966369*rho_c[0]*uxl_r+0.3952847075210474*rho_l[2]*uxc_l+0.3952847075210474*rho_c[2]*uxc_l+0.3061862178478972*rho_l[1]*uxc_l-0.3061862178478972*rho_c[1]*uxc_l+0.1767766952966369*rho_l[0]*uxc_l+0.1767766952966369*rho_c[0]*uxc_l-0.5*apdq_l[0]+0.5*amdq_l[0]; 
   Ghat_rhoux_l = 0.5*Ghat_rho_l*uxl_r+0.5*Ghat_rho_l*uxc_l+0.5*pl_r+0.5*pc_l-0.5*apdq_l[1]+0.5*amdq_l[1]; 
