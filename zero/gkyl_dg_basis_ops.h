1b301c11882f04592cabbd9a84afa0ef83ec9a08
ammarhakim <a.hakim777@gmail.com>
2023-12-03 11:55:00 -0500

    Given values, x- y- and xy-derivatives at the corners of a 2D cell we can not construct a local piece-wise cubic expansion. This expansion needs to be a tensor-product basis as only then can we ensure continuity of all first derivatives at the edges of the cell. This latter is required to compute the needed tangents and duals for use in metric computations.

--- zero/gkyl_dg_basis_ops.h
+++ zero/gkyl_dg_basis_ops.h
@@ -5,7 +5,7 @@
 #include <gkyl_range.h>
 
 /**
- * Given values and gradients at the edges of a 1D cell, compute the
+ * Given values and gradients at the corner of a 1D cell, compute the
  * expansion cofficients of modal cubic (p=3) basis functions. The
  * output @a coeff must be preallocated and have at least 4 elements.
  *
@@ -14,3 +14,27 @@
  * @param coeff On output, the DG expansion coefficients for p=3 basis.
  */
 void gkyl_dg_calc_cubic_1d(const double val[2], const double grad[2], double *coeff);
+
+/**
+ * Given values and gradients at the corners of a 2D cell, compute the
+ * expansion cofficients of modal cubic (p=3) tensor basis functions. The
+ * output @a coeff must be preallocated and have at least 16 elements. The nodes
+ * are numbered as follows.
+ *
+ *  2       4
+ *  *-------*
+ *  |       |
+ *  |       |
+ *  |       |
+ *  *-------*
+ *  1       3
+ *
+ * @param val val[i] is the value at node i
+ * @param gradx gradx[i] is the x-derivative at node i
+ * @param grady grady[i] is the y-derivative at node i
+ * @param gradxy gradxy[i] is the xy- (cross) derivative at node i
+ * @param coeff On output, the DG expansion coefficients for p=3 tensor basis.
+ */
+void gkyl_dg_calc_cubic_2d(const double val[4],
+  const double gradx[4], const double grady[4], const double gradxy[4],
+  double *coeff);
