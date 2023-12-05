f2c285fdb9049a4c39f07446f6709a81aa1b522b
Akash Shukla <akashukla@utexas.edu>
2023-12-05 11:53:47 -0500

    simplification to efit reader

--- zero/efit.c
+++ zero/efit.c
@@ -14,61 +14,6 @@
 #include <gkyl_range.h>
 #include <gkyl_nodal_ops.h>
 
-void nodal_array_to_modal_psi(const struct gkyl_array *nodal_array, struct gkyl_array *modal_array, const struct gkyl_range *update_range, const struct gkyl_range *nrange, const struct gkyl_basis *basis, const struct gkyl_rect_grid *grid, int num_ret_vals){
-  double xc[GKYL_MAX_DIM], xmu[GKYL_MAX_DIM];
-  int num_basis = basis->num_basis;
-  int poly_order = basis->poly_order;
-  //initialize the nodes
-  struct gkyl_array *nodes = gkyl_array_new(GKYL_DOUBLE, grid->ndim, basis->num_basis);
-  basis->node_list(gkyl_array_fetch(nodes, 0));
-  double fnodal[num_basis]; // to store nodal function values
-
-  struct gkyl_range_iter iter;
-  gkyl_range_iter_init(&iter, update_range);
-  int nidx[3];
-  long lin_nidx[num_basis];
-  
-  while (gkyl_range_iter_next(&iter)) {
-     gkyl_rect_grid_cell_center(grid, iter.idx, xc);
-
-    for (int i=0; i<num_basis; ++i) {
-      const double* temp  = gkyl_array_cfetch(nodes,i);
-      for( int j = 0; j < grid->ndim; j++){
-        if(poly_order==1){
-          //if(j==1 && ginp->bcs[1]==1) // this conversion is valid if ghost cells are included
-          //  nidx[j] = iter.idx[j] + (temp[j]+1)/2 ;
-          //else // otherwise this conversion is valid
-            nidx[j] = iter.idx[j]-1 + (temp[j]+1)/2 ;
-        }
-        if (poly_order==2)
-          nidx[j] = 2*(iter.idx[j]-1) + (temp[j] + 1) ;
-      }
-      lin_nidx[i] = gkyl_range_idx(nrange, nidx);
-    }
-
-    long lidx = gkyl_range_idx(update_range, iter.idx);
-    double *arr_p = gkyl_array_fetch(modal_array, lidx); // pointer to expansion in cell
-    double fao[num_basis*num_ret_vals];
-  
-    for (int i=0; i<num_basis; ++i) {
-      const double* temp = gkyl_array_cfetch(nodal_array, lin_nidx[i]);
-      for (int j=0; j<num_ret_vals; ++j) {
-        fao[i*num_ret_vals + j] = temp[j];
-      }
-    }
-
-    for (int i=0; i<num_ret_vals; ++i) {
-      // copy so nodal values for each return value are contiguous
-      // (recall that function can have more than one return value)
-      for (int k=0; k<num_basis; ++k)
-        fnodal[k] = fao[num_ret_vals*k+i];
-      // transform to modal expansion
-      basis->nodal_to_modal(fnodal, &arr_p[num_basis*i]);
-    }
-  }
-
-}
-
 
 
 gkyl_efit* gkyl_efit_new(const char *filepath, int rz_poly_order, int flux_poly_order, bool use_gpu)
@@ -204,10 +149,9 @@ gkyl_efit* gkyl_efit_new(const char *filepath, int rz_poly_order, int flux_poly_
     }
   }
   // We filled psizr_nodal
-  nodal_array_to_modal_psi(psizr_n, up->psizr, up->rzlocal, &nrange, up->rzbasis, up->rzgrid, 1);
-  nodal_array_to_modal_psi(psibyrzr_n, up->psibyrzr, up->rzlocal, &nrange, up->rzbasis, up->rzgrid, 1);
-  nodal_array_to_modal_psi(psibyr2zr_n, up->psibyr2zr, up->rzlocal, &nrange, up->rzbasis, up->rzgrid, 1);
-  
+  gkyl_nodal_ops_n2m( up->rzbasis, up->rzgrid, &nrange, up->rzlocal, 1, psizr_n, up->psizr);
+  gkyl_nodal_ops_n2m( up->rzbasis, up->rzgrid, &nrange, up->rzlocal, 1, psibyrzr_n, up->psibyrzr);
+  gkyl_nodal_ops_n2m( up->rzbasis, up->rzgrid, &nrange, up->rzlocal, 1, psibyr2zr_n, up->psibyr2zr);
  
   // Now lets read the q profile
   struct gkyl_array *qflux_n = gkyl_array_new(GKYL_DOUBLE, 1, flux_nrange.volume);
