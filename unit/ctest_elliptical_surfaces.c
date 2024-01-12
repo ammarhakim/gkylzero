71620c6c77e0ed20b08f8d16cbe9bbdea14eb8f6
Akash Shukla <akashukla@utexas.edu>
2024-01-12 11:19:21 -0500

    add elliptical surfacues with Bphi=0 efit file and associated unit test

---
+++ unit/ctest_elliptical_surfaces.c
@@ -0,0 +1,91 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+
+#include <acutest.h>
+#include <gkyl_array.h>
+#include <gkyl_array_rio.h>
+#include <gkyl_array_ops.h>
+#include <gkyl_eval_on_nodes.h>
+#include <gkyl_range.h>
+#include <gkyl_rect_grid.h>
+#include <gkyl_rect_decomp.h>
+#include <gkyl_util.h>
+#include <gkyl_basis.h>
+
+#include <gkyl_efit.h>
+#include <gkyl_calc_bmag.h>
+#include <gkyl_tok_geo.h>
+
+
+#include <gkyl_calc_metric.h>
+#include <gkyl_calc_derived_geo.h>
+
+#include <gkyl_gk_geometry.h>
+#include <gkyl_gk_geometry_tok.h>
+
+
+
+
+
+
+
+void
+test_elliptical()
+{
+  clock_t start, end;
+  double cpu_time_used;
+  start = clock();
+
+
+
+  struct gkyl_tok_geo_efit_inp inp = {
+      // psiRZ and related inputs
+      .filepath = "./efit_data/elliptical.geqdsk",
+      .rzpoly_order = 2,
+      .fluxpoly_order = 1,
+      .plate_spec = false,
+      .quad_param = {  .eps = 1e-10 }
+    };
+
+  double psisep = -4.0;
+  double clower[] = { -5.0, -0.01, -M_PI+1e-14 };
+  double cupper[] = {psisep, 0.01, M_PI-1e-14 };
+
+  int ccells[] = { 8, 1, 16 };
+
+
+
+  struct gkyl_rect_grid cgrid;
+  gkyl_rect_grid_init(&cgrid, 3, clower, cupper, ccells);
+  struct gkyl_range clocal, clocal_ext;
+  int cnghost[GKYL_MAX_CDIM] = { 1, 1, 1 };
+  gkyl_create_grid_ranges(&cgrid, cnghost, &clocal_ext, &clocal);
+  int cpoly_order = 1;
+  struct gkyl_basis cbasis;
+  gkyl_cart_modal_serendip(&cbasis, 3, cpoly_order);
+
+  struct gkyl_tok_geo_grid_inp ginp = {
+    .ftype = GKYL_SOL_DN_OUT,
+    .rclose = 6.0,
+    .zmin = -3.0,
+    .zmax = 3.0,
+    .write_node_coord_array = true,
+    .node_file_nm = "elliptical_nodes.gkyl"
+  }; 
+
+  struct gk_geometry* up = gkyl_gk_geometry_tok_new(&cgrid, &clocal, &clocal_ext, &cbasis, &inp, &ginp, false); 
+  gkyl_gk_geometry_release(up);
+
+  end = clock();
+  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  printf("total time = %g\n", cpu_time_used);
+}
+
+TEST_LIST = {
+  { "test_elliptical", test_elliptical},
+  { NULL, NULL },
+};
