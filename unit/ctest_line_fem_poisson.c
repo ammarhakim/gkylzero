a7c756a5e27a6dc9ec293e012ed6f2c366a5dbe5
Akash Shukla <akashukla@utexas.edu>
2024-01-18 10:47:37 -0500

    The line solver is actually converging as expected. There was a mistake in my unit test with how I set epsilon - I didn't understand how shiftc worked

--- unit/ctest_line_fem_poisson.c
+++ unit/ctest_line_fem_poisson.c
@@ -33,28 +33,16 @@ double calc_l2_avg(struct gkyl_rect_grid grid, struct gkyl_range range, struct g
       diff_i[i] = f1[i] - f2[i];
     }
   }
-  //struct gkyl_array *l2_diff = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, range_ext.volume);
-  //for(int i = 0; i <basis.num_basis; i++)
-  //  gkyl_dg_calc_l2_range(basis, i, l2_diff, i, diff, range);
+  struct gkyl_array *l2_diff = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, range_ext.volume);
+  for(int i = 0; i <basis.num_basis; i++)
+    gkyl_dg_calc_l2_range(basis, i, l2_diff, i, diff, range);
 
+  double l2[basis.num_basis];
+  gkyl_array_reduce_range(l2, l2_diff, GKYL_SUM, &range);
   double l2_sum = 0.0;
-  gkyl_range_iter_init(&iter, &range);
-  while (gkyl_range_iter_next(&iter)) {
-    long lidx = gkyl_range_idx(&range, iter.idx);
-    const double *diff_i = gkyl_array_cfetch(diff, lidx);
-    for(int i = 0; i <basis.num_basis; i++){
-      l2_sum += diff_i[i]*diff_i[i];
-    }
-  }
-  printf("cellvol = %g\n", grid.cellVolume);
-  return sqrt(l2_sum/grid.cellVolume);
-
-  //double l2[basis.num_basis];
-  //gkyl_array_reduce_range(l2, l2_diff, GKYL_SUM, &range);
-  //double l2_sum = 0.0;
-  //for(int i = 0; i <basis.num_basis; i++)
-  //  l2_sum += l2[i];
-  //return l2_sum/range.volume;
+  for(int i = 0; i <basis.num_basis; i++)
+    l2_sum += l2[i];
+  return l2_sum;
 }
 
 
@@ -64,7 +52,7 @@ rho_func_zdep_nd(double t, const double *xn, double *fout, void *ctx)
 {
   double x = xn[0];
   double z = xn[1];
-  fout[0] = 4*cos(3*z)*cos(2*x);
+  fout[0] = 4*cos(z)*cos(2*x);
 }
 
 void
@@ -72,7 +60,7 @@ phi_func_zdep_nd(double t, const double *xn, double *fout, void *ctx)
 {
   double x = xn[0];
   double z = xn[1];
-  fout[0] = cos(3*z)*cos(2*x);
+  fout[0] = cos(z)*cos(2*x);
 }
 
 void
@@ -106,6 +94,22 @@ phi_func_zind_dd(double t, const double *xn, double *fout, void *ctx)
   double z = xn[1];
   fout[0] = cos(2*x - M_PI/2);
 }
+
+void
+rho_func_zind_dd_1x(double t, const double *xn, double *fout, void *ctx)
+{
+  double x = xn[0];
+  fout[0] = 4*cos(2*x - M_PI/2);
+}
+
+void
+phi_func_zind_dd_1x(double t, const double *xn, double *fout, void *ctx)
+{
+  double x = xn[0];
+  fout[0] = cos(2*x - M_PI/2);
+}
+
+
 
 
 void
@@ -165,7 +169,7 @@ test_zdep_nd_nxnz(int nx, int ny){
 
   struct gkyl_array *phi = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
   struct gkyl_array *epsilon = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
-  gkyl_array_shiftc(epsilon, sqrt(2.0), 0); 
+  gkyl_array_shiftc(epsilon, 2.0, 0); 
 #ifdef GKYL_HAVE_CUDA
   struct gkyl_array *epsilon_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
   gkyl_array_copy(epsilon_dev, epsilon);
@@ -261,7 +265,7 @@ test_simplez_dd_nxnz(int nx, int ny){
 
   struct gkyl_array *phi = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
   struct gkyl_array *epsilon = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
-  gkyl_array_shiftc(epsilon, sqrt(2.0), 0); 
+  gkyl_array_shiftc(epsilon, 2.0, 0); 
 #ifdef GKYL_HAVE_CUDA
   struct gkyl_array *epsilon_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
   gkyl_array_copy(epsilon_dev, epsilon);
@@ -299,6 +303,93 @@ test_simplez_dd_nxnz(int nx, int ny){
   gkyl_array_release(field);
   gkyl_array_release(phi);
   gkyl_array_release(epsilon);
+
+}
+
+void test_fem_poisson_zind_dd_nx(int nx){
+  double lower[] = { -M_PI}, upper[] = { M_PI};
+  int cells[] = { nx };
+  struct gkyl_rect_grid grid;
+  gkyl_rect_grid_init(&grid, 1, lower, upper, cells);
+
+  //ranges
+  struct gkyl_range local, local_ext;
+  int nghost[GKYL_MAX_CDIM] = { 1 };
+  gkyl_create_grid_ranges(&grid, nghost, &local_ext, &local);
+
+  // basis function
+  int poly_order = 1;
+  struct gkyl_basis basis;
+  gkyl_cart_modal_serendip(&basis, 1, poly_order);
+
+  bool use_gpu = false;
+#ifdef GKYL_HAVE_CUDA
+  use_gpu = true;
+  struct gkyl_basis *basis_on_dev = gkyl_cu_malloc(sizeof(struct gkyl_basis));
+  gkyl_cart_modal_serendip_cu_dev(basis_on_dev, 2, poly_order);
+#else
+  struct gkyl_basis *basis_on_dev = &basis;
+#endif
+
+  // project initial function on 2d field
+  struct gkyl_array *field = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+  gkyl_proj_on_basis *proj = gkyl_proj_on_basis_new(&grid, &basis, 2, 1, &rho_func_zind_dd_1x, 0);
+  gkyl_proj_on_basis_advance(proj, 0.0, &local, field);
+  gkyl_proj_on_basis_release(proj);
+  gkyl_grid_sub_array_write(&grid, &local, field, "in_field.gkyl");
+
+#ifdef GKYL_HAVE_CUDA
+  struct gkyl_array *field_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+  gkyl_array_copy(field_dev, field);
+#else
+  struct gkyl_array *field_dev = field;
+#endif
+
+  struct gkyl_poisson_bc poisson_bc;
+  poisson_bc.lo_type[0] = GKYL_POISSON_DIRICHLET;
+  poisson_bc.up_type[0] = GKYL_POISSON_DIRICHLET;
+  poisson_bc.lo_value[0].v[0] = 0.;
+  poisson_bc.up_value[0].v[0] = 0.;
+
+  struct gkyl_array *phi = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+  struct gkyl_array *epsilon = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+  gkyl_array_shiftc(epsilon, sqrt(2.0), 0); 
+#ifdef GKYL_HAVE_CUDA
+  struct gkyl_array *epsilon_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+  gkyl_array_copy(epsilon_dev, epsilon);
+  struct gkyl_array *phi_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+#else
+  struct gkyl_array *phi_dev = phi;
+  struct gkyl_array *epsilon_dev = epsilon;
+#endif
+
+  struct gkyl_fem_poisson* fem_poisson = gkyl_fem_poisson_new(&local, &grid, basis, &poisson_bc, epsilon_dev, 0, false, use_gpu);
+  gkyl_fem_poisson_set_rhs(fem_poisson, field);
+  gkyl_fem_poisson_solve(fem_poisson, phi);
+  gkyl_grid_sub_array_write(&grid, &local, phi, "out_field.gkyl");
+  //
+  // project analytic solution
+  struct gkyl_array *sol = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+  gkyl_proj_on_basis *proj_sol = gkyl_proj_on_basis_new(&grid, &basis, 2, 1, &phi_func_zind_dd_1x, 0);
+  gkyl_proj_on_basis_advance(proj_sol, 0.0, &local, sol);
+  gkyl_proj_on_basis_release(proj_sol);
+  gkyl_grid_sub_array_write(&grid, &local, sol, "sol_field.gkyl");
+
+
+  double l2 = calc_l2_avg(grid, local,local_ext, basis, phi, sol);
+  printf("l2 = %g\n", l2);
+
+  gkyl_fem_poisson_release(fem_poisson);
+#ifdef gkyl_have_cuda
+  gkyl_cu_free(basis_on_dev);
+  gkyl_array_release(field_dev);
+  gkyl_array_release(phi_dev);
+  gkyl_array_release(epsilon_dev);
+#endif
+  gkyl_array_release(field);
+  gkyl_array_release(phi);
+  gkyl_array_release(epsilon);
+
 
 }
 
@@ -358,7 +449,7 @@ test_zind_dd_nxnz(int nx, int ny){
 
   struct gkyl_array *phi = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
   struct gkyl_array *epsilon = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
-  gkyl_array_shiftc(epsilon, sqrt(2.0), 0); 
+  gkyl_array_shiftc(epsilon, 2.0, 0); 
 #ifdef GKYL_HAVE_CUDA
   struct gkyl_array *epsilon_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
   gkyl_array_copy(epsilon_dev, epsilon);
@@ -405,33 +496,44 @@ test_zind_dd_nxnz(int nx, int ny){
 void test_zind_dd(){
   printf("\n");
   int ny = 32;
-  for(int nx = 2; nx < 49; nx*=2){
+  for(int nx = 4; nx < 129; nx*=2){
     printf("nx, ny = %d, %d\n", nx, ny);
     test_zind_dd_nxnz(nx,ny);
   }
 }
+
+
+void test_fem_poisson_zind_dd(){
+  printf("\n");
+  for(int nx = 4; nx < 129; nx*=2){
+    printf("nx = %d\n", nx);
+    test_fem_poisson_zind_dd_nx(nx);
+  }
+}
+
 void test_simplez_dd(){
   printf("\n");
   int ny = 32;
-  for(int nx = 2; nx < 49; nx*=2){
+  for(int nx = 4; nx < 129; nx*=2){
     printf("nx, ny = %d, %d\n", nx, ny);
     test_simplez_dd_nxnz(nx,ny);
   }
 }
+
 void test_zdep_nd(){
   printf("\n");
   int ny = 32;
-  for(int nx = 2; nx < 17; nx*=2){
+  for(int nx = 4; nx < 129; nx*=2){
     printf("nx, ny = %d, %d\n", nx, ny);
     test_zdep_nd_nxnz(nx,ny);
   }
 }
 
 
-
 TEST_LIST = {
+  //{ "test_fem_poisson_zind_dd", test_fem_poisson_zind_dd},
   { "test_zind_dd", test_zind_dd},
-  //{ "test_simplez_dd", test_simplez_dd},
-  //{ "test_zdep_nd", test_zdep_nd},
+  { "test_simplez_dd", test_simplez_dd},
+  { "test_zdep_nd", test_zdep_nd},
   { NULL, NULL },
 };
