14c32d26b146c54d5c106d1d0a97562ec4300ea4
akashukla <akashukla@utexas.edu>
2024-01-18 14:27:54 -0500

    Make nodal ops deflated test gpu capable and also add TEST_CHECK of similarity after m2n_deflated then n2m back.

--- unit/ctest_n2m.c
+++ unit/ctest_n2m.c
@@ -13,6 +13,19 @@
 #include <gkyl_deflate_zsurf.h>
 
 
+void check_same(struct gkyl_range range, struct gkyl_basis basis, struct gkyl_array *field1, struct gkyl_array* field2)
+{
+  struct gkyl_range_iter iter;
+  gkyl_range_iter_init(&iter, &range);
+  while (gkyl_range_iter_next(&iter)) {
+    long lidx = gkyl_range_idx(&range, iter.idx);
+    const double *f1 = gkyl_array_cfetch(field1, lidx);
+    const double *f2 = gkyl_array_cfetch(field2, lidx);
+    for(int i = 0; i< basis.num_basis; i++)
+      TEST_CHECK( gkyl_compare(f1[i], f2[i], 1e-12) );
+  }
+}
+
 void
 proj_func(double t, const double *xn, double *fout, void *ctx)
 {
@@ -21,188 +34,225 @@ proj_func(double t, const double *xn, double *fout, void *ctx)
 
 void
 test_p1(){
-  // create RZ grid
+  // create  grid, ranges, basis
   double lower[] = { 0.0, -1.5 }, upper[] = { 1.5, 1.5 };
-  // as ellipitical surfaces are exact, we only need 1 cell in each
-  // direction
-  int cells[] = { 64, 128 };
-
-
-  struct gkyl_rect_grid rzgrid;
-  gkyl_rect_grid_init(&rzgrid, 2, lower, upper, cells);
-
-  // RZ ranges
-  struct gkyl_range rzlocal, rzlocal_ext;
-  int nghost[GKYL_MAX_CDIM] = { 1, 1 };
-  gkyl_create_grid_ranges(&rzgrid, nghost, &rzlocal_ext, &rzlocal);
-
-  // RZ basis function
-  int rzpoly_order = 1;
-  struct gkyl_basis rzbasis;
-  gkyl_cart_modal_serendip(&rzbasis, 2, rzpoly_order);
-
-  struct gkyl_array *funcdg = gkyl_array_new(GKYL_DOUBLE, rzbasis.num_basis, rzlocal_ext.volume);
-  
-  gkyl_eval_on_nodes *eon = gkyl_eval_on_nodes_new(&rzgrid, &rzbasis, 1, &proj_func, 0);
-  gkyl_eval_on_nodes_advance(eon, 0.0, &rzlocal, funcdg);
-  gkyl_eval_on_nodes_release(eon);
-  gkyl_grid_sub_array_write(&rzgrid, &rzlocal, funcdg, "proj_func.gkyl");
-
-  int poly_order = rzpoly_order;
-  int nodes[3] = { 1, 1, 1 };
-  if (poly_order == 1){
-    for (int d=0; d<rzgrid.ndim; ++d)
-      nodes[d] = rzgrid.cells[d] + 1;
-  }
-                   
-  if (poly_order == 2){
-    for (int d=0; d<rzgrid.ndim; ++d)
-      nodes[d] = 2*(rzgrid.cells[d]) + 1;
-  }
-
-  struct gkyl_range nrange;
-  gkyl_range_init_from_shape(&nrange, rzgrid.ndim, nodes);
-
-
-  struct gkyl_array* nodal_fld = gkyl_array_new(GKYL_DOUBLE, rzgrid.ndim, nrange.volume);
-  struct gkyl_nodal_ops *n2m = gkyl_nodal_ops_new(&rzbasis, &rzgrid, false);
-  gkyl_nodal_ops_m2n(n2m, &rzbasis, &rzgrid, &nrange, &rzlocal, 1, nodal_fld, funcdg);
-
-  struct gkyl_array *funcdg2 = gkyl_array_new(GKYL_DOUBLE, rzbasis.num_basis, rzlocal_ext.volume);
-  gkyl_nodal_ops_n2m(n2m, &rzbasis, &rzgrid, &nrange, &rzlocal, 1, nodal_fld, funcdg2);
-  gkyl_nodal_ops_release(n2m);
-  gkyl_grid_sub_array_write(&rzgrid, &rzlocal, funcdg2, "proj_func2.gkyl");
-
-}
-
-void
-test_p2(){
-  // create RZ grid
-  double lower[] = { 0.0, -1.5 }, upper[] = { 1.5, 1.5 };
-  // as ellipitical surfaces are exact, we only need 1 cell in each
-  // direction
-  int cells[] = { 64, 128 };
-
-
-  struct gkyl_rect_grid rzgrid;
-  gkyl_rect_grid_init(&rzgrid, 2, lower, upper, cells);
-
-  // RZ ranges
-  struct gkyl_range rzlocal, rzlocal_ext;
-  int nghost[GKYL_MAX_CDIM] = { 1, 1 };
-  gkyl_create_grid_ranges(&rzgrid, nghost, &rzlocal_ext, &rzlocal);
-
-  // RZ basis function
-  int rzpoly_order = 2;
-  struct gkyl_basis rzbasis;
-  gkyl_cart_modal_serendip(&rzbasis, 2, rzpoly_order);
-
-  struct gkyl_array *funcdg = gkyl_array_new(GKYL_DOUBLE, rzbasis.num_basis, rzlocal_ext.volume);
-  
-  gkyl_eval_on_nodes *eon = gkyl_eval_on_nodes_new(&rzgrid, &rzbasis, 1, &proj_func, 0);
-  gkyl_eval_on_nodes_advance(eon, 0.0, &rzlocal, funcdg);
-  gkyl_eval_on_nodes_release(eon);
-  gkyl_grid_sub_array_write(&rzgrid, &rzlocal, funcdg, "proj_func.gkyl");
-
-  int poly_order = rzpoly_order;
-  int nodes[3] = { 1, 1, 1 };
-  if (poly_order == 1){
-    for (int d=0; d<rzgrid.ndim; ++d)
-      nodes[d] = rzgrid.cells[d] + 1;
-  }
-                   
-  if (poly_order == 2){
-    for (int d=0; d<rzgrid.ndim; ++d)
-      nodes[d] = 2*(rzgrid.cells[d]) + 1;
-  }
-
-  struct gkyl_range nrange;
-  gkyl_range_init_from_shape(&nrange, rzgrid.ndim, nodes);
-
-
-  struct gkyl_array* nodal_fld = gkyl_array_new(GKYL_DOUBLE, rzgrid.ndim, nrange.volume);
-  struct gkyl_nodal_ops *n2m = gkyl_nodal_ops_new(&rzbasis, &rzgrid, false);
-  gkyl_nodal_ops_m2n(n2m, &rzbasis, &rzgrid, &nrange, &rzlocal, 1, nodal_fld, funcdg);
-
-  struct gkyl_array *funcdg2 = gkyl_array_new(GKYL_DOUBLE, rzbasis.num_basis, rzlocal_ext.volume);
-  gkyl_nodal_ops_n2m(n2m, &rzbasis, &rzgrid, &nrange, &rzlocal, 1, nodal_fld, funcdg2);
-  gkyl_nodal_ops_release(n2m);
-  gkyl_grid_sub_array_write(&rzgrid, &rzlocal, funcdg2, "proj_func2.gkyl");
-
-}
-
-void
-test_p1_cu(){
-  // create RZ grid
-  double lower[] = { 0.0, -1.5 }, upper[] = { 1.5, 1.5 };
-  // as ellipitical surfaces are exact, we only need 1 cell in each
-  // direction
   int cells[] = { 8, 16 };
-
-
-  struct gkyl_rect_grid rzgrid;
-  gkyl_rect_grid_init(&rzgrid, 2, lower, upper, cells);
-
-  // RZ ranges
-  struct gkyl_range rzlocal, rzlocal_ext;
+  struct gkyl_rect_grid grid;
+  gkyl_rect_grid_init(&grid, 2, lower, upper, cells);
+  struct gkyl_range local, local_ext;
   int nghost[GKYL_MAX_CDIM] = { 1, 1 };
-  gkyl_create_grid_ranges(&rzgrid, nghost, &rzlocal_ext, &rzlocal);
-
-  // RZ basis function
-  int rzpoly_order = 1;
-  struct gkyl_basis rzbasis;
-  gkyl_cart_modal_serendip(&rzbasis, 2, rzpoly_order);
-
-  struct gkyl_basis *rzbasis_on_dev = gkyl_cu_malloc(sizeof(struct gkyl_basis));
-  gkyl_cart_modal_serendip_cu_dev(rzbasis_on_dev, 2, rzpoly_order);
-
-  struct gkyl_array *funcdg = gkyl_array_new(GKYL_DOUBLE, rzbasis.num_basis, rzlocal_ext.volume);
+  gkyl_create_grid_ranges(&grid, nghost, &local_ext, &local);
+  int poly_order = 1;
+  struct gkyl_basis basis;
+  gkyl_cart_modal_serendip(&basis, 2, poly_order);
+  bool use_gpu = false;
+#ifdef GKYL_HAVE_CUDA
+  use_gpu = true;
+  struct gkyl_basis *basis_on_dev = gkyl_cu_malloc(sizeof(struct gkyl_basis));
+  gkyl_cart_modal_serendip_cu_dev(basis_on_dev, 2, poly_order);
+#else
+  struct gkyl_basis *basis_on_dev = &basis;
+#endif
   
-  gkyl_eval_on_nodes *eon = gkyl_eval_on_nodes_new(&rzgrid, &rzbasis, 1, &proj_func, 0);
-  gkyl_eval_on_nodes_advance(eon, 0.0, &rzlocal, funcdg);
-  gkyl_eval_on_nodes_release(eon);
-  gkyl_grid_sub_array_write(&rzgrid, &rzlocal, funcdg, "proj_func.gkyl");
-  struct gkyl_array *funcdg_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, rzbasis.num_basis, rzlocal_ext.volume);
+  // Project initial function
+  struct gkyl_array *funcdg = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+  struct gkyl_eval_on_nodes *eon = gkyl_eval_on_nodes_new(&grid, &basis, 1, &proj_func, 0);
+  gkyl_eval_on_nodes_advance(eon, 0.0, &local, funcdg);
+  gkyl_grid_sub_array_write(&grid, &local, funcdg, "proj_func.gkyl");
+#ifdef GKYL_HAVE_CUDA
+  struct gkyl_array *funcdg_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
   gkyl_array_copy(funcdg_dev, funcdg);
+#else
+  struct gkyl_array *funcdg_dev = funcdg;
+#endif
 
-  int poly_order = rzpoly_order;
+  // Construct nrange and nodal field
   int nodes[3] = { 1, 1, 1 };
-  if (poly_order == 1){
-    for (int d=0; d<rzgrid.ndim; ++d)
-      nodes[d] = rzgrid.cells[d] + 1;
-  }
-                   
-  if (poly_order == 2){
-    for (int d=0; d<rzgrid.ndim; ++d)
-      nodes[d] = 2*(rzgrid.cells[d]) + 1;
-  }
-
+  for (int d=0; d<grid.ndim; ++d)
+    nodes[d] = grid.cells[d] + 1;
   struct gkyl_range nrange;
-  gkyl_range_init_from_shape(&nrange, rzgrid.ndim, nodes);
+  gkyl_range_init_from_shape(&nrange, grid.ndim, nodes);
+  struct gkyl_array* nodal_fld = gkyl_array_new(GKYL_DOUBLE, grid.ndim, nrange.volume);
+#ifdef GKYL_HAVE_CUDA
+  struct gkyl_array *nodal_fld_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, grid.ndim, nrange.volume);
+#else
+  struct gkyl_array *nodal_fld_dev = nodal_fld;
+#endif
 
 
-  struct gkyl_array* nodal_fld_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, rzgrid.ndim, nrange.volume);
-  struct gkyl_nodal_ops *n2m = gkyl_nodal_ops_new(&rzbasis, &rzgrid, true);
-  gkyl_nodal_ops_m2n(n2m, rzbasis_on_dev, &rzgrid, &nrange, &rzlocal, 1, nodal_fld_dev, funcdg_dev);
+  // Create output field
+  struct gkyl_array *funcdg2 = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+#ifdef GKYL_HAVE_CUDA
+  struct gkyl_array *funcdg2_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+#else
+  struct gkyl_array *funcdg2_dev = funcdg2;
+#endif
 
-  struct gkyl_array *funcdg2 = gkyl_array_new(GKYL_DOUBLE, rzbasis.num_basis, rzlocal_ext.volume);
-  struct gkyl_array *funcdg2_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, rzbasis.num_basis, rzlocal_ext.volume);
-  gkyl_nodal_ops_n2m(n2m, rzbasis_on_dev, &rzgrid, &nrange, &rzlocal, 1, nodal_fld_dev, funcdg2_dev);
-  gkyl_nodal_ops_release(n2m);
+  // Trnasform forward and back
+  struct gkyl_nodal_ops *n2m = gkyl_nodal_ops_new(&basis, &grid, use_gpu);
+  gkyl_nodal_ops_m2n(n2m, basis_on_dev, &grid, &nrange, &local, 1, nodal_fld_dev, funcdg_dev);
+  gkyl_nodal_ops_n2m(n2m, basis_on_dev, &grid, &nrange, &local, 1, nodal_fld_dev, funcdg2_dev);
+
+#ifdef GKYL_HAVE_CUDA
   gkyl_array_copy(funcdg2, funcdg2_dev);
-  gkyl_grid_sub_array_write(&rzgrid, &rzlocal, funcdg2, "proj_func2.gkyl");
+#endif
+  check_same(local, basis, funcdg, funcdg2);
+  gkyl_grid_sub_array_write(&grid, &local, funcdg2, "proj_func2.gkyl");
+
+
+
+#ifdef GKYL_HAVE_CUDA
+  gkyl_cu_free(basis_on_dev);
+  gkyl_array_release(funcdg_dev);
+  gkyl_array_release(funcdg2_dev);
+  gkyl_array_release(nodal_fld_dev);
+#endif
+  gkyl_array_release(funcdg);
+  gkyl_array_release(funcdg2);
+  gkyl_array_release(nodal_fld);
+  gkyl_eval_on_nodes_release(eon);
+  gkyl_nodal_ops_release(n2m);
 
 }
 
 void
 test_p1_deflated(){
+  // create  grid, ranges, basis
+  double lower[] = { 0.0, -1.5 }, upper[] = { 1.5, 1.5 };
+  int cells[] = { 8, 16 };
+  struct gkyl_rect_grid grid;
+  gkyl_rect_grid_init(&grid, 2, lower, upper, cells);
+  struct gkyl_range local, local_ext;
+  int nghost[GKYL_MAX_CDIM] = { 1, 1 };
+  gkyl_create_grid_ranges(&grid, nghost, &local_ext, &local);
+  int poly_order = 1;
+  struct gkyl_basis basis;
+  gkyl_cart_modal_serendip(&basis, 2, poly_order);
+  bool use_gpu = false;
+#ifdef GKYL_HAVE_CUDA
+  use_gpu = true;
+  struct gkyl_basis *basis_on_dev = gkyl_cu_malloc(sizeof(struct gkyl_basis));
+  gkyl_cart_modal_serendip_cu_dev(basis_on_dev, 2, poly_order);
+#else
+  struct gkyl_basis *basis_on_dev = &basis;
+#endif
+
+  // Project Initial Function
+  struct gkyl_array *funcdg = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+  gkyl_eval_on_nodes *eon = gkyl_eval_on_nodes_new(&grid, &basis, 1, &proj_func, 0);
+  gkyl_eval_on_nodes_advance(eon, 0.0, &local, funcdg);
+  gkyl_grid_sub_array_write(&grid, &local, funcdg, "proj_func.gkyl");
+#ifdef GKYL_HAVE_CUDA
+  struct gkyl_array *funcdg_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+  gkyl_array_copy(funcdg_dev, funcdg);
+#else
+  struct gkyl_array *funcdg_dev = funcdg;
+#endif
+
+  // Construct Nodal range, field, and nodal ops object
+  int nodes[3] = { 1, 1, 1 };
+  for (int d=0; d<grid.ndim; ++d)
+    nodes[d] = grid.cells[d] + 1;
+  struct gkyl_range nrange;
+  gkyl_range_init_from_shape(&nrange, grid.ndim, nodes);
+  struct gkyl_array* nodal_fld = gkyl_array_new(GKYL_DOUBLE, grid.ndim, nrange.volume);
+#ifdef GKYL_HAVE_CUDA
+  struct gkyl_array *nodal_fld_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, grid.ndim, nrange.volume);
+#else
+  struct gkyl_array *nodal_fld_dev = nodal_fld;
+#endif
+  struct gkyl_nodal_ops *n2m = gkyl_nodal_ops_new(&basis, &grid, use_gpu);
+
+  // Create deflated 1d grid, ranges, basis, nodal range
+  struct gkyl_rect_grid deflated_grid;
+  struct gkyl_basis deflated_basis;
+  struct gkyl_range deflated_local, deflated_local_ext;
+  double deflated_lower[1] = { grid.lower[0]}, deflated_upper[1] = { grid.upper[0]};
+  int deflated_cells[1] = { grid.cells[0] };
+  gkyl_rect_grid_init(&deflated_grid, 1, deflated_lower, deflated_upper, deflated_cells);
+  int deflated_nghost[GKYL_MAX_CDIM] = { 1 };
+  gkyl_create_grid_ranges(&deflated_grid, deflated_nghost, &deflated_local_ext, &deflated_local);
+  gkyl_cart_modal_serendip(&deflated_basis, 1, poly_order);
+#ifdef GKYL_HAVE_CUDA
+  use_gpu = true;
+  struct gkyl_basis *deflated_basis_on_dev = gkyl_cu_malloc(sizeof(struct gkyl_basis));
+  gkyl_cart_modal_serendip_cu_dev(deflated_basis_on_dev, deflated_grid.ndim, poly_order);
+#else
+  struct gkyl_basis *deflated_basis_on_dev = &deflated_basis;
+#endif
+  int deflated_nodes[1];
+  for (int d=0; d<deflated_grid.ndim; ++d)
+    deflated_nodes[d] = deflated_grid.cells[d] + 1;
+  struct gkyl_range deflated_nrange;
+  gkyl_range_init_from_shape(&deflated_nrange, deflated_grid.ndim, deflated_nodes);
+
+  // Create deflated modal field 
+  struct gkyl_array *deflated_funcdg = gkyl_array_new(GKYL_DOUBLE, deflated_basis.num_basis, local_ext.volume);
+#ifdef GKYL_HAVE_CUDA
+  struct gkyl_array *deflated_funcdg_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, deflated_basis.num_basis, local_ext.volume);
+#else
+  struct gkyl_array *deflated_funcdg_dev = deflated_funcdg;
+#endif
+
+  // Created deflator objects and 1d nodal ops object
+  struct gkyl_deflate_zsurf *deflator_lo = gkyl_deflate_zsurf_new(&basis, &deflated_basis, 0, use_gpu);
+  struct gkyl_deflate_zsurf *deflator_up = gkyl_deflate_zsurf_new(&basis, &deflated_basis, 1, use_gpu);
+  struct gkyl_nodal_ops *n2m_1d = gkyl_nodal_ops_new(&deflated_basis, &deflated_grid, use_gpu);
+
+  // Loop along last dim (z) and call the 1d deflator to populate the 2d nodal field
+  for(int zidx = local.lower[1]; zidx <= local.upper[1]; zidx++){
+    gkyl_deflate_zsurf_advance(deflator_lo, zidx, &local, &deflated_local, funcdg_dev, deflated_funcdg_dev, 1);
+    gkyl_nodal_ops_m2n_deflated(n2m_1d, deflated_basis_on_dev, &deflated_grid, &nrange, &deflated_nrange, &deflated_local, 1, nodal_fld_dev, deflated_funcdg_dev, zidx-1);
+    if (zidx == local.upper[1]) {
+      gkyl_deflate_zsurf_advance(deflator_up, zidx, &local, &deflated_local, funcdg_dev, deflated_funcdg_dev, 1);
+      gkyl_nodal_ops_m2n_deflated(n2m_1d, deflated_basis_on_dev, &deflated_grid, &nrange, &deflated_nrange, &deflated_local, 1, nodal_fld_dev, deflated_funcdg_dev, zidx);
+    }
+  }
+
+  // Create output field
+  struct gkyl_array *funcdg2 = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+#ifdef GKYL_HAVE_CUDA
+  struct gkyl_array *funcdg2_dev = gkyl_array_cu_dev_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+#else
+  struct gkyl_array *funcdg2_dev = funcdg2;
+#endif
+
+  // Transform back to modal
+  gkyl_nodal_ops_n2m(n2m, basis_on_dev, &grid, &nrange, &local, 1, nodal_fld_dev, funcdg2_dev);
+#ifdef GKYL_HAVE_CUDA
+  gkyl_array_copy(funcdg2, funcdg2_dev);
+#endif
+  check_same(local, basis, funcdg, funcdg2);
+  gkyl_grid_sub_array_write(&grid, &local, funcdg2, "proj_func2.gkyl");
+
+#ifdef GKYL_HAVE_CUDA
+  gkyl_cu_free(basis_on_dev);
+  gkyl_array_release(funcdg_dev);
+  gkyl_array_release(deflated_funcdg_dev);
+  gkyl_array_release(funcdg2_dev);
+  gkyl_array_release(nodal_fld_dev);
+#endif
+  gkyl_array_release(funcdg);
+  gkyl_array_release(deflated_funcdg);
+  gkyl_array_release(funcdg2);
+  gkyl_array_release(nodal_fld);
+
+  gkyl_eval_on_nodes_release(eon);
+  gkyl_nodal_ops_release(n2m);
+  gkyl_nodal_ops_release(n2m_1d);
+
+}
+
+void
+test_p2(){
   // create  grid
   double lower[] = { 0.0, -1.5 }, upper[] = { 1.5, 1.5 };
   // as ellipitical surfaces are exact, we only need 1 cell in each
   // direction
-  int cells[] = { 4, 6 };
+  int cells[] = { 64, 128 };
 
 
+  int poly_order = 2;
   struct gkyl_rect_grid grid;
   gkyl_rect_grid_init(&grid, 2, lower, upper, cells);
 
@@ -212,7 +262,6 @@ test_p1_deflated(){
   gkyl_create_grid_ranges(&grid, nghost, &local_ext, &local);
 
   //  basis function
-  int poly_order = 1;
   struct gkyl_basis basis;
   gkyl_cart_modal_serendip(&basis, 2, poly_order);
 
@@ -240,47 +289,7 @@ test_p1_deflated(){
 
   struct gkyl_array* nodal_fld = gkyl_array_new(GKYL_DOUBLE, grid.ndim, nrange.volume);
   struct gkyl_nodal_ops *n2m = gkyl_nodal_ops_new(&basis, &grid, false);
-
-  // Create deflated 1d grid, ranges, basis, and nodal range
-  struct gkyl_rect_grid deflated_grid;
-  struct gkyl_basis deflated_basis;
-  struct gkyl_range deflated_local, deflated_local_ext;
-  double deflated_lower[1] = { grid.lower[0]}, deflated_upper[1] = { grid.upper[0]};
-  int deflated_cells[1] = { grid.cells[0] };
-  gkyl_rect_grid_init(&deflated_grid, 1, deflated_lower, deflated_upper, deflated_cells);
-  int deflated_nghost[GKYL_MAX_CDIM] = { 1 };
-  gkyl_create_grid_ranges(&deflated_grid, deflated_nghost, &deflated_local_ext, &deflated_local);
-  gkyl_cart_modal_serendip(&deflated_basis, 1, poly_order);
-
-  struct gkyl_basis *deflated_basis_on_dev = &deflated_basis;
-  nodal_fld = gkyl_array_new(GKYL_DOUBLE, grid.ndim, nrange.volume);
-
-  int deflated_nodes[1];
-  if (poly_order == 1){
-    for (int d=0; d<deflated_grid.ndim; ++d)
-      deflated_nodes[d] = deflated_grid.cells[d] + 1;
-  }
-  if (poly_order == 2){
-    for (int d=0; d<deflated_grid.ndim; ++d)
-      deflated_nodes[d] = 2*(deflated_grid.cells[d]) + 1;
-  }
-  struct gkyl_range deflated_nrange;
-  gkyl_range_init_from_shape(&deflated_nrange, deflated_grid.ndim, deflated_nodes);
-  struct gkyl_array *deflated_phi = gkyl_array_new(GKYL_DOUBLE, deflated_basis.num_basis, deflated_nrange.volume);
-
-  struct gkyl_deflate_zsurf *deflator_lo = gkyl_deflate_zsurf_new(&basis, &deflated_basis, 0, false);
-  struct gkyl_deflate_zsurf *deflator_up = gkyl_deflate_zsurf_new(&basis, &deflated_basis, 1, false);
-  struct gkyl_nodal_ops *n2m_1d = gkyl_nodal_ops_new(&deflated_basis, &deflated_grid, false);
-
-  for(int zidx = local.lower[1]; zidx <= local.upper[1]; zidx++){
-    gkyl_deflate_zsurf_advance(deflator_lo, zidx, &local, &deflated_local, funcdg, deflated_phi, 1);
-    gkyl_nodal_ops_m2n_deflated(n2m_1d, deflated_basis_on_dev, &deflated_grid, &nrange, &deflated_nrange, &deflated_local, 1, nodal_fld, deflated_phi, zidx-1);
-    if (zidx == local.upper[1]) {
-      gkyl_deflate_zsurf_advance(deflator_up, zidx, &local, &deflated_local, funcdg, deflated_phi, 1);
-      gkyl_nodal_ops_m2n_deflated(n2m_1d, deflated_basis_on_dev, &deflated_grid, &nrange, &deflated_nrange, &deflated_local, 1, nodal_fld, deflated_phi, zidx);
-    }
-  }
-
+  gkyl_nodal_ops_m2n(n2m, &basis, &grid, &nrange, &local, 1, nodal_fld, funcdg);
 
   struct gkyl_array *funcdg2 = gkyl_array_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
   gkyl_nodal_ops_n2m(n2m, &basis, &grid, &nrange, &local, 1, nodal_fld, funcdg2);
@@ -296,12 +305,11 @@ test_p1_deflated(){
 
 
 
+
+
 TEST_LIST = {
-  //{ "test_p1", test_p1},
+  { "test_p1", test_p1},
   { "test_p1_deflated", test_p1_deflated},
   //{ "test_p2", test_p2},
-#ifdef GKYL_HAVE_CUDA
-  { "test_p1_cu", test_p1_cu},
-#endif
   { NULL, NULL },
 };
