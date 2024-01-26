6f839975b0adeee22adc2450ea3c893211e70b91
Junoravin <junoravin@gmail.com>
2024-01-26 13:41:14 -0500

    Commiting a GPU version of mom_cross_bgk. Will move to my workstation to make sure this compiles

--- zero/gkyl_mom_cross_bgk.h
+++ zero/gkyl_mom_cross_bgk.h
@@ -10,11 +10,18 @@ typedef struct gkyl_mom_cross_bgk_gyrokinetic gkyl_mom_cross_bgk_gyrokinetic;
 /**
  * Allocate memory for use in cross moments calculation.
  *
- * @ param phase_basis Phase space basis functions
- * @ param conf_basis Configuration space basis functions
- * @ param use_gpu Boolian to determine if on GPU
+ * @param phase_basis Phase space basis functions
+ * @param conf_basis Configuration space basis functions
+ * @param use_gpu Boolian to determine if on GPU
  */
 gkyl_mom_cross_bgk_gyrokinetic* gkyl_mom_cross_bgk_gyrokinetic_new(
+  const struct gkyl_basis *phase_basis, const struct gkyl_basis *conf_basis, bool use_gpu);
+
+/**
+ * Create new updater to compute cross BGK moments on
+ * NV-GPU. See new() method for documentation.
+ */
+gkyl_mom_cross_bgk_gyrokinetic* gkyl_mom_cross_bgk_gyrokinetic_cu_dev_new(
   const struct gkyl_basis *phase_basis, const struct gkyl_basis *conf_basis, bool use_gpu);
 
 /**
@@ -33,22 +40,26 @@ gkyl_mom_cross_bgk_gyrokinetic* gkyl_mom_cross_bgk_gyrokinetic_new(
  */
 void gkyl_mom_cross_bgk_gyrokinetic_advance(
   gkyl_mom_cross_bgk_gyrokinetic *up,
-  const struct gkyl_range *conf_rng, const double beta,
-  const double m_self, const struct gkyl_array *moms_self,
-  const double m_other, const struct gkyl_array *moms_other,
+  const struct gkyl_range *conf_rng, double beta,
+  double m_self, const struct gkyl_array *moms_self,
+  double m_other, const struct gkyl_array *moms_other,
   const struct gkyl_array *nu_sr, const struct gkyl_array *nu_rs, 
   struct gkyl_array *moms_cross);
-/*
-void gkyl_mom_cross_bgk_gyrokinetic_advance_cu(
-  gkyl_mom_cross_bgk_gyrokinetic *up,
-  const struct gkyl_range *conf_rng, const double beta,
-  const double m_self, const struct gkyl_array *moms_self,
-  const double m_other, const struct gkyl_array *moms_other,
-  const double nu_sr, const double nu_rs, struct gkyl_array *moms_cross);
-*/
+
 /**
  * Release memory needed in the cross moments calculation.
  *
  * @param up Memory to release.
  */
 void gkyl_mom_cross_bgk_gyrokinetic_release(gkyl_mom_cross_bgk_gyrokinetic *up);
+
+/**
+ * Host-side wrappers for cross BGK moments operations on device
+ */
+void gkyl_mom_cross_bgk_gyrokinetic_advance_cu(
+  gkyl_mom_cross_bgk_gyrokinetic *up,
+  const struct gkyl_range *conf_rng, double beta,
+  double m_self, const struct gkyl_array *moms_self,
+  double m_other, const struct gkyl_array *moms_other,
+  const struct gkyl_array *nu_sr, const struct gkyl_array *nu_rs, 
+  struct gkyl_array *moms_cross);
--- zero/gkyl_mom_cross_bgk_priv.h
+++ zero/gkyl_mom_cross_bgk_priv.h
@@ -46,16 +46,11 @@ static gyrokinetic_mom_cross_bgk_t
 choose_mom_cross_bgk_gyrokinetic_kern(int cdim, int vdim, int poly_order){
   return ser_mom_cross_gyrokinetic_list[cv_index[cdim].vdim[vdim]].kernels[poly_order];
 }
-/*
-static gyrokinetic_mom_cross_bgk_t
-choose_mom_cross_bgk_gyrokinetic_kern_cu(int cdim, int vdim, int poly_order){
-  return ser_mom_cross_gyrokinetic_list[cv_index[cdim].vdim[vdim]].kernels[poly_order];
-}
-*/
 
 // Primary struct in this updater
 struct gkyl_mom_cross_bgk_gyrokinetic {
   bool use_gpu;
   gyrokinetic_mom_cross_bgk_t mom_cross_calc; // a pointer to cross moments kernel
-  //gyrokinetic_mom_cross_bgk_t mom_cross_calc_cu; // device copy
+
+  struct gkyl_mom_cross_bgk_gyrokinetic *on_dev; 
 };
