128c2b15fa0550997f28aa0d08a617184cf159b3
tnbernard <tess.bernard@gmail.com>
2024-01-31 10:56:02 -0800

    download-adas.sh now copies .npy files to data/adas directory.

--- install-deps/download-adas.sh
+++ install-deps/download-adas.sh
@@ -10,7 +10,8 @@ python ../data/adas/download_adas.py
 echo "Converting ADAS data to numpy .."
 python ../data/adas/adas_to_numpy.py
 rm *.dat
-mv *.npy $ADAS_DIR/.
+cp *.npy $ADAS_DIR/.
+mv *.npy ../data/adas/.
 echo "ADAS data downloaded to $ADAS_DIR"
 
 # # Install prefix
