e03f9cc5fdcf7fda96457b4b038f47da5844d557
JonathanGorard <jg865@cantab.ac.uk>
2024-02-22 10:12:43 -0500

    Hard-coded some prototype infrastructure for static refinement in 10-moment equations.

--- amr/gkyl_amr_block_priv.h
+++ amr/gkyl_amr_block_priv.h
@@ -24,6 +24,7 @@
 #include <gkyl_wave_prop.h>
 #include <gkyl_wv_apply_bc.h>
 #include <gkyl_wv_euler.h>
+#include <gkyl_wv_ten_moment.h>
 #include <gkyl_wv_maxwell.h>
 #include <rt_arg_parse.h>
 
@@ -214,7 +215,153 @@ void euler_write_sol(const char* fbase, int num_blocks, const struct euler_block
 */
 double euler_max_dt(int num_blocks, const struct euler_block_data bdata[]);
 
+// Block-structured data (including grid, range and boundary condition information) for the 10-moment equations.
+struct ten_moment_block_data {
+  struct gkyl_rect_grid grid;
+  gkyl_fv_proj *fv_proj;
+  
+  struct gkyl_range ext_range, range;
+  struct gkyl_array *fdup;
+  struct gkyl_array *f[9];
+  
+  struct gkyl_wave_geom *geom;
+  
+  struct gkyl_wv_eqn *ten_moment;
+  gkyl_wave_prop *slvr[2];
+  
+  struct skin_ghost_ranges skin_ghost;
+  struct gkyl_array *bc_buffer;
+  
+  struct gkyl_wv_apply_bc *lower_bc[2];
+  struct gkyl_wv_apply_bc *upper_bc[2];
+};
+
+// Job pool information context for updating block-structured data for the 10-moment equations using threads.
+struct ten_moment_update_block_ctx {
+  const struct ten_moment_block_data *bdata;
+  int bidx;
+  int dir;
+  double t_curr;
+  double dt;
+  struct gkyl_wave_prop_status stat;
+};
+
+/**
+* Boundary condition function for applying transmissive boundary conditions for the 10-moment equations.
+*
+* @param t Current simulation time.
+* @param nc Number of boundary cells to apply transmissive conditions to.
+* @param skin Skin cells in the boundary region (from which values are copied).
+* @param ghost Ghost cells in the boundary region (to which values are copied).
+* @param ctx Context to pass to the function.
+*/
+static void ten_moment_transmissive_bc(double t, int nc, const double* skin, double* GKYL_RESTRICT ghost, void* ctx);
+
+/**
+* Initialize updaters for both physical (outer-block) and non-physical (inter-block) boundary conditions for the 10-moment equations.
+*
+* @param bdata Block-structured data for the 10-moment equations.
+* @param conn Topology/connectivity data for the block hierarchy.
+*/
+void ten_moment_block_bc_updaters_init(struct ten_moment_block_data* bdata, const struct gkyl_block_connections* conn);
+
+/**
+* Release updaters for both physical (outer-block) and non-physical (inter-block) boundary conditions for the 10-moment equations.
+*
+* @param bdata Block-structured data for the 10-moment equations.
+*/
+void ten_moment_block_bc_updaters_release(struct ten_moment_block_data* bdata);
+
+/**
+* Apply both physical (outer-block) and non-physical (inter-block) boundary conditions for the 10-moment equations.
+*
+* @param bdata Block-structured data for the 10-moment equations.
+* @param tm Simulation time at which the boundary conditions are applied.
+* @param fld Output array.
+*/
+void ten_moment_block_bc_updaters_apply(const struct ten_moment_block_data* bdata, double tm, struct gkyl_array* fld);
+
+/**
+* Synchronize all blocks in the block hierarchy by applying all appropriate physical (outer-block) and non-physical (inter-block) boundary conditions for the 10-moment equations.
+*
+* @param btopo Topology/connectivity information for the entire block hierarchy.
+* @param bdata Block-structured data for the 10-moment equations.
+* @param fld Output array.
+*/
+void ten_moment_sync_blocks(const struct gkyl_block_topo* btopo, const struct ten_moment_block_data bdata[], struct gkyl_array* fld[]);
+
+/**
+* Write block-structured simulation data for the 10-moment equations onto disk.
+*
+* @param fileNm File name schema to use for the simulation output.
+* @param bdata Block-structured data for the 10-moment equations.
+*/
+void ten_moment_block_data_write(const char* fileNm, const struct ten_moment_block_data* bdata);
+
+/**
+* Calculate the maximum stable time-step for the block-structured 10-moment equations.
+*
+* @param bdata Block-structured data for the 10-moment equations.
+*/
+double ten_moment_block_data_max_dt(const struct ten_moment_block_data* bdata);
+
+/**
+* Update the block-structured 10-moment equations simulation data using the thread-based job pool.
+*
+* @param ctx Context to pass to the function.
+*/
+void ten_moment_update_block_job_func(void *ctx);
+
+/**
+* Update all blocks in the block hierarchy by using the thread-based job pool for the 10-moment equations.
+*
+* @param job_pool Job pool for updating block-structured data for the 10-moment equations using threads.
+* @param btopo Topology/connectivity informations for the entire block hierarchy.
+* @param bdata Block-structured data for the 10-moment equations.
+* @param t_curr Current simulation time.
+* @param dt Current stable time-step for the simulation.
+*/
+struct gkyl_update_status ten_moment_update_all_blocks(const struct gkyl_job_pool* job_pool, const struct gkyl_block_topo* btopo,
+  const struct ten_moment_block_data bdata[], double t_curr, double dt);
+
+/**
+* Initialize a new job in the thread-based job pool for updating the block-structured 10-moment equations simulation data.
+*
+* @param ctx Context to pass to the function.
+*/
+void ten_moment_init_job_func(void* ctx);
+
 /**
 * Set up the topology/connectivity information for the block hierarchy for a mesh with a single refinement patch.
 */
 struct gkyl_block_topo* create_block_topo();
+
+/**
+* Take a single time step across the entire block hierarchy for the 10-moment equations.
+*
+* @param job_pool Job pool for updating block-structured data for the 10-moment equations using threads.
+* @param btopo Topology/connectivity information for the entire block hierarchy.
+* @param bdata Block-structured data for the 10-moment equations.
+* @param t_curr Current simulation time.
+* @param dt0 Initial guess for the maximum stable time-step.
+* @param stats Simulation statistics (allowing for tracking of the number of failed time-steps).
+*/
+struct gkyl_update_status ten_moment_update(const struct gkyl_job_pool* job_pool, const struct gkyl_block_topo* btopo,
+  const struct ten_moment_block_data bdata[], double t_curr, double dt0, struct sim_stats* stats);
+
+/**
+* Write the complete simulation output for the entire block hierarchy for the 10-moment equations onto disk.
+*
+* @param fbase Base file name schema to use for the simulation output.
+* @param num_blocks Number of blocks in the block hierarchy.
+* @param bdata Array of block-structured data for the 10-moment equations.
+*/
+void ten_moment_write_sol(const char* fbase, int num_blocks, const struct ten_moment_block_data bdata[]);
+
+/**
+* Calculate the maximum stable time-step across all blocks in the block hierarchy for the 10-moment equations.
+*
+* @param num_blocks Number of blocks in the block hierarchy.
+* @param bdata Array of block-structured data for the 10-moment equations.
+*/
+double ten_moment_max_dt(int num_blocks, const struct ten_moment_block_data bdata[]);
--- amr/gkyl_amr_core.h
+++ amr/gkyl_amr_core.h
@@ -34,3 +34,35 @@ struct euler2d_level1_init {
 * @param init Initialization data for the 2D Euler equations simulation.
 */
 void euler2d_run_level1(int argc, char **argv, struct euler2d_level1_init* init);
+
+// Initialization data for a 2D 10-moment equations simulation, run using static, block-structured mesh refinement with a single refinement patch of level 1.
+struct ten_moment_2d_level1_init {
+    long baseNx;
+    long baseNy;
+    long ref_factor;
+
+    double coarse_x1;
+    double coarse_y1;
+    double coarse_x2;
+    double coarse_y2;
+
+    double refined_x1;
+    double refined_y1;
+    double refined_x2;
+    double refined_y2;
+
+    evalf_t eval;
+    double k0;
+
+    double cfl_frac;
+    double t_end;
+};
+
+/**
+* Run a 2D 10-moment equations simulation, using static, block-structured mesh refinement with a single refinement patch of level 1.
+*
+* @param argc Number of command line arguments passed to the function.
+* @param argv Array of command line arguments passed to the function.
+* @param init Initialization data for the 2D 10-moment equations simulation.
+*/
+void ten_moment_2d_run_level1(int argc, char **argv, struct ten_moment_2d_level1_init* init);
