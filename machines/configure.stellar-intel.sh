d4fdae204235f7cb69e9d7626ecca7e965d4e7e2
akashukla <akashukla@utexas.edu>
2024-03-07 13:56:22 -0500

    Remove unnecessary include paths in stellar intel configure file

--- machines/configure.stellar-intel.sh
+++ machines/configure.stellar-intel.sh
@@ -2,5 +2,5 @@ module load intel/2021.1.2
 module load openmpi/intel-2021.1/4.1.2 
 
 : "${PREFIX:=$HOME/gkylsoft}"
-./configure CC=cc --prefix=$PREFIX --lapack-inc=$PREFIX/OpenBLAS/include --lapack-lib=$PREFIX/OpenBLAS/lib/libopenblas.a --superlu-inc=$PREFIX/superlu/include --superlu-lib=$PREFIX/superlu/lib/libsuperlu.a --use-mpi=yes --mpi-inc=$MPI_HOME/include --mpi-lib=$MPI_HOME/lib64 
+./configure CC=cc --prefix=$PREFIX --use-mpi=yes --mpi-inc=$MPI_HOME/include --mpi-lib=$MPI_HOME/lib64 --use-adas=yes
 
