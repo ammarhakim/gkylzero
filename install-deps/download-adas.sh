e822b1d176aa7bcf4687ba122e236e21fe70502e
Tess Bernard <tnbernar@login39.chn.perlmutter.nersc.gov>
2024-03-11 16:38:33 -0700

    Adding command to create share/adas dir in case it doesn't exist.

--- install-deps/download-adas.sh
+++ install-deps/download-adas.sh
@@ -4,12 +4,14 @@ source ./build-opts.sh
 
 # Install prefix
 PREFIX=$GKYLSOFT
-ADAS_DIR="$GKYLSOFT/gkylzero/share/adas"
+ADAS_DIR="$GKYLSOFT/gkylzero/share"
 
+mkdir $ADAS_DIR/adas
+#cd -
 python ../data/adas/download_adas.py
 echo "Converting ADAS data to numpy .."
 python ../data/adas/adas_to_numpy.py
 rm *.dat
-cp *.npy $ADAS_DIR/.
+cp *.npy $ADAS_DIR/adas/.
 mv *.npy ../data/adas/.
-echo "ADAS data downloaded to $ADAS_DIR"
+echo "ADAS data downloaded to $ADAS_DIR/adas"
