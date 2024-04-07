5b842dae3483436e85176ef9aad56cde0a50c0bd
ammarhakim <a.hakim777@gmail.com>
2024-04-07 12:58:25 -0400

    Refacting the Lua-C code to remove common structs and functions to a new private header and file. This reduces duplication between the various App Lua-C wrappers.

---
+++ apps/gkyl_lw_priv.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#ifdef GKYL_HAVE_LUA
+
+#include <gkyl_basis.h>
+#include <gkyl_util.h>
+
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#include <string.h>
+
+// Get basis type from string
+static enum gkyl_basis_type
+get_basis_type(const char *bnm)
+{
+  if (strcmp(bnm, "serendipity") == 0)
+    return GKYL_BASIS_MODAL_SERENDIPITY;
+  if (strcmp(bnm, "tensor") == 0)
+    return GKYL_BASIS_MODAL_TENSOR;
+  if (strcmp(bnm, "hybrid") == 0)
+    return GKYL_BASIS_MODAL_HYBRID;
+
+  return GKYL_BASIS_MODAL_SERENDIPITY;
+}
+
+// Used in call back passed to the initial conditions
+struct lua_func_ctx {
+  int func_ref; // reference to Lua function in registery
+  int ndim, nret; // dimensions of function, number of return values
+  lua_State *L; // Lua state
+};
+
+/**
+ * Wrapper around Lua function for use in eval callbacks.
+ */
+void gkyl_lw_eval_cb(double t, const double* GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx);
+
+#endif
---
+++ apps/lw_priv.c
@@ -0,0 +1,33 @@
+#ifdef GKYL_HAVE_LUA
+
+#include <gkyl_lw_priv.h>
+
+void
+gkyl_lw_eval_cb(double t, const double* GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct lua_func_ctx *fr = ctx;
+  lua_State *L = fr->L;
+
+  int ndim = fr->ndim;
+  int nret = fr->nret;
+  lua_rawgeti(L, LUA_REGISTRYINDEX, fr->func_ref);
+  lua_pushnumber(L, t);
+  lua_createtable(L, GKYL_MAX_DIM, 0);
+
+  for (int i=0; i<ndim; ++i) {
+    lua_pushnumber(L, xn[i]);
+    lua_rawseti(L, -2, i+1); 
+  }
+
+  if (lua_pcall(L, 2, nret, 0)) {
+    const char* ret = lua_tostring(L, -1);
+    luaL_error(L, "*** eval_ic ERROR: %s\n", ret);
+  }
+
+  for (int i=nret-1; i>=0; --i) { // need to fetch in reverse order
+    fout[i] = lua_tonumber(L, -1);
+    lua_pop(L, 1);
+  }  
+}
+
+#endif
