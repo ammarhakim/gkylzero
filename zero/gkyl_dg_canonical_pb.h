fd6c6c33260cdcab0b1538f6c57ab9ed4469a508
johnson452 <grj@princeton.edu>
2024-04-18 16:15:02 -0400

    Fixed header function signeature for cu file

--- zero/gkyl_dg_canonical_pb.h
+++ zero/gkyl_dg_canonical_pb.h
@@ -36,14 +36,12 @@ struct gkyl_dg_eqn* gkyl_dg_canonical_pb_new(const struct gkyl_basis* cbasis,
  *
  * @param cbasis Configuration space basis functions
  * @param pbasis Phase-space basis functions
- * @param conf_range Configuration space range for use in indexing EM field
- * @param vel_range Velocity space range for use in indexing p/gamma (velocity)
- * @param field_id enum to determine what type of EM fields 
+ * @param phase_range Phase space range
  * (special relativistic Vlasov-Maxwell vs. special relativistic neutrals)
  * @return Pointer to special relativistic Vlasov equation object
  */
 struct gkyl_dg_eqn* gkyl_dg_canonical_pb_cu_dev_new(const struct gkyl_basis* cbasis,
-  const struct gkyl_basis* pbasis, const struct gkyl_range* phase_range, bool use_gpu);
+  const struct gkyl_basis* pbasis, const struct gkyl_range* phase_range);
 
 /**
  * Set the auxiliary fields (e.g. q/m*EM) needed in updating the force terms.
