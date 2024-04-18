69e9069d8e308da24b9990054fa60e1559db1445
johnson452 <grj@princeton.edu>
2024-04-18 15:25:02 -0400

    Added cu file for can_pb_vars

--- zero/gkyl_dg_calc_canonical_pb_vars.h
+++ zero/gkyl_dg_calc_canonical_pb_vars.h
@@ -13,7 +13,6 @@ typedef struct gkyl_dg_calc_canonical_pb_vars gkyl_dg_calc_canonical_pb_vars;
  * Create new updater to compute canonical_pb variables needed in 
  * updates for general geometry. Methods compute:
  * alpha_surf : Surface expansion of phase space flux alpha for streaming in general geometry (v^i = v . e^i)
- * cot_vec : Volume expansion of contangent vectors for volume term of streaming in general geometry
  * 
  * @param phase_grid Phase space grid (for getting cell spacing and cell center)
  * @param conf_basis Configuration space basis functions
@@ -53,15 +52,11 @@ void gkyl_dg_calc_canonical_pb_vars_alpha_surf(struct gkyl_dg_calc_canonical_pb_
   struct gkyl_array* alpha_surf, struct gkyl_array* sgn_alpha_surf, struct gkyl_array* const_sgn_alpha);
 
 /**
- * Compute volume expansion of cotangent vectors e^i = g^ij e_j
- * 
- * Note: Each cell stores the surface expansion on the *lower* edge of the cell
- * @param up Updater for computing general geometry canonical_pb variables 
- * @param conf_range Configuration space range (should only be local range because geometry only defined on local range)
- * @param cot_vec Output volume expansion of cotangent vectors. 
+ * Host-side wrappers for canonical_pb general geometry variable operations on device
  */
-void gkyl_dg_calc_canonical_pb_vars_cot_vec(struct gkyl_dg_calc_canonical_pb_vars *up, 
-  const struct gkyl_range *conf_range, struct gkyl_array* cot_vec);
+void gkyl_dg_calc_canonical_pb_vars_alpha_surf_cu(struct gkyl_dg_calc_canonical_pb_vars *up, 
+  const struct gkyl_range *conf_range, const struct gkyl_range *phase_range, const struct gkyl_range *phase_ext_range, 
+  struct gkyl_array* alpha_surf, struct gkyl_array* sgn_alpha_surf, struct gkyl_array* const_sgn_alpha);
 
 /**
  * Delete pointer to updater to compute canonical_pb general geometry variables.
@@ -69,13 +64,3 @@ void gkyl_dg_calc_canonical_pb_vars_cot_vec(struct gkyl_dg_calc_canonical_pb_var
  * @param up Updater to delete.
  */
 void gkyl_dg_calc_canonical_pb_vars_release(struct gkyl_dg_calc_canonical_pb_vars *up);
-
-/**
- * Host-side wrappers for canonical_pb general geometry variable operations on device
- */
-void gkyl_dg_calc_canonical_pb_vars_alpha_surf_cu(struct gkyl_dg_calc_canonical_pb_vars *up, 
-  const struct gkyl_range *conf_range, const struct gkyl_range *phase_range, const struct gkyl_range *phase_ext_range, 
-  struct gkyl_array* alpha_surf, struct gkyl_array* sgn_alpha_surf, struct gkyl_array* const_sgn_alpha);
-
-void gkyl_dg_calc_canonical_pb_vars_cot_vec_cu(struct gkyl_dg_calc_canonical_pb_vars *up, 
-  const struct gkyl_range *conf_range, struct gkyl_array* cot_vec);
