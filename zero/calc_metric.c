869e3c6d210a32c2395d75b333a730520f8540d1
akashukla <akashukla@utexas.edu>
2024-05-09 16:18:46 -0500

    Fix wrong argument order in call to m2n

--- zero/calc_metric.c
+++ zero/calc_metric.c
@@ -281,8 +281,8 @@ void gkyl_calc_metric_advance_bcart(gkyl_calc_metric *up, struct gkyl_range *nra
 
 
   // Fill the inputs
-  gkyl_nodal_ops_m2n(up->n2m, up->cbasis, up->grid, nrange, update_range, 9, dualFld, dualFld_nodal);
-  gkyl_nodal_ops_m2n(up->n2m, up->cbasis, up->grid, nrange, update_range, 3, biFld, biFld_nodal);
+  gkyl_nodal_ops_m2n(up->n2m, up->cbasis, up->grid, nrange, update_range, 9, dualFld_nodal, dualFld);
+  gkyl_nodal_ops_m2n(up->n2m, up->cbasis, up->grid, nrange, update_range, 3, biFld_nodal, biFld);
   
   for(int ia=nrange->lower[AL_IDX]; ia<=nrange->upper[AL_IDX]; ++ia) {
       for (int ip=nrange->lower[PSI_IDX]; ip<=nrange->upper[PSI_IDX]; ++ip) {
