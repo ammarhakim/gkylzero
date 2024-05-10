9f087b1fbd1df696b84401caf4acd59f3e1efc39
JonathanGorard <jg865@cantab.ac.uk>
2024-05-10 17:28:23 -0400

    Refactoring of LBO collision operator neutral shock tube tests (1v, 2v and 3v cases).

--- ci/vlasov_regression.c
+++ ci/vlasov_regression.c
@@ -595,8 +595,8 @@ regenerateTest(const char* test_name, const int test_output_count, const char te
 int
 main(int argc, char **argv)
 {
-  int test_count = 27 - 2;
-  char test_names[27 - 2][64] = {
+  int test_count = 30 - 2;
+  char test_names[30 - 2][64] = {
     "accel_1x1v",
     "dg_euler_sodshock_p1",
     "dg_euler_sodshock_p2",
@@ -624,8 +624,11 @@ main(int argc, char **argv)
     //"neut_bgk_sod_shock_1x2v_p2",
     "neut_bgk_sod_shock_1x3v_p1",
     //"neut_bgk_sod_shock_1x3v_p2",
+    "neut_lbo_sod_shock_1x1v_p2",
+    "neut_lbo_sod_shock_1x2v_p2",
+    "neut_lbo_sod_shock_1x3v_p2",
   };
-  char test_names_human[27][128] = {
+  char test_names_human[30 - 2][128] = {
     "1x1v Acceleration Test with p = 1",
     "Euler Sod-Type Shock Tube Test p = 1",
     "Euler Sod-Type Shock Tube Test p = 2",
@@ -653,9 +656,12 @@ main(int argc, char **argv)
     //"1x2v BGK Collision Neutrals Shock Tube Test with p = 2",
     "1x3v BGK Collision Neutrals Shock Tube Test with p = 1",
     //"1x3v BGK Collision Neutrals Shock Tube Test with p = 2",
+    "1x1v LBO Collision Neutrals Shock Tube Test with p = 2",
+    "1x2v LBO Collision Neutrals Shock Tube Test with p = 2",
+    "1x3v LBO Collision Neutrals Shock Tube Test with p = 2",
   };
-  int test_output_count[27 - 2] = { 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, /*1,*/ 1, /*1*/ };
-  char test_outputs[27 - 2][64][64] = {
+  int test_output_count[30 - 2] = { 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, /*1,*/ 1, /*1,*/ 1, 1, 1 };
+  char test_outputs[30 - 2][64][64] = {
     { "elc_1" },
     { "euler_1" },
     { "euler_1" },
@@ -683,6 +689,9 @@ main(int argc, char **argv)
     //{ "neut_1" },
     { "neut_1" },
     //{ "neut_1" },
+    { "neut_1" },
+    { "neut_1" },
+    { "neut_1" },
   };
 
   system("clear");
