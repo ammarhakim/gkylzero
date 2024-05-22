d334442d973d0ad334f889bd4febdb292b86c0f2
kltrbrds <kb8689@princeton.edu>
2024-05-22 09:38:15 -0400

    added sheath test for emission BCs

---
+++ regression/rt_test.c
@@ -0,0 +1,300 @@
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+
+#include <gkyl_alloc.h>
+#include <gkyl_vlasov.h>
+#include <rt_arg_parse.h>
+#include <gkyl_bc_emission_spectrum.h>
+#include <gkyl_vlasov_priv.h>
+
+struct sheath_ctx {
+  double chargeElc; // electron charge
+  double massElc; // electron mass
+  double chargeIon; // ion charge
+  double massIon; // ion mass
+  double n0;
+  double Te; // electron to ion temperature ratio
+  double Ti;
+  double vte; // electron thermal velocity
+  double vti; // ion thermal velocity
+  double Lx; // size of the box
+};
+
+static inline double sq(double x) { return x*x; }
+
+void
+evalDistFuncElc(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct sheath_ctx *app = ctx;
+  double x = xn[0], v = xn[1];
+  double vt = app->vte;
+  double n = app->n0;
+  double fv = n/sqrt(2.0*M_PI*sq(vt))*(exp(-sq(v)/(2*sq(vt))));
+  fout[0] = fv;
+}
+
+void
+evalDistFuncElcSource(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct sheath_ctx *app = ctx;
+  double x = xn[0], v = xn[1];
+  double vt = app->vte;
+  double fv = 1.0/sqrt(2.0*M_PI*sq(vt))*(exp(-sq(v)/(2*sq(vt))));
+  if(fabs(x) < 100) {
+    fout[0] = (100 - fabs(x))/100*fv;
+  } else {
+    fout[0] = 0.0;
+  }
+}
+
+void
+evalDistFuncIon(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct sheath_ctx *app = ctx;
+  double x = xn[0], v = xn[1];
+  double vt = app->vti;
+  double fv = 1.0/sqrt(2.0*M_PI*sq(vt))*(exp(-sq(v)/(2*sq(vt))));
+  fout[0] = fv;
+}
+
+void
+evalDistFuncIonSource(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct sheath_ctx *app = ctx;
+  double x = xn[0], v = xn[1];
+  double vt = app->vti;
+  double fv = 1.0/sqrt(2.0*M_PI*sq(vt))*(exp(-sq(v)/(2*sq(vt))));
+  if(fabs(x) < 100.0) {
+    fout[0] = (100.0 - fabs(x))/100.0*fv;
+  } else {
+    fout[0] = 0.0;
+  }
+}
+
+void
+evalFieldFunc(double t, const double* GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct sheath_ctx *app = ctx;
+  double x = xn[0];
+  
+  fout[0] = 0.0; fout[1] = 0.0, fout[2] = 0.0;
+  fout[3] = 0.0; fout[4] = 0.0; fout[5] = 0.0;
+  fout[6] = 0.0; fout[7] = 0.0;
+}
+
+struct sheath_ctx
+create_ctx(void)
+{
+  double massElc = 9.109e-31;
+  double q0 = 1.602e-19;
+  struct sheath_ctx ctx = {
+    .chargeElc = -q0,
+    .massElc = 9.109e-31,
+    .chargeIon = q0,
+    .massIon = 1836.153*massElc,
+    .n0 = 1.0e17,
+    .Te = 10.0*q0,
+    .Ti = 10.0*q0,
+    .vte = sqrt(10.0*q0/massElc),
+    .vti = sqrt(10.0*q0/ctx.massIon),
+    .Lx = 128.0
+  };
+  return ctx;
+}
+
+int
+main(int argc, char **argv)
+{
+  struct gkyl_app_args app_args = parse_app_args(argc, argv);
+
+  if (app_args.trace_mem) {
+    gkyl_cu_dev_mem_debug_set(true);
+    gkyl_mem_debug_set(true);
+  }
+  struct sheath_ctx ctx = create_ctx(); // context for init functions
+  struct gkyl_bc_emission_spectrum_norm_chung_everhart *chung_ctx = gkyl_malloc(sizeof(struct gkyl_bc_emission_spectrum_norm_chung_everhart));
+  struct gkyl_bc_emission_spectrum_yield_furman_pivi *furman_ctx = gkyl_malloc(sizeof(struct gkyl_bc_emission_spectrum_yield_furman_pivi));
+  
+  chung_ctx->mass = 9.109e-31;
+  chung_ctx->charge = -1.602e-19;
+  chung_ctx->phi = 4.68;
+
+  furman_ctx->mass = 9.109e-31;
+  furman_ctx->charge = -1.602e-19;
+  furman_ctx->deltahat_ts = 1.885;
+  furman_ctx->Ehat_ts = 276.8;
+  furman_ctx->t1 = 0.66;
+  furman_ctx->t2 = 0.8;
+  furman_ctx->t3 = 0.7;
+  furman_ctx->t4 = 1.0;
+  furman_ctx->s = 1.54;
+
+  struct vm_emission_ctx bc_ctx = {
+    .num_species = 1,
+    .norm_type = { GKYL_BC_CHUNG_EVERHART },
+    .yield_type = { GKYL_BC_FURMAN_PIVI },
+    .norm_params = { chung_ctx },
+    .yield_params = { furman_ctx },
+    .in_species = { "elc" },
+  };
+
+  // electrons
+  struct gkyl_vlasov_species elc = {
+    .name = "elc",
+    .charge = ctx.chargeElc, .mass = ctx.massElc,
+    .lower = { -6.0 * ctx.vte},
+    .upper = { 6.0 * ctx.vte}, 
+    .cells = { 64 },
+
+    .projection = {
+      .proj_id = GKYL_PROJ_FUNC,
+      .func = evalDistFuncElc,
+      .ctx_func = &ctx,
+    },
+
+    .bcx = {
+      .lower = { .type = GKYL_SPECIES_ABSORB, },
+      .upper = { .type = GKYL_SPECIES_EMISSION,
+                 .aux_ctx = &bc_ctx, },
+    },
+
+    /* .source = { */
+    /*   .source_id = GKYL_BFLUX_SOURCE, */
+    /*   .source_length = 100.0, */
+    /*   .source_species = "ion", */
+    /*   .projection = { */
+    /*     .proj_id = GKYL_PROJ_FUNC, */
+    /*     .func = evalDistFuncElcSource, */
+    /*     .ctx_func = &ctx, */
+    /*   }, */
+    /* }, */
+    
+    .num_diag_moments = 3,
+    .diag_moments = { "M0", "M1i", "M2" },
+  };
+
+  // ions
+  struct gkyl_vlasov_species ion = {
+    .name = "ion",
+    .charge = ctx.chargeIon, .mass = ctx.massIon,
+    .lower = { -6.0 * ctx.vti},
+    .upper = { 6.0 * ctx.vti}, 
+    .cells = { 64 },
+
+    .projection = {
+      .proj_id = GKYL_PROJ_FUNC,
+      .func = evalDistFuncIon,
+      .ctx_func = &ctx,
+    },
+
+    .bcx = {
+      .lower = { .type = GKYL_SPECIES_ABSORB, },
+      .upper = { .type = GKYL_SPECIES_ABSORB, },
+    },
+
+    /* .source = { */
+    /*   .source_id = GKYL_BFLUX_SOURCE, */
+    /*   .source_length = 100.0, */
+    /*   .source_species = "ion", */
+    /*   .projection = { */
+    /*     .proj_id = GKYL_PROJ_FUNC, */
+    /*     .func = evalDistFuncIonSource, */
+    /*     .ctx_func = &ctx, */
+    /*   }, */
+    /* }, */
+    
+    .num_diag_moments = 3,
+    .diag_moments = { "M0", "M1i", "M2" },
+  };
+
+  // field
+  struct gkyl_vlasov_field field = {
+    .epsilon0 = 1.0, .mu0 = 1.0,
+    .elcErrorSpeedFactor = 0.0,
+    .mgnErrorSpeedFactor = 0.0,
+
+    .ctx = &ctx,
+    .init = evalFieldFunc,
+
+    .bcx = { GKYL_FIELD_PEC_WALL, GKYL_FIELD_PEC_WALL }
+  };
+
+  // VM app
+  struct gkyl_vm vm = {
+    .name = "test",
+
+    .cdim = 1, .vdim = 1,
+    .lower = { -ctx.Lx },
+    .upper = { ctx.Lx },
+    .cells = { 256 },
+    .poly_order = 2,
+    .basis_type = app_args.basis_type,
+
+    .num_periodic_dir = 0,
+    .periodic_dirs = { },
+
+    .num_species = 2,
+    .species = { elc, ion },
+    .field = field,
+
+    .use_gpu = app_args.use_gpu,
+  };
+
+  // create app object
+  gkyl_vlasov_app *app = gkyl_vlasov_app_new(&vm);
+
+  // start, end and initial time-step
+  double tcurr = 0.0, tend = 20.0;
+  double dt = tend-tcurr;
+
+  // initialize simulation
+  gkyl_vlasov_app_apply_ic(app, tcurr);
+  
+  gkyl_vlasov_app_write(app, tcurr, 0);
+  gkyl_vlasov_app_calc_mom(app); gkyl_vlasov_app_write_mom(app, tcurr, 0);
+  gkyl_vlasov_app_calc_integrated_mom(app, tcurr);
+
+  long step = 1, num_steps = app_args.num_steps;
+  while ((tcurr < tend) && (step <= num_steps)) {
+    printf("Taking time-step at t = %g ...", tcurr);
+    struct gkyl_update_status status = gkyl_vlasov_update(app, dt);
+    printf(" dt = %g\n", status.dt_actual);
+    gkyl_vlasov_app_calc_integrated_mom(app, tcurr);
+    
+    if (!status.success) {
+      printf("** Update method failed! Aborting simulation ....\n");
+      break;
+    }
+    tcurr += status.dt_actual;
+    dt = status.dt_suggested;
+    step += 1;
+  }
+
+  gkyl_vlasov_app_write(app, tcurr, 1);
+  gkyl_vlasov_app_calc_mom(app); gkyl_vlasov_app_write_mom(app, tcurr, 1);
+  gkyl_vlasov_app_stat_write(app);
+  gkyl_vlasov_app_write_integrated_mom(app);
+
+  // fetch simulation statistics
+  struct gkyl_vlasov_stat stat = gkyl_vlasov_app_stat(app);
+
+  // simulation complete, free app
+  gkyl_vlasov_app_release(app);
+
+  printf("\n");
+  printf("Number of update calls %ld\n", stat.nup);
+  printf("Number of forward-Euler calls %ld\n", stat.nfeuler);
+  printf("Number of RK stage-2 failures %ld\n", stat.nstage_2_fail);
+  if (stat.nstage_2_fail > 0) {
+    printf("Max rel dt diff for RK stage-2 failures %g\n", stat.stage_2_dt_diff[1]);
+    printf("Min rel dt diff for RK stage-2 failures %g\n", stat.stage_2_dt_diff[0]);
+  }  
+  printf("Number of RK stage-3 failures %ld\n", stat.nstage_3_fail);
+  printf("Species RHS calc took %g secs\n", stat.species_rhs_tm);
+  printf("Field RHS calc took %g secs\n", stat.field_rhs_tm);
+  printf("Current evaluation and accumulate took %g secs\n", stat.current_tm);
+  printf("Updates took %g secs\n", stat.total_tm);
+  
+  return 0;
+}
