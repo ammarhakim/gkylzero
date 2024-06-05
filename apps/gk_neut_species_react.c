f225ebc823a466382425ab11112adce99ffa6ab2
Akash Shukla <akashukla@utexas.edu>
2024-06-05 15:36:16 -0500

    Set ion m0 in cross moms for neutral recombination recations

--- apps/gk_neut_species_react.c
+++ apps/gk_neut_species_react.c
@@ -149,6 +149,7 @@ gk_neut_species_react_cross_moms(gkyl_gyrokinetic_app *app, const struct gk_neut
       }
 
       gkyl_array_set_range(react->m0_elc[i], 1.0, react->moms_elc[i].marr, &app->local);
+      gkyl_array_set_range(react->m0_ion[i], 1.0, react->moms_ion[i].marr, &app->local);
       
       // compute recombination reaction rate
       gkyl_dg_recomb_coll(react->recomb[i], react->moms_elc[i].marr, react->moms_ion[i].marr,
