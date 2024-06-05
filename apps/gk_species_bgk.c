1e37f2e9f958688e49d97545011c9e16f8f063d7
Manaure Francisquez <mfrancisquez@gmail.com>
2024-06-05 13:08:24 -0700

    Correct MAX that should've been a MIN in GK BGK app.

--- apps/gk_species_bgk.c
+++ apps/gk_species_bgk.c
@@ -38,7 +38,7 @@ gk_species_bgk_init(struct gkyl_gyrokinetic_app *app, struct gk_species *s, stru
 
     // Compute a minimum representable temperature based on the smallest dv in the grid.
     double dv_min[vdim];
-    gkyl_velocity_map_reduce_dv_range(s->vel_map, GKYL_MAX, dv_min, s->vel_map->local_vel);
+    gkyl_velocity_map_reduce_dv_range(s->vel_map, GKYL_MIN, dv_min, s->vel_map->local_vel);
 
     double tpar_min = (s->info.mass/6.0)*pow(dv_min[0],2);
     double tperp_min = vdim>1 ? (bmag_mid/3.0)*dv_min[1] : tpar_min;
