5f638217ad6e90e7cf3c0ccf9cad8f0be20ed5ad
Junoravin <junoravin@gmail.com>
2024-06-08 21:25:44 -0400

    Adding MPI to the soliton test to run in parallel since it's useful to run this test for a long period of time when doing science with it.

--- regression/rt_pkpm_alf_soliton_1x_p2.c
+++ regression/rt_pkpm_alf_soliton_1x_p2.c
@@ -1,9 +1,20 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include <gkyl_alloc.h>
 #include <gkyl_pkpm.h>
+#include <gkyl_util.h>
+#include <gkyl_wv_euler.h>
+
+#include <gkyl_null_comm.h>
+
+#ifdef GKYL_HAVE_MPI
+#include <mpi.h>
+#include <gkyl_mpi_comm.h>
+#endif
+
 #include <rt_arg_parse.h>
 
 struct pkpm_alf_ctx {
@@ -27,9 +38,12 @@ struct pkpm_alf_ctx {
   double delta_B0;
   double Lx; // Domain size (x-direction).
   int Nx; // Cell count (x-direction).
+  double cfl_frac; // CFL coefficient.
   double t_end; // Final simulation time.
+  int num_frames; // Number of output frames.
   double init_dt; // Initial time step guess so first step does not generate NaN
-  double min_dt;
+  double dt_failure_tol; // Minimum allowable fraction of initial time-step.
+  int num_failures_max; // Maximum allowable number of consecutive small time-steps.
   bool use_gpu;
 };
 
@@ -260,10 +274,13 @@ create_ctx(void)
   double Lx = 5.0*(di/a);
   int Nx = 16; 
   double dx = Lx/Nx;
-  double t_end = 100.0/omegaCi;
-  // initial dt guess so first step does not generate NaN
+  double cfl_frac = 1.0; // CFL coefficient.
+  double t_end = 100.0/omegaCi; // Final simulation time.
+  int num_frames = 1; // Number of output frames.
+  double dt_failure_tol = 1.0e-4; // Minimum allowable fraction of initial time-step.
+  int num_failures_max = 20; // Maximum allowable number of consecutive small time-steps.
   double init_dt = (Lx/Nx)/(5.0);
-  
+
   struct pkpm_alf_ctx ctx = {
     .epsilon0 = epsilon0,
     .mu0 = mu0,
@@ -285,24 +302,39 @@ create_ctx(void)
     .delta_B0 = delta_B0,
     .Lx = Lx,
     .Nx = Nx, 
-    .t_end = t_end, 
+    .cfl_frac = cfl_frac,
+    .t_end = t_end,
+    .num_frames = num_frames,
+    .dt_failure_tol = dt_failure_tol,
+    .num_failures_max = num_failures_max,
     .init_dt = init_dt, 
-    .min_dt = 1.0e-2, 
   };
   return ctx;
 }
 
 void
-write_data(struct gkyl_tm_trigger *iot, gkyl_pkpm_app *app, double tcurr)
+write_data(struct gkyl_tm_trigger* iot, gkyl_pkpm_app* app, double t_curr, bool force_write)
 {
-  if (gkyl_tm_trigger_check_and_bump(iot, tcurr)) 
-    gkyl_pkpm_app_write(app, tcurr, iot->curr-1);
+  if (gkyl_tm_trigger_check_and_bump(iot, t_curr)) {
+    int frame = iot->curr - 1;
+    if (force_write) {
+      frame = iot->curr;
+    }
+
+    gkyl_pkpm_app_write(app, t_curr, iot->curr-1);
+  }
 }
 
 int
 main(int argc, char **argv)
 {
   struct gkyl_app_args app_args = parse_app_args(argc, argv);
+
+#ifdef GKYL_HAVE_MPI
+  if (app_args.use_mpi) {
+    MPI_Init(&argc, &argv);
+  }
+#endif
 
   struct pkpm_alf_ctx ctx = create_ctx(); // context for init functions
 
@@ -366,6 +398,80 @@ main(int argc, char **argv)
     .init = evalFieldFunc
   };
 
+  int nrank = 1; // Number of processes in simulation.
+#ifdef GKYL_HAVE_MPI
+  if (app_args.use_mpi) {
+    MPI_Comm_size(MPI_COMM_WORLD, &nrank);
+  }
+#endif
+
+  // Create global range.
+  int cells[] = { NX };
+  int dim = sizeof(cells) / sizeof(cells[0]);
+  struct gkyl_range global_r;
+  gkyl_create_global_range(dim, cells, &global_r);
+
+  // Create decomposition.
+  int cuts[dim];
+#ifdef GKYL_HAVE_MPI
+  for (int d = 0; d < dim; d++) {
+    if (app_args.use_mpi) {
+      cuts[d] = app_args.cuts[d];
+    }
+    else {
+      cuts[d] = 1;
+    }
+  }
+#else
+  for (int d = 0; d < dim; d++) {
+    cuts[d] = 1;
+  }
+#endif
+
+  struct gkyl_rect_decomp *decomp = gkyl_rect_decomp_new_from_cuts(dim, cuts, &global_r);
+
+  // Construct communicator for use in app.
+  struct gkyl_comm *comm;
+#ifdef GKYL_HAVE_MPI
+  if (app_args.use_mpi) {
+    comm = gkyl_mpi_comm_new( &(struct gkyl_mpi_comm_inp) {
+        .mpi_comm = MPI_COMM_WORLD,
+        .decomp = decomp
+      }
+    );
+  }
+  else {
+    comm = gkyl_null_comm_inew( &(struct gkyl_null_comm_inp) {
+        .decomp = decomp,
+        .use_gpu = app_args.use_gpu
+      }
+    );
+  }
+#else
+  comm = gkyl_null_comm_inew( &(struct gkyl_null_comm_inp) {
+      .decomp = decomp,
+      .use_gpu = app_args.use_gpu
+    }
+  );
+#endif
+
+  int my_rank;
+  gkyl_comm_get_rank(comm, &my_rank);
+  int comm_size;
+  gkyl_comm_get_size(comm, &comm_size);
+
+  int ncuts = 1;
+  for (int d = 0; d < dim; d++) {
+    ncuts *= cuts[d];
+  }
+
+  if (ncuts != comm_size) {
+    if (my_rank == 0) {
+      fprintf(stderr, "*** Number of ranks, %d, does not match total cuts, %d!\n", comm_size, ncuts);
+    }
+    goto mpifinalize;
+  }
+
   // pkpm app
   struct gkyl_pkpm pkpm = {
     .name = "pkpm_alf_soliton_1x_p2",
@@ -376,6 +482,7 @@ main(int argc, char **argv)
     .cells = { NX},
     .poly_order = 2,
     .basis_type = app_args.basis_type,
+    .cfl_frac = ctx.cfl_frac,
 
     // .use_explicit_source = true, 
     
@@ -387,57 +494,71 @@ main(int argc, char **argv)
     .field = field,
 
     .use_gpu = app_args.use_gpu,
+
+    .has_low_inp = true,
+    .low_inp = {
+      .local_range = decomp -> ranges[my_rank],
+      .comm = comm
+    }
   };
 
   // create app object
   gkyl_pkpm_app *app = gkyl_pkpm_app_new(&pkpm);
 
   // start, end and initial time-step
-  double tcurr = 0.0, tend = ctx.t_end;
+  double t_curr = 0.0, t_end = ctx.t_end;
   double dt = ctx.init_dt;
-  int nframe = 100;
-  // create trigger for IO
-  struct gkyl_tm_trigger io_trig = { .dt = tend/nframe };
+
+  // Create trigger for IO.
+  int num_frames = ctx.num_frames;
+  struct gkyl_tm_trigger io_trig = { .dt = t_end / num_frames };
 
   // initialize simulation
-  gkyl_pkpm_app_apply_ic(app, tcurr);
-  write_data(&io_trig, app, tcurr);
-  gkyl_pkpm_app_calc_field_energy(app, tcurr);
-  gkyl_pkpm_app_calc_integrated_L2_f(app, tcurr);
-  gkyl_pkpm_app_calc_integrated_mom(app, tcurr);
+  gkyl_pkpm_app_apply_ic(app, t_curr);
+  write_data(&io_trig, app, t_curr, false);  
 
-  long step = 1, num_steps = app_args.num_steps;
-  while ((tcurr < tend) && (step <= num_steps)) {
-    gkyl_pkpm_app_cout(app, stdout, "Taking time-step at t = %g ...", tcurr);
+  // Initialize small time-step check.
+  double dt_init = -1.0, dt_failure_tol = ctx.dt_failure_tol;
+  int num_failures = 0, num_failures_max = ctx.num_failures_max;
+
+  long step = 1;
+  while ((t_curr < t_end) && (step <= app_args.num_steps)) {
+    gkyl_pkpm_app_cout(app, stdout, "Taking time-step %ld at t = %g ...", step, t_curr);
     struct gkyl_update_status status = gkyl_pkpm_update(app, dt);
     gkyl_pkpm_app_cout(app, stdout, " dt = %g\n", status.dt_actual);
-    if (step % 100 == 0) {
-      gkyl_pkpm_app_calc_field_energy(app, tcurr);
-      gkyl_pkpm_app_calc_integrated_L2_f(app, tcurr);
-      gkyl_pkpm_app_calc_integrated_mom(app, tcurr);
-    }
+    
     if (!status.success) {
       gkyl_pkpm_app_cout(app, stdout, "** Update method failed! Aborting simulation ....\n");
       break;
     }
-    if (status.dt_actual < ctx.min_dt) {
-      gkyl_pkpm_app_cout(app, stdout, "** Time step crashing! Aborting simulation and writing out last output ....\n");
-      gkyl_pkpm_app_write(app, tcurr, 1000);
-      break;
-    }
-    tcurr += status.dt_actual;
+
+    t_curr += status.dt_actual;
     dt = status.dt_suggested;
 
-    write_data(&io_trig, app, tcurr);
+    write_data(&io_trig, app, t_curr, false);
+
+    if (dt_init < 0.0) {
+      dt_init = status.dt_actual;
+    }
+    else if (status.dt_actual < dt_failure_tol * dt_init) {
+      num_failures += 1;
+
+      gkyl_pkpm_app_cout(app, stdout, "WARNING: Time-step dt = %g", status.dt_actual);
+      gkyl_pkpm_app_cout(app, stdout, " is below %g*dt_init ...", dt_failure_tol);
+      gkyl_pkpm_app_cout(app, stdout, " num_failures = %d\n", num_failures);
+      if (num_failures >= num_failures_max) {
+        gkyl_pkpm_app_cout(app, stdout, "ERROR: Time-step was below %g*dt_init ", dt_failure_tol);
+        gkyl_pkpm_app_cout(app, stdout, "%d consecutive times. Aborting simulation ....\n", num_failures_max);
+        break;
+      }
+    }
+    else {
+      num_failures = 0;
+    }
 
     step += 1;
   }
-  gkyl_pkpm_app_calc_field_energy(app, tcurr);
-  gkyl_pkpm_app_calc_integrated_L2_f(app, tcurr);
-  gkyl_pkpm_app_calc_integrated_mom(app, tcurr);
-  gkyl_pkpm_app_write_field_energy(app);
-  gkyl_pkpm_app_write_integrated_L2_f(app);
-  gkyl_pkpm_app_write_integrated_mom(app);
+  write_data(&io_trig, app, t_curr, false);
   gkyl_pkpm_app_stat_write(app);
 
   // fetch simulation statistics
@@ -460,13 +581,28 @@ main(int argc, char **argv)
   gkyl_pkpm_app_cout(app, stdout, "Species collisional moments took %g secs\n", stat.species_coll_mom_tm);
   gkyl_pkpm_app_cout(app, stdout, "EM Variables (bvar) calculation took %g secs\n", stat.field_em_vars_tm);
   gkyl_pkpm_app_cout(app, stdout, "Current evaluation and accumulate took %g secs\n", stat.current_tm);
-  gkyl_pkpm_app_cout(app, stdout, "Updates took %g secs\n", stat.total_tm);
 
+  gkyl_pkpm_app_cout(app, stdout, "Species BCs took %g secs\n", stat.species_bc_tm);
+  gkyl_pkpm_app_cout(app, stdout, "Fluid Species BCs took %g secs\n", stat.fluid_species_bc_tm);
+  gkyl_pkpm_app_cout(app, stdout, "Field BCs took %g secs\n", stat.field_bc_tm);
+  
+  gkyl_pkpm_app_cout(app, stdout, "Updates took %g secs\n", stat.total_tm);
+  
   gkyl_pkpm_app_cout(app, stdout, "Number of write calls %ld,\n", stat.nio);
   gkyl_pkpm_app_cout(app, stdout, "IO time took %g secs \n", stat.io_tm);
 
+  gkyl_rect_decomp_release(decomp);
+  gkyl_comm_release(comm);
+
   // simulation complete, free app
   gkyl_pkpm_app_release(app);
+
+  mpifinalize:
+  ;
+#ifdef GKYL_HAVE_MPI
+  if (app_args.use_mpi)
+    MPI_Finalize();
+#endif  
   
   return 0;
 }
