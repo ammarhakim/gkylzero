f06d8d93410c6a61ffc5eb210095462a05fb1a42
Junoravin <junoravin@gmail.com>
2024-06-08 21:09:20 -0400

    Adding a new regression test based on some work being proposed with PKPM to initialize and propagate an Alfven soliton. This test will require some iteration but should be a nice regression test for the implicit fluid-em coupling, because the box size is very large with respect to di so the soliton solution is well-posed, and thus the time step will inevitably be large compared to scales like the plasma and cycltron frequency (assuming one does not run at very very high resolution).

---
+++ regression/rt_pkpm_alf_soliton_1x_p2.c
@@ -0,0 +1,472 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <gkyl_alloc.h>
+#include <gkyl_pkpm.h>
+#include <rt_arg_parse.h>
+
+struct pkpm_alf_ctx {
+  double epsilon0;
+  double mu0;
+  double chargeElc; // electron charge
+  double massElc; // electron mass
+  double chargeIon; // ion charge
+  double massIon; // ion mass
+  double Te_Ti; // electron to ion temperature ratio
+  double n0;
+  double vAe;
+  double B0;
+  double beta;
+  double vtElc;
+  double vtIon;
+  double nuElc;
+  double nuIon;
+  double di; 
+  double a;
+  double delta_B0;
+  double Lx; // Domain size (x-direction).
+  int Nx; // Cell count (x-direction).
+  double t_end; // Final simulation time.
+  double init_dt; // Initial time step guess so first step does not generate NaN
+  double min_dt;
+  bool use_gpu;
+};
+
+static inline double
+maxwellian(double n, double v, double vth)
+{
+  double v2 = v*v;
+  return n/sqrt(2*M_PI*vth*vth)*exp(-v2/(2*vth*vth));
+}
+
+static inline double
+sech(double x)
+{
+  return 1.0/(cosh(x));
+}
+
+static inline double
+sech2(double x)
+{
+  return 1.0/(cosh(x)*cosh(x));
+}
+
+static inline double
+tanh2(double x)
+{
+  return tanh(x)*tanh(x);
+}
+
+void
+evalDistFuncElc(double t, const double* GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct pkpm_alf_ctx *app = ctx;
+  
+  double x = xn[0], vx = xn[1];
+
+  double me = app->massElc;
+  double B0 = app->B0;
+  double B0perp = app->delta_B0;
+  double a = app->a;
+  double di = app->di; 
+  double n = 2.0*a*sech(a*x/di);
+
+  double arg = 0.5*a*x/di;
+  double phi = 4.0*atan(tanh(arg));
+  double B_x = B0;
+  double B_y = B0perp*sin(phi);
+  double B_z = B0perp*cos(phi);
+
+  double magB2 = 0.5*(B_x*B_x + B_y*B_y + B_z*B_z);
+  double Te = app->vtElc*app->vtElc*me*(magB2/n);
+  double vt_elc = sqrt(Te/me);
+  
+  double fv = maxwellian(n, vx, vt_elc);
+    
+  fout[0] = fv;
+  fout[1] = vt_elc*vt_elc*fv;
+}
+void
+evalDistFuncIon(double t, const double* GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct pkpm_alf_ctx *app = ctx;
+  
+  double x = xn[0], vx = xn[1];
+
+  double mi = app->massIon;
+  double B0 = app->B0;
+  double B0perp = app->delta_B0;
+  double a = app->a;
+  double di = app->di; 
+  double n = 2.0*a*sech(a*x/di);
+
+  double arg = 0.5*a*x/di;
+  double phi = 4.0*atan(tanh(arg));
+  double B_x = B0;
+  double B_y = B0perp*sin(phi);
+  double B_z = B0perp*cos(phi);
+
+  double magB2 = 0.5*(B_x*B_x + B_y*B_y + B_z*B_z);
+  double Ti = app->vtIon*app->vtIon*mi*(magB2/n);
+  double vt_ion = sqrt(Ti/mi);
+
+  double fv = maxwellian(n, vx, vt_ion);
+    
+  fout[0] = fv;
+  fout[1] = vt_ion*vt_ion*fv;
+}
+
+void
+evalFluidElc(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct pkpm_alf_ctx *app = ctx;
+  
+  double x = xn[0];
+
+  double qe = app->chargeElc;
+  double qi = app->chargeIon;
+  double me = app->massElc;
+  double mi = app->massIon;
+  double B0perp = app->delta_B0;
+
+  double a = app->a;
+  double di = app->di; 
+  double arg = 0.5*a*x/di;
+  double phi = 4.0*atan(tanh(arg));
+  double Jy = 2.0*B0perp*(a*sech2(arg)*sin(phi)/(di*tanh2(arg) + di));
+  double Jz = 2.0*B0perp*(a*sech2(arg)*cos(phi)/(di*tanh2(arg) + di));
+
+  double vdrift_x = 0.0;
+  double vdrift_y = Jy/qe;
+  double vdrift_z = Jz/qe;
+
+  fout[0] = me*vdrift_x;
+  fout[1] = me*vdrift_y;
+  fout[2] = me*vdrift_z;
+}
+
+void
+evalFluidIon(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct pkpm_alf_ctx *app = ctx;
+  
+  double x = xn[0];
+
+  double qe = app->chargeElc;
+  double qi = app->chargeIon;
+  double me = app->massElc;
+  double mi = app->massIon;
+  double B0perp = app->delta_B0;
+
+  double vdrift_x = 0.0;
+  double vdrift_y = 0.0;
+  double vdrift_z = 0.0;
+
+  fout[0] = mi*vdrift_x;
+  fout[1] = mi*vdrift_y;
+  fout[2] = mi*vdrift_z;
+}
+
+void
+evalFieldFunc(double t, const double* GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct pkpm_alf_ctx *app = ctx;
+
+  double x = xn[0];
+
+  double qe = app->chargeElc;
+  double qi = app->chargeIon;
+  double L = app->Lx;
+  double B0 = app->B0;
+  double B0perp = app->delta_B0;
+
+  double a = app->a;
+  double di = app->di; 
+  double arg = 0.5*a*x/di;
+  double phi = 4.0*atan(tanh(arg));
+
+  double B_x = B0;
+  double B_y = B0perp*sin(phi);
+  double B_z = B0perp*cos(phi);
+
+  // Assumes qi = abs(qe)
+  double Jy = 2.0*B0perp*(a*sech2(arg)*sin(phi)/(di*tanh2(arg) + di));
+  double Jz = 2.0*B0perp*(a*sech2(arg)*cos(phi)/(di*tanh2(arg) + di));
+  double n = 2.0*a*sech(a*x/di);
+  double u_xe = 0.0;
+  double u_ye = Jy/(n*qe);
+  double u_ze = Jz/(n*qe);
+
+  // E = - v_e x B ~  (J - u) x B
+  double E_x = - (u_ye*B_z - u_ze*B_y);
+  double E_y = - (u_ze*B_x - u_xe*B_z);
+  double E_z = - (u_xe*B_y - u_ye*B_x);
+  
+  fout[0] = E_x; fout[1] = E_y, fout[2] = E_z;
+  fout[3] = B_x; fout[4] = B_y; fout[5] = B_z;
+  fout[6] = 0.0; fout[7] = 0.0;
+}
+
+void
+evalNuElc(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct pkpm_alf_ctx *app = ctx;
+  fout[0] = app->nuElc;
+}
+
+void
+evalNuIon(double t, const double * GKYL_RESTRICT xn, double* GKYL_RESTRICT fout, void *ctx)
+{
+  struct pkpm_alf_ctx *app = ctx;
+  fout[0] = app->nuIon;
+}
+
+struct pkpm_alf_ctx
+create_ctx(void)
+{
+  double epsilon0 = 1.0; // permittivity of free space
+  double mu0 = 1.0; // pemiability of free space
+
+  double massElc = 1.0; // electron mass
+  double chargeElc = -1.0; // electron charge
+  double massIon = 100.0; // ion mass
+  double chargeIon = 1.0; // ion charge
+
+  double Te_Ti = 1.0; // ratio of electron to ion temperature
+  double n0 = 1.0; // initial number density
+  double vAe = 0.1;
+  double beta = 0.1;
+
+  double B0 = vAe*sqrt(mu0*n0*massElc);
+  double vtElc = vAe*sqrt(beta/2.0);
+  // ion velocities
+  double vAi = vAe/sqrt(massIon);
+  double vtIon = vtElc/sqrt(massIon); //Ti/Te = 1.0
+
+  // ion cyclotron frequency and gyroradius
+  double omegaCi = chargeIon*B0/massIon;
+  double di = vAi/omegaCi;
+  double rhoi = sqrt(2.0)*vtIon/omegaCi;
+
+  // collision frequencies
+  double nuElc = 0.01*omegaCi;
+  double nuIon = 0.01*omegaCi/sqrt(massIon);
+
+  // initial conditions
+  double a = 0.1;
+  double delta_B0 = a*B0;
+
+  double Lx = 5.0*(di/a);
+  int Nx = 16; 
+  double dx = Lx/Nx;
+  double t_end = 100.0/omegaCi;
+  // initial dt guess so first step does not generate NaN
+  double init_dt = (Lx/Nx)/(5.0);
+  
+  struct pkpm_alf_ctx ctx = {
+    .epsilon0 = epsilon0,
+    .mu0 = mu0,
+    .chargeElc = chargeElc,
+    .massElc = massElc,
+    .chargeIon = chargeIon,
+    .massIon = massIon,
+    .Te_Ti = Te_Ti,
+    .n0 = n0,
+    .vAe = vAe,
+    .B0 = B0,
+    .beta = beta,
+    .vtElc = vtElc,
+    .vtIon = vtIon,
+    .nuElc = nuElc,
+    .nuIon = nuIon,
+    .di = di, 
+    .a = a, 
+    .delta_B0 = delta_B0,
+    .Lx = Lx,
+    .Nx = Nx, 
+    .t_end = t_end, 
+    .init_dt = init_dt, 
+    .min_dt = 1.0e-2, 
+  };
+  return ctx;
+}
+
+void
+write_data(struct gkyl_tm_trigger *iot, gkyl_pkpm_app *app, double tcurr)
+{
+  if (gkyl_tm_trigger_check_and_bump(iot, tcurr)) 
+    gkyl_pkpm_app_write(app, tcurr, iot->curr-1);
+}
+
+int
+main(int argc, char **argv)
+{
+  struct gkyl_app_args app_args = parse_app_args(argc, argv);
+
+  struct pkpm_alf_ctx ctx = create_ctx(); // context for init functions
+
+  int NX = APP_ARGS_CHOOSE(app_args.xcells[0], ctx.Nx);
+  int VX = APP_ARGS_CHOOSE(app_args.vcells[0], 32);
+
+  if (app_args.trace_mem) {
+    gkyl_cu_dev_mem_debug_set(true);
+    gkyl_mem_debug_set(true);
+  }
+  
+  // electrons
+  struct gkyl_pkpm_species elc = {
+    .name = "elc",
+    .charge = ctx.chargeElc, .mass = ctx.massElc,
+    .lower = { -6.0 * ctx.vtElc},
+    .upper = { 6.0 * ctx.vtElc}, 
+    .cells = { VX },
+
+    .ctx_dist = &ctx,
+    .ctx_fluid = &ctx,
+    .init_dist = evalDistFuncElc,
+    .init_fluid = evalFluidElc,
+
+    .collisions = {
+      .collision_id = GKYL_LBO_COLLISIONS,
+
+      .ctx = &ctx,
+      .self_nu = evalNuElc,
+    },    
+  };
+  
+  // ions
+  struct gkyl_pkpm_species ion = {
+    .name = "ion",
+    .charge = ctx.chargeIon, .mass = ctx.massIon,
+    .lower = { -6.0 * ctx.vtIon},
+    .upper = { 6.0 * ctx.vtIon}, 
+    .cells = { VX },
+
+    .ctx_dist = &ctx,
+    .ctx_fluid = &ctx,
+    .init_dist = evalDistFuncIon,
+    .init_fluid = evalFluidIon,
+
+    .collisions = {
+      .collision_id = GKYL_LBO_COLLISIONS,
+
+      .ctx = &ctx,
+      .self_nu = evalNuIon,
+    },    
+  };
+
+  // field
+  struct gkyl_pkpm_field field = {
+    .epsilon0 = 1.0, .mu0 = 1.0,
+    .elcErrorSpeedFactor = 0.0,
+    .mgnErrorSpeedFactor = 0.0,
+
+    .ctx = &ctx,
+    .init = evalFieldFunc
+  };
+
+  // pkpm app
+  struct gkyl_pkpm pkpm = {
+    .name = "pkpm_alf_soliton_1x_p2",
+
+    .cdim = 1, .vdim = 1,
+    .lower = { -ctx.Lx },
+    .upper = { ctx.Lx },
+    .cells = { NX},
+    .poly_order = 2,
+    .basis_type = app_args.basis_type,
+
+    // .use_explicit_source = true, 
+    
+    .num_periodic_dir = 1,
+    .periodic_dirs = { 0 },
+
+    .num_species = 2,
+    .species = { elc, ion },
+    .field = field,
+
+    .use_gpu = app_args.use_gpu,
+  };
+
+  // create app object
+  gkyl_pkpm_app *app = gkyl_pkpm_app_new(&pkpm);
+
+  // start, end and initial time-step
+  double tcurr = 0.0, tend = ctx.t_end;
+  double dt = ctx.init_dt;
+  int nframe = 100;
+  // create trigger for IO
+  struct gkyl_tm_trigger io_trig = { .dt = tend/nframe };
+
+  // initialize simulation
+  gkyl_pkpm_app_apply_ic(app, tcurr);
+  write_data(&io_trig, app, tcurr);
+  gkyl_pkpm_app_calc_field_energy(app, tcurr);
+  gkyl_pkpm_app_calc_integrated_L2_f(app, tcurr);
+  gkyl_pkpm_app_calc_integrated_mom(app, tcurr);
+
+  long step = 1, num_steps = app_args.num_steps;
+  while ((tcurr < tend) && (step <= num_steps)) {
+    gkyl_pkpm_app_cout(app, stdout, "Taking time-step at t = %g ...", tcurr);
+    struct gkyl_update_status status = gkyl_pkpm_update(app, dt);
+    gkyl_pkpm_app_cout(app, stdout, " dt = %g\n", status.dt_actual);
+    if (step % 100 == 0) {
+      gkyl_pkpm_app_calc_field_energy(app, tcurr);
+      gkyl_pkpm_app_calc_integrated_L2_f(app, tcurr);
+      gkyl_pkpm_app_calc_integrated_mom(app, tcurr);
+    }
+    if (!status.success) {
+      gkyl_pkpm_app_cout(app, stdout, "** Update method failed! Aborting simulation ....\n");
+      break;
+    }
+    if (status.dt_actual < ctx.min_dt) {
+      gkyl_pkpm_app_cout(app, stdout, "** Time step crashing! Aborting simulation and writing out last output ....\n");
+      gkyl_pkpm_app_write(app, tcurr, 1000);
+      break;
+    }
+    tcurr += status.dt_actual;
+    dt = status.dt_suggested;
+
+    write_data(&io_trig, app, tcurr);
+
+    step += 1;
+  }
+  gkyl_pkpm_app_calc_field_energy(app, tcurr);
+  gkyl_pkpm_app_calc_integrated_L2_f(app, tcurr);
+  gkyl_pkpm_app_calc_integrated_mom(app, tcurr);
+  gkyl_pkpm_app_write_field_energy(app);
+  gkyl_pkpm_app_write_integrated_L2_f(app);
+  gkyl_pkpm_app_write_integrated_mom(app);
+  gkyl_pkpm_app_stat_write(app);
+
+  // fetch simulation statistics
+  struct gkyl_pkpm_stat stat = gkyl_pkpm_app_stat(app);
+
+  gkyl_pkpm_app_cout(app, stdout, "\n");
+  gkyl_pkpm_app_cout(app, stdout, "Number of update calls %ld\n", stat.nup);
+  gkyl_pkpm_app_cout(app, stdout, "Number of forward-Euler calls %ld\n", stat.nfeuler);
+  gkyl_pkpm_app_cout(app, stdout, "Number of RK stage-2 failures %ld\n", stat.nstage_2_fail);
+  if (stat.nstage_2_fail > 0) {
+    gkyl_pkpm_app_cout(app, stdout, "Max rel dt diff for RK stage-2 failures %g\n", stat.stage_2_dt_diff[1]);
+    gkyl_pkpm_app_cout(app, stdout, "Min rel dt diff for RK stage-2 failures %g\n", stat.stage_2_dt_diff[0]);
+  }  
+  gkyl_pkpm_app_cout(app, stdout, "Number of RK stage-3 failures %ld\n", stat.nstage_3_fail);
+  gkyl_pkpm_app_cout(app, stdout, "Species RHS calc took %g secs\n", stat.species_rhs_tm);
+  gkyl_pkpm_app_cout(app, stdout, "Species collisions RHS calc took %g secs\n", stat.species_coll_tm);
+  gkyl_pkpm_app_cout(app, stdout, "Fluid Species RHS calc took %g secs\n", stat.fluid_species_rhs_tm);
+  gkyl_pkpm_app_cout(app, stdout, "Field RHS calc took %g secs\n", stat.field_rhs_tm);
+  gkyl_pkpm_app_cout(app, stdout, "Species PKPM Vars took %g secs\n", stat.species_pkpm_vars_tm);
+  gkyl_pkpm_app_cout(app, stdout, "Species collisional moments took %g secs\n", stat.species_coll_mom_tm);
+  gkyl_pkpm_app_cout(app, stdout, "EM Variables (bvar) calculation took %g secs\n", stat.field_em_vars_tm);
+  gkyl_pkpm_app_cout(app, stdout, "Current evaluation and accumulate took %g secs\n", stat.current_tm);
+  gkyl_pkpm_app_cout(app, stdout, "Updates took %g secs\n", stat.total_tm);
+
+  gkyl_pkpm_app_cout(app, stdout, "Number of write calls %ld,\n", stat.nio);
+  gkyl_pkpm_app_cout(app, stdout, "IO time took %g secs \n", stat.io_tm);
+
+  // simulation complete, free app
+  gkyl_pkpm_app_release(app);
+  
+  return 0;
+}
