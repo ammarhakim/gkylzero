4890a29ab15246ede7b4d9c1587fd51751229e32
Dingyun Liu <dingyunl@princeton.edu>
2024-07-08 13:30:37 -0400

    Add the 3x2v case.

--- unit/ctest_proj_gkmaxwellian_on_basis_prim_mom.c
+++ unit/ctest_proj_gkmaxwellian_on_basis_prim_mom.c
@@ -3,6 +3,8 @@
 #include <acutest.h>
 
 #include <gkyl_array_rio.h>
+#include <gkyl_gk_geometry.h>
+#include <gkyl_gk_geometry_mapc2p.h>
 #include <gkyl_proj_maxwellian_on_basis.h>
 #include <gkyl_proj_on_basis.h>
 #include <gkyl_velocity_map.h>
@@ -256,7 +258,12 @@ test_1x2v_gk(int poly_order, bool use_gpu)
 
   // write distribution function to file
   char fname[1024];
-  sprintf(fname, "ctest_proj_maxwellian_on_basis_test_1x2v_p%d.gkyl", poly_order);
+  if (use_gpu) {
+    sprintf(fname, "ctest_proj_gkmaxwellian_on_basis_prim_mom_test_1x2v_p%d_gpu.gkyl", poly_order);
+  } 
+  else {
+    sprintf(fname, "ctest_proj_gkmaxwellian_on_basis_prim_mom_test_1x2v_p%d_cpu.gkyl", poly_order);
+  }   
   gkyl_grid_sub_array_write(&grid, &local, 0, distf, fname);
 
   gkyl_array_release(den); 
@@ -288,21 +295,339 @@ test_1x2v_gk(int poly_order, bool use_gpu)
 #endif  
 }
 
+void eval_den_3x(double t, const double *xn, double* restrict fout, void *ctx)
+{
+  double x = xn[0], y = xn[1], z = xn[2];
+  fout[0] = 1.0*(1.0+0.2*cos(x))*(1+0.2*cos(y))*(1.0+0.2*cos(z));
+}
+
+void
+mapc2p_3x(double t, const double *xc, double* GKYL_RESTRICT xp, void *ctx)
+{
+  xp[0] = xc[0]; xp[1] = xc[1]; xp[2] = xc[2];
+}
+
+void
+bmag_func_3x(double t, const double *xc, double* GKYL_RESTRICT fout, void *ctx)
+{
+  double x = xc[0], y = xc[1], z = xc[2];
+  fout[0] = 1.0;
+}
+
+void
+test_3x2v_gk(int poly_order, bool use_gpu)
+{
+  double mass = 1.0;
+  double lower[] = {0.1, 0.1, 0.1, -6.0, 0.0}, upper[] = {1.0, 1.0, 1.0, 6.0, 6.0};
+  int cells[] = {2, 2, 2, 16, 16};
+  const int vdim = 2;
+  const int ndim = sizeof(cells)/sizeof(cells[0]);
+  const int cdim = ndim-vdim;
+
+  double confLower[cdim], confUpper[cdim];
+  int confCells[cdim];
+  for (int d=0; d<cdim; d++) {
+    confLower[d] = lower[d];
+    confUpper[d] = upper[d];
+    confCells[d] = cells[d];
+  }
+  double vLower[vdim], vUpper[vdim];
+  int vCells[vdim];
+  for (int d=0; d<vdim; d++) {
+    vLower[d] = lower[cdim+d];
+    vUpper[d] = upper[cdim+d];
+    vCells[d] = cells[cdim+d];
+  }
+
+  // grids
+  struct gkyl_rect_grid grid;
+  gkyl_rect_grid_init(&grid, ndim, lower, upper, cells);
+  struct gkyl_rect_grid confGrid;
+  gkyl_rect_grid_init(&confGrid, cdim, confLower, confUpper, confCells);
+  struct gkyl_rect_grid vGrid;
+  gkyl_rect_grid_init(&vGrid, vdim, vLower, vUpper, vCells);
+
+  // basis functions
+  struct gkyl_basis basis, confBasis;
+  if (poly_order == 1) 
+    gkyl_cart_modal_gkhybrid(&basis, cdim, vdim);
+  else
+    gkyl_cart_modal_serendip(&basis, ndim, poly_order);
+  gkyl_cart_modal_serendip(&confBasis, cdim, poly_order);
+
+  struct gkyl_basis *basis_on_dev, *conf_basis_on_dev;
+  if (use_gpu) {
+#ifdef GKYL_HAVE_CUDA
+    basis_on_dev = gkyl_cu_malloc(sizeof(struct gkyl_basis));
+    conf_basis_on_dev = gkyl_cu_malloc(sizeof(struct gkyl_basis));
+    if (poly_order == 1) 
+      gkyl_cart_modal_gkhybrid_cu_dev(basis_on_dev, cdim, vdim);
+    else
+      gkyl_cart_modal_serendip_cu_dev(basis_on_dev, ndim, poly_order);
+    gkyl_cart_modal_serendip_cu_dev(conf_basis_on_dev, cdim, poly_order);
+#endif
+  }
+  else { 
+    basis_on_dev = &basis;
+    conf_basis_on_dev = &confBasis;
+  }
+
+  int confGhost[] = { 1, 1, 1 };
+  struct gkyl_range confLocal, confLocal_ext; // local, local-ext conf-space ranges
+  gkyl_create_grid_ranges(&confGrid, confGhost, &confLocal_ext, &confLocal);
+  struct skin_ghost_ranges confSkin_ghost; // conf-space skin/ghost
+  skin_ghost_ranges_init(&confSkin_ghost, &confLocal_ext, confGhost);
+
+  int ghost[] = { confGhost[0], confGhost[1], confGhost[2], 0, 0 };
+  struct gkyl_range local, local_ext; // local, local-ext phase-space ranges
+  gkyl_create_grid_ranges(&grid, ghost, &local_ext, &local);
+  struct skin_ghost_ranges skin_ghost; // phase-space skin/ghost
+  skin_ghost_ranges_init(&skin_ghost, &local_ext, ghost);
+
+  int vGhost[] = {0, 0};
+  struct gkyl_range vLocal, vLocal_ext;
+  gkyl_create_grid_ranges(&vGrid, vGhost, &vLocal_ext, &vLocal);
+
+  // create primitive moment arrays
+  struct gkyl_array *den, *udrift, *vtsq;
+  den = mkarr(confBasis.num_basis, confLocal_ext.volume);
+  udrift = mkarr(confBasis.num_basis, confLocal_ext.volume);
+  vtsq = mkarr(confBasis.num_basis, confLocal_ext.volume);
+
+  gkyl_proj_on_basis *proj_den = gkyl_proj_on_basis_new(&confGrid, &confBasis,
+    poly_order+1, 1, eval_den_3x, NULL);
+  gkyl_proj_on_basis *proj_udrift = gkyl_proj_on_basis_new(&confGrid, &confBasis,
+    poly_order+1, vdim, eval_udrift_2v_gk, NULL);
+  gkyl_proj_on_basis *proj_vtsq = gkyl_proj_on_basis_new(&confGrid, &confBasis,
+    poly_order+1, 1, eval_vtsq, NULL);
+
+  gkyl_proj_on_basis_advance(proj_den, 0.0, &confLocal, den);
+  gkyl_proj_on_basis_advance(proj_udrift, 0.0, &confLocal, udrift);
+  gkyl_proj_on_basis_advance(proj_vtsq, 0.0, &confLocal, vtsq);
+
+  // proj_maxwellian expects the primitive moments as a single array.
+  struct gkyl_array *prim_moms_ho = mkarr(3*confBasis.num_basis, confLocal_ext.volume);
+  gkyl_array_set_offset(prim_moms_ho, 1., den, 0*confBasis.num_basis);
+  gkyl_array_set_offset(prim_moms_ho, 1., udrift, 1*confBasis.num_basis);
+  gkyl_array_set_offset(prim_moms_ho, 1., vtsq  , 2*confBasis.num_basis);
+  struct gkyl_array *prim_moms;
+  if (use_gpu) { // copy host array to device
+    prim_moms = gkyl_array_cu_dev_new(GKYL_DOUBLE, 3*confBasis.num_basis, confLocal_ext.volume);
+    gkyl_array_copy(prim_moms, prim_moms_ho);
+  } else {
+    prim_moms = prim_moms_ho;
+  }
+
+  // create bmag and jacob_tot arrays
+  /*
+  struct gkyl_array *bmag, *jacob_tot;
+  bmag = mkarr(confBasis.num_basis, confLocal_ext.volume);
+  jacob_tot = mkarr(confBasis.num_basis, confLocal_ext.volume);
+  struct gkyl_array *bmag_cu, *jacob_tot_cu;
+  if (use_gpu) { // create device copies
+    bmag_cu = gkyl_array_cu_dev_new(GKYL_DOUBLE, confBasis.num_basis, confLocal_ext.volume);
+    jacob_tot_cu = gkyl_array_cu_dev_new(GKYL_DOUBLE, confBasis.num_basis, confLocal_ext.volume);
+  }
+  gkyl_proj_on_basis *proj_bmag = gkyl_proj_on_basis_new(&confGrid, &confBasis,
+    poly_order+1, 1, eval_bmag, NULL);
+  gkyl_proj_on_basis *proj_jac = gkyl_proj_on_basis_new(&confGrid, &confBasis,
+    poly_order+1, 1, eval_jacob_tot, NULL);
+  gkyl_proj_on_basis_advance(proj_bmag, 0.0, &confLocal, bmag);
+  gkyl_proj_on_basis_advance(proj_jac, 0.0, &confLocal, jacob_tot);
+  if (use_gpu) {  // copy host array to device
+    gkyl_array_copy(bmag_cu, bmag);
+    gkyl_array_copy(jacob_tot_cu, jacob_tot);
+  }
+  */
+  // Initialize geometry
+  struct gkyl_gk_geometry_inp geometry_input = {
+      .geometry_id = GKYL_MAPC2P,
+      .mapc2p = mapc2p_3x, // mapping of computational to physical space
+      .c2p_ctx = 0,
+      .bmag_func = bmag_func_3x, // magnetic field magnitude
+      .bmag_ctx =0 ,
+      .grid = confGrid,
+      .local = confLocal,
+      .local_ext = confLocal_ext,
+      .global = confLocal,
+      .global_ext = confLocal_ext,
+      .basis = confBasis,
+      .geo_grid = confGrid,
+      .geo_local = confLocal,
+      .geo_local_ext = confLocal_ext,
+      .geo_global = confLocal,
+      .geo_global_ext = confLocal_ext,
+      .geo_basis = confBasis,
+  };
+  struct gk_geometry* gk_geom;
+  gk_geom = gkyl_gk_geometry_mapc2p_new(&geometry_input);
+
+  // create distribution function array
+  struct gkyl_array *distf;
+  distf = mkarr(basis.num_basis, local_ext.volume);
+  struct gkyl_array *distf_cu;
+  if (use_gpu)  // create device copy.
+    distf_cu  = gkyl_array_cu_dev_new(GKYL_DOUBLE, basis.num_basis, local_ext.volume);
+
+  // Velocity space mapping.
+  struct gkyl_mapc2p_inp c2p_in = { };
+  struct gkyl_velocity_map *gvm = gkyl_velocity_map_new(c2p_in, grid, vGrid,
+    local, local_ext, vLocal, vLocal_ext, use_gpu);
+
+  // projection updater to compute Maxwellian
+  struct gkyl_proj_maxwellian_on_basis_inp inp_proj = {
+    .grid = &grid,
+    .phase_basis = &basis,
+    .conf_basis = &confBasis,
+    .phase_basis_on_dev = basis_on_dev, 
+    .conf_basis_on_dev = conf_basis_on_dev, 
+    .phase_range_ext = &local_ext, 
+    .conf_range_ext = &confLocal_ext, 
+    .vel_map = gvm,
+    .use_gpu = use_gpu,
+  };
+  gkyl_proj_maxwellian_on_basis *proj_max = gkyl_proj_maxwellian_on_basis_inew(&inp_proj);
+  
+  if (use_gpu) {
+    gkyl_proj_gkmaxwellian_on_basis_prim_mom(proj_max, &local, &confLocal, prim_moms,
+      gk_geom->bmag, gk_geom->jacobtot, mass, distf_cu);
+      //bmag_cu, jacob_tot_cu, mass, distf_cu);
+    gkyl_array_copy(distf, distf_cu);
+  } else {
+    gkyl_proj_gkmaxwellian_on_basis_prim_mom(proj_max, &local, &confLocal, prim_moms,
+      gk_geom->bmag, gk_geom->jacobtot, mass, distf);
+      //bmag, jacob_tot, mass, distf);
+  }
+
+  // values to compare  at index (1, 1, 1, 9, 9) [remember, lower-left index is (1,1,1)]
+  double p1_vals[] = {  
+    2.4243050727223950e-02, -1.6882464773457025e-04, -1.6882464773457199e-04,
+   -1.6882464773457155e-04,  6.4367806805923898e-04, -2.6141835388891624e-03,
+    1.1756672872340893e-06,  1.1756672872319208e-06,  1.1756672872319208e-06,
+   -4.4824689894557621e-06, -4.4824689894555452e-06, -4.4824689894551116e-06,
+    1.8204747415343751e-05,  1.8204747415343968e-05,  1.8204747415343643e-05,
+   -6.9409276447821217e-05, -8.1871550658971042e-09,  3.1215182306927454e-08,
+    3.1215182306986178e-08,  3.1215182307144295e-08, -1.2677488918737411e-07,
+   -1.2677488918650675e-07, -1.2677488918661517e-07,  4.8335487054231844e-07,
+    4.8335487054236820e-07,  4.8335487054221002e-07, -2.1737743278163100e-10,
+    8.8283963297070298e-10, -3.3660044136686707e-09, -3.3660044141023515e-09,
+   -3.3660044141023515e-09,  2.3440305414670191e-11, -5.0135600263416188e-04,
+    3.4913613590419644e-06,  3.4913613590417975e-06,  3.4913613590416391e-06,
+    5.4062363023384633e-05, -2.4313270560376134e-08, -2.4313270561026655e-08,
+   -2.4313270560918235e-08, -3.7648147074447582e-07, -3.7648147074447582e-07,
+   -3.7648147074469266e-07,  1.6931364740599317e-10,  2.6217555038242103e-09,
+    2.6217555040410507e-09,  2.6217555040410507e-09, -1.8257477671200626e-11
+  };
+  double p2_vals[] = { 
+    2.4243271148107627e-02, -1.6844589780429920e-04, -1.6844589780430397e-04,
+   -1.6844589780430353e-04,  6.4368392046165463e-04, -2.6182788029943962e-03,
+    1.1703874577738927e-06,  1.1703874577719411e-06,  1.1703874577718327e-06,
+   -4.4724127871178736e-06, -4.4724127871176025e-06, -4.4724127871178194e-06,
+    1.8192195309699262e-05,  1.8192195309699208e-05,  1.8192195309699167e-05,
+   -6.9518009944984270e-05, -2.9116288832330320e-05, -2.9116288832326417e-05,
+   -2.9116288832326417e-05, -5.0136056102635131e-04,  1.2653453167179150e-04,
+   -8.1320282624845902e-09,  3.1074997374563754e-08,  3.1074997374780594e-08,
+    3.1074997374726384e-08, -1.2640211187902229e-07, -1.2640211187891387e-07,
+   -1.2640211187896808e-07,  4.8302159915686031e-07,  4.8302159915691452e-07,
+    4.8302159915684676e-07,  2.0230435831401641e-07,  2.0230435831618481e-07,
+    2.0230435831542587e-07,  2.0230435831553429e-07,  2.0230435831596797e-07,
+    2.0230435831607639e-07, -7.7306757946756566e-07, -7.7306757946751145e-07,
+   -7.7306757946767408e-07,  3.4835286587286006e-06,  3.4835286587286006e-06,
+    3.4835286587284921e-06,  3.1445658222361780e-06,  3.1445658222366930e-06,
+    3.1445658222365439e-06,  5.4147054725953100e-05, -8.7918097605268546e-07,
+   -8.7918097605265835e-07, -8.7918097605272612e-07,  3.3596226731406998e-06,
+   -2.1591376005545164e-10,  8.7826090376862898e-10, -3.3561067908167493e-09,
+   -3.3561067907896443e-09, -3.3561067907625392e-09, -1.4056411382700062e-09,
+   -1.4056411383784264e-09, -1.4056411384868466e-09,  5.3713899286200678e-09,
+    5.3713899286742779e-09,  5.3713899289995386e-09,  5.3713899289453285e-09,
+    5.3713899286742779e-09,  5.3713899286742779e-09, -2.4204081572204428e-08,
+   -2.4204081572123112e-08, -2.4204081572177323e-08, -2.1848916752811268e-08,
+   -2.1848916752838373e-08, -2.1848916752960345e-08, -2.1848916752960345e-08,
+   -2.1848916752770610e-08, -2.1848916752797715e-08,  8.3491474571778701e-08,
+    8.3491474571765148e-08,  8.3491474571860016e-08, -3.7622188817055589e-07,
+   -3.7622188817055589e-07, -3.7622188817074563e-07,  6.1086817838469061e-09,
+    6.1086817838062485e-09,  6.1086817838469061e-09, -2.3343164130186359e-08,
+   -2.3343164130145701e-08, -2.3343164130172806e-08,  2.3318735367687375e-11,
+   -3.7321226043770821e-11, -3.7321226097980930e-11, -3.7321225881140495e-11,
+    1.6817360273756146e-10,  1.5180956285367659e-10,  1.5180956285367659e-10,
+    1.5180956288078164e-10, -5.8011133512704236e-10, -5.8011133512704236e-10,
+   -5.8011133522191005e-10, -5.8011133516769994e-10, -5.8011133516769994e-10,
+   -5.8011133519480499e-10,  2.6140463198124999e-09,  2.6140463198396050e-09,
+    2.6140463198124999e-09, -4.2444040868023796e-11,  1.6219181891657465e-10,
+    1.6219181869973422e-10,  1.6219181865907664e-10,  4.0307008461021814e-12,
+    4.0307008732072357e-12,  4.0307008189971271e-12, -1.8162787487545735e-11,
+   -1.1269332393403825e-12
+  };
+
+  const double *fv = gkyl_array_cfetch(distf, gkyl_range_idx(&local_ext, (int[5]) { 1, 1, 1, 9, 9 }));
+  if (poly_order == 1) {
+    for (int i=0; i<basis.num_basis; ++i)
+      TEST_CHECK( gkyl_compare_double(p1_vals[i], fv[i], 1e-8) );
+  }
+
+  if (poly_order == 2) {
+    for (int i=0; i<basis.num_basis; ++i)
+      TEST_CHECK( gkyl_compare_double(p2_vals[i], fv[i], 1e-8) );
+  }
+  // write distribution function to file
+  char fname[1024];
+  if (use_gpu) {
+    sprintf(fname, "ctest_proj_gkmaxwellian_on_basis_prim_mom_test_3x2v_p%d_gpu.gkyl", poly_order);
+  } 
+  else {
+    sprintf(fname, "ctest_proj_gkmaxwellian_on_basis_prim_mom_test_3x2v_p%d_cpu.gkyl", poly_order);
+  }   
+  gkyl_grid_sub_array_write(&grid, &local, 0, distf, fname);
+
+  gkyl_array_release(den); 
+  gkyl_array_release(udrift); 
+  gkyl_array_release(vtsq);
+  gkyl_proj_on_basis_release(proj_den);
+  gkyl_proj_on_basis_release(proj_udrift);
+  gkyl_proj_on_basis_release(proj_vtsq);
+
+  gkyl_array_release(prim_moms_ho);
+  //gkyl_array_release(bmag); 
+  //gkyl_array_release(jacob_tot);
+  if (use_gpu) {
+    gkyl_array_release(prim_moms);
+    //gkyl_array_release(bmag_cu); 
+    //gkyl_array_release(jacob_tot_cu);
+  }
+
+  gkyl_gk_geometry_release(gk_geom);
+  gkyl_array_release(distf);
+  if (use_gpu)
+    gkyl_array_release(distf_cu);
+  gkyl_proj_maxwellian_on_basis_release(proj_max);
+  gkyl_velocity_map_release(gvm);
+
+#ifdef GKYL_HAVE_CUDA
+  if (use_gpu) 
+    gkyl_cu_free(basis_on_dev);
+    gkyl_cu_free(conf_basis_on_dev);
+#endif  
+}
+
 void test_1x2v_p1_gk() { test_1x2v_gk(1, false); }
 void test_1x2v_p2_gk() { test_1x2v_gk(2, false); }
+void test_3x2v_p1_gk() { test_3x2v_gk(1, false); }
 
 #ifdef GKYL_HAVE_CUDA
 void test_1x2v_p1_gk_gpu() { test_1x2v_gk(1, true); }
 void test_1x2v_p2_gk_gpu() { test_1x2v_gk(2, true); }
+void test_3x2v_p1_gk_gpu() { test_3x2v_gk(1, true); }
 #endif
 
 TEST_LIST = {
   { "test_1x2v_p1_gk", test_1x2v_p1_gk },
   { "test_1x2v_p2_gk", test_1x2v_p2_gk },
+  { "test_3x2v_p1_gk", test_3x2v_p1_gk },
 
 #ifdef GKYL_HAVE_CUDA
   { "test_1x2v_p1_gk_gpu", test_1x2v_p1_gk_gpu },
   { "test_1x2v_p2_gk_gpu", test_1x2v_p2_gk_gpu },
+  { "test_3x2v_p1_gk_gpu", test_3x2v_p1_gk_gpu },
 #endif
   { NULL, NULL },
 };
