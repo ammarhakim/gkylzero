2df36effd2e443de841203f27468bb9acd1e4b18
manauref <mfrancisquez@gmail.com>
2024-07-18 21:19:41 +0200

    Bring the polarization density initialization out of the mirror-fix branch.

---
+++ kernels/gyrokinetic_pol_density/gkyl_gyrokinetic_pol_density_kernels.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <gkyl_util.h>
+
+EXTERN_C_BEG
+
+GKYL_CU_DH void gkyl_gyrokinetic_pol_density_1x_ser_p1(const double *dx, const double *epsilon, const double *phi, double *out);
+GKYL_CU_DH void gkyl_gyrokinetic_pol_density_2x_ser_p1(const double *dx, const double *epsilon, const double *phi, double *out);
+GKYL_CU_DH void gkyl_gyrokinetic_pol_density_3x_ser_p1(const double *dx, const double *epsilon, const double *phi, double *out);
+
+
+EXTERN_C_END
+
