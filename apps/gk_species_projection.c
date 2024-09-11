b7ddd6dbfa21dc20179f7ec3ab05278db2695206
Junoravin <junoravin@gmail.com>
2024-09-11 12:55:48 -0400

    Just realized I need to be careful to multiply by the velocity space Jacobian if one is using PROJ_FUNC as one's initial conditions. I moved the multiplication by the velocity space Jacobian inside the project Maxwellian routine so that we didn't need so many scale_by_cell calls (especially with the correction routine) but without this fact PROJ_FUNC will not have the correct prefactor.

--- apps/gk_species_projection.c
+++ apps/gk_species_projection.c
@@ -133,7 +133,9 @@ gk_species_projection_calc(gkyl_gyrokinetic_app *app, const struct gk_species *s
 
     // Multiply by the gyrocenter coord jacobian (bmag).
     gkyl_dg_mul_conf_phase_op_range(&app->confBasis, &app->basis, f, 
-        app->gk_geom->bmag, f, &app->local, &s->local);      
+        app->gk_geom->bmag, f, &app->local, &s->local); 
+    // Multiply by the velocity-space jacobian. 
+    gkyl_array_scale_by_cell(f, s->vel_map->jacobvel);     
   }
   else if (proj->proj_id == GKYL_PROJ_MAXWELLIAN_PRIM) { 
     gkyl_proj_on_basis_advance(proj->proj_dens, tm, &app->local_ext, proj->dens); 
