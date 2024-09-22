2a41f1761afadcd3daca7578ad1b99cb2f6c647b
Jonathan Roeltgen <roeltgen@utexas.edu>
2024-09-21 17:24:51 -0700

    Update ar_react regression test to match tests in gk-g0-app

--- regression/rt_ar_react_1x2v_quadraticV.c
+++ regression/rt_ar_react_1x2v_quadraticV.c
@@ -385,8 +385,18 @@ main(int argc, char **argv)
   for (int d=0; d<ctx.vdim; d++)
     cells_v[d] = APP_ARGS_CHOOSE(app_args.vcells[d], ctx.cells[ctx.cdim+d]);
 
+  // Create decomposition.
+  struct gkyl_rect_decomp *decomp = gkyl_gyrokinetic_comms_decomp_new(ctx.cdim, cells_x, app_args.cuts, app_args.use_mpi, stderr);
+  
   // Construct communicator for use in app.
-  struct gkyl_comm *comm = gkyl_gyrokinetic_comms_new(app_args.use_mpi, app_args.use_gpu, stderr);
+  struct gkyl_comm *comm = gkyl_gyrokinetic_comms_new(app_args.use_mpi, app_args.use_gpu, decomp, stderr);
+
+  int my_rank = 0;
+#ifdef GKYL_HAVE_MPI
+  if (app_args.use_mpi)
+    gkyl_comm_get_rank(comm, &my_rank);
+#endif
+  
   // electrons
   struct gkyl_gyrokinetic_species elc = {
     .name = "elc",
@@ -661,11 +671,13 @@ main(int argc, char **argv)
     .field = field,
     .skip_field=true,
 
-    .parallelism = {
-      .use_gpu = app_args.use_gpu,
-      .cuts = { app_args.cuts[0] },
-      .comm = comm,
-    },  
+    .use_gpu = app_args.use_gpu,
+
+    .has_low_inp = true,
+    .low_inp = {
+      .local_range = decomp->ranges[my_rank],
+      .comm = comm
+    }
   };
   
   // create app object
@@ -777,7 +789,7 @@ main(int argc, char **argv)
   freeresources:
   // Free resources after simulation completion
   gkyl_gyrokinetic_app_release(app);
-  gkyl_gyrokinetic_comms_release(comm);
+  gkyl_gyrokinetic_comms_release(decomp, comm);
 
 #ifdef GKYL_HAVE_MPI
   if (app_args.use_mpi) {
