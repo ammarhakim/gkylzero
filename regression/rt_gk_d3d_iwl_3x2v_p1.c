c8b3ab12c263f93ec5c6bc53c54bb56318b60282
manauref <mfrancisquez@gmail.com>
2024-10-17 10:48:53 -0700

    Fix a regression test that had not been updated with the parallelism interface.

--- regression/rt_gk_d3d_iwl_3x2v_p1.c
+++ regression/rt_gk_d3d_iwl_3x2v_p1.c
@@ -647,17 +647,8 @@ main(int argc, char **argv)
   for (int d=0; d<ctx.vdim; d++)
     cells_v[d] = APP_ARGS_CHOOSE(app_args.vcells[d], ctx.cells[ctx.cdim+d]);
 
-  // Create decomposition.
-  struct gkyl_rect_decomp *decomp = gkyl_gyrokinetic_comms_decomp_new(ctx.cdim, cells_x, app_args.cuts, app_args.use_mpi, stderr);
-
   // Construct communicator for use in app.
   struct gkyl_comm *comm = gkyl_gyrokinetic_comms_new(app_args.use_mpi, app_args.use_gpu, decomp, stderr);
-
-  int my_rank = 0;
-#ifdef GKYL_HAVE_MPI
-  if (app_args.use_mpi)
-    gkyl_comm_get_rank(comm, &my_rank);
-#endif
 
   // electrons
   struct gkyl_gyrokinetic_species elc = {
@@ -825,7 +816,7 @@ main(int argc, char **argv)
   struct gkyl_gk gk = {
     .name = "gk_d3d_iwl_3x2v_p1",
 
-    .cdim = 3, .vdim = 2,
+    .cdim = ctx.cdim, .vdim = ctx.vdim,
     .lower = { ctx.x_min, ctx.y_min, ctx.z_min },
     .upper = { ctx.x_max, ctx.y_max, ctx.z_max },
     .cells = { cells_x[0], cells_x[1], cells_x[2] },
@@ -847,11 +838,9 @@ main(int argc, char **argv)
     .species = { elc, ion },
     .field = field,
 
-    .use_gpu = app_args.use_gpu,
-
-    .has_low_inp = true,
-    .low_inp = {
-      .local_range = decomp->ranges[my_rank],
+    .parallelism = {
+      .use_gpu = app_args.use_gpu,
+      .cuts = { app_args.cuts[0], app_args.cuts[1], app_args.cuts[2] },
       .comm = comm
     }
   };
