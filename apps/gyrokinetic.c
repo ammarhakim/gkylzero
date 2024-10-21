907072e43442ae4036ae78134a30ea0729ed9aa5
mrquell <mrquell@gmail.com>
2024-10-21 17:21:49 -0400

    Fix for the single species quasineutrality solve with the positivity fix

--- apps/gyrokinetic.c
+++ apps/gyrokinetic.c
@@ -2430,9 +2430,15 @@ rk3(gkyl_gyrokinetic_app* app, double dt0)
             // Rescale each species to enforce quasineutrality.
             for (int i=0; i<app->num_species; ++i) {
               struct gk_species *gks = &app->species[i];
-              gkyl_positivity_shift_gyrokinetic_quasineutrality_scale(gks->pos_shift_op, &app->local, &gks->local,
-                gks->ps_delta_m0, gks->ps_delta_m0s_tot, gks->ps_delta_m0r_tot, gks->m0.marr, gks->f);
-
+              if (gks->info.charge > 0.0 && app->num_species > 1) {
+                struct gk_species *gkelc = &app->species[elc_idx];
+                gkyl_positivity_shift_gyrokinetic_quasineutrality_scale(gks->pos_shift_op, &app->local, &gks->local,
+                  gks->ps_delta_m0, app->ps_delta_m0_ions, gkelc->ps_delta_m0, gks->m0.marr, gks->f);
+              }
+              else {
+                gkyl_positivity_shift_gyrokinetic_quasineutrality_scale(gks->pos_shift_op, &app->local, &gks->local,
+                  gks->ps_delta_m0, gks->ps_delta_m0, app->ps_delta_m0_ions, gks->m0.marr, gks->f);
+              }
               gkyl_array_accumulate(gks->fnew, 1.0, gks->f);
             }
           }
