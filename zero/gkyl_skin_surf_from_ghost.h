754f0d24ca0e39b94bbea6c792d3710dd7ce2e7e
Antoinehoff <antoine-hoffmann@hotmail.com>
2024-10-23 14:05:16 -0400

    update the comments and descriptions of the header functions

--- zero/gkyl_skin_surf_from_ghost.h
+++ zero/gkyl_skin_surf_from_ghost.h
@@ -9,34 +9,31 @@
 typedef struct gkyl_skin_surf_from_ghost gkyl_skin_surf_from_ghost;
 
 /**
- * Create a new updater to copy the ghost node values to the skin node values
+ * Create a new updater to copy ghost cell values to skin cells (boundary cells).
  *
- * @param dir Direction in which to apply BC.
- * @param edge Lower or upper edge at which to apply BC (see gkyl_edge_loc).
- * @param basis Basis on which coefficients in array are expanded (a device pointer if use_gpu=true).
- * @param skin_r Skin range.
- * @param ghost_r Ghost range.
- * @param use_gpu Boolean to indicate whether to use the GPU.
- * @return New updater pointer.
+ * @param dir Direction along which to apply the skin-ghost boundary copy.
+ * @param edge Lower or upper edge where the boundary condition is applied (see gkyl_edge_loc).
+ * @param basis Basis on which the array coefficients are expanded (use a device pointer if use_gpu=true).
+ * @param skin_r Range representing the skin (boundary) region.
+ * @param ghost_r Range representing the ghost (outer boundary) region.
+ * @param use_gpu Boolean flag to indicate whether GPU computation should be used.
+ * @return Pointer to the newly created updater.
  */
 struct gkyl_skin_surf_from_ghost* gkyl_skin_surf_from_ghost_new(int dir, enum gkyl_edge_loc edge,
   const struct gkyl_basis *basis, const struct gkyl_range *skin_r, const struct gkyl_range *ghost_r,
   bool use_gpu);
 
 /**
- * Apply the sheath BC with the skin_surf_from_ghost object.
+ * Copy the values from the ghost region to the adjacent skin region (boundary cells).
  *
- * @param up BC updater.
- * @param phi Electrostatic potential.
- * @param phi_wall Wall potential.
- * @param distf Distribution function array to apply BC to.
- * @param conf_r Configuration space range (to index phi).
+ * @param up Pointer to the boundary condition updater.
+ * @param field Array representing the field values to update (currently works only in configuration space).
  */
-void gkyl_skin_surf_from_ghost_advance(const struct gkyl_skin_surf_from_ghost *up, struct gkyl_array *phi);
+void gkyl_skin_surf_from_ghost_advance(const struct gkyl_skin_surf_from_ghost *up, struct gkyl_array *field);
 
 /**
- * Free memory associated with skin_surf_from_ghost updater.
+ * Free the memory associated with the skin_surf_from_ghost updater.
  *
- * @param up BC updater.
+ * @param up Pointer to the boundary condition updater to be released.
  */
 void gkyl_skin_surf_from_ghost_release(struct gkyl_skin_surf_from_ghost *up);
