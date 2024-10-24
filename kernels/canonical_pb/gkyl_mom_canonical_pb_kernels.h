d7bafc2811ff56244ed130ec0b43386697f69c65
johnson452 <grj@princeton.edu>
2024-10-24 11:36:30 -0400

    Extended the eval on nodes unit test to include testing hybrid the basis 1x1v and 2x2v

--- kernels/canonical_pb/gkyl_mom_canonical_pb_kernels.h
+++ kernels/canonical_pb/gkyl_mom_canonical_pb_kernels.h
@@ -15,9 +15,6 @@ GKYL_CU_DH void canonical_pb_int_mom_2x2v_ser_p1(const double *dxv, const double
 GKYL_CU_DH void canonical_pb_MEnergy_2x2v_ser_p2(const double *dxv, const double *hamil, const double *f, double* GKYL_RESTRICT out); 
 GKYL_CU_DH void canonical_pb_int_mom_2x2v_ser_p2(const double *dxv, const double *hamil, const double *f, double* GKYL_RESTRICT out); 
 
-GKYL_CU_DH void canonical_pb_MEnergy_3x3v_ser_p1(const double *dxv, const double *hamil, const double *f, double* GKYL_RESTRICT out); 
-GKYL_CU_DH void canonical_pb_int_mom_3x3v_ser_p1(const double *dxv, const double *hamil, const double *f, double* GKYL_RESTRICT out); 
-
 GKYL_CU_DH void canonical_pb_MEnergy_1x1v_tensor_p1(const double *dxv, const double *hamil, const double *f, double* GKYL_RESTRICT out); 
 GKYL_CU_DH void canonical_pb_int_mom_1x1v_tensor_p1(const double *dxv, const double *hamil, const double *f, double* GKYL_RESTRICT out); 
 
