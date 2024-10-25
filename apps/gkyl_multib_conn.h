6bf214ff8a911d8911936ea1151360f054f6ec8a
akashukla <akashukla@utexas.edu>
2024-10-25 11:50:29 -0500

    Based on DR review session, Modify multib_conn.c signature so it does not break layering

--- apps/gkyl_multib_conn.h
+++ apps/gkyl_multib_conn.h
@@ -10,5 +10,29 @@ enum gkyl_conn_id {
   GKYL_CONN_CORNER = 2, // Blocks connected by a corner
 };
 
+/** 
+ * Given a block topology, connection type, block id, and direction, 
+ * get the number of connected blocks
+ *
+ * @param block_topo block topology object
+ * @param bidx block index
+ * @param dir direction in which to find neighbors or connected blocks
+ * @param conn_id type of connection : GKYL_CONN_NEIGHBOR, _ALL, or _CORNER
+ * return number of connected blocks
+ */
+int gkyl_multib_conn_get_num_connected(struct gkyl_block_topo *block_topo, int bidx, int dir, enum gkyl_conn_id conn_id);
 
-void gkyl_multib_conn_get_connection(gkyl_gyrokinetic_multib_app *mbapp, int bidx, int dir, enum gkyl_conn_id conn_id);
+
+/** 
+ * Given a block topology, connection type, block id, and direction, 
+ * get the number of connected blocks and a list of their ids
+ *
+ * @param block_topo block topology object
+ * @param bidx block index
+ * @param dir direction in which to find neighbors or connected blocks
+ * @param conn_id type of connection : GKYL_CONN_NEIGHBOR, _ALL, or _CORNER
+ * @param block_list on output, list of connected block ids
+ * return number of connected blocks
+ */
+int gkyl_multib_conn_get_connection(struct gkyl_block_topo *block_topo, int bidx, int dir, enum gkyl_conn_id conn_id, int *block_list);
+
