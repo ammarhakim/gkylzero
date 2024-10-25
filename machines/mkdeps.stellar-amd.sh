4f75da0ae4c903d14d8df297b6813cd4fb95439f
Antoinehoff <antoine-hoffmann@hotmail.com>
2024-10-25 14:17:32 -0400

    reset the prefix definition to the conventional one

--- machines/mkdeps.stellar-amd.sh
+++ machines/mkdeps.stellar-amd.sh
@@ -1,5 +1,5 @@
 module load cudatoolkit/12.0
 module load openmpi/cuda-11.1/gcc/4.1.1
 cd install-deps
-: "${PREFIX:=$HOME/amd_gkyl_fresh/gkylsoft}"
+: "${PREFIX:=$HOME/gkylsoft}"
 ./mkdeps.sh --build-openblas=yes --build-superlu=yes --prefix=$PREFIX
