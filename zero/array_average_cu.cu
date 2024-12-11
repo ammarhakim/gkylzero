1cdaaade205e049c3fc9ba7fb65437ba80b89ae6
Antoinehoff <antoine-hoffmann@hotmail.com>
2024-12-11 08:04:39 -0800

    commenting out everything to compile on perlmutter

--- zero/array_average_cu.cu
+++ zero/array_average_cu.cu
@@ -27,52 +27,52 @@ gkyl_array_average_set_ker_cu(struct gkyl_array_average *up)
 struct gkyl_array_average*
 gkyl_array_average_cu_dev_new(gkyl_array_average *up)
 {
-  // Copy struct to device.
-  struct gkyl_array_average *up_cu = (struct gkyl_array_average*) gkyl_cu_malloc(sizeof(struct gkyl_array_average));
-  gkyl_cu_memcpy(up_cu, up, sizeof(struct gkyl_array_average), GKYL_CU_MEMCPY_H2D);
+  // // Copy struct to device.
+  // struct gkyl_array_average *up_cu = (struct gkyl_array_average*) gkyl_cu_malloc(sizeof(struct gkyl_array_average));
+  // gkyl_cu_memcpy(up_cu, up, sizeof(struct gkyl_array_average), GKYL_CU_MEMCPY_H2D);
 
-  // Set the kernel.
-  gkyl_array_average_set_ker_cu<<<1,1>>>(up_cu);
+  // // Set the kernel.
+  // gkyl_array_average_set_ker_cu<<<1,1>>>(up_cu);
 
-  up->on_dev = up_cu;
+  // up->on_dev = up_cu;
 
-  return up;
+  // return up;
 }
 
-template <unsigned int BLOCKSIZE>
-__global__ void
-array_integrate_blockRedAtomic_cub(struct gkyl_array_average *up, 
-  const struct gkyl_range *full_rng, const struct gkyl_range *local_avg,
-  const struct gkyl_array *GKYL_RESTRICT fin, struct gkyl_array *GKYL_RESTRICT *avgout)
-{
-  int full_idx[GKYL_MAX_CDIM], sub_idx[GKYL_MAX_CDIM];
+// template <unsigned int BLOCKSIZE>
+// __global__ void
+// array_integrate_blockRedAtomic_cub(struct gkyl_array_average *up, 
+//   const struct gkyl_range *full_rng, const struct gkyl_range *local_avg,
+//   const struct gkyl_array *GKYL_RESTRICT fin, struct gkyl_array *GKYL_RESTRICT *avgout)
+// {
+//   int full_idx[GKYL_MAX_CDIM], sub_idx[GKYL_MAX_CDIM];
 
   /*
   The cuda code needs to loop over the full range and then
   converts the full dim index to the sub dim index
   */
-  for(unsigned long tid = threadIdx.x + blockIdx.x*blockDim.x;
-      tid < phase_range.volume; tid += blockDim.x*gridDim.x) {
-    gkyl_sub_range_inv_idx(&phase_range, tid, full_idx);
+  // for(unsigned long tid = threadIdx.x + blockIdx.x*blockDim.x;
+  //     tid < phase_range.volume; tid += blockDim.x*gridDim.x) {
+  //   gkyl_sub_range_inv_idx(&phase_range, tid, full_idx);
 
-    long full_lidx = gkyl_range_idx(&phase_range, full_idx);
-    const double* fptr = (const double*) gkyl_array_cfetch(fin, full_lidx);
+  //   long full_lidx = gkyl_range_idx(&phase_range, full_idx);
+  //   const double* fptr = (const double*) gkyl_array_cfetch(fin, full_lidx);
 
-    // get sub-space linear index using the full to sub dimension mapping
-    for (unsigned int k = 0; k < local_avg->ndim; k++)
-      sub_idx[k] = full_idx[up->sub_dir[k]];
-    long sub_lidx = gkyl_range_idx(local_avg, sub_idx);
+  //   // get sub-space linear index using the full to sub dimension mapping
+  //   for (unsigned int k = 0; k < local_avg->ndim; k++)
+  //     sub_idx[k] = full_idx[up->sub_dir[k]];
+  //   long sub_lidx = gkyl_range_idx(local_avg, sub_idx);
 
-    double* avgptr = (double*) gkyl_array_fetch(avgout, sub_lidx);
+  //   double* avgptr = (double*) gkyl_array_fetch(avgout, sub_lidx);
 
-    // reduce local f to local avg
-    up->kernel(fptr, avgLocal, up->subvol);
+  //   // reduce local f to local avg
+  //   up->kernel(fptr, avgLocal, up->subvol);
 
-    for (unsigned int k = 0; k < mout->ncomp; ++k) {
-       if (tid < full_rng.volume)
-         atomicAdd(&avgptr[k], avgLocal[k]);
-    }
-  }
+  //   for (unsigned int k = 0; k < mout->ncomp; ++k) {
+  //      if (tid < full_rng.volume)
+  //        atomicAdd(&avgptr[k], avgLocal[k]);
+  //   }
+  // }
   // Integrate in this cell
   
 
@@ -92,50 +92,50 @@ array_integrate_blockRedAtomic_cub(struct gkyl_array_average *up,
   //   if (threadIdx.x == 0)
   //     atomicAdd(&out[k], bResult);
   // }
-}
+// }
 
 __global__ static void
 gkyl_array_average_advance_cu_ker(gkyl_array_average *up, 
   const struct gkyl_array *fin, struct gkyl_array *avgout)
 {
-  int pidx[GKYL_MAX_DIM];
+  // int pidx[GKYL_MAX_DIM];
 
-  const int num_cbasis = 20; // MF 2024/09/03: Hardcoded to p=2 3x ser for now.
+  // const int num_cbasis = 20; // MF 2024/09/03: Hardcoded to p=2 3x ser for now.
 
-  for(unsigned long tid = threadIdx.x + blockIdx.x*blockDim.x;
-      tid < up->tot_rng.volume; tid += blockDim.x*gridDim.x) {
-    gkyl_sub_range_inv_idx(&up->tot_rng, tid, pidx);
+  // for(unsigned long tid = threadIdx.x + blockIdx.x*blockDim.x;
+  //     tid < up->tot_rng.volume; tid += blockDim.x*gridDim.x) {
+  //   gkyl_sub_range_inv_idx(&up->tot_rng, tid, pidx);
 
-    long clinidx = gkyl_range_idx(&up->sub_rng, pidx);
-    const int *shiftedf_c = (const int*) gkyl_array_cfetch(shiftedf, clinidx);
+  //   long clinidx = gkyl_range_idx(&up->sub_rng, pidx);
+  //   const int *shiftedf_c = (const int*) gkyl_array_cfetch(shiftedf, clinidx);
 
-    if (shiftedf_c[0]) {
-      const double *delta_m0_c = (const double*) gkyl_array_cfetch(delta_m0, clinidx);
-      if (kers->is_m0_positive(delta_m0_c)) {
-        // Rescale f so it has the same m0 at this conf-space cell.
-        const double *m0_c = (const double*) gkyl_array_cfetch(m0, clinidx);
-        double m0ratio_c[num_cbasis];
-        kers->conf_inv_op(m0_c, m0ratio_c);
-        kers->conf_mul_op(delta_m0_c, m0ratio_c, m0ratio_c);
+  //   if (shiftedf_c[0]) {
+  //     const double *delta_m0_c = (const double*) gkyl_array_cfetch(delta_m0, clinidx);
+  //     if (kers->is_m0_positive(delta_m0_c)) {
+  //       // Rescale f so it has the same m0 at this conf-space cell.
+  //       const double *m0_c = (const double*) gkyl_array_cfetch(m0, clinidx);
+  //       double m0ratio_c[num_cbasis];
+  //       kers->conf_inv_op(m0_c, m0ratio_c);
+  //       kers->conf_mul_op(delta_m0_c, m0ratio_c, m0ratio_c);
 
-        long plinidx = gkyl_range_idx(&up->tot_rng, pidx);
-        double *fin_i = (double*) gkyl_array_fetch(fin, plinidx);
-        kers->conf_phase_mul_op(m0ratio_c, fin_i, distf_c);
-      }
-    }
-  }
+  //       long plinidx = gkyl_range_idx(&up->tot_rng, pidx);
+  //       double *fin_i = (double*) gkyl_array_fetch(fin, plinidx);
+  //       kers->conf_phase_mul_op(m0ratio_c, fin_i, distf_c);
+  //     }
+  //   }
+  // }
 }
 
 void gkyl_array_average_advance_cu(gkyl_array_average *up, 
   const struct gkyl_array *fin, struct gkyl_array *avgout)
 {
 
-  int nblocks_tot = up->tot_rng.nblocks, nthreads_tot = up->tot_rng.nthreads;
-  int nblocks_sub = up->sub_rng.nblocks, nthreads_sub = up->sub_rng.nthreads;
+  // int nblocks_tot = up->local.nblocks, nthreads_tot = up->local.nthreads;
+  // int nblocks_sub = up->local_avg.nblocks, nthreads_sub = up->local_avg.nthreads;
 
-  gkyl_array_clear_range(avgout, 0.0, &up->sub_rng);
+  // gkyl_array_clear_range(avgout, 0.0, &up->local_avg);
 
-  gkyl_array_average_advance_cu_ker<<nblocks_tot, nthreads_tot>>(up,fin,avgout);
+  // gkyl_array_average_advance_cu_ker<<nblocks_tot, nthreads_tot>>(up,fin,avgout);
 
   // gkyl_cu_memset(out, 0, up->num_comp*sizeof(double));
   // const int nthreads = GKYL_DEFAULT_NUM_THREADS;
