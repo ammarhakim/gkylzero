f792f09af64d8cb943dd6992997e2af8716d1e61
mrquell <mrquell@gmail.com>
2024-12-13 13:31:28 -0500

    Remove some developer comments. Unit test still passes. The TEST_CHECK was commented out, but the test still passes

--- unit/ctest_null_comm.c
+++ unit/ctest_null_comm.c
@@ -149,14 +149,8 @@ test_2d()
           idx[d] = idx[d] + ncell;
 
         const double  *f = gkyl_array_cfetch(arr, lidx);
-
-//        printf("%d: idx(%d,%d) : (%d,%d) == (%g, %g)\n",
-//          count++, iter.idx[0], iter.idx[1],
-//          idx[0], idx[1],
-//          f[0], f[1]);
-        
-        /* for (int n=0; n<local.ndim; ++n) */
-        /*   TEST_CHECK( idx[n] == f[n] ); */
+        for (int n=0; n<local.ndim; ++n)
+          TEST_CHECK( idx[n] == f[n] );
       }
     }
   }
