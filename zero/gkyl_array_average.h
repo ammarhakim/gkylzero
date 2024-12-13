76b173d19d9939060c85ee95aa1c1786e3a61cd8
Antoinehoff <antoine-hoffmann@hotmail.com>
2024-12-13 14:03:20 -0800

    cleaning

--- zero/gkyl_array_average.h
+++ zero/gkyl_array_average.h
@@ -57,8 +57,3 @@ void gkyl_array_average_advance(const struct gkyl_array_average *up,
  * @param up array_average updater.
  */
 void gkyl_array_average_release(struct gkyl_array_average *up);
-
-#ifdef GKYL_HAVE_CUDA
-void gkyl_array_average_advance_cu(const struct gkyl_array_average *up, 
-  const struct gkyl_array *fin, struct gkyl_array *avgout);
-#endif
