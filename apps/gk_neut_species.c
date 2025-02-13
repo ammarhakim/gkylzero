a79ddbd072d302d80c3fe6dc9879a46ca4339a47
tnbernard <bernardt@fusion.gat.com>
2025-02-12 19:51:24 -0800

    Reverting gk neut basis to hybrid for comparison with main. Regression tests gk_step_out and gk_static are identical to results in main branch.

--- apps/gk_neut_species.c
+++ apps/gk_neut_species.c
@@ -686,7 +686,8 @@ gk_neut_species_init(struct gkyl_gk *gk, struct gkyl_gyrokinetic_app *app, struc
     gkyl_cart_modal_serendip(&s->basis, pdim, app->poly_order);
   }
   else if (app->poly_order == 1) {
-    gkyl_cart_modal_tensor(&s->basis, pdim, app->poly_order);
+    gkyl_cart_modal_hybrid(&s->basis, cdim, vdim); // p=2 in v for neutral species
+    // gkyl_cart_modal_tensor(&s->basis, pdim, app->poly_order); // for canonical PB
   }
 
   if (app->use_gpu) {
@@ -694,7 +695,8 @@ gk_neut_species_init(struct gkyl_gk *gk, struct gkyl_gyrokinetic_app *app, struc
       gkyl_cart_modal_serendip_cu_dev(s->basis_on_dev, pdim, app->poly_order);
     }
     else if (app->poly_order == 1) {
-      gkyl_cart_modal_tensor_cu_dev(s->basis_on_dev, pdim, app->poly_order);
+      gkyl_cart_modal_hybrid_cu_dev(s->basis_on_dev, cdim, vdim); // p=2 in v for neutral species
+      // gkyl_cart_modal_tensor_cu_dev(s->basis_on_dev, pdim, app->poly_order); // for canonical PB
     }
   }
   
