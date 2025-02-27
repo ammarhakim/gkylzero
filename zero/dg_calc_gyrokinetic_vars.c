4825255171a3657a8b0bc885f4f535197335e2e2
Akash Shukla <akashukla@utexas.edu>
2025-02-26 18:27:02 -0600

    Forgot to git add zero/dg_calc_gyrokinetic_vars.c

--- zero/dg_calc_gyrokinetic_vars.c
+++ zero/dg_calc_gyrokinetic_vars.c
@@ -103,7 +103,7 @@ void gkyl_dg_calc_gyrokinetic_vars_alpha_surf(struct gkyl_dg_calc_gyrokinetic_va
     for (int dir = 0; dir<cdim+1; ++dir) {
       const double *bmag_surf_d, *jacobtot_inv_surf_d, *cmag_surf_d, *b_i_surf_d;
       if (dir < cdim) {
-        int surf_dir = dir == cdim-1? 2 : dir;
+        int surf_dir = dir;
         bmag_surf_d = gkyl_array_cfetch(up->gk_geom->geo_surf[surf_dir]->bmag, loc_conf);
         jacobtot_inv_surf_d = gkyl_array_cfetch(up->gk_geom->geo_surf[surf_dir]->jacobtot_inv, loc_conf);
         cmag_surf_d = gkyl_array_cfetch(up->gk_geom->geo_surf[surf_dir]->cmag, loc_conf);
@@ -127,7 +127,7 @@ void gkyl_dg_calc_gyrokinetic_vars_alpha_surf(struct gkyl_dg_calc_gyrokinetic_va
         long loc_conf_ext = gkyl_range_idx(conf_ext_range, idx_edge);
         long loc_phase_ext = gkyl_range_idx(phase_ext_range, idx_edge);
 
-        int surf_dir = dir == cdim-1? 2 : dir;
+        int surf_dir = dir;
         bmag_surf_d = gkyl_array_cfetch(up->gk_geom->geo_surf[surf_dir]->bmag, loc_conf_ext);
         jacobtot_inv_surf_d = gkyl_array_cfetch(up->gk_geom->geo_surf[surf_dir]->jacobtot_inv, loc_conf_ext);
         cmag_surf_d = gkyl_array_cfetch(up->gk_geom->geo_surf[surf_dir]->cmag, loc_conf_ext);
