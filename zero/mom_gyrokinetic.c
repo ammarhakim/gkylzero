e2ac0238c0feee2afe6d033fc9a2282a93cf5cd5
Antoinehoff <antoine-hoffmann@hotmail.com>
2025-03-10 13:49:36 -0400

    forgot to add the connection to the new kernels in the cuda code.

--- zero/mom_gyrokinetic.c
+++ zero/mom_gyrokinetic.c
@@ -198,12 +198,12 @@ gkyl_int_mom_gyrokinetic_new(const struct gkyl_basis* cbasis, const struct gkyl_
   // set kernel pointer
   switch (cbasis->b_type) {
     case GKYL_BASIS_MODAL_SERENDIPITY:
-      int_three_moments_kernels = ser_int_three_moments_kernels;
-      int_four_moments_kernels = ser_int_four_moments_kernels;
-      int_hamiltonian_moments_kernels = ser_int_hamiltonian_moments_kernels;
       int_M0_kernels = ser_int_M0_kernels;
       int_M1_kernels = ser_int_M1_kernels;
       int_M2_kernels = ser_int_M2_kernels;
+      int_three_moments_kernels = ser_int_three_moments_kernels;
+      int_four_moments_kernels = ser_int_four_moments_kernels;
+      int_hamiltonian_moments_kernels = ser_int_hamiltonian_moments_kernels;
       break;
 
     default:
