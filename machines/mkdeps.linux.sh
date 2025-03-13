ac2a8c74d8aa45aa0da8a00e4cae3c9629a49eb4
Maxwell-Rosen <mrquell@gmail.com>
2025-03-13 15:56:41 -0400

    Update openblas and superLU to work with my laptop's fedora. Everything builds fine. There are no unusual segfaults from this build when running ALL the regression and unit tests

--- machines/mkdeps.linux.sh
+++ machines/mkdeps.linux.sh
@@ -1,3 +1,6 @@
+# Make sure you have cmake and gfortran installed
+cmake --version
+gfortran --version
 cd install-deps
 : "${PREFIX:=$HOME/gkylsoft}"
 ./mkdeps.sh --build-openblas=yes --build-superlu=yes --build-openmpi=yes --build-luajit=yes --prefix=$PREFIX
