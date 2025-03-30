7a7637234af43321f0521f5a4aee7fbdd1424b3d
JonathanGorard <jg865@cantab.ac.uk>
2025-03-30 11:45:07 +0100

    Updated ADAS data download script to reflect forthcoming changes in installation and file paths from the directory refactor.

--- install-deps/download-adas.sh
+++ install-deps/download-adas.sh
@@ -4,14 +4,14 @@ source ./build-opts.sh
 
 # Install prefix
 PREFIX=$GKYLSOFT
-ADAS_DIR="$GKYLSOFT/gkylzero/share"
+ADAS_DIR="$GKYLSOFT/greorg/share"
 
 mkdir -p $ADAS_DIR/adas
 #cd -
-python ../data/adas/download_adas.py
+python ../gyrokinetic/data/adas/download_adas.py
 echo "Converting ADAS data to numpy .."
-python ../data/adas/adas_to_numpy.py
+python ../gyrokinetic/data/adas/adas_to_numpy.py
 rm *.dat
 cp *.npy $ADAS_DIR/adas/.
-mv *.npy ../data/adas/.
+mv *.npy ../gyrokinetic/data/adas/.
 echo "ADAS data downloaded to $ADAS_DIR/adas"
