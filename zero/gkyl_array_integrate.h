04e514c523f45506c95c78c6215230000eccd591
Antoinehoff <antoine-hoffmann@hotmail.com>
2025-04-01 10:48:18 -0400

    add a warning in the comments of array integrate because the weights are not used in serendipity basis case.

--- zero/gkyl_array_integrate.h
+++ zero/gkyl_array_integrate.h
@@ -38,7 +38,7 @@ gkyl_array_integrate_new(const struct gkyl_rect_grid* grid, const struct gkyl_ba
  * @param up array_integrate updater.
  * @param fin Input gkyl_array.
  * @param factor Factor to multiply by.
- * @param weight Weight field we multiply by inside the integral.
+ * @param weight Weight field we multiply by inside the integral. (WARNING: not used in serendipity basis)
  * @param range Range we'll integrate over.
  * @param weight_range Range of the weight.
  * @param out Output integral result(s). On device memory if use_gpu=true.
