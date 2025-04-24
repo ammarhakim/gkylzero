56eb96f5ae08dc71ae2cce6004a8bf446f278cb0
Junoravin <junoravin@gmail.com>
2025-04-24 11:01:31 -0400

    Bug fix in source writes for getting the right string length.

--- apps/vlasov.c
+++ apps/vlasov.c
@@ -849,7 +849,7 @@ gkyl_vlasov_app_write_mom(gkyl_vlasov_app* app, double tm, int frame)
       if (vm_s->source_id) {
         if (vm_s->src.write_source) {
           const char *fmt_source = "%s-%s_source_%s_%d.gkyl";
-          int sz_source = gkyl_calc_strlen(fmt, app->name, vm_s->info.name,
+          int sz_source = gkyl_calc_strlen(fmt_source, app->name, vm_s->info.name,
             vm_s->info.diag_moments[m], frame);
           char fileNm_source[sz_source+1]; // ensures no buffer overflow
           snprintf(fileNm_source, sizeof fileNm_source, fmt_source, app->name, vm_s->info.name,
@@ -864,7 +864,7 @@ gkyl_vlasov_app_write_mom(gkyl_vlasov_app* app, double tm, int frame)
           // Write out the adaptive density 
           if (vm_s->source_id == GKYL_PROJ_ADAPT_DENSITY_SOURCE) {
             const char *fmt_source_M0 = "%s-%s_source_M0_adapt_%d.gkyl";
-            int sz_source_M0 = gkyl_calc_strlen(fmt, app->name, vm_s->info.name, frame);
+            int sz_source_M0 = gkyl_calc_strlen(fmt_source_M0, app->name, vm_s->info.name, frame);
             char fileNm_source_M0[sz_source_M0+1]; // ensures no buffer overflow
             snprintf(fileNm_source_M0, sizeof fileNm_source_M0, fmt_source_M0, app->name, vm_s->info.name, frame);
             gkyl_comm_array_write(app->comm, &app->grid, &app->local, 
