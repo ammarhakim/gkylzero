c20b75cc1217a574c7e7e6207dee9414fbb6f771
manauref <mfrancisquez@gmail.com>
2025-05-08 10:30:28 -0700

    Add correct Can PB moment kernels after fixing bug in Maxima (gkylcas d3fe892fb966abe637284cb075fe828fd82b198f).

--- kernels/canonical_pb/mom_canonical_pb_2x2v_tensor_p1.c
+++ kernels/canonical_pb/mom_canonical_pb_2x2v_tensor_p1.c
@@ -34,7 +34,7 @@ GKYL_CU_DH void canonical_pb_int_five_moments_2x2v_tensor_p1(const double *dxv, 
   const double rdvy2 = 2.0/dxv[3];
 
   out[0] += 4.0*f[0]*volFact; 
-  out[1] += (1.732050807568877*f[12]*hamil[15]*rdvx2+1.732050807568877*f[9]*hamil[14]*rdvx2+1.732050807568877*f[8]*hamil[13]*rdvx2+1.732050807568877*f[5]*hamil[11]*rdvx2+1.732050807568877*f[4]*hamil[10]*rdvx2+1.732050807568877*f[2]*hamil[7]*rdvx2+1.732050807568877*f[1]*hamil[6]*rdvx2+1.732050807568877*f[0]*hamil[3]*rdvx2)*volFact; 
-  out[2] += (1.732050807568877*f[11]*hamil[15]*rdvy2+1.732050807568877*f[7]*hamil[14]*rdvy2+1.732050807568877*f[6]*hamil[13]*rdvy2+1.732050807568877*f[5]*hamil[12]*rdvy2+1.732050807568877*f[3]*hamil[10]*rdvy2+1.732050807568877*f[2]*hamil[9]*rdvy2+1.732050807568877*f[1]*hamil[8]*rdvy2+1.732050807568877*f[0]*hamil[4]*rdvy2)*volFact; 
+  out[1] += (1.7320508075688772*f[12]*hamil[15]*rdvx2+1.7320508075688772*f[9]*hamil[14]*rdvx2+1.7320508075688772*f[8]*hamil[13]*rdvx2+1.7320508075688772*f[5]*hamil[11]*rdvx2+1.7320508075688772*f[4]*hamil[10]*rdvx2+1.7320508075688772*f[2]*hamil[7]*rdvx2+1.7320508075688772*f[1]*hamil[6]*rdvx2+1.7320508075688772*f[0]*hamil[3]*rdvx2)*volFact; 
+  out[2] += (1.7320508075688772*f[11]*hamil[15]*rdvy2+1.7320508075688772*f[7]*hamil[14]*rdvy2+1.7320508075688772*f[6]*hamil[13]*rdvy2+1.7320508075688772*f[5]*hamil[12]*rdvy2+1.7320508075688772*f[3]*hamil[10]*rdvy2+1.7320508075688772*f[2]*hamil[9]*rdvy2+1.7320508075688772*f[1]*hamil[8]*rdvy2+1.7320508075688772*f[0]*hamil[4]*rdvy2)*volFact; 
   out[3] += (f[15]*hamil[15]+f[14]*hamil[14]+f[13]*hamil[13]+f[12]*hamil[12]+f[11]*hamil[11]+f[10]*hamil[10]+f[9]*hamil[9]+f[8]*hamil[8]+f[7]*hamil[7]+f[6]*hamil[6]+f[5]*hamil[5]+f[4]*hamil[4]+f[3]*hamil[3]+f[2]*hamil[2]+f[1]*hamil[1]+f[0]*hamil[0])*volFact; 
 } 
