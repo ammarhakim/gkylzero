be5d1802976f675d2e5b9918d08d3bff9b976ac1
Maxwell-Rosen <mrquell@gmail.com>
2025-07-15 16:25:25 -0400

    mkdeps linux builds mpi, but it doesn't link. Maybe Ammar made it this way for his local build. It breaks my build and some gkeyll executable things need mpi built so it would need this flag

--- machines/configure.linux.cpu.sh
+++ machines/configure.linux.cpu.sh
@@ -1,3 +1,3 @@
 : "${PREFIX:=$HOME/gkylsoft}"
 : "${MPI_HOME:=$HOME/gkylsoft/openmpi}"
-./configure CC=clang --prefix=$PREFIX --use-lua=yes
+./configure CC=clang --prefix=$PREFIX --use-lua=yes --use-mpi=yes --mpi-inc=$MPI_HOME/include
